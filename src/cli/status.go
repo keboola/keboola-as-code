9c3c97604754009d6436e59231aa7c82165acb72
Michal Jurečko <michal.jurecko@gmail.com>
2021-06-30 10:32:03 +0200

    Status command

--- src/cli/status.go
+++ src/cli/status.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"keboola-as-code/src/manifest"
 )
 
 const statusShortDescription = `Print info about project directory`
@@ -15,13 +17,25 @@ func statusCommand(root *rootCommand) *cobra.Command {
 		Use:   "status",
 		Short: statusShortDescription,
 		Long:  statusLongDescription,
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			if err := root.ValidateOptions([]string{"projectDirectory"}); err != nil {
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			// Validate
+			if !root.options.HasProjectDirectory() {
+				root.logger.Infof(`Start by running the "init" sub-command in an empty directory.`)
+				return fmt.Errorf("none of this and parent directories is project dir")
+			}
+
+			// Load manifest
+			projectDir := root.options.ProjectDir()
+			metadataDir := root.options.MetadataDir()
+			projectManifest, err := manifest.LoadManifest(projectDir, metadataDir)
+			if err != nil {
 				return err
 			}
-			return nil
-		},
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			root.logger.Infof("Working directory:  %s", root.options.WorkingDirectory())
+			root.logger.Infof("Project directory:  %s", root.options.ProjectDir())
+			root.logger.Infof("Metadata directory: %s", root.options.MetadataDir())
+			root.logger.Infof("Manifest path:      %s", projectManifest.Path())
 			return nil
 		},
 	}
