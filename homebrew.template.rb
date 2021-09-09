a257aa5a59a918efb6130fa4ed549361536c467f
Michal Jurečko <michal.jurecko@gmail.com>
2021-09-09 11:57:44 +0200

    Fix VERSION/TAG env

--- homebrew.template.rb
+++ homebrew.template.rb
@@ -5,23 +5,23 @@ class KeboolaAsCode < Formula
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/keboola/keboola-as-code/releases/download/${TARGET_VERSION}/kbc_${TARGET_VERSION}_darwin_arm64.zip"
+    url "https://github.com/keboola/keboola-as-code/releases/download/${TAG}/kbc_${VERSION}_darwin_arm64.zip"
     sha256 "${DARWIN_ARM_TARGET_SHA256}"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/keboola/keboola-as-code/releases/download/${TARGET_VERSION}/kbc_${TARGET_VERSION}_darwin_amd64.zip"
+    url "https://github.com/keboola/keboola-as-code/releases/download/${TAG}/kbc_${VERSION}_darwin_amd64.zip"
     sha256 "${DARWIN_AMD_TARGET_SHA256}"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/keboola/keboola-as-code/releases/download/${TARGET_VERSION}/kbc_${TARGET_VERSION}_linux_amd64.zip"
+    url "https://github.com/keboola/keboola-as-code/releases/download/${TAG}/kbc_${VERSION}_linux_amd64.zip"
     sha256 "${LINUX_AMD_TARGET_SHA256}"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/keboola/keboola-as-code/releases/download/${TARGET_VERSION}/kbc_${TARGET_VERSION}_linux_armv6.zip"
+    url "https://github.com/keboola/keboola-as-code/releases/download/${TAG}/kbc_${VERSION}_linux_armv6.zip"
     sha256 "${LINUX_ARM_TARGET_SHA256}"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/keboola/keboola-as-code/releases/download/${TARGET_VERSION}/kbc_${TARGET_VERSION}_linux_arm64.zip"
+    url "https://github.com/keboola/keboola-as-code/releases/download/${TAG}/kbc_${VERSION}_linux_arm64.zip"
     sha256 "${LINUX_ARM64_TARGET_SHA256}"
   end
 
