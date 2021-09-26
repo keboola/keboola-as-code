ee18f129a6701fa51289744567299cfae52a0e37
Michal Jurečko <michal.jurecko@gmail.com>
2021-09-26 13:37:05 +0200

    Add shared code naming to tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -19,6 +19,6 @@ echo
 echo "Running tests ..."
 export KBC_VERSION_CHECK=false # do not check the latest version in the tests
 richgo clean -testcache
-RICHGO_FORCE_COLOR=1 richgo test -p 1 -timeout 360s -v -race -coverprofile=/tmp/profile.out ./... $@
+RICHGO_FORCE_COLOR=1 richgo test -p 1 -timeout 600s -v -race -coverprofile=/tmp/profile.out ./... $@
 echo "Ok. All tests passed."
 echo
