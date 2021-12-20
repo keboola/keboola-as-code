5fb763305b3494583d951b01181c71ab6c00259b
Michal Jurečko <michal.jurecko@gmail.com>
2021-12-20 19:52:50 +0100

    Create manifest interface

---
+++ internal/pkg/manifest/manifest.go
@@ -0,0 +1,21 @@
+package manifest
+
+import (
+	"github.com/keboola/keboola-as-code/internal/pkg/model"
+	"github.com/keboola/keboola-as-code/internal/pkg/naming"
+)
+
+// Manifest is common interface form Project and Template manifest.
+type Manifest interface {
+	All() []model.ObjectManifest
+	AllPersisted() []model.ObjectManifest
+	SortBy() string
+	NamingTemplate() naming.Template
+	NamingRegistry() *naming.Registry
+	ResolveParentPath(record model.ObjectManifest) error
+	IsObjectIgnored(object model.Object) bool
+	GetRecord(key model.Key) (model.ObjectManifest, bool)
+	CreateOrGetRecord(key model.Key) (record model.ObjectManifest, found bool, err error)
+	PersistRecord(model.ObjectManifest) error
+	Delete(object model.WithKey)
+}
