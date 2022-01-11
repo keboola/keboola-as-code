bf3b8c29d949cf71880eb214d992418dd8726ad8
Michal Jurečko <michal.jurecko@gmail.com>
2022-01-11 16:48:30 +0100

    Project and template mappers

--- internal/pkg/project/project.go
+++ internal/pkg/project/project.go
@@ -2,8 +2,13 @@ package project
 
 import (
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/manifest"
+	"github.com/keboola/keboola-as-code/internal/pkg/mapper"
 	projectManifest "github.com/keboola/keboola-as-code/internal/pkg/project/manifest"
+	"github.com/keboola/keboola-as-code/internal/pkg/remote"
+	"github.com/keboola/keboola-as-code/internal/pkg/scheduler"
+	"github.com/keboola/keboola-as-code/internal/pkg/state"
 )
 
 type Manifest = projectManifest.Manifest
@@ -12,15 +17,23 @@ func LoadManifest(fs filesystem.Fs) (*Manifest, error) {
 	return projectManifest.Load(fs)
 }
 
+type dependencies interface {
+	Logger() log.Logger
+	StorageApi() (*remote.StorageApi, error)
+	SchedulerApi() (*scheduler.Api, error)
+}
+
 type Project struct {
+	dependencies
 	fs       filesystem.Fs
 	manifest *Manifest
 }
 
-func New(fs filesystem.Fs, manifest *Manifest) *Project {
+func New(fs filesystem.Fs, manifest *Manifest, d dependencies) *Project {
 	return &Project{
-		fs:       fs,
-		manifest: manifest,
+		dependencies: d,
+		fs:           fs,
+		manifest:     manifest,
 	}
 }
 
@@ -30,4 +43,8 @@ func (p *Project) Fs() filesystem.Fs {
 
 func (p *Project) Manifest() manifest.Manifest {
 	return p.manifest
+}
+
+func (p *Project) MappersFor(state *state.State) mapper.Mappers {
+	return MappersFor(state, p.dependencies)
 }
--- internal/pkg/template/template.go
+++ internal/pkg/template/template.go
@@ -2,8 +2,13 @@ package template
 
 import (
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/manifest"
+	"github.com/keboola/keboola-as-code/internal/pkg/mapper"
 	"github.com/keboola/keboola-as-code/internal/pkg/model"
+	"github.com/keboola/keboola-as-code/internal/pkg/remote"
+	"github.com/keboola/keboola-as-code/internal/pkg/scheduler"
+	"github.com/keboola/keboola-as-code/internal/pkg/state"
 	templateManifest "github.com/keboola/keboola-as-code/internal/pkg/template/manifest"
 )
 
@@ -13,15 +18,23 @@ func LoadManifest(fs filesystem.Fs) (*Manifest, error) {
 	return templateManifest.Load(fs)
 }
 
+type dependencies interface {
+	Logger() log.Logger
+	StorageApi() (*remote.StorageApi, error)
+	SchedulerApi() (*scheduler.Api, error)
+}
+
 type Template struct {
+	dependencies
 	fs       filesystem.Fs
 	manifest *Manifest
 }
 
-func New(fs filesystem.Fs, manifest *Manifest) *Template {
+func New(fs filesystem.Fs, manifest *Manifest, d dependencies) *Template {
 	return &Template{
-		fs:       fs,
-		manifest: manifest,
+		dependencies: d,
+		fs:           fs,
+		manifest:     manifest,
 	}
 }
 
@@ -31,6 +44,10 @@ func (p *Template) Fs() filesystem.Fs {
 
 func (p *Template) Manifest() manifest.Manifest {
 	return p.manifest
+}
+
+func (p *Template) MappersFor(state *state.State) mapper.Mappers {
+	return MappersFor(state, p.dependencies)
 }
 
 // --- move next code - ReplacesKeys to mapper
