fb0ff78b197e7d8ccd39435164bda1318e20c310
Michal Jurečko <michal.jurecko@gmail.com>
2022-01-14 08:23:50 +0100

    Moved test TestLocalUnitOfWork_workersFor

--- internal/pkg/local/manager_test.go
+++ internal/pkg/local/manager_test.go
@@ -64,44 +64,6 @@ func (t *testMapper) OnLocalChange(changes *model.LocalChanges) error {
 	return nil
 }
 
-func TestLocalUnitOfWork_workersFor(t *testing.T) {
-	t.Parallel()
-	manager, _ := newTestLocalManager(t)
-	uow := manager.NewUnitOfWork(context.Background())
-
-	lock := &sync.Mutex{}
-	var order []int
-
-	for _, level := range []int{3, 2, 4, 1} {
-		level := level
-		uow.workersFor(level).AddWorker(func() error {
-			lock.Lock()
-			defer lock.Unlock()
-			order = append(order, level)
-			return nil
-		})
-		uow.workersFor(level).AddWorker(func() error {
-			lock.Lock()
-			defer lock.Unlock()
-			order = append(order, level)
-			return nil
-		})
-	}
-
-	// Not started
-	time.Sleep(10 * time.Millisecond)
-	assert.Empty(t, order)
-
-	// Invoke
-	assert.NoError(t, uow.Invoke())
-	assert.Equal(t, []int{1, 1, 2, 2, 3, 3, 4, 4}, order)
-
-	// Cannot be reused
-	assert.PanicsWithError(t, `invoked local.UnitOfWork cannot be reused`, func() {
-		uow.Invoke()
-	})
-}
-
 func TestLocalSaveMapper(t *testing.T) {
 	t.Parallel()
 	manager, mapper := newTestLocalManager(t)
--- internal/pkg/local/workers_test.go
+++ internal/pkg/local/workers_test.go
@@ -3,6 +3,7 @@ package local
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
@@ -63,4 +64,42 @@ func TestWorkersErrors(t *testing.T) {
 	err := w.StartAndWait()
 	assert.Error(t, err)
 	assert.Equal(t, "- first\n- second\n- third", err.Error())
+}
+
+func TestLocalUnitOfWork_workersFor(t *testing.T) {
+	t.Parallel()
+	manager := newTestLocalManager(t)
+	uow := manager.NewUnitOfWork(context.Background())
+
+	lock := &sync.Mutex{}
+	var order []int
+
+	for _, level := range []int{3, 2, 4, 1} {
+		level := level
+		uow.workersFor(level).AddWorker(func() error {
+			lock.Lock()
+			defer lock.Unlock()
+			order = append(order, level)
+			return nil
+		})
+		uow.workersFor(level).AddWorker(func() error {
+			lock.Lock()
+			defer lock.Unlock()
+			order = append(order, level)
+			return nil
+		})
+	}
+
+	// Not started
+	time.Sleep(10 * time.Millisecond)
+	assert.Empty(t, order)
+
+	// Invoke
+	assert.NoError(t, uow.Invoke())
+	assert.Equal(t, []int{1, 1, 2, 2, 3, 3, 4, 4}, order)
+
+	// Cannot be reused
+	assert.PanicsWithError(t, `invoked local.UnitOfWork cannot be reused`, func() {
+		uow.Invoke()
+	})
 }
