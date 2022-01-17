b8aa7005d46f0b2ce9fc6f354aa4873eb4db772e
Michal Jurečko <michal.jurecko@gmail.com>
2022-01-17 11:03:01 +0100

    Repository manifest: json file structure

--- internal/pkg/template/repository/manifest/manifest.go
+++ internal/pkg/template/repository/manifest/manifest.go
@@ -6,6 +6,7 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/build"
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
 	"github.com/keboola/keboola-as-code/internal/pkg/json"
+	"github.com/keboola/keboola-as-code/internal/pkg/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils"
 	"github.com/keboola/keboola-as-code/internal/pkg/validator"
 )
@@ -13,29 +14,35 @@ import (
 const FileName = `repository.json`
 
 type Manifest struct {
-	fs       filesystem.Fs
-	changed  bool
-	*Content `validate:"required,dive"` // content of the file, updated only on load/save
+	fs        filesystem.Fs
+	changed   bool
+	templates []*TemplateRecord
 }
 
-type Content struct {
-	Version int `json:"version" validate:"required,min=1,max=2"`
+type TemplateRecord struct {
+	Name          string `json:"name" validate:"required"`
+	Description   string `json:"description" validate:"required"`
+	model.AbsPath `validate:"dive"`
+	Versions      []*VersionRecord `json:"versions" validate:"required,dive"`
 }
 
-func New(fs filesystem.Fs) (*Manifest, error) {
-	m := newManifest(fs)
-	if err := m.validate(); err != nil {
-		return nil, err
-	}
-	return m, nil
+type VersionRecord struct {
+	Version       string `json:"version" validate:"required,semver"`
+	Description   string `json:"description" validate:"required"`
+	Stable        bool   `json:"stable" validate:"required"`
+	model.AbsPath `validate:"dive"`
 }
 
-func newManifest(fs filesystem.Fs) *Manifest {
+// file is repository manifest JSON file.
+type file struct {
+	Version   int               `json:"version" validate:"required,min=1,max=2"`
+	Templates []*TemplateRecord `json:"templates"`
+}
+
+func New(fs filesystem.Fs) *Manifest {
 	return &Manifest{
-		fs: fs,
-		Content: &Content{
-			Version: build.MajorVersion,
-		},
+		fs:        fs,
+		templates: make([]*TemplateRecord, 0),
 	}
 }
 
@@ -48,25 +55,15 @@ func (m *Manifest) Path() string {
 }
 
 func Load(fs filesystem.Fs) (*Manifest, error) {
-	// Exists?
-	path := filesystem.Join(filesystem.MetadataDir, FileName)
-	if !fs.IsFile(path) {
-		return nil, fmt.Errorf("manifest \"%s\" not found", path)
-	}
-
-	// Read JSON file
-	m := newManifest(fs)
-	if err := fs.ReadJsonFileTo(path, "manifest", &m.Content); err != nil {
+	// Read manifest file
+	manifestContent, err := loadManifestFile(fs)
+	if err != nil {
 		return nil, err
 	}
 
-	// Set new version
-	m.Content.Version = build.MajorVersion
-
-	// Validate
-	if err := m.validate(); err != nil {
-		return nil, err
-	}
+	// Create manifest struct
+	m := New(fs)
+	m.templates = manifestContent.Templates
 
 	// Track if manifest was changed after load
 	m.changed = false
@@ -76,19 +73,11 @@ func Load(fs filesystem.Fs) (*Manifest, error) {
 }
 
 func (m *Manifest) Save() error {
-	// Validate
-	err := m.validate()
-	if err != nil {
-		return err
-	}
+	content := newFile()
+	content.Templates = m.templates
 
-	// Write JSON file
-	content, err := json.EncodeString(m.Content, true)
-	if err != nil {
-		return utils.PrefixError(`cannot encode manifest`, err)
-	}
-	file := filesystem.NewFile(m.Path(), content)
-	if err := m.fs.WriteFile(file); err != nil {
+	// Save manifest file
+	if err := saveManifestFile(m.fs, content); err != nil {
 		return err
 	}
 
@@ -96,8 +85,67 @@ func (m *Manifest) Save() error {
 	return nil
 }
 
-func (m *Manifest) validate() error {
-	if err := validator.Validate(m); err != nil {
+func newFile() *file {
+	return &file{
+		Version:   build.MajorVersion,
+		Templates: make([]*TemplateRecord, 0),
+	}
+}
+
+func loadManifestFile(fs filesystem.Fs) (*file, error) {
+	// Check if file exists
+	path := Path()
+	if !fs.IsFile(path) {
+		return nil, fmt.Errorf("manifest \"%s\" not found", path)
+	}
+
+	// Read JSON file
+	manifestContent := newFile()
+	if err := fs.ReadJsonFileTo(path, "manifest", manifestContent); err != nil {
+		return nil, err
+	}
+
+	// Fill in parent paths
+	for _, template := range manifestContent.Templates {
+		template.AbsPath.SetParentPath(``)
+		for _, version := range template.Versions {
+			version.AbsPath.SetParentPath(template.Path())
+		}
+	}
+
+	// Validate
+	if err := manifestContent.validate(); err != nil {
+		return nil, err
+	}
+
+	// Set new version
+	manifestContent.Version = build.MajorVersion
+
+	return manifestContent, nil
+}
+
+func saveManifestFile(fs filesystem.Fs, manifestContent *file) error {
+	// Validate
+	err := manifestContent.validate()
+	if err != nil {
+		return err
+	}
+
+	// Write JSON file
+	content, err := json.EncodeString(manifestContent, true)
+	if err != nil {
+		return utils.PrefixError(`cannot encode manifest`, err)
+	}
+	file := filesystem.NewFile(Path(), content)
+	if err := fs.WriteFile(file); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *file) validate() error {
+	if err := validator.Validate(c); err != nil {
 		return utils.PrefixError("manifest is not valid", err)
 	}
 	return nil
