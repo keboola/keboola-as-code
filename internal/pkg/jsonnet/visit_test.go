ba84453cd099975c226a6b3b56c533dd13c21276
Michal Jurečko <michal.jurecko@gmail.com>
2022-01-28 20:56:55 +0100

    Add function jsonnet.VisitAst

---
+++ internal/pkg/jsonnet/visit_test.go
@@ -0,0 +1,60 @@
+package jsonnet
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/google/go-jsonnet/ast"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVisitAst(t *testing.T) {
+	t.Parallel()
+	code := `
+{
+  values: [
+    {
+      key: {
+        int: 123,
+        string: "str",
+        op: 1 + 2,
+      }
+    }
+  ]
+}
+`
+
+	expected := `
+{
+  values: [
+    {
+      key: {
+        int: "replaced type",
+        string: "modified-str",
+        op: 1001 + 1002,
+      },
+    },
+  ],
+}
+`
+
+	// Visit & modify
+	node, err := ToAst(code)
+	assert.NoError(t, err)
+	VisitAst(&node, func(nodePtr *ast.Node) {
+		switch v := (*nodePtr).(type) {
+		case *ast.LiteralString:
+			v.Value = "modified-" + v.Value
+		case *ast.LiteralNumber:
+			if v.OriginalString == "123" {
+				*nodePtr = &ast.LiteralString{Value: "replaced type", Kind: ast.StringDouble}
+			} else {
+				v.OriginalString = "100" + v.OriginalString
+			}
+		}
+	})
+
+	output, err := FormatAst(node)
+	assert.NoError(t, err)
+	assert.Equal(t, strings.TrimLeft(expected, "\n"), output)
+}
