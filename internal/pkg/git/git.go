6a066156edbee8a40feee3156ef17d036dfbe294
Jakub Matějka <jakub@keboola.com>
2022-02-02 16:24:29 +0100

    Improve error handling of git commands

--- internal/pkg/git/git.go
+++ internal/pkg/git/git.go
@@ -1,10 +1,15 @@
 package git
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
+	"syscall"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem/aferofs"
@@ -26,24 +31,44 @@ func CheckoutTemplateRepository(url string, ref string, logger log.Logger) (file
 		return nil, err
 	}
 
-	cmd := exec.Command("git", "clone", "--no-checkout", url, dir)
-	cmd.Stdout = logger.DebugWriter()
-	cmd.Stderr = logger.DebugWriter()
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "GIT_TERMINAL_PROMPT=0")
-	err = cmd.Run()
+	err, stdErr, exitCode := runGitCommand(logger, dir, []string{"clone", "--no-checkout", "-q", url, dir})
 	if err != nil {
-		return nil, fmt.Errorf(`git repository not found on url "%s"`, url)
+		if exitCode == 128 {
+			return nil, fmt.Errorf(`git repository not found on url "%s"`, url)
+		}
+		return nil, fmt.Errorf(stdErr)
 	}
 
-	cmd = exec.Command("git", "checkout", ref)
-	cmd.Dir = dir
-	cmd.Stdout = logger.DebugWriter()
-	cmd.Stderr = logger.DebugWriter()
-	err = cmd.Run()
+	err, stdErr, exitCode = runGitCommand(logger, dir, []string{"checkout", ref})
 	if err != nil {
-		return nil, fmt.Errorf(`branch "%s" not found in the repository`, ref)
+		if exitCode == 1 {
+			return nil, fmt.Errorf(`branch "%s" not found in the repository`, ref)
+		}
+		return nil, fmt.Errorf(stdErr)
 	}
 
 	return aferofs.NewLocalFs(logger, dir, "")
+}
+
+func runGitCommand(logger log.Logger, dir string, args []string) (err error, stdErr string, exitCode int) {
+	logger.Debug(fmt.Sprintf(`Running git command: git %s`, strings.Join(args, " ")))
+	var stdErrBuffer bytes.Buffer
+	cmd := exec.Command("git", args...)
+	cmd.Dir = dir
+	cmd.Stdout = logger.DebugWriter()
+	cmd.Stderr = logger.DebugWriter()
+	cmd.Stderr = io.MultiWriter(logger.DebugWriter(), &stdErrBuffer)
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "GIT_TERMINAL_PROMPT=0")
+	err = cmd.Run()
+	stdErr = stdErrBuffer.String()
+	exitCode = 0
+	if err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			exitCode = ws.ExitStatus()
+		}
+	}
+	return
 }
