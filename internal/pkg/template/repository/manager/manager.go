9429aabf3fca7437b5e31e41d92c8d11c06ee4bc
Jakub Matějka <jakub@keboola.com>
2022-03-18 11:56:48 +0100

    Improve template repository hash

--- internal/pkg/template/repository/manager/manager.go
+++ internal/pkg/template/repository/manager/manager.go
@@ -1,7 +1,6 @@
 package manager
 
 import (
-	"crypto/sha256"
 	"fmt"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
@@ -21,31 +20,24 @@ type GitRepository struct {
 	Fs  filesystem.Fs
 }
 
-func hashRepositoryName(repo model.TemplateRepository) string {
-	h := sha256.New()
-	h.Write([]byte(fmt.Sprintf("%s:%s", repo.Url, repo.Ref)))
-	return string(h.Sum(nil))
-}
-
 func New(logger log.Logger) (*Manager, error) {
 	m := &Manager{
 		logger:       logger,
 		repositories: make(map[string]*GitRepository),
 	}
-	err := m.AddRepository(repository.DefaultRepository())
-	if err != nil {
+	if err := m.AddRepository(repository.DefaultRepository()); err != nil {
 		return nil, err
 	}
 	return m, nil
 }
 
 func (m *Manager) AddRepository(repo model.TemplateRepository) error {
-	hash := hashRepositoryName(repo)
+	hash := repo.Hash()
 	if _, ok := m.repositories[hash]; ok {
 		return fmt.Errorf("repository already exists")
 	}
 
-	fs, dir, err := git.CheckoutWholeRepository(repo, m.logger)
+	fs, dir, err := git.CheckoutTemplateRepositoryFull(repo, m.logger)
 	if err != nil {
 		return err
 	}
