be0fab088918af3b727d7bc2f6a9e2ce2fd351c7
Michal Jurečko <michal.jurecko@gmail.com>
2022-04-04 11:06:52 +0200

    Endpoints definition

--- internal/pkg/template/api/service/service.go
+++ internal/pkg/template/api/service/service.go
@@ -6,11 +6,11 @@ import (
 	"time"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/template/api/dependencies"
-	"github.com/keboola/keboola-as-code/internal/pkg/template/api/gen/templates"
+	. "github.com/keboola/keboola-as-code/internal/pkg/template/api/gen/templates"
 	"github.com/keboola/keboola-as-code/internal/pkg/template/repository"
 )
 
-type Service struct {
+type service struct {
 	dependencies        dependencies.Container
 	lock                *sync.Mutex
 	repositoriesManager *repository.Manager
@@ -18,12 +18,12 @@ type Service struct {
 
 const TemplateRepositoriesPullInterval = 5 * time.Minute
 
-func New(d dependencies.Container) (*Service, error) {
+func New(d dependencies.Container) (Service, error) {
 	repoManager, err := repository.NewManager(d.Logger())
 	if err != nil {
 		return nil, err
 	}
-	s := &Service{
+	s := &service{
 		dependencies:        d,
 		lock:                &sync.Mutex{},
 		repositoriesManager: repoManager,
@@ -32,7 +32,7 @@ func New(d dependencies.Container) (*Service, error) {
 	return s, nil
 }
 
-func (s *Service) StartCron() {
+func (s *service) StartCron() {
 	go func() {
 		interval := TemplateRepositoriesPullInterval
 
@@ -54,39 +54,86 @@ func (s *Service) StartCron() {
 	}()
 }
 
-func (s *Service) pullTemplateRepositories() {
+func (s *service) pullTemplateRepositories() {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
 	s.repositoriesManager.Pull()
 }
 
-func (s *Service) IndexRoot(_ dependencies.Container) (err error) {
+func (s *service) IndexRoot(dependencies.Container) (err error) {
 	// Redirect / -> /v1
 	return nil
 }
 
-func (s *Service) HealthCheck(_ dependencies.Container) (res string, err error) {
-	return "OK", nil
-}
-
-func (s *Service) IndexEndpoint(_ dependencies.Container) (res *templates.Index, err error) {
-	res = &templates.Index{
+func (s *service) Index(dependencies.Container) (res *ServiceIndex, err error) {
+	res = &ServiceIndex{
 		API:           "templates",
 		Documentation: "https://templates.keboola.com/v1/documentation",
 	}
 	return res, nil
 }
 
-func (s *Service) Foo(d dependencies.Container, payload *templates.FooPayload) (res string, err error) {
-	api, err := d.StorageApi()
-	if err != nil {
-		return "", err
-	}
+func (s *service) HealthCheck(dependencies.Container) (res string, err error) {
+	return "OK", nil
+}
 
-	token := api.Token()
-	msg := fmt.Sprintf("token is OK, owner=%s", token.Owner.Name)
+func (s *service) RepositoriesIndex(dependencies.Container, *RepositoriesIndexPayload) (res *Repositories, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
 
-	d.Logger().Info(msg)
-	return msg, nil
+func (s *service) RepositoryIndex(dependencies.Container, *RepositoryIndexPayload) (res *Repository, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) TemplatesIndex(dependencies.Container, *TemplatesIndexPayload) (res *Templates, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) TemplateIndex(dependencies.Container, *TemplateIndexPayload) (res *Template, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) VersionIndex(dependencies.Container, *VersionIndexPayload) (res *TemplateVersion, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) Inputs(dependencies.Container, *InputsPayload) (res *InputsIndex, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) InputsValidate(dependencies.Container, *InputsValidatePayload) (res *ValidationResult, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) VersionUse(dependencies.Container, *VersionUsePayload) (res *UseTemplateResult, err error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (s *service) InstancesIndex(dependencies.Container, *InstancesIndexPayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) InstanceIndex(dependencies.Container, *InstanceIndexPayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) InstanceUpdate(dependencies.Container, *InstanceUpdatePayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) InstanceDelete(dependencies.Container, *InstanceDeletePayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) Upgrade(dependencies.Container, *UpgradePayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) UpgradeInputs(dependencies.Container, *UpgradeInputsPayload) (err error) {
+	return fmt.Errorf("not implemented")
+}
+
+func (s *service) UpgradeInputsValidate(dependencies.Container, *UpgradeInputsValidatePayload) (err error) {
+	return fmt.Errorf("not implemented")
 }
