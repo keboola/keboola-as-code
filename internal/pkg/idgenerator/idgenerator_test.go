da73d113da4a88aa00f21bf09c6cdf7c286df7cc
Michal Jurečko <michal.jurecko@gmail.com>
2022-04-07 20:44:07 +0200

    idgenerator pkg

---
+++ internal/pkg/idgenerator/idgenerator_test.go
@@ -0,0 +1,19 @@
+package idgenerator_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/keboola/keboola-as-code/internal/pkg/idgenerator"
+)
+
+func TestRequestId(t *testing.T) {
+	t.Parallel()
+	assert.Len(t, RequestId(), RequestIdLength)
+}
+
+func TestTemplateInstanceId(t *testing.T) {
+	t.Parallel()
+	assert.Len(t, TemplateInstanceId(), TemplateInstanceIdLength)
+}
