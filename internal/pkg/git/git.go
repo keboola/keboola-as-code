0c18d4cab7acf96eb931b1b36adcbe92bdedb940
Michal Jurečko <michal.jurecko@gmail.com>
2022-04-14 10:31:40 +0200

    Modify git pkg: do only Git work

--- internal/pkg/git/git.go
+++ internal/pkg/git/git.go
@@ -13,231 +13,176 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem/aferofs"
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
-	"github.com/keboola/keboola-as-code/internal/pkg/model"
-	"github.com/keboola/keboola-as-code/internal/pkg/template"
-	"github.com/keboola/keboola-as-code/internal/pkg/template/repository/manifest"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils"
 )
+
+type Repository struct {
+	url    string
+	ref    string
+	sparse bool
+	logger log.Logger
+	lock   *sync.RWMutex
+	fs     filesystem.Fs
+}
+
+type cmdResult struct {
+	exitCode int
+	stdOut   string
+	stdErr   string
+}
 
 func Available() bool {
 	_, err := exec.LookPath("git")
 	return err == nil
 }
 
-type repoRef = model.TemplateRepository
+func Checkout(url, ref string, sparse bool, logger log.Logger) (*Repository, error) {
+	if !Available() {
+		return nil, fmt.Errorf("git command is not available, you have to install it first")
+	}
 
-type Repository struct {
-	repoRef
-	fs     filesystem.Fs
-	logger log.Logger
-	lock   *sync.RWMutex
+	// Create a temp dir, it must be later cleared by calling Repository.Clear()
+	dir, err := ioutil.TempDir("", "keboola-as-code-git-")
+	if err != nil {
+		return nil, err
+	}
+	fs, err := aferofs.NewLocalFs(logger, dir, "")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create repository
+	r := &Repository{url: url, ref: ref, sparse: sparse, logger: logger, lock: &sync.RWMutex{}, fs: fs}
+
+	// Clone parameters
+	params := []string{"clone", "-q", "--branch", r.ref}
+	if r.sparse {
+		params = append(params, "--depth=1", "--no-checkout", "--sparse", "--filter=blob:none")
+	}
+	params = append(params, "--", r.url, dir)
+
+	// Clone repository
+	result, err := r.runGitCmd(params...)
+	if err != nil {
+		if result.exitCode == 128 {
+			if strings.Contains(result.stdErr, fmt.Sprintf("Remote branch %s not found", r.ref)) {
+				return nil, fmt.Errorf(`reference "%s" not found in the git repository "%s"`, r.ref, r.url)
+			}
+			return nil, fmt.Errorf(`git repository not found on url "%s"`, r.url)
+		}
+		return nil, utils.PrefixError("cannot checkout git repository", fmt.Errorf(result.stdErr))
+	}
+	return r, nil
+}
+
+func (r *Repository) String() string {
+	return fmt.Sprintf("%s:%s", r.url, r.ref)
+}
+
+// Url to Git repository.
+func (r *Repository) Url() string {
+	return r.url
+}
+
+// Ref is Git branch or tag.
+func (r *Repository) Ref() string {
+	return r.ref
+}
+
+// RLock acquire read lock, before reading from repository Fs.
+// Pull() is blocked until the read is finished.
+func (r *Repository) RLock() {
+	r.lock.RLock()
+}
+
+// RUnlock release read lock.
+func (r *Repository) RUnlock() {
+	r.lock.RUnlock()
+}
+
+// Fs return repository filesystem.
+// It must be used together with RLock and RUnlock method, to sync Pull() with Fs() reads.
+func (r *Repository) Fs() filesystem.Fs {
+	return r.fs
+}
+
+// Clear deletes temp directory with all files.
+func (r *Repository) Clear() {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	if err := os.RemoveAll(r.fs.BasePath()); err != nil { // nolint: forbidigo
+		r.logger.Warnf(`cannot remove temp dir "%s": %w`, r.fs.BasePath(), err)
+	}
+	r.fs = nil
 }
 
 func (r *Repository) CommitHash() (string, error) {
 	r.lock.RLock()
 	defer r.lock.RUnlock()
-
-	err, stdErr, _, stdOut := runGitCommand(r.logger, r.fs.BasePath(), []string{"rev-parse", "HEAD"})
+	result, err := r.runGitCmd("rev-parse", "HEAD")
 	if err != nil {
-		return "", utils.PrefixError("cannot get repository hash", fmt.Errorf(stdErr))
+		return "", utils.PrefixError("cannot get repository hash", fmt.Errorf(result.stdErr))
 	}
-	return strings.TrimSuffix(stdOut, "\n"), nil
+	return strings.TrimSuffix(result.stdOut, "\n"), nil
 }
 
-func (r *Repository) RLock() {
-	r.lock.RLock()
-}
-
-func (r *Repository) RUnlock() {
-	r.lock.RUnlock()
-}
-
-func (r *Repository) Fs() filesystem.Fs {
-	return r.fs
+// Load a path from the remote git repository, if sparse mode is used.
+func (r *Repository) Load(path string) error {
+	if !r.sparse {
+		return fmt.Errorf("sparse checkout is not allowed")
+	}
+	if _, err := r.runGitCmd("sparse-checkout", "add", fmt.Sprintf("/%s", path)); err != nil {
+		return err
+	}
+	if _, err := r.runGitCmd("checkout"); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *Repository) Pull() error {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 
-	err, stdErr, _, _ := runGitCommand(r.logger, r.fs.BasePath(), []string{"fetch", "origin"})
+	// Check remote changes
+	result, err := r.runGitCmd("fetch", "origin")
 	if err != nil {
-		return utils.PrefixError("cannot fetch template repository", fmt.Errorf(stdErr))
+		return utils.PrefixError("cannot fetch repository", fmt.Errorf(result.stdErr))
 	}
 
-	err, stdErr, _, _ = runGitCommand(r.logger, r.fs.BasePath(), []string{"reset", "--hard", fmt.Sprintf("origin/%s", r.Ref)})
+	// Reset is used, because it works also with force push (edge-case)
+	result, err = r.runGitCmd("reset", "--hard", fmt.Sprintf("origin/%s", r.ref))
 	if err != nil {
-		return utils.PrefixError("cannot reset template repository to the origin", fmt.Errorf(stdErr))
+		return utils.PrefixError("cannot reset repository to the origin", fmt.Errorf(result.stdErr))
 	}
 
 	return nil
 }
 
-type CheckoutOptions struct {
-	CloneParams        []string // Params for git clone
-	Partial            bool     // Partially checkout just the selected template repository (and the repository manifest)
-	ToMemory           bool     // Move the FS with the repository to memory
-	TemplateRepository model.TemplateRepository
-	TemplateRef        model.TemplateRef
-}
+func (r *Repository) runGitCmd(args ...string) (cmdResult, error) {
+	r.logger.Debug(fmt.Sprintf(`Running git command: git %s`, strings.Join(args, " ")))
 
-func CheckoutTemplateRepository(opts CheckoutOptions, logger log.Logger) (filesystem.Fs, error) {
-	if !Available() {
-		return nil, fmt.Errorf("git command is not available, if you want to use templates from a git repository you have to install it first")
-	}
-
-	// Create a temp dir
-	dir, err := ioutil.TempDir("", "keboola-as-code-templates-")
-	if err != nil {
-		return nil, err
-	}
-	if opts.ToMemory {
-		defer func() {
-			if err = os.RemoveAll(dir); err != nil { // nolint: forbidigo
-				logger.Warnf(`cannot remove temp dir "%s": %w`, dir, err)
-			}
-		}()
-	}
-
-	// Clone the repository
-	cloneParams := append([]string{"clone"}, opts.CloneParams...)
-	cloneParams = append(cloneParams, dir)
-	err, stdErr, exitCode, _ := runGitCommand(logger, dir, cloneParams)
-	if err != nil {
-		if exitCode == 128 {
-			if strings.Contains(stdErr, fmt.Sprintf("Remote branch %s not found", opts.TemplateRepository.Ref)) {
-				return nil, fmt.Errorf(`reference "%s" not found in the templates git repository "%s"`, opts.TemplateRepository.Ref, opts.TemplateRepository.Url)
-			}
-			return nil, fmt.Errorf(`templates git repository not found on url "%s"`, opts.TemplateRepository.Url)
-		}
-		return nil, utils.PrefixError("cannot load template source directory", fmt.Errorf(stdErr))
-	}
-
-	// Create FS from the cloned repository
-	localFs, err := aferofs.NewLocalFs(logger, dir, "")
-	if err != nil {
-		return nil, err
-	}
-
-	if opts.Partial {
-		// Checkout repository.json
-		err, stdErr, _, _ = runGitCommand(logger, dir, []string{"sparse-checkout", "add", "/.keboola/repository.json"})
-		if err != nil {
-			return nil, fmt.Errorf(stdErr)
-		}
-		err, stdErr, _, _ = runGitCommand(logger, dir, []string{"checkout"})
-		if err != nil {
-			return nil, utils.PrefixError("cannot load template repository manifest", fmt.Errorf(stdErr))
-		}
-
-		versionRecord, err := getVersionFromRepositoryManifest(opts, localFs)
-		if err != nil {
-			return nil, err
-		}
-
-		// Checkout template src directory
-		srcDir := filesystem.Join(versionRecord.Path(), template.SrcDirectory)
-		err, stdErr, _, _ = runGitCommand(logger, dir, []string{"sparse-checkout", "add", fmt.Sprintf("/%s", srcDir)})
-		if err != nil {
-			return nil, fmt.Errorf(stdErr)
-		}
-		if !localFs.Exists(srcDir) {
-			e := utils.NewMultiError()
-			e.Append(fmt.Errorf(`searched in git repository "%s"`, opts.TemplateRepository.Url))
-			e.Append(fmt.Errorf(`reference "%s"`, opts.TemplateRepository.Ref))
-			return nil, utils.PrefixError(fmt.Sprintf(`folder "%s" not found`, srcDir), e)
-		}
-	}
-
-	if opts.ToMemory {
-		memFs, err := aferofs.NewMemoryFs(logger, ".")
-		if err != nil {
-			return nil, err
-		}
-
-		err = aferofs.CopyFs2Fs(localFs, "", memFs, "")
-		if err != nil {
-			return nil, err
-		}
-
-		return memFs, nil
-	}
-
-	return localFs, nil
-}
-
-func getVersionFromRepositoryManifest(opts CheckoutOptions, localFs filesystem.Fs) (manifest.VersionRecord, error) {
-	// Load the repository manifest
-	m, err := manifest.Load(localFs)
-	if err != nil {
-		return manifest.VersionRecord{}, err
-	}
-
-	// Get version record
-	version := opts.TemplateRef.Version()
-	versionRecord, err := m.GetVersion(opts.TemplateRef.TemplateId(), version)
-	if err != nil {
-		// version or template not found
-		e := utils.NewMultiError()
-		e.Append(fmt.Errorf(`searched in git repository "%s"`, opts.TemplateRepository.Url))
-		e.Append(fmt.Errorf(`reference "%s"`, opts.TemplateRepository.Ref))
-		return manifest.VersionRecord{}, utils.PrefixError(err.Error(), e)
-	}
-	return versionRecord, nil
-}
-
-func CheckoutTemplateRepositoryFull(templateRepo model.TemplateRepository, logger log.Logger) (*Repository, error) {
-	localFs, err := CheckoutTemplateRepository(CheckoutOptions{
-		Partial:            false,
-		ToMemory:           false,
-		TemplateRepository: templateRepo,
-		TemplateRef:        nil,
-		CloneParams:        []string{"--branch", templateRepo.Ref, "-q", templateRepo.Url},
-	}, logger)
-	if err != nil {
-		return nil, err
-	}
-
-	repo := &Repository{repoRef: templateRepo, fs: localFs, logger: logger, lock: &sync.RWMutex{}}
-	hash, err := repo.CommitHash()
-	if err != nil {
-		logger.Warnf(`repository "%s:%s" checked out but the commit hash was not retrieved due to: %w`, repo.Url, repo.Ref, err)
-		return repo, nil
-	}
-
-	logger.Infof(`repository "%s:%s" checked out to %s`, repo.Url, repo.Ref, hash)
-	return repo, nil
-}
-
-func CheckoutTemplateRepositoryPartial(ref model.TemplateRef, logger log.Logger) (filesystem.Fs, error) {
-	return CheckoutTemplateRepository(CheckoutOptions{
-		Partial:            true,
-		ToMemory:           true,
-		TemplateRepository: ref.Repository(),
-		TemplateRef:        ref,
-		CloneParams:        []string{"--branch", ref.Repository().Ref, "--depth=1", "--no-checkout", "--sparse", "--filter=blob:none", "-q", ref.Repository().Url},
-	}, logger)
-}
-
-func runGitCommand(logger log.Logger, dir string, args []string) (err error, stdErr string, exitCode int, stdOut string) {
-	logger.Debug(fmt.Sprintf(`Running git command: git %s`, strings.Join(args, " ")))
 	var stdOutBuffer bytes.Buffer
 	var stdErrBuffer bytes.Buffer
+
 	cmd := exec.Command("git", args...)
-	cmd.Dir = dir
-	cmd.Stdout = io.MultiWriter(logger.DebugWriter(), &stdOutBuffer)
-	cmd.Stderr = io.MultiWriter(logger.DebugWriter(), &stdErrBuffer)
+	cmd.Dir = r.fs.BasePath()
+	cmd.Stdout = io.MultiWriter(r.logger.DebugWriter(), &stdOutBuffer)
+	cmd.Stderr = io.MultiWriter(r.logger.DebugWriter(), &stdErrBuffer)
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "GIT_TERMINAL_PROMPT=0")
-	err = cmd.Run()
-	stdOut = stdOutBuffer.String()
-	stdErr = stdErrBuffer.String()
-	exitCode = 0
+
+	err := cmd.Run()
+	result := cmdResult{}
+	result.stdOut = stdOutBuffer.String()
+	result.stdErr = stdErrBuffer.String()
+	result.exitCode = 0
 	if err != nil {
 		// nolint: errorlint
 		if exitError, ok := err.(*exec.ExitError); ok {
-			exitCode = exitError.ExitCode()
+			result.exitCode = exitError.ExitCode()
 		}
 	}
-	return
+
+	return result, err
 }
