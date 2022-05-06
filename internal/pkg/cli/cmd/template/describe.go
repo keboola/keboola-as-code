c2da52834f9706ed57eef995dae4eba0b5facacb
Jakub Matějka <jakub@keboola.com>
2022-05-06 17:00:42 +0200

    Describe template command

--- internal/pkg/cli/cmd/template/describe.go
+++ internal/pkg/cli/cmd/template/describe.go
@@ -1,25 +1,44 @@
 package template
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/cli/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/cli/helpmsg"
+	"github.com/keboola/keboola-as-code/internal/pkg/model"
+	describeOp "github.com/keboola/keboola-as-code/pkg/lib/operation/template/local/repository/describe"
 )
 
-func DescribeCommand(_ dependencies.Provider) *cobra.Command {
+func DescribeCommand(p dependencies.Provider) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "describe",
+		Use:   "describe <template> <version>",
 		Short: helpmsg.Read(`template/describe/short`),
 		Long:  helpmsg.Read(`template/describe/long`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return fmt.Errorf(`not implemented`)
+			d := p.Dependencies()
+
+			// Get template repository
+			repo, err := d.LocalTemplateRepository()
+			if err != nil {
+				return err
+			}
+
+			// Template definition
+			templateDef, err := model.NewTemplateRefFromString(repo.Ref(), args[0], args[1])
+			if err != nil {
+				return err
+			}
+
+			// Load template
+			template, err := d.Template(templateDef)
+			if err != nil {
+				return err
+			}
+
+			// Describe template
+			return describeOp.Run(template, d)
 		},
 	}
 
-	// Flags
-	cmd.Flags().SortFlags = true
 	return cmd
 }
---
+++ pkg/lib/operation/template/local/repository/describe/operation.go
@@ -0,0 +1,70 @@
+package describe
+
+import (
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/template"
+	"github.com/keboola/keboola-as-code/internal/pkg/template/repository"
+)
+
+type Options struct {
+	Template *repository.TemplateRecord
+	Version  *repository.VersionRecord
+}
+
+type dependencies interface {
+	Logger() log.Logger
+}
+
+func Run(tmpl *template.Template, d dependencies) (err error) {
+	logger := d.Logger()
+
+	logger.Infof("Template ID:          %s", tmpl.TemplateRecord().Id)
+	logger.Infof("Name:                 %s", tmpl.TemplateRecord().Name)
+	logger.Infof("Description:          %s", tmpl.TemplateRecord().Description)
+	logger.Infof("")
+
+	v := tmpl.VersionRecord()
+	logger.Infof("Version:              %s", v.Version.String())
+	logger.Infof("Stable:               %t", v.Stable)
+	logger.Infof("Description:          %s", v.Description)
+	logger.Infof("")
+
+	// Groups
+	for _, group := range tmpl.Inputs().ToExtended() {
+		logger.Infof("Group ID:             %s", group.Id)
+		logger.Infof("Description:          %s", group.Description)
+		logger.Infof("Required:             %s", string(group.Required))
+		logger.Infof("")
+
+		// Steps
+		for _, step := range group.Steps {
+			logger.Infof("  Step ID:            %s", step.Id)
+			logger.Infof("  Name:               %s", step.Name)
+			logger.Infof("  Description:        %s", step.Description)
+			logger.Infof("  Dialog Name:        %s", step.NameFoDialog())
+			logger.Infof("  Dialog Description: %s", step.DescriptionForDialog())
+			logger.Infof("")
+
+			// Inputs
+			for _, in := range step.Inputs {
+				logger.Infof("    Input ID:         %s", in.Id)
+				logger.Infof("    Name:             %s", in.Name)
+				logger.Infof("    Description:      %s", in.Description)
+				logger.Infof("    Type:             %s", in.Type)
+				logger.Infof("    Kind:             %s", string(in.Kind))
+				if in.Default != nil {
+					logger.Infof("    Default:          %#v", in.DefaultOrEmpty())
+				}
+				if len(in.Options) > 0 {
+					logger.Infof("    Options:")
+					for _, opt := range in.Options {
+						logger.Infof("      %s:  %s", opt.Value, opt.Label)
+					}
+				}
+				logger.Infof("")
+			}
+		}
+	}
+
+	return nil
+}
