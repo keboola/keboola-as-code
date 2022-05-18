3a72ee9a6268d5b53f36d6956085f6d0c007f45a
Jakub Matějka <jakub@keboola.com>
2022-05-18 16:22:50 +0200

    Fix default repository setting in RepositoryManager

--- internal/pkg/api/server/templates/dependencies/dependencies.go
+++ internal/pkg/api/server/templates/dependencies/dependencies.go
@@ -141,12 +141,10 @@ func (v *container) PrefixLogger() log.PrefixLogger {
 
 func (v *container) RepositoryManager() (*repository.Manager, error) {
 	if v.repositoryManager == nil {
-		manager := repository.NewManager(v.Ctx(), v.Logger())
+		v.repositoryManager = repository.NewManager(v.Ctx(), v.Logger())
 		if v.defaultRepository.Type == model.RepositoryTypeGit {
-			if err := manager.AddRepository(v.defaultRepository); err != nil {
+			if err := v.repositoryManager.AddRepository(v.defaultRepository); err != nil {
 				return nil, err
-			} else {
-				v.repositoryManager = manager
 			}
 		}
 	}
