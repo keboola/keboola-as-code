4f7b97c639cfbebf4dd8139278c658d624e51c39
Michal Jurečko <michal.jurecko@gmail.com>
2022-05-18 11:25:47 +0200

    CLI/API: ignore scheduler, if target config is missing

--- internal/pkg/mapper/ignore/remote_test.go
+++ internal/pkg/mapper/ignore/remote_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/model"
 )
 
-func TestIgnoreMapper_AfterRemoteOperation(t *testing.T) {
+func TestIgnoreMapper_AfterRemoteOperation_Variables(t *testing.T) {
 	t.Parallel()
 	state, d := createStateWithMapper(t)
 	logger := d.DebugLogger()
@@ -57,5 +57,66 @@ func TestIgnoreMapper_AfterRemoteOperation(t *testing.T) {
 	assert.Equal(t, []model.ObjectState{
 		targetConfig,
 		targetVars,
+	}, state.All())
+}
+
+func TestIgnoreMapper_AfterRemoteOperation_Scheduler(t *testing.T) {
+	t.Parallel()
+	state, d := createStateWithMapper(t)
+	logger := d.DebugLogger()
+
+	// Target for valid scheduler
+	targetKey := model.ConfigKey{BranchId: 1, ComponentId: "keboola.foo-bar", Id: "1"}
+	targetConfig := &model.ConfigState{
+		ConfigManifest: &model.ConfigManifest{ConfigKey: targetKey},
+		Remote:         &model.Config{ConfigKey: targetKey},
+	}
+	assert.NoError(t, state.Set(targetConfig))
+
+	// Valid scheduler
+	validSchedulerKey := model.ConfigKey{BranchId: 1, ComponentId: model.SchedulerComponentId, Id: "2"}
+	validScheduler := &model.ConfigState{
+		ConfigManifest: &model.ConfigManifest{ConfigKey: validSchedulerKey},
+		Remote: &model.Config{
+			ConfigKey: validSchedulerKey,
+			Relations: model.Relations{
+				&model.SchedulerForRelation{
+					ComponentId: targetKey.ComponentId,
+					ConfigId:    targetKey.Id,
+				},
+			},
+		},
+	}
+	assert.NoError(t, state.Set(validScheduler))
+
+	// Ignored scheduler
+	missingTargetKey := model.ConfigKey{BranchId: 1, ComponentId: "keboola.foo-bar", Id: "789"}
+	ignoredSchedulerKey := model.ConfigKey{BranchId: 1, ComponentId: model.SchedulerComponentId, Id: "3"}
+	ignoredScheduler := &model.ConfigState{
+		ConfigManifest: &model.ConfigManifest{ConfigKey: ignoredSchedulerKey},
+		Remote: &model.Config{
+			ConfigKey: ignoredSchedulerKey,
+			Relations: model.Relations{
+				&model.SchedulerForRelation{
+					ComponentId: missingTargetKey.ComponentId,
+					ConfigId:    missingTargetKey.Id,
+				},
+			},
+		},
+	}
+	assert.NoError(t, state.Set(ignoredScheduler))
+
+	// Invoke
+	changes := model.NewRemoteChanges()
+	changes.AddLoaded(targetConfig)
+	changes.AddLoaded(ignoredScheduler)
+	changes.AddLoaded(validScheduler)
+	assert.NoError(t, state.Mapper().AfterRemoteOperation(changes))
+	assert.Equal(t, "DEBUG  Ignored scheduler config \"branch:1/component:keboola.scheduler/config:3\", target config \"branch:1/component:keboola.foo-bar/config:789\" not found\n", logger.AllMessages())
+
+	// Unattached variables are removed
+	assert.Equal(t, []model.ObjectState{
+		targetConfig,
+		validScheduler,
 	}, state.All())
 }
