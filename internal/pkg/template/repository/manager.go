df7e5f40c3dc10f1a2a7d90c61d30405578560cf
Jakub Matějka <jakub@keboola.com>
2022-06-22 14:07:17 +0200

    Do not fail on errors during getting commit hash

--- internal/pkg/template/repository/manager.go
+++ internal/pkg/template/repository/manager.go
@@ -84,7 +84,7 @@ func pullRepo(ctx context.Context, logger log.Logger, repo *git.Repository) erro
 	logger.Infof(`repository "%s" update started`, repo)
 	oldHash, err := repo.CommitHash(ctx)
 	if err != nil {
-		return err
+		logger.Errorf("cannot get original commit hash", err)
 	}
 
 	err = repo.Pull(ctx)
@@ -94,7 +94,7 @@ func pullRepo(ctx context.Context, logger log.Logger, repo *git.Repository) erro
 
 	newHash, err := repo.CommitHash(ctx)
 	if err != nil {
-		return err
+		logger.Errorf("cannot get new commit hash", err)
 	}
 
 	if oldHash == newHash {
