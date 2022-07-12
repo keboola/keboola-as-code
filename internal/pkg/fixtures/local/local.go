fdbaabb3e33e54361dbfe750495255b03f5e5d00
Jakub Matějka <jakub@keboola.com>
2022-07-12 15:08:55 +0200

    Extract reusable function to get local state fixture

---
+++ internal/pkg/fixtures/local/local.go
@@ -0,0 +1,36 @@
+package fixtures
+
+import (
+	"runtime"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
+	"github.com/keboola/keboola-as-code/internal/pkg/project/manifest"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/testfs"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/testhelper"
+)
+
+func LoadFS(dirName string, envs testhelper.EnvProvider) (filesystem.Fs, error) {
+	// Objects dir
+	_, thisFile, _, _ := runtime.Caller(0)
+	fixturesDir := filesystem.Dir(thisFile)
+	stateDir := filesystem.Join(fixturesDir, dirName)
+
+	// Create Fs
+	fs := testfs.NewMemoryFsFrom(stateDir)
+	testhelper.ReplaceEnvsDir(fs, `/`, envs)
+
+	return fs, nil
+}
+
+func LoadManifest(dirName string, envs testhelper.EnvProvider) (*manifest.Manifest, filesystem.Fs, error) {
+	fs, err := LoadFS(dirName, envs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	m, err := manifest.Load(fs, false)
+	if err != nil {
+		return nil, nil, err
+	}
+	return m, fs, nil
+}
