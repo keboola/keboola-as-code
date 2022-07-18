5ddb3482883e93f58d6f6ebd1f3baf0646bd8ee8
Jakub Matějka <jakub@keboola.com>
2022-07-18 14:51:24 +0200

    Fix Datadog Source Code Integration

--- provisioning/azure-devops-deploy.sh
+++ provisioning/azure-devops-deploy.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-COMMIT=$(echo "$BUILD_BUILDID" | cut -d'-' -f 2)
-export COMMIT
-
 # Checkout correct commit
 if [ "$RELEASE_ARTIFACTS__KEBOOLA_KEBOOLA_AS_CODE_SOURCEBRANCH" == "master" ]; then
+  COMMIT=$(echo "$BUILD_BUILDID" | cut -d'-' -f 2)
   git checkout "$COMMIT" -f
 fi
+
+COMMIT_HASH=$(git rev-parse HEAD)
+export COMMIT_HASH
 
 export RELEASE_ID="$BUILD_BUILDID-$RELEASE_RELEASENAME"
 
