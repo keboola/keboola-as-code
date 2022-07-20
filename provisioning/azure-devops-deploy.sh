85e91b058f25c48a39d05b533d43e38fe4465cc1
Jakub Matějka <jakub@keboola.com>
2022-07-20 14:14:19 +0200

    Remove faulty git checkout

--- provisioning/azure-devops-deploy.sh
+++ provisioning/azure-devops-deploy.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-# Checkout correct commit
-if [ "$RELEASE_ARTIFACTS__KEBOOLA_KEBOOLA_AS_CODE_SOURCEBRANCH" == "master" ]; then
-  COMMIT=$(echo "$BUILD_BUILDID" | cut -d'-' -f 2)
-  git checkout "$COMMIT" -f
-fi
-
 COMMIT_HASH=$(git rev-parse HEAD)
 export COMMIT_HASH
 
