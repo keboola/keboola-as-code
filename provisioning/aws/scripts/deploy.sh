5b925abdc47a0cbb2657cade3e4af48cbf10b781
Michal Jurečko <michal.jurecko@gmail.com>
2022-08-30 16:20:50 +0200

    Remove temporary scripts

--- provisioning/aws/scripts/deploy.sh
+++ provisioning/aws/scripts/deploy.sh
@@ -6,12 +6,6 @@ kubectl config set-context --current --namespace templates-api
 
 ./provisioning/kubernetes/build.sh
 
-# TEMPORARY
-kubectl delete all -n default -l app=templates-api --ignore-not-found
-kubectl delete configmap -n default templates-api --ignore-not-found
-kubectl delete ingress -n default templates-api --ignore-not-found
-kubectl delete poddisruptionbudget -n default templates-api-pdb --ignore-not-found
-
 kubectl apply -f ./provisioning/kubernetes/deploy/namespace.yaml
 kubectl apply -f ./provisioning/kubernetes/deploy/etcd.yaml
 kubectl apply -f ./provisioning/kubernetes/deploy/config-map.yaml
