3a1682810050306b30542fe45a289fb9630efb8f
Jakub Matějka <jakub@keboola.com>
2022-09-09 17:50:58 +0200

    Fix alias kbc template test

--- internal/pkg/cli/cmd/template/test/cmd.go
+++ internal/pkg/cli/cmd/template/test/cmd.go
@@ -12,7 +12,17 @@ func Commands(d dependencies.Provider) *cobra.Command {
 		Use:   `test`,
 		Short: helpmsg.Read(`template/test/short`),
 		Long:  helpmsg.Read(`template/test/long`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunCommand(d).RunE(cmd, args)
+		},
 	}
+
+	cmd.Flags().SortFlags = true
+	cmd.Flags().String("test-name", "", "name of a single test to be run")
+	cmd.Flags().Bool("local-only", false, "run a local test only")
+	cmd.Flags().Bool("remote-only", false, "run a remote test only")
+	cmd.Flags().Bool("verbose", false, "show details about running tests")
+
 	cmd.AddCommand(
 		CreateCommand(d),
 		RunCommand(d),
