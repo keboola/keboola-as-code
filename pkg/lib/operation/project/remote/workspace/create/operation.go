c1c5a81881ba7baef7320bbfd739c3b8d7b217d1
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-10-11 12:01:37 +0200

    Add sandbox id to create workspace output

--- pkg/lib/operation/project/remote/workspace/create/operation.go
+++ pkg/lib/operation/project/remote/workspace/create/operation.go
@@ -65,7 +65,7 @@ func Run(ctx context.Context, o CreateOptions, d dependencies) (err error) {
 
 	sandbox := s.Sandbox
 
-	logger.Infof(`Created new workspace "%s".`, o.Name)
+	logger.Infof(`Created new workspace "%s" (%s).`, o.Name, s.Sandbox.ID)
 	switch sandbox.Type {
 	case sandboxesapi.TypeSnowflake:
 		logger.Infof(
