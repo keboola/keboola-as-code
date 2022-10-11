9286a70a86e29115d5387f69ccb67e72597c9a38
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-10-11 15:51:16 +0200

    Add `remote workspace detail` command

---
+++ pkg/lib/operation/project/remote/workspace/detail/operation.go
@@ -0,0 +1,68 @@
+package detail
+
+import (
+	"context"
+	"time"
+
+	"github.com/keboola/go-client/pkg/client"
+	"github.com/keboola/go-client/pkg/sandboxesapi"
+	"github.com/keboola/go-client/pkg/storageapi"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/telemetry"
+)
+
+type dependencies interface {
+	Tracer() trace.Tracer
+	Logger() log.Logger
+	StorageApiClient() client.Sender
+	SandboxesApiClient() client.Sender
+}
+
+func Run(ctx context.Context, d dependencies, configId sandboxesapi.ConfigID) (err error) {
+	ctx, span := d.Tracer().Start(ctx, "kac.lib.operation.project.remote.workspace.create")
+	defer telemetry.EndSpan(span, &err)
+
+	logger := d.Logger()
+
+	branch, err := storageapi.GetDefaultBranchRequest().Send(ctx, d.StorageApiClient())
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Minute)
+	defer cancel()
+
+	sandbox, err := sandboxesapi.Get(ctx, d.StorageApiClient(), d.SandboxesApiClient(), branch.ID, configId)
+	if err != nil {
+		return err
+	}
+
+	c, s := sandbox.Config, sandbox.Sandbox
+
+	logger.Infof(`Created new workspace "%s" (%s).`, c.Name, s.ID)
+	logger.Infof("%s (ID: %s, Type: %s, Size: %s)", c.Name, c.ID, s.Type, s.Size)
+	switch s.Type {
+	case sandboxesapi.TypeSnowflake:
+		logger.Infof(
+			"Credentials:\n  Host: %s\n  User: %s\n  Password: %s\n  Database: %s\n  Schema: %s\n  Warehouse: %s",
+			s.Host,
+			s.User,
+			s.Password,
+			s.Details.Connection.Database,
+			s.Details.Connection.Schema,
+			s.Details.Connection.Warehouse,
+		)
+	case sandboxesapi.TypePython:
+		fallthrough
+	case sandboxesapi.TypeR:
+		logger.Infof(
+			"Credentials:\n  Host: %s\n  Password: %s",
+			s.Host,
+			s.Password,
+		)
+	}
+
+	return nil
+}
