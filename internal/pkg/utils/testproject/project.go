1164eeb6a0150d5b72c201eeaceaed930be3d5ba
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-10-19 10:46:56 +0200

    Remove comment

--- internal/pkg/utils/testproject/project.go
+++ internal/pkg/utils/testproject/project.go
@@ -373,7 +373,6 @@ func (p *Project) createBucketsTables(buckets []*fixtures.Bucket) error {
 	return nil
 }
 
-// TODO: set env, log ids
 func (p *Project) createSandboxes(defaultBranchId storageapi.BranchID, sandboxes []*fixtures.Sandbox) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
