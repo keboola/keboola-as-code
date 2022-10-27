38bb69ea04bb66fc5f52eca4b852861723f3737d
Michal Jurečko <michal.jurecko@gmail.com>
2022-10-27 16:04:30 +0200

    Create MasterTokenRequiredError, with 401 HTTP code

--- internal/pkg/dependencies/project_test.go
+++ internal/pkg/dependencies/project_test.go
@@ -1,17 +1,20 @@
 package dependencies
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/keboola/go-client/pkg/storageapi"
 	"github.com/stretchr/testify/assert"
+	goaHttp "goa.design/goa/v3/http"
 )
 
-func TestNewProjectDeps(t *testing.T) {
+func TestNewProjectDeps_MasterTokenRequiredError(t *testing.T) {
 	t.Parallel()
 	d := NewMockedDeps()
 	token := storageapi.Token{IsMaster: false}
 	_, err := newProjectDeps(d, d, token)
 	assert.Error(t, err)
 	assert.Equal(t, "a master token of a project administrator is required", err.Error())
+	assert.Equal(t, http.StatusUnauthorized, err.(goaHttp.Statuser).StatusCode())
 }
