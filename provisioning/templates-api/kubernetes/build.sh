079c22b77a40ed25c86a061d6598d9ae3848f369
Michal Jurečko <michal.jurecko@gmail.com>
2022-10-31 12:50:09 +0100

    Templates API etcd: use helm directly

--- provisioning/templates-api/kubernetes/build.sh
+++ provisioning/templates-api/kubernetes/build.sh
@@ -4,19 +4,8 @@ set -Eeuo pipefail
 # CD to the script directory
 cd "$(dirname "$0")"
 
-# Etcd cluster for Templates API
-: "${TEMPLATES_API_ETCD_REPLICAS:=1}"
-ETCD_ROOT_PASSWORD_BASE64=$(kubectl get secret --namespace templates-api templates-api-etcd -o jsonpath="{.data.etcd-root-password}" 2>/dev/null || echo -e '')
-if [[ "$ETCD_ROOT_PASSWORD_BASE64" == "" ]]; then
-  # Generate random root password if it not set
-  ETCD_ROOT_PASSWORD_BASE64=$(LC_CTYPE=C tr -dc A-Za-z0-9 </dev/urandom | head -c 17 | base64 || echo -e '')
-fi
-# Generate list of cluster endpoints
-ETCD_INITIAL_CLUSTER=$(seq 0 $(($TEMPLATES_API_ETCD_REPLICAS-1)) | awk '{ print "templates-api-etcd-"$0"=http://templates-api-etcd-"$0".templates-api-etcd-headless.templates-api.svc.cluster.local:2380"}' | sed -e 'H;${x;s/\n/,/g;s/^,//;p;};d')
-export TEMPLATES_API_ETCD_REPLICAS
-export ETCD_ROOT_PASSWORD_BASE64
-export ETCD_INITIAL_CLUSTER
-envsubst < templates/etcd.yaml > deploy/etcd.yaml
+# Etcd
+envsubst < templates/etcd-values.yaml > deploy/etcd-values.yaml
 
 # Templates API
 envsubst < templates/namespace.yaml > deploy/namespace.yaml
