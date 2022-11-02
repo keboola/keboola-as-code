ab0c79d0954af03158488fe0b4fac4548eb70f8d
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-02 07:06:53 +0100

    MiniKube: enable Network Policies support, use Calico CNI

--- provisioning/templates-api/local.sh
+++ provisioning/templates-api/local.sh
@@ -13,7 +13,14 @@ if ! minikube status > /dev/null; then
     echo
     echo "Starting minikube ..."
     echo "--------------------------"
-    minikube start
+    # 1. Use v1.24 - https://github.com/kubernetes/minikube/issues/15173
+    # 2. Enable Network Policies support.
+    #    Kindnet - default CNI, does not support Network Policies, by design.
+    #    https://minikube.sigs.k8s.io/docs/handbook/network_policy/
+    minikube start \
+    --wait "apiserver,system_pods,default_sa,apps_running,node_ready,kubelet" \
+    --kubernetes-version=v1.24.7 \
+    --network-plugin=cni --cni=calico
 fi
 
 # Build Docker image in the local Docker, so it is cached, if Minikube is destroyed
