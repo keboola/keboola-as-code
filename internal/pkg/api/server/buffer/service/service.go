b5139447bdbd4e6f76de60c4239d06dbfd804d9d
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-11-15 13:49:53 +0100

    Implement list/get receiver in service

--- internal/pkg/api/server/buffer/service/service.go
+++ internal/pkg/api/server/buffer/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"fmt"
 	"io"
+	"sort"
 
 	dependencies "github.com/keboola/keboola-as-code/internal/pkg/api/server/buffer/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/api/server/buffer/gen/buffer"
@@ -10,6 +11,7 @@ import (
 	. "github.com/keboola/keboola-as-code/internal/pkg/api/server/common/service"
 	"github.com/keboola/keboola-as-code/internal/pkg/idgenerator"
 	"github.com/keboola/keboola-as-code/internal/pkg/model"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/strhelper"
 )
 
@@ -37,38 +39,36 @@ func (*service) HealthCheck(dependencies.ForPublicRequest) (res string, err erro
 }
 
 func (*service) CreateReceiver(d dependencies.ForProjectRequest, payload *buffer.CreateReceiverPayload) (res *buffer.Receiver, err error) {
-	ctx := d.RequestCtx()
+	ctx, store := d.RequestCtx(), d.ConfigStore()
 
-	store := d.ConfigStore()
-
-	config := model.Receiver{
+	receiver := model.Receiver{
 		ProjectID: d.ProjectID(),
 		Name:      payload.Name,
 	}
 
 	// Generate receiver ID from Name if needed
 	if payload.ReceiverID != nil {
-		config.ID = *payload.ReceiverID
+		receiver.ID = *payload.ReceiverID
 	} else {
-		config.ID = strhelper.NormalizeName(config.Name)
+		receiver.ID = strhelper.NormalizeName(receiver.Name)
 	}
 
 	// Generate Secret
-	config.Secret = idgenerator.ReceiverSecret()
+	receiver.Secret = idgenerator.ReceiverSecret()
 
 	// Persist receiver
-	err = store.CreateReceiver(ctx, config)
+	err = store.CreateReceiver(ctx, receiver)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, "failed to create receiver \"%s\"", receiver.ID)
 	}
 
 	// nolint: godox
 	// TODO: create exports
 
-	url := fmt.Sprintf("https://%s/v1/import/%s/#/%s", d.BufferApiHost(), config.ID, config.Secret)
+	url := formatUrl(d.BufferApiHost(), receiver.ID, receiver.Secret)
 	resp := &buffer.Receiver{
-		ReceiverID: &config.ID,
-		Name:       &config.Name,
+		ReceiverID: &receiver.ID,
+		Name:       &receiver.Name,
 		URL:        &url,
 		Exports:    []*Export{},
 	}
@@ -76,12 +76,58 @@ func (*service) CreateReceiver(d dependencies.ForProjectRequest, payload *buffer
 	return resp, nil
 }
 
-func (*service) GetReceiver(dependencies.ForProjectRequest, *buffer.GetReceiverPayload) (res *buffer.Receiver, err error) {
-	return nil, &NotImplementedError{}
+func (*service) GetReceiver(d dependencies.ForProjectRequest, payload *buffer.GetReceiverPayload) (res *buffer.Receiver, err error) {
+	ctx, store := d.RequestCtx(), d.ConfigStore()
+
+	projectId, receiverId := d.ProjectID(), payload.ReceiverID
+
+	receiver, err := store.GetReceiver(ctx, projectId, receiverId)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get receiver \"%s\" in project \"%d\"", receiverId, projectId)
+	}
+
+	// nolint: godox
+	// TODO: get exports
+
+	url := formatUrl(d.BufferApiHost(), receiver.ID, receiver.Secret)
+	resp := &buffer.Receiver{
+		ReceiverID: &receiver.ID,
+		Name:       &receiver.Name,
+		URL:        &url,
+		Exports:    []*Export{},
+	}
+
+	return resp, nil
 }
 
-func (*service) ListReceivers(dependencies.ForProjectRequest, *buffer.ListReceiversPayload) (res []*buffer.Receiver, err error) {
-	return nil, &NotImplementedError{}
+func (*service) ListReceivers(d dependencies.ForProjectRequest, _ *buffer.ListReceiversPayload) (res []*buffer.Receiver, err error) {
+	ctx, store := d.RequestCtx(), d.ConfigStore()
+
+	projectId := d.ProjectID()
+
+	receivers, err := store.ListReceivers(ctx, projectId)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list receivers in project \"%d\"", projectId)
+	}
+
+	bufferApiHost := d.BufferApiHost()
+
+	resp := make([]*buffer.Receiver, 0, len(receivers))
+	for _, receiver := range receivers {
+		url := formatUrl(bufferApiHost, receiver.ID, receiver.Secret)
+		resp = append(resp, &buffer.Receiver{
+			ReceiverID: &receiver.ID,
+			Name:       &receiver.Name,
+			URL:        &url,
+			Exports:    []*Export{},
+		})
+	}
+
+	sort.SliceStable(resp, func(i, j int) bool {
+		return *resp[i].ReceiverID < *resp[j].ReceiverID
+	})
+
+	return resp, nil
 }
 
 func (*service) DeleteReceiver(dependencies.ForProjectRequest, *buffer.DeleteReceiverPayload) (res *buffer.Receiver, err error) {
@@ -106,4 +152,8 @@ func (*service) DeleteExport(dependencies.ForProjectRequest, *buffer.DeleteExpor
 
 func (*service) Import(dependencies.ForPublicRequest, *buffer.ImportPayload, io.ReadCloser) (err error) {
 	return &NotImplementedError{}
+}
+
+func formatUrl(bufferApiHost string, receiverId string, secret string) string {
+	return fmt.Sprintf("https://%s/v1/import/%s/#/%s", bufferApiHost, receiverId, secret)
 }
