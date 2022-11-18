5e3e50dec20ae0140dfb0622153fea61a2ebec23
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-18 10:55:17 +0100

    K8S resources for Buffer API/Worker

--- provisioning/buffer/kubernetes/build.sh
+++ provisioning/buffer/kubernetes/build.sh
@@ -10,3 +10,13 @@ envsubst < templates/namespace.yaml > deploy/namespace.yaml
 # Etcd
 cp ../../common/etcd/values.yaml deploy/etcd/values_common.yaml
 cp templates/etcd.yaml deploy/etcd/values_buffer.yaml
+
+# API
+envsubst < templates/api/config-map.yaml > deploy/api/config-map.yaml
+envsubst < templates/api/pdb.yaml        > deploy/api/pdb.yaml
+envsubst < templates/api/deployment.yaml > deploy/api/deployment.yaml
+
+# Worker
+envsubst < templates/worker/config-map.yaml > deploy/worker/config-map.yaml
+envsubst < templates/worker/pdb.yaml        > deploy/worker/pdb.yaml
+envsubst < templates/worker/deployment.yaml > deploy/worker/deployment.yaml
