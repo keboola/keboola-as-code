7cdf7b0cfe6e4a1c7f8bc0c5d129ba75fdd45d1a
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-21 08:17:01 +0100

    Add install MiniKube script

---
+++ provisioning/common/scripts/minikube/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd /tmp
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
