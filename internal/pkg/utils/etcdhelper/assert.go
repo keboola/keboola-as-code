0892f2317b1be9c8f3a80bd542aa64d804346ca4
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-23 08:38:39 +0100

    Use wildcards

--- internal/pkg/utils/etcdhelper/assert.go
+++ internal/pkg/utils/etcdhelper/assert.go
@@ -5,14 +5,16 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/keboola/go-utils/pkg/wildcards"
 	etcd "go.etcd.io/etcd/client/v3"
 )
 
+// AssertKVs dumps all KVs from an etcd database and compares them with the expected string.
+// In the expected string, a wildcards can be used, see the wildcards package.
 func AssertKVs(t *testing.T, client etcd.KV, expected string) {
 	dump, err := DumpAll(context.Background(), client)
 	if err != nil {
 		t.Fatalf(`cannot dump etcd KVs: %s`, err)
 	}
-	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(dump), `unexpected etcd state`)
+	wildcards.Assert(t, strings.TrimSpace(expected), strings.TrimSpace(dump), `unexpected etcd state`)
 }
