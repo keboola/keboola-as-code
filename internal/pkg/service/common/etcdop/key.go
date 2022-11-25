d9290f1142b646da1b197b1fbab350eea2237403
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-25 12:39:39 +0100

    etcdop: add KeyT and PrefixT generic types + support for encode/decode/validate

--- internal/pkg/service/common/etcdop/key.go
+++ internal/pkg/service/common/etcdop/key.go
@@ -1,7 +1,8 @@
 package etcdop
 
 import (
-	"go.etcd.io/etcd/api/v3/mvccpb"
+	"context"
+
 	etcd "go.etcd.io/etcd/client/v3"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
@@ -10,64 +11,166 @@ import (
 // Key represents an etcd key - one key, not a prefix.
 type Key string
 
+type key = Key
+
+// KeyT extends Key with generic functionality, contains type of the serialized value.
+type KeyT[T any] struct {
+	key
+	serialization Serialization
+}
+
 func (v Key) Key() string {
 	return string(v)
 }
 
 func (v Key) Exists(opts ...etcd.OpOption) BoolOp {
 	opts = append([]etcd.OpOption{etcd.WithCountOnly()}, opts...)
-	return NewBoolOp(etcd.OpGet(v.Key(), opts...), func(r etcd.OpResponse) (bool, error) {
-		count := r.Get().Count
-		if count == 0 {
-			return false, nil
-		} else if count == 1 {
-			return true, nil
-		} else {
-			return false, errors.Errorf(`etcd exists: at most one result result expected, found %d results`, count)
-		}
-	})
+	return NewBoolOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpGet(v.Key(), opts...)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, r etcd.OpResponse) (bool, error) {
+			count := r.Get().Count
+			if count == 0 {
+				return false, nil
+			} else if count == 1 {
+				return true, nil
+			} else {
+				return false, errors.Errorf(`etcd exists: at most one result result expected, found %d results`, count)
+			}
+		},
+	)
 }
 
 func (v Key) Get(opts ...etcd.OpOption) GetOneOp {
-	return NewGetOneOp(etcd.OpGet(v.Key(), opts...), func(r etcd.OpResponse) (*mvccpb.KeyValue, error) {
-		count := r.Get().Count
-		if count == 0 {
-			return nil, nil
-		} else if count == 1 {
-			return r.Get().Kvs[0], nil
-		} else {
-			return nil, errors.Errorf(`etcd get: at most one result result expected, found %d results`, count)
-		}
-	})
+	return NewGetOneOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpGet(v.Key(), opts...)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, r etcd.OpResponse) (*KeyValue, error) {
+			count := r.Get().Count
+			if count == 0 {
+				return nil, nil
+			} else if count == 1 {
+				return r.Get().Kvs[0], nil
+			} else {
+				return nil, errors.Errorf(`etcd get: at most one result result expected, found %d results`, count)
+			}
+		},
+	)
 }
 
 func (v Key) Delete(opts ...etcd.OpOption) BoolOp {
-	return NewBoolOp(etcd.OpDelete(v.Key(), opts...), func(r etcd.OpResponse) (bool, error) {
-		count := r.Del().Deleted
-		if count == 0 {
-			return false, nil
-		} else if count == 1 {
-			return true, nil
-		} else {
-			return false, errors.Errorf(`etcd delete: at most one result result expected, found %d results`, count)
-		}
-	})
+	return NewBoolOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpDelete(v.Key(), opts...)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, r etcd.OpResponse) (bool, error) {
+			count := r.Del().Deleted
+			if count == 0 {
+				return false, nil
+			} else if count == 1 {
+				return true, nil
+			} else {
+				return false, errors.Errorf(`etcd delete: at most one result result expected, found %d results`, count)
+			}
+		},
+	)
 }
 
 func (v Key) Put(val string, opts ...etcd.OpOption) NoResultOp {
-	return NewNoResultOp(etcd.OpPut(v.Key(), val, opts...), func(_ etcd.OpResponse) error {
-		// response is always OK
-		return nil
-	})
+	return NewNoResultOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpPut(v.Key(), val, opts...)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, _ etcd.OpResponse) error {
+			// response is always OK
+			return nil
+		},
+	)
 }
 
 func (v Key) PutIfNotExists(val string, opts ...etcd.OpOption) BoolOp {
-	op := etcd.OpTxn(
-		[]etcd.Cmp{etcd.Compare(etcd.Version(v.Key()), "=", 0)},
-		[]etcd.Op{etcd.OpPut(v.Key(), val, opts...)},
-		[]etcd.Op{},
+	return NewBoolOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpTxn(
+				[]etcd.Cmp{etcd.Compare(etcd.Version(v.Key()), "=", 0)},
+				[]etcd.Op{etcd.OpPut(v.Key(), val, opts...)},
+				[]etcd.Op{},
+			)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, r etcd.OpResponse) (bool, error) {
+			return r.Txn().Succeeded, nil
+		},
 	)
-	return NewBoolOp(op, func(r etcd.OpResponse) (bool, error) {
-		return r.Txn().Succeeded, nil
-	})
+}
+
+func (v KeyT[T]) Get(opts ...etcd.OpOption) GetOneTOp[T] {
+	return NewGetOneTOp(
+		func(_ context.Context) (*etcd.Op, error) {
+			etcdOp := etcd.OpGet(v.Key(), opts...)
+			return &etcdOp, nil
+		},
+		func(ctx context.Context, r etcd.OpResponse) (*KeyValueT[T], error) {
+			count := r.Get().Count
+			if count == 0 {
+				return nil, nil
+			} else if count == 1 {
+				kv := r.Get().Kvs[0]
+				target := new(T)
+				if err := v.serialization.decodeAndValidate(ctx, kv, target); err != nil {
+					return nil, invalidKeyError(string(kv.Key), err)
+				}
+				return &KeyValueT[T]{Value: *target, KV: kv}, nil
+			} else {
+				return nil, errors.Errorf(`etcd get: at most one result result expected, found %d results`, count)
+			}
+		},
+	)
+}
+
+func (v KeyT[T]) Put(val T, opts ...etcd.OpOption) NoResultOp {
+	return NewNoResultOp(
+		func(ctx context.Context) (*etcd.Op, error) {
+			encoded, err := v.serialization.validateAndEncode(ctx, &val)
+			if err != nil {
+				return nil, invalidKeyError(v.Key(), err)
+			}
+			etcdOp := etcd.OpPut(v.Key(), encoded, opts...)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, _ etcd.OpResponse) error {
+			// response is always OK
+			return nil
+		},
+	)
+}
+
+func (v KeyT[T]) PutIfNotExists(val string, opts ...etcd.OpOption) BoolOp {
+	return NewBoolOp(
+		func(ctx context.Context) (*etcd.Op, error) {
+			encoded, err := v.serialization.validateAndEncode(ctx, &val)
+			if err != nil {
+				return nil, invalidKeyError(v.Key(), err)
+			}
+			etcdOp := etcd.OpTxn(
+				[]etcd.Cmp{etcd.Compare(etcd.Version(v.Key()), "=", 0)},
+				[]etcd.Op{etcd.OpPut(v.Key(), encoded, opts...)},
+				[]etcd.Op{},
+			)
+			return &etcdOp, nil
+		},
+		func(_ context.Context, r etcd.OpResponse) (bool, error) {
+			return r.Txn().Succeeded, nil
+		},
+	)
+}
+
+func invalidKeyError(key string, err error) error {
+	return errors.Errorf(`invalid etcd key "%s": %w`, key, err)
 }
--- internal/pkg/service/common/etcdop/key_test.go
+++ internal/pkg/service/common/etcdop/key_test.go
@@ -6,8 +6,11 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/keboola/keboola-as-code/internal/pkg/encoding/json"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 )
+
+type fooType string
 
 func TestKeyOperations(t *testing.T) {
 	t.Parallel()
@@ -86,6 +89,85 @@ func TestKeyOperations(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, kv)
 	assert.Equal(t, []byte("value1"), kv.Value)
+}
+
+func TestTypedKeyOperations(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(t)
+
+	k := typedKeyForTest()
+
+	// Get - not found
+	kv, err := k.Get().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.Nil(t, kv)
+
+	// Exists - not found
+	found, err := k.Exists().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.False(t, found)
+
+	// ------
+	// Put
+	err = k.Put("bar").Do(ctx, etcd)
+	assert.NoError(t, err)
+
+	// Get - found
+	kv, err = k.Get().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, fooType("bar"), kv.Value)
+
+	// Exists - found
+	found, err = k.Exists().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.True(t, found)
+
+	// ------
+	// Delete - found
+	found, err = k.Delete().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.True(t, found)
+
+	// Delete - not found
+	found, err = k.Delete().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.False(t, found)
+
+	// Get - not found
+	kv, err = k.Get().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.Nil(t, kv)
+
+	// Exists - not found
+	found, err = k.Exists().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.False(t, found)
+
+	// ------
+	// PutIfNotExists - key not found -> ok
+	ok, err := k.PutIfNotExists("value1").Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	// Get - found - value 1
+	kv, err = k.Get().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, fooType("value1"), kv.Value)
+
+	// PutIfNotExists - key found -> not ok
+	ok, err = k.PutIfNotExists("value1").Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	// Get - found - value 1
+	kv, err = k.Get().Do(ctx, etcd)
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+	assert.Equal(t, fooType("value1"), kv.Value)
 }
 
 func BenchmarkKey_Exists(b *testing.B) {
@@ -174,5 +256,118 @@ func BenchmarkKey_PutIfNotExists(b *testing.B) {
 		if err != nil || ok != (i == 0) {
 			b.Fatalf("unexpected result")
 		}
+	}
+}
+
+func BenchmarkKeyT_Exists(b *testing.B) {
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(b)
+
+	k := typedKeyForTest()
+	if err := k.Put("bar").Do(ctx, etcd); err != nil {
+		b.Fatalf("cannot create etcd key: %s", err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		found, err := k.Exists().Do(ctx, etcd)
+		if err != nil || !found {
+			b.Fatalf("unexpected result")
+		}
+	}
+}
+
+func BenchmarkKeyT_Get(b *testing.B) {
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(b)
+
+	k := typedKeyForTest()
+	if err := k.Put("bar").Do(ctx, etcd); err != nil {
+		b.Fatalf("cannot create etcd key: %s", err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		kv, err := k.Get().Do(ctx, etcd)
+		if err != nil || kv == nil {
+			b.Fatalf("unexpected result")
+		}
+	}
+}
+
+func BenchmarkKeyT_Delete(b *testing.B) {
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(b)
+
+	k := typedKeyForTest()
+	if err := k.Put("bar").Do(ctx, etcd); err != nil {
+		b.Fatalf("cannot create etcd key: %s", err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		found, err := k.Delete().Do(ctx, etcd)
+		if err != nil || found != (i == 0) {
+			b.Fatalf("unexpected result")
+		}
+	}
+}
+
+func BenchmarkKeyT_Put(b *testing.B) {
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(b)
+
+	k := typedKeyForTest()
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		err := k.Put("bar").Do(ctx, etcd)
+		if err != nil {
+			b.Fatalf("unexpected result")
+		}
+	}
+}
+
+func BenchmarkKeyT_PutIfNotExists(b *testing.B) {
+	ctx := context.Background()
+	etcd := etcdhelper.ClientForTest(b)
+
+	k := typedKeyForTest()
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		ok, err := k.PutIfNotExists("bar").Do(ctx, etcd)
+		if err != nil || ok != (i == 0) {
+			b.Fatalf("unexpected result")
+		}
+	}
+}
+
+func typedKeyForTest() KeyT[fooType] {
+	return KeyT[fooType]{
+		key:           Key("foo"),
+		serialization: JSONSerialization(nil),
+	}
+}
+
+func JSONSerialization(validate validateFn) Serialization {
+	if validate == nil {
+		validate = func(_ context.Context, _ any) error {
+			return nil
+		}
+	}
+	return Serialization{
+		encode: func(_ context.Context, value any) (string, error) {
+			return json.EncodeString(value, false)
+		},
+		decode: func(_ context.Context, data []byte, target any) error {
+			return json.Decode(data, target)
+		},
+		validate: validate,
 	}
 }
