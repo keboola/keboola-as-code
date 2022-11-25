409b15d84b8becdfbd22ee242a9a95f95c3a9d47
Michal Jurečko <michal.jurecko@gmail.com>
2022-11-25 16:01:57 +0100

    Update schema definitions

--- internal/pkg/service/buffer/model/schema/schema.go
+++ internal/pkg/service/buffer/model/schema/schema.go
@@ -1,6 +1,33 @@
 // Package schema defines etcd keys for the Buffer service.
 package schema
 
-import . "github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
+import (
+	"context"
 
-type prefix = Prefix
+	"github.com/keboola/keboola-as-code/internal/pkg/encoding/json"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
+)
+
+type Schema struct {
+	serialization etcdop.Serialization
+}
+
+type validateFn func(ctx context.Context, value any) error
+
+type prefix = etcdop.Prefix
+
+func New(validate validateFn) *Schema {
+	return &Schema{
+		serialization: etcdop.NewSerialization(
+			func(ctx context.Context, value any) (string, error) {
+				return json.EncodeString(value, false)
+			},
+			func(ctx context.Context, data []byte, target any) error {
+				return json.Decode(data, target)
+			},
+			func(ctx context.Context, value any) error {
+				return validate(ctx, value)
+			},
+		),
+	}
+}
