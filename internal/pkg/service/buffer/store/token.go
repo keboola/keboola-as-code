66825a983e9759f9e98171d712e0559754a90608
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-11-29 14:36:51 +0100

    Store token ops

---
+++ internal/pkg/service/buffer/store/token.go
@@ -0,0 +1,53 @@
+package store
+
+import (
+	"context"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model"
+	serviceError "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	etcd "go.etcd.io/etcd/client/v3"
+)
+
+func (s *Store) createTokenOp(_ context.Context, exportKey key.ExportKey, token model.Token) op.BoolOp {
+	return s.schema.
+		Secrets().
+		Tokens().
+		InExport(exportKey).
+		PutIfNotExists(token).
+		WithProcessor(func(_ context.Context, _ etcd.OpResponse, ok bool, err error) (bool, error) {
+			if !ok && err == nil {
+				return false, serviceError.NewResourceAlreadyExistsError("token", exportKey.String(), "export")
+			}
+			return ok, err
+		})
+}
+
+func (s *Store) getTokenOp(_ context.Context, exportKey key.ExportKey) op.ForType[*op.KeyValueT[model.Token]] {
+	return s.schema.
+		Secrets().
+		Tokens().
+		InExport(exportKey).
+		Get().
+		WithProcessor(func(_ context.Context, _ etcd.OpResponse, kv *op.KeyValueT[model.Token], err error) (*op.KeyValueT[model.Token], error) {
+			if kv == nil && err == nil {
+				return nil, serviceError.NewResourceNotFoundError("token", exportKey.String())
+			}
+			return kv, err
+		})
+}
+
+func (s *Store) deleteTokenOp(_ context.Context, exportKey key.ExportKey) op.BoolOp {
+	return s.schema.
+		Secrets().
+		Tokens().
+		InExport(exportKey).
+		Delete().
+		WithProcessor(func(_ context.Context, _ etcd.OpResponse, result bool, err error) (bool, error) {
+			if result == false && err == nil {
+				return false, serviceError.NewResourceNotFoundError("token", exportKey.String())
+			}
+			return result, err
+		})
+}
