90a5656800646d3a910e2744a6a9ced3197665eb
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-11-29 13:25:12 +0100

    Add panics

--- internal/pkg/service/buffer/store/schema/token.go
+++ internal/pkg/service/buffer/store/schema/token.go
@@ -6,6 +6,7 @@ import (
 	storeKey "github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model"
 	. "github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 type tokens = PrefixT[model.Token]
@@ -22,6 +23,15 @@ func (v SecretsRoot) Tokens() Tokens {
 }
 
 func (v Tokens) InExport(k storeKey.ExportKey) KeyT[model.Token] {
+	if k.ProjectID == 0 {
+		panic(errors.New("record projectID cannot be empty"))
+	}
+	if k.ReceiverID == "" {
+		panic(errors.New("record receiverID cannot be empty"))
+	}
+	if k.ExportID == "" {
+		panic(errors.New("record exportID cannot be empty"))
+	}
 	return v.tokens.
 		Add(strconv.Itoa(k.ProjectID)).
 		Add(k.ReceiverID).
