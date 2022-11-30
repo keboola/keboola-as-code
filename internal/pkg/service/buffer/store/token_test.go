9d09acd2e6239ff83022d8671e4929572aa363e4
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-11-30 11:11:14 +0100

    Add tests for new mappings

--- internal/pkg/service/buffer/store/token_test.go
+++ internal/pkg/service/buffer/store/token_test.go
@@ -60,7 +60,7 @@ secret/export/token/1000/receiver1/export1
 `)
 }
 
-func TestStore_DeleteTokenOp(t *testing.T) {
+func TestStore_DeleteExportTokenOp(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
@@ -108,7 +108,103 @@ secret/export/token/1000/receiver1/export1
 >>>>>
 `)
 
-	_, err = store.deleteTokenOp(ctx, exportKey).Do(ctx, store.client)
+	_, err = store.deleteExportTokenOp(ctx, exportKey).Do(ctx, store.client)
+	assert.NoError(t, err)
+
+	etcdhelper.AssertKVs(t, store.client, ``)
+}
+
+func TestStore_DeleteReceiverTokensOp(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	store := newStoreForTest(t)
+
+	receiverKey := key.ReceiverKey{ProjectID: 1000, ReceiverID: "receiver1"}
+
+	input := []model.TokenForExport{
+		{
+			ExportKey: key.ExportKey{ReceiverKey: receiverKey, ExportID: "export0"},
+			Token:     model.Token{Token: "test0"},
+		},
+		{
+			ExportKey: key.ExportKey{ReceiverKey: receiverKey, ExportID: "export1"},
+			Token:     model.Token{Token: "test1"},
+		},
+	}
+
+	for _, token := range input {
+		_, err := store.createTokenOp(ctx, token).Do(ctx, store.client)
+		assert.NoError(t, err)
+	}
+
+	token, err := store.getTokenOp(ctx, input[0].ExportKey).Do(ctx, store.client)
+	assert.NoError(t, err)
+	assert.Equal(t, input[0], token.Value)
+
+	token, err = store.getTokenOp(ctx, input[1].ExportKey).Do(ctx, store.client)
+	assert.NoError(t, err)
+	assert.Equal(t, input[1], token.Value)
+
+	etcdhelper.AssertKVs(t, store.client, `
+<<<<<
+secret/export/token/1000/receiver1/export0
+-----
+{
+  "projectId": 1000,
+  "receiverId": "receiver1",
+  "exportId": "export0",
+  "token": "test0",
+  "id": "",
+  "description": "",
+  "isMasterToken": false,
+  "canManageBuckets": false,
+  "canManageTokens": false,
+  "canReadAllFileUploads": false,
+  "canPurgeTrash": false,
+  "created": "0001-01-01T00:00:00Z",
+  "refreshed": "0001-01-01T00:00:00Z",
+  "expires": null,
+  "isExpired": false,
+  "isDisabled": false,
+  "owner": {
+    "id": 0,
+    "name": "",
+    "features": null
+  }
+}
+>>>>>
+
+<<<<<
+secret/export/token/1000/receiver1/export1
+-----
+{
+  "projectId": 1000,
+  "receiverId": "receiver1",
+  "exportId": "export1",
+  "token": "test1",
+  "id": "",
+  "description": "",
+  "isMasterToken": false,
+  "canManageBuckets": false,
+  "canManageTokens": false,
+  "canReadAllFileUploads": false,
+  "canPurgeTrash": false,
+  "created": "0001-01-01T00:00:00Z",
+  "refreshed": "0001-01-01T00:00:00Z",
+  "expires": null,
+  "isExpired": false,
+  "isDisabled": false,
+  "owner": {
+    "id": 0,
+    "name": "",
+    "features": null
+  }
+}
+>>>>>
+`)
+
+	_, err = store.deleteReceiverTokensOp(ctx, receiverKey).Do(ctx, store.client)
 	assert.NoError(t, err)
 
 	etcdhelper.AssertKVs(t, store.client, ``)
