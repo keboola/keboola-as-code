7feeab6c8a93b5c2eb7eba822f8544233ae778a8
Jakub Matějka <jakub@keboola.com>
2022-11-30 12:53:14 +0100

    Resolve import conditions

--- internal/pkg/service/buffer/store/model/model.go
+++ internal/pkg/service/buffer/store/model/model.go
@@ -62,6 +62,42 @@ type ImportConditions struct {
 	Time  time.Duration     `json:"time" validate:"min=30000000000,max=86400000000000"` // 30s-24h
 }
 
+func (c ImportConditions) ShouldImport(s CurrentImportState) (bool, string) {
+	if s.Count == 0 {
+		return false, "no data to import"
+	}
+
+	defaults := DefaultConditions()
+	if c.Count == 0 {
+		c.Count = defaults.Count
+	}
+	if c.Size == 0 {
+		c.Size = defaults.Size
+	}
+	if c.Time == 0 {
+		c.Time = defaults.Time
+	}
+
+	if s.Count >= c.Count {
+		return true, fmt.Sprintf("import count limit met, received: %d rows, limit: %d rows", s.Count, c.Count)
+	}
+	if s.Size >= c.Size {
+		return true, fmt.Sprintf("import size limit met, received: %s, limit: %s", s.Size.String(), c.Size.String())
+	}
+	sinceLastImport := s.Now.Sub(s.LastImportAt).Truncate(time.Second)
+	if sinceLastImport >= c.Time {
+		return true, fmt.Sprintf("import time limit met, last import at: %s, passed: %s limit: %s", s.LastImportAt.Format(time.Stamp), sinceLastImport.String(), c.Time.String())
+	}
+	return false, "conditions not met"
+}
+
+type CurrentImportState struct {
+	Count        int
+	Size         datasize.ByteSize
+	Now          time.Time
+	LastImportAt time.Time
+}
+
 type TableID struct {
 	Stage  string `json:"stage" validate:"required,oneof=in out sys"`
 	Bucket string `json:"bucketName" validate:"required,min=1,max=96"`
--- internal/pkg/service/buffer/store/model/model_test.go
+++ internal/pkg/service/buffer/store/model/model_test.go
@@ -2,7 +2,9 @@ package model_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/c2h5oh/datasize"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model"
@@ -22,4 +24,106 @@ func TestTableID_String(t *testing.T) {
 	parsed, err := model.ParseTableID(s)
 	assert.NoError(t, err)
 	assert.Equal(t, tableID, parsed)
+}
+
+func TestImportConditions_ShouldImport_Defaults(t *testing.T) {
+	t.Parallel()
+
+	// Defaults
+	ic := model.ImportConditions{
+		Count: 0,
+		Size:  0,
+		Time:  0,
+	}
+	// Defaults not met
+	res, desc := ic.ShouldImport(model.CurrentImportState{
+		Count:        50,
+		Size:         1 * datasize.KB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.False(t, res)
+	assert.Equal(t, "conditions not met", desc)
+
+	// Default count met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        2000,
+		Size:         1 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Equal(t, "import count limit met, received: 2000 rows, limit: 1000 rows", desc)
+
+	// Default size met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        100,
+		Size:         5 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Equal(t, "import size limit met, received: 5MB, limit: 1MB", desc)
+
+	// Default time met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        100,
+		Size:         1 * datasize.KB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-10 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Contains(t, desc, "import time limit met, last import at")
+	assert.Contains(t, desc, "limit: 5m0s")
+}
+
+func TestImportConditions_ShouldImport_Custom(t *testing.T) {
+	t.Parallel()
+
+	// Defaults
+	ic := model.ImportConditions{
+		Count: 100,
+		Size:  5 * datasize.MB,
+		Time:  10 * time.Minute,
+	}
+	// Not met
+	res, desc := ic.ShouldImport(model.CurrentImportState{
+		Count:        50,
+		Size:         1 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.False(t, res)
+	assert.Equal(t, "conditions not met", desc)
+
+	// Count met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        200,
+		Size:         1 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Equal(t, "import count limit met, received: 200 rows, limit: 100 rows", desc)
+
+	// Size met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        50,
+		Size:         10 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-1 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Equal(t, "import size limit met, received: 10MB, limit: 5MB", desc)
+
+	// Time met
+	res, desc = ic.ShouldImport(model.CurrentImportState{
+		Count:        50,
+		Size:         1 * datasize.MB,
+		Now:          time.Now(),
+		LastImportAt: time.Now().Add(-20 * time.Minute),
+	})
+	assert.True(t, res)
+	assert.Contains(t, desc, "import time limit met, last import at")
+	assert.Contains(t, desc, "limit: 10m0s")
 }
