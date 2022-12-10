6ab82627d86c06d7a62397430158ac77774f6c60
Michal Jurečko <michal.jurecko@gmail.com>
2022-12-10 17:40:21 +0100

    Raw watch - test

--- internal/pkg/service/common/etcdop/watch_test.go
+++ internal/pkg/service/common/etcdop/watch_test.go
@@ -12,6 +12,71 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 )
 
+func TestPrefix_Watch(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c := etcdhelper.ClientForTest(t)
+	pfx := prefixForTest()
+
+	// Create watcher
+	errHandler := func(err error) {
+		assert.FailNow(t, `unexpected watch error`, err.Error())
+	}
+	ch := pfx.Watch(ctx, c, errHandler, etcd.WithRev(1)) // rev=1, always include complete history
+
+	// CREATE key
+	go func() {
+		assert.NoError(t, pfx.Key("key1").Put("foo").Do(ctx, c))
+	}()
+
+	// Wait for CREATE event
+	assertDone(t, func() {
+		expected := Event{}
+		expected.Type = CreateEvent
+		expected.KeyValue = &mvccpb.KeyValue{
+			Key:   []byte("my/prefix/key1"),
+			Value: []byte("foo"),
+		}
+		assert.Equal(t, expected, clearEvent(<-ch))
+	}, "CREATE timeout")
+
+	// UPDATE key
+	go func() {
+		assert.NoError(t, pfx.Key("key1").Put("new").Do(ctx, c))
+	}()
+
+	// Wait for UPDATE event
+	assertDone(t, func() {
+		expected := Event{}
+		expected.Type = UpdateEvent
+		expected.KeyValue = &mvccpb.KeyValue{
+			Key:   []byte("my/prefix/key1"),
+			Value: []byte("new"),
+		}
+		assert.Equal(t, expected, clearEvent(<-ch))
+	}, "UPDATE timeout")
+
+	// DELETE key
+	go func() {
+		ok, err := pfx.Key("key1").Delete().Do(ctx, c)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+	}()
+
+	// Wait for DELETE event
+	assertDone(t, func() {
+		expected := Event{}
+		expected.Type = DeleteEvent
+		expected.KeyValue = &mvccpb.KeyValue{
+			Key: []byte("my/prefix/key1"),
+		}
+		assert.Equal(t, expected, clearEvent(<-ch))
+	}, "DELETE timeout")
+}
+
 func TestPrefixT_Watch(t *testing.T) {
 	t.Parallel()
 
@@ -41,7 +106,7 @@ func TestPrefixT_Watch(t *testing.T) {
 			Key:   []byte("my/prefix/key1"),
 			Value: []byte(`"foo"`),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "CREATE timeout")
 
 	// UPDATE key
@@ -58,7 +123,7 @@ func TestPrefixT_Watch(t *testing.T) {
 			Key:   []byte("my/prefix/key1"),
 			Value: []byte(`"new"`),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "UPDATE timeout")
 
 	// DELETE key
@@ -75,7 +140,7 @@ func TestPrefixT_Watch(t *testing.T) {
 		expected.KV = &mvccpb.KeyValue{
 			Key: []byte("my/prefix/key1"),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "DELETE timeout")
 }
 
@@ -106,7 +171,7 @@ func TestPrefixT_GetAllAndWatch(t *testing.T) {
 			Key:   []byte("my/prefix/key1"),
 			Value: []byte(`"foo1"`),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "CREATE1 timeout")
 
 	// CREATE key2
@@ -123,7 +188,7 @@ func TestPrefixT_GetAllAndWatch(t *testing.T) {
 			Key:   []byte("my/prefix/key2"),
 			Value: []byte(`"foo2"`),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "CREATE2 timeout")
 
 	// UPDATE key
@@ -140,7 +205,7 @@ func TestPrefixT_GetAllAndWatch(t *testing.T) {
 			Key:   []byte("my/prefix/key2"),
 			Value: []byte(`"new"`),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "UPDATE timeout")
 
 	// DELETE key
@@ -157,11 +222,20 @@ func TestPrefixT_GetAllAndWatch(t *testing.T) {
 		expected.KV = &mvccpb.KeyValue{
 			Key: []byte("my/prefix/key1"),
 		}
-		assert.Equal(t, expected, clearEvent(<-ch))
+		assert.Equal(t, expected, clearEventT(<-ch))
 	}, "DELETE timeout")
 }
 
-func clearEvent(event EventT[fooType]) EventT[fooType] {
+func clearEvent(event Event) Event {
+	event.Header = nil
+	event.KeyValue.CreateRevision = 0
+	event.KeyValue.ModRevision = 0
+	event.KeyValue.Version = 0
+	event.KeyValue.Lease = 0
+	return event
+}
+
+func clearEventT(event EventT[fooType]) EventT[fooType] {
 	event.Header = nil
 	event.KV.CreateRevision = 0
 	event.KV.ModRevision = 0
