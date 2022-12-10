4fac1fb979b24885b9554611c0f6642040e7e274
Michal Jurečko <michal.jurecko@gmail.com>
2022-12-10 23:06:43 +0100

    Use distribution pkg

--- internal/pkg/service/buffer/worker/service/service.go
+++ internal/pkg/service/buffer/worker/service/service.go
@@ -2,16 +2,18 @@ package service
 
 import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/worker/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/worker/distribution"
 )
 
 type Service struct {
 	dependencies.ForWorker
+	dist *distribution.Node
 }
 
-func New(d dependencies.ForWorker) *Service {
-	return &Service{ForWorker: d}
-}
-
-func (s *Service) Start() {
-	s.Logger().Info("starting worker goroutines")
+func New(d dependencies.ForWorker) (*Service, error) {
+	dist, err := distribution.NewNode(d)
+	if err != nil {
+		return nil, err
+	}
+	return &Service{ForWorker: d, dist: dist}, nil
 }
