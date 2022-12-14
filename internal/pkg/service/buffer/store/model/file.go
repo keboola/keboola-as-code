316c31e625d50144f24374b0aa66dcab12cdd5de
Michal Jurečko <michal.jurecko@gmail.com>
2022-12-14 18:26:55 +0100

    Create filestate pkg

--- internal/pkg/service/buffer/store/model/file.go
+++ internal/pkg/service/buffer/store/model/file.go
@@ -5,26 +5,31 @@ import (
 
 	"github.com/keboola/go-client/pkg/storageapi"
 
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/filestate"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
 )
 
+// File represent a file with records.
+// A copy of the mapping is stored for retrieval optimization.
+// A change in the mapping causes a new file to be created so the mapping is immutable.
 type File struct {
 	key.FileKey
+	State              filestate.State  `json:"state" validate:"required,oneof=opened closing closed importing imported failed"`
 	Mapping            Mapping          `json:"mapping" validate:"required,dive"`
 	StorageResource    *storageapi.File `json:"storageResource" validate:"required"`
 	ClosingAt          *time.Time       `json:"closingAt,omitempty"`
 	ClosedAt           *time.Time       `json:"closedAt,omitempty"`
-	ManifestUploadedAt *time.Time       `json:"manifestUploadedAt,omitempty"`
+	ImportingAt        *time.Time       `json:"importingAt,omitempty"`
 	ImportedAt         *time.Time       `json:"importedAt,omitempty"`
+	ManifestUploadedAt *time.Time       `json:"manifestUploadedAt,omitempty"`
 	FailedAt           *time.Time       `json:"failedAt,omitempty"`
-	ImportStartedAt    *time.Time       `json:"importStartedAt,omitempty"`
-	ImportFinishedAt   *time.Time       `json:"importFinishedAt,omitempty"`
 	LastError          string           `json:"lastError,omitempty"`
 }
 
 func NewFile(exportKey key.ExportKey, now time.Time, mapping Mapping, resource *storageapi.File) File {
 	return File{
 		FileKey:         key.FileKey{ExportKey: exportKey, FileID: now},
+		State:           filestate.Opened,
 		Mapping:         mapping,
 		StorageResource: resource,
 	}
