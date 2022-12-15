db36564a1f780a62efce0269546c10341d4649cd
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-12-15 13:43:26 +0100

    Update manager to be more testable

--- internal/pkg/service/buffer/stats/manager.go
+++ internal/pkg/service/buffer/stats/manager.go
@@ -2,10 +2,10 @@ package stats
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/benbjohnson/clock"
-
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model"
@@ -13,10 +13,13 @@ import (
 
 type Manager struct {
 	ch       chan update
-	store    *store.Store
+	syncFn   syncFn
 	clock    clock.Clock
+	ticker   *clock.Ticker
 	perSlice map[key.SliceStatsKey]*stats
 }
+
+type syncFn func(context.Context, []model.SliceStats)
 
 type update struct {
 	key            key.SliceStatsKey
@@ -32,22 +35,23 @@ type stats struct {
 	changed        bool
 }
 
-func New(store *store.Store, clock clock.Clock) Manager {
+func New(store *store.Store) Manager {
+	clock := clock.New()
 	return Manager{
 		// channel needs to be large enough to not block under average load
 		ch:       make(chan update, 2048),
-		store:    store,
+		syncFn:   syncToStore(store),
 		clock:    clock,
+		ticker:   clock.Ticker(time.Second),
 		perSlice: make(map[key.SliceStatsKey]*stats),
 	}
 }
 
-func (m *Manager) Notify(key key.SliceStatsKey, count uint64, size uint64) {
+func (m *Manager) Notify(key key.SliceStatsKey, size uint64) {
 	m.ch <- update{
 		key:            key,
-		count:          count,
 		size:           size,
-		lastReceivedAt: time.Now(),
+		lastReceivedAt: m.clock.Now(),
 	}
 }
 
@@ -56,11 +60,15 @@ func (m *Manager) Watch(ctx context.Context) {
 		for {
 			select {
 			case <-ctx.Done():
+				m.ticker.Stop()
+				fmt.Println("stats watcher done")
 				return
 			case update := <-m.ch:
+				fmt.Println("stats watcher update")
 				m.handleUpdate(update)
-			case <-m.clock.After(time.Second):
-				m.syncStats(ctx)
+			case <-m.ticker.C:
+				fmt.Println("stats watcher sync")
+				m.handleSync(ctx)
 			}
 		}
 	}()
@@ -74,7 +82,7 @@ func (m *Manager) handleUpdate(update update) {
 
 	// Update stats
 	stats := m.perSlice[update.key]
-	stats.count += update.count
+	stats.count += 1
 	stats.size += update.size
 	if update.lastReceivedAt.After(stats.lastReceivedAt) {
 		stats.lastReceivedAt = update.lastReceivedAt
@@ -82,12 +90,19 @@ func (m *Manager) handleUpdate(update update) {
 	stats.changed = true
 }
 
-func (m *Manager) syncStats(ctx context.Context) {
+func (m *Manager) handleSync(ctx context.Context) {
 	stats := make([]model.SliceStats, 0, len(m.perSlice))
 	for k, v := range m.perSlice {
 		if v.changed {
 			stats = append(stats, model.NewSliceStats(k, v.count, v.size, v.lastReceivedAt))
 		}
 	}
-	_ = m.store.UpdateSliceStats(ctx, stats)
+
+	m.syncFn(ctx, stats)
+}
+
+func syncToStore(store *store.Store) syncFn {
+	return func(ctx context.Context, stats []model.SliceStats) {
+		_ = store.UpdateSliceStats(ctx, stats)
+	}
 }
