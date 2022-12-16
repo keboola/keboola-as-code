1e1052a852fe356b58b89c6f3e92e72c2eee5e91
Michal Jurečko <michal.jurecko@gmail.com>
2022-12-16 14:47:41 +0100

    Define separated types for IDs, unify date format in the JSON

--- internal/pkg/service/buffer/store/key/key.go
+++ internal/pkg/service/buffer/store/key/key.go
@@ -1,57 +1,70 @@
+// Package key defines the hierarchy of objects in the Buffer API.
+// Each key is also part of the object value in the database.
 package key
 
 import (
+	jsonlib "encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 )
 
+// TimeFormat is a time format with fixed length, so it can be used for lexicographic sorting in etcd.
+const TimeFormat = "2006-01-02T15:04:05.000Z"
+
+// UTCTime serializes to the JSON as the UTC time in the TimeFormat.
+type UTCTime time.Time
+
+type (
+	ProjectID  int
+	ReceiverID string
+	ExportID   string
+	RevisionID int
+	FileID     UTCTime
+	SliceID    UTCTime
+	ReceivedAt UTCTime
+)
+
 type ReceiverKey struct {
-	ProjectID  int    `json:"projectId" validate:"required,min=1"`
-	ReceiverID string `json:"receiverId" validate:"required,min=1,max=48"`
+	ProjectID  ProjectID  `json:"projectId" validate:"required,min=1"`
+	ReceiverID ReceiverID `json:"receiverId" validate:"required,min=1,max=48"`
 }
 
 type ExportKey struct {
 	ReceiverKey
-	ExportID string `json:"exportId" validate:"required,min=1,max=48"`
+	ExportID ExportID `json:"exportId" validate:"required,min=1,max=48"`
 }
 
 type MappingKey struct {
 	ExportKey
-	RevisionID int `json:"revisionId" validate:"min=1,max=100"`
+	RevisionID RevisionID `json:"revisionId" validate:"min=1,max=100"`
 }
 
 type FileKey struct {
 	ExportKey
-	FileID time.Time `json:"fileId" validate:"required"`
+	FileID FileID `json:"fileId" validate:"required"`
 }
 
 type SliceKey struct {
 	FileKey
-	SliceID time.Time `json:"sliceId" validate:"required"`
+	SliceID SliceID `json:"sliceId" validate:"required"`
 }
 
 type RecordKey struct {
-	ExportKey
-	SliceID      time.Time `json:"sliceId" validate:"required"`
-	ReceivedAt   time.Time
+	SliceKey
+	ReceivedAt   ReceivedAt
 	RandomSuffix string
 }
 
 func FormatTime(t time.Time) string {
-	return t.UTC().Format("2006-01-02T15:04:05.000Z")
+	return t.UTC().Format(TimeFormat)
 }
 
-func NewRecordKey(projectID int, receiverID string, exportID string, sliceID time.Time, now time.Time) RecordKey {
-	k := RecordKey{}
-	k.ProjectID = projectID
-	k.ReceiverID = receiverID
-	k.ExportID = exportID
-	k.SliceID = sliceID
-	k.ReceivedAt = now
-	return k
+func NewRecordKey(sliceKey SliceKey, now time.Time) RecordKey {
+	return RecordKey{SliceKey: sliceKey, ReceivedAt: ReceivedAt(now)}
 }
 
-func (v ReceiverKey) String() string {
+func (v *ReceiverKey) String() string {
 	return fmt.Sprintf("project:%d/receiver:%s", v.ProjectID, v.ReceiverID)
 }
 
@@ -63,18 +76,115 @@ func (v MappingKey) String() string {
 	return fmt.Sprintf("%s/mapping:%08d", v.ExportKey.String(), v.RevisionID)
 }
 
+func (v ProjectID) String() string {
+	return strconv.Itoa(int(v))
+}
+
+func (v ReceiverID) String() string {
+	return string(v)
+}
+
+func (v ExportID) String() string {
+	return string(v)
+}
+
+func (v RevisionID) String() string {
+	return strconv.Itoa(int(v))
+}
+
 func (v FileKey) String() string {
-	return fmt.Sprintf("%s/file:%s", v.ExportKey.String(), FormatTime(v.FileID))
+	return fmt.Sprintf("%s/file:%s", v.ExportKey.String(), v.FileID.String())
 }
 
 func (v SliceKey) String() string {
-	return fmt.Sprintf("%s/slice:%s", v.FileKey.String(), FormatTime(v.SliceID))
+	return fmt.Sprintf("%s/slice:%s", v.FileKey.String(), v.SliceID.String())
 }
 
 func (v RecordKey) Key() string {
-	return FormatTime(v.ReceivedAt) + "_" + v.RandomSuffix
+	return v.ReceivedAt.String() + "_" + v.RandomSuffix
 }
 
 func (v RecordKey) String() string {
 	return fmt.Sprintf("%s/record:%s", v.ExportKey.String(), v.Key())
+}
+
+func (v UTCTime) String() string {
+	return FormatTime(time.Time(v))
+}
+
+func (v FileID) String() string {
+	return UTCTime(v).String()
+}
+
+func (v SliceID) String() string {
+	return UTCTime(v).String()
+}
+
+func (v ReceivedAt) String() string {
+	return UTCTime(v).String()
+}
+
+func (v UTCTime) IsZero() bool {
+	return time.Time(v).IsZero()
+}
+
+func (v FileID) IsZero() bool {
+	return UTCTime(v).IsZero()
+}
+
+func (v SliceID) IsZero() bool {
+	return UTCTime(v).IsZero()
+}
+
+func (v ReceivedAt) IsZero() bool {
+	return UTCTime(v).IsZero()
+}
+
+func (v UTCTime) After(target UTCTime) bool {
+	return time.Time(v).After(time.Time(target))
+}
+
+func (v ReceivedAt) After(target ReceivedAt) bool {
+	return UTCTime(v).After(UTCTime(target))
+}
+
+func (v UTCTime) MarshalJSON() ([]byte, error) {
+	return jsonlib.Marshal(v.String())
+}
+
+func (v FileID) MarshalJSON() ([]byte, error) {
+	return UTCTime(v).MarshalJSON()
+}
+
+func (v SliceID) MarshalJSON() ([]byte, error) {
+	return UTCTime(v).MarshalJSON()
+}
+
+func (v ReceivedAt) MarshalJSON() ([]byte, error) {
+	return UTCTime(v).MarshalJSON()
+}
+
+func (v *UTCTime) UnmarshalJSON(b []byte) error {
+	var str string
+	if err := jsonlib.Unmarshal(b, &str); err != nil {
+		return err
+	}
+	out, err := time.Parse(TimeFormat, str)
+	if err != nil {
+		return err
+	}
+	*v = UTCTime(out)
+	return nil
+}
+
+func (v *FileID) UnmarshalJSON(b []byte) error {
+	return (*UTCTime)(v).UnmarshalJSON(b)
+}
+
+func (v *SliceID) UnmarshalJSON(b []byte) error {
+	return (*UTCTime)(v).UnmarshalJSON(b)
+}
+
+func (v *ReceivedAt) UnmarshalJSON(b []byte) error {
+	return (*UTCTime)(v).UnmarshalJSON(b)
 }
