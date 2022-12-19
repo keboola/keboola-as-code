6ac7ae217e14bdab28d8736b73ff662c448c3e13
Michal Jurečko <michal.jurecko@gmail.com>
2022-12-19 17:27:35 +0100

    Add utility to handle rollbacks

---
+++ internal/pkg/service/common/rollback/rollback.go
@@ -0,0 +1,137 @@
+// Package rollback provides a utility to handle rollback operations.
+// - Start with the New function.
+// - Then use Add method to add a rollback callback.
+// - Or use AddLIFO or AddParallel methods to add a sub-container.
+// - Finally call Invoke method if a rollback is needed.
+package rollback
+
+import (
+	"context"
+	"sync"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+const (
+	StrategyLIFO     = strategy("lifo")
+	StrategyParallel = strategy("parallel")
+)
+
+type Builder interface {
+	Add(cb func(ctx context.Context) error)
+	AddLIFO() *container
+	AddParallel() *container
+}
+
+// Container is main LIFO container.
+// Invoke method logs a warning.
+type Container struct {
+	logger log.Logger
+	*container
+}
+
+// container is a sub-container with a defined strategy.
+// Invoke method returns a error.
+type container struct {
+	strategy strategy
+
+	lock      *sync.Mutex
+	callbacks []callback
+}
+
+type strategy string
+
+type callback func(ctx context.Context) error
+
+// New creates top-level container with LIFO strategy.
+// Errors are logged as an warning.
+func New(logger log.Logger) *Container {
+	return &Container{
+		logger:    logger,
+		container: newContainer(StrategyLIFO),
+	}
+}
+
+func newContainer(strategy strategy) *container {
+	return &container{
+		strategy: strategy,
+		lock:     &sync.Mutex{},
+	}
+}
+
+func (v *Container) InvokeIfErr(ctx context.Context, errPtr *error) {
+	if errPtr != nil && *errPtr != nil {
+		v.Invoke(ctx)
+	}
+}
+
+func (v *Container) Invoke(ctx context.Context) {
+	if err := v.container.invokeOrErr(ctx); err != nil {
+		v.logger.Warn(errors.PrefixError(err, "rollback failed"))
+	}
+}
+
+// Add a rollback callback.
+// Individual callbacks are invoked according to the LIFO or parallel strategy.
+func (v *container) Add(cb func(ctx context.Context) error) {
+	v.lock.Lock()
+	v.callbacks = append(v.callbacks, cb)
+	v.lock.Unlock()
+}
+
+func (v *container) AddLIFO() *container {
+	container := newContainer(StrategyLIFO)
+	v.Add(container.invokeOrErr)
+	return container
+}
+
+func (v *container) AddParallel() *container {
+	container := newContainer(StrategyParallel)
+	v.Add(container.invokeOrErr)
+	return container
+}
+
+func (v *container) invokeOrErr(ctx context.Context) error {
+	switch v.strategy {
+	case StrategyLIFO:
+		return v.invokeLIFO(ctx)
+	case StrategyParallel:
+		return v.invokeParallel(ctx)
+	default:
+		panic(errors.Errorf(`unexpected strategy "%s"`, v.strategy))
+	}
+}
+
+func (v *container) invokeLIFO(ctx context.Context) error {
+	errs := errors.NewMultiError()
+
+	// Iterate callbacks in LIFO order
+	for i := len(v.callbacks) - 1; i >= 0; i-- {
+		if err := v.callbacks[i](ctx); err != nil {
+			errs.Append(err)
+		}
+	}
+
+	return errs.ErrorOrNil()
+}
+
+func (v *container) invokeParallel(ctx context.Context) error {
+	errs := errors.NewMultiError()
+
+	// Invoke in parallel
+	wg := &sync.WaitGroup{}
+	for _, cb := range v.callbacks {
+		cb := cb
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := cb(ctx); err != nil {
+				errs.Append(err)
+			}
+		}()
+	}
+
+	wg.Wait()
+	return errs.ErrorOrNil()
+}
---
+++ internal/pkg/service/common/rollback/rollback_test.go
@@ -0,0 +1,87 @@
+package rollback_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	. "github.com/keboola/keboola-as-code/internal/pkg/service/common/rollback"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+func TestRollback(t *testing.T) {
+	t.Parallel()
+
+	logger := log.NewDebugLogger()
+	main := New(logger)
+
+	main.Add(func(ctx context.Context) error {
+		logger.Debug("main 1")
+		_ = logger.Sync()
+		return nil
+	})
+	main.Add(func(ctx context.Context) error {
+		logger.Debug("main 2")
+		_ = logger.Sync()
+		return nil
+	})
+	main.Add(func(ctx context.Context) error {
+		return errors.New("main 3 failed")
+	})
+
+	sub1 := main.AddParallel()
+	sub1.Add(func(ctx context.Context) error {
+		logger.Debug("parallel operation")
+		_ = logger.Sync()
+		return nil
+	})
+	sub1.Add(func(ctx context.Context) error {
+		logger.Debug("parallel operation")
+		_ = logger.Sync()
+		return nil
+	})
+	sub1.Add(func(ctx context.Context) error {
+		return errors.New("parallel operation failed")
+	})
+
+	sub2 := main.AddLIFO()
+	sub2.Add(func(ctx context.Context) error {
+		logger.Debug("lifo 1")
+		_ = logger.Sync()
+		return nil
+	})
+	sub2.Add(func(ctx context.Context) error {
+		logger.Debug("lifo 2")
+		_ = logger.Sync()
+		return nil
+	})
+	sub2.Add(func(ctx context.Context) error {
+		return errors.New("lifo 3 failed")
+	})
+
+	main.Add(func(ctx context.Context) error {
+		logger.Debug("main 4")
+		_ = logger.Sync()
+		return nil
+	})
+
+	main.Invoke(context.Background())
+
+	expected := `
+DEBUG  main 4
+DEBUG  lifo 2
+DEBUG  lifo 1
+DEBUG  parallel operation
+DEBUG  parallel operation
+DEBUG  main 2
+DEBUG  main 1
+WARN  rollback failed:
+- lifo 3 failed
+- parallel operation failed
+- main 3 failed
+`
+	assert.Equal(t, strings.TrimLeft(expected, "\n"), logger.AllMessages())
+}
