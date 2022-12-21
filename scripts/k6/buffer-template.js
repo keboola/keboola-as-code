0cb99041c7e79d945cba918401fc925f2db8e5cd
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-12-21 17:56:24 +0100

    Fix template + add more checks for easier debugging

--- scripts/k6/buffer-template.js
+++ scripts/k6/buffer-template.js
@@ -4,11 +4,19 @@ import { post, del } from "./common.js";
 
 export const options = common.options;
 
+const RECEIVER_ID = "buffer-template";
+
 export function setup() {
   common.setup();
 
-  const res = post("v1/receivers", {
-    id: "buffer-template",
+  let res = del(`v1/receivers/${RECEIVER_ID}`);
+  if (res.status !== 200 && res.status !== 404) {
+    console.error(res);
+    throw new Error("failed to delete receiver");
+  }
+
+  res = post("v1/receivers", {
+    id: RECEIVER_ID,
     name: "Buffer API Template Benchmark",
     exports: [
       {
@@ -24,7 +32,7 @@ export function setup() {
               template: {
                 language: "jsonnet",
                 undefinedValueStrategy: "null",
-                content: `Body.a.b+":"+Body.c.f.g`,
+                content: `Body().a+":"+Body().c.f.g`,
               },
             },
           ],
@@ -32,13 +40,12 @@ export function setup() {
       },
     ],
   });
-
   if (res.status !== 200) {
     console.error(res);
     throw new Error("failed to create receiver");
   }
 
-  const { id: receiverId, url } = res.json();
+  const { url } = res.json();
   const endpoint = url.slice(url.indexOf("v1"));
 
   const data = { a: "b", c: { d: "e", f: { g: "h" } } };
@@ -46,7 +53,13 @@ export function setup() {
     "My-Custom-Header": "custom header value abcd",
   };
 
-  return { receiverId, endpoint, data, headers };
+  res = post(endpoint, data, headers);
+  if (res.status !== 200) {
+    console.error(res);
+    throw new Error("failed to import data");
+  }
+
+  return { endpoint, data, headers };
 }
 
 export default function (input) {
@@ -54,7 +67,11 @@ export default function (input) {
   sleep(1);
 }
 
-export function teardown(input) {
-  del(`v1/receivers/${input.receiverId}`);
+export function teardown() {
+  const res = del(`v1/receivers/${RECEIVER_ID}`);
+  if (res.status !== 200 && res.status !== 404) {
+    console.error(res);
+    throw new Error("failed to delete receiver");
+  }
 }
 
