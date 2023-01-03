e092bd8d0ee4161467d45c351c0962a05c0338d1
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-03 17:55:17 +0100

    Improve stats model

--- internal/pkg/service/buffer/store/model/stats.go
+++ internal/pkg/service/buffer/store/model/stats.go
@@ -4,23 +4,37 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
 )
 
+type StatsProvider interface {
+	GetStats() Stats
+}
+
+type Stats struct {
+	Count  uint64      `json:"count" validate:"required"`
+	Size   uint64      `json:"size" validate:"required"`
+	LastAt key.UTCTime `json:"lastAt" validate:"required"`
+}
+
 type SliceStats struct {
 	key.SliceKey
-	Count          uint64         `json:"count" validate:"required"`
-	Size           uint64         `json:"size" validate:"required"`
-	LastReceivedAt key.ReceivedAt `json:"lastReceivedAt" validate:"required"`
+	Stats
 }
 
 func NewSliceStats(
-	key key.SliceKey,
+	sliceKey key.SliceKey,
 	count uint64,
 	size uint64,
 	lastReceivedAt key.ReceivedAt,
 ) SliceStats {
 	return SliceStats{
-		SliceKey:       key,
-		Count:          count,
-		Size:           size,
-		LastReceivedAt: lastReceivedAt,
+		SliceKey: sliceKey,
+		Stats: Stats{
+			Count:  count,
+			Size:   size,
+			LastAt: key.UTCTime(lastReceivedAt),
+		},
 	}
+}
+
+func (s Stats) GetStats() Stats {
+	return s
 }
