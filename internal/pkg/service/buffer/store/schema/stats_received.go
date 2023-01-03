bfad13c872c7df9d5ea5165a951dafb4565a1c1a
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-03 17:55:33 +0100

    Improve schema

--- internal/pkg/service/buffer/store/schema/stats_received.go
+++ internal/pkg/service/buffer/store/schema/stats_received.go
@@ -9,57 +9,81 @@ import (
 
 type sliceStats = PrefixT[model.SliceStats]
 
-type SliceStats struct {
+type ReceivedStats struct {
 	sliceStats
 }
 
-type SliceReceiverStats struct {
+type ReceivedStatsByReceiver struct {
 	sliceStats
 }
 
-type SliceNodeStats struct {
+type ReceivedStatsByExport struct {
 	sliceStats
 }
 
-func (v *Schema) SliceStats() SliceStats {
-	return SliceStats{
+type ReceivedStatsByFile struct {
+	sliceStats
+}
+
+type ReceivedStatsBySlice struct {
+	sliceStats
+}
+
+type ReceivedStatsBySliceAndNode struct {
+	sliceStats
+}
+
+func (v *Schema) ReceivedStats() ReceivedStats {
+	return ReceivedStats{
 		sliceStats: NewTypedPrefix[model.SliceStats]("stats/received", v.serde),
 	}
 }
 
-func (v SliceStats) InReceiver(k storeKey.ReceiverKey) SliceReceiverStats {
+func (v ReceivedStats) InReceiver(k storeKey.ReceiverKey) ReceivedStatsByReceiver {
 	if k.ProjectID == 0 {
 		panic(errors.New("stats projectID cannot be empty"))
 	}
 	if k.ReceiverID == "" {
 		panic(errors.New("stats receiverID cannot be empty"))
 	}
-	return SliceReceiverStats{
+	return ReceivedStatsByReceiver{
 		sliceStats: v.sliceStats.
 			Add(k.ProjectID.String()).
 			Add(k.ReceiverID.String()),
 	}
 }
 
-func (v SliceStats) InSlice(k storeKey.SliceKey) SliceNodeStats {
+func (v ReceivedStats) InExport(k storeKey.ExportKey) ReceivedStatsByExport {
+	if k.ExportID == "" {
+		panic(errors.New("stats receiverID cannot be empty"))
+	}
+	return ReceivedStatsByExport{
+		sliceStats: v.InReceiver(k.ReceiverKey).sliceStats.Add(k.ExportID.String()),
+	}
+}
+
+func (v ReceivedStats) InFile(k storeKey.FileKey) ReceivedStatsByFile {
 	if k.ExportID == "" {
 		panic(errors.New("stats exportID cannot be empty"))
 	}
 	if k.FileID.IsZero() {
 		panic(errors.New("stats fileID cannot be empty"))
 	}
-	if k.SliceID.IsZero() {
-		panic(errors.New("stats sliceID cannot be empty"))
-	}
-	return SliceNodeStats{
-		sliceStats: v.InReceiver(k.ReceiverKey).sliceStats.
-			Add(k.ExportID.String()).
-			Add(k.FileID.String()).
-			Add(k.SliceID.String()),
+	return ReceivedStatsByFile{
+		sliceStats: v.InExport(k.ExportKey).sliceStats.Add(k.FileID.String()),
 	}
 }
 
-func (v SliceNodeStats) ByNodeID(nodeID string) KeyT[model.SliceStats] {
+func (v ReceivedStats) InSlice(k storeKey.SliceKey) ReceivedStatsBySlice {
+	if k.SliceID.IsZero() {
+		panic(errors.New("stats sliceID cannot be empty"))
+	}
+	return ReceivedStatsBySlice{
+		sliceStats: v.InFile(k.FileKey).sliceStats.Add(k.SliceID.String()),
+	}
+}
+
+func (v ReceivedStatsBySlice) ByNodeID(nodeID string) KeyT[model.SliceStats] {
 	if nodeID == "" {
 		panic(errors.New("stats nodeID cannot be empty"))
 	}
