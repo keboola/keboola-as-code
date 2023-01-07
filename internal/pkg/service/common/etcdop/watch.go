e6179218a33b5da8eef360db4dc860733bbeab86
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-07 13:01:07 +0100

    etcdop Watch: stream events in batches, same as in the low-level client

--- internal/pkg/service/common/etcdop/watch.go
+++ internal/pkg/service/common/etcdop/watch.go
@@ -2,6 +2,7 @@ package etcdop
 
 import (
 	"context"
+	"sort"
 	"strconv"
 	"time"
 
@@ -21,11 +22,20 @@ const (
 	DeleteEvent
 )
 
+const (
+	// getAllBatchSize defines batch size for "getAll" phase of the GetAllAndWatch operation.
+	getAllBatchSize = 100
+)
+
 type Event struct {
 	Kv     *op.KeyValue
 	PrevKv *op.KeyValue
 	Type   EventType
+}
+
+type Events struct {
 	Header *etcdserverpb.ResponseHeader
+	Events []Event
 }
 
 type EventT[T any] struct {
@@ -33,7 +43,11 @@ type EventT[T any] struct {
 	Kv     *op.KeyValue
 	PrevKv *op.KeyValue
 	Type   EventType
+}
+
+type EventsT[T any] struct {
 	Header *etcdserverpb.ResponseHeader
+	Events []EventT[T]
 }
 
 func (v EventType) String() string {
@@ -49,150 +63,174 @@ func (v EventType) String() string {
 	}
 }
 
-func (e *EventT[T]) Rev() int64 {
+func (e *Events) Rev() int64 {
 	return e.Header.Revision
 }
 
-func (v Prefix) Watch(ctx context.Context, client etcd.Watcher, handleErr func(error), opts ...etcd.OpOption) <-chan Event {
-	ch := make(chan Event)
-	v.doWatch(ctx, client, handleErr, ch, opts...)
-	return ch
+func (e *EventsT[T]) Rev() int64 {
+	return e.Header.Revision
+}
+
+func (v Prefix) Watch(ctx context.Context, client etcd.Watcher, handleErr func(error), opts ...etcd.OpOption) <-chan Events {
+	outCh := make(chan Events)
+	go func() {
+		defer close(outCh)
+		v.doWatch(ctx, client, handleErr, outCh, opts...)
+	}()
+	return outCh
 }
 
 // GetAllAndWatch loads all keys in the prefix by the iterator and then watch for changes.
 // initDone channel signals end of the load phase and start of the watch phase.
-func (v Prefix) GetAllAndWatch(ctx context.Context, client *etcd.Client, handleErr func(err error), opts ...etcd.OpOption) (out <-chan Event, initDone <-chan error) {
-	outCh := make(chan Event)
+func (v Prefix) GetAllAndWatch(ctx context.Context, client *etcd.Client, handleErr func(err error), opts ...etcd.OpOption) (out <-chan Events, initDone <-chan error) {
+	outCh := make(chan Events)
 	initDoneCh := make(chan error)
 
 	go func() {
-		// GetAll
+		defer close(outCh)
+
+		// Get all iterator
 		itr := v.GetAll().Do(ctx, client)
-		err := itr.ForEach(func(kv *op.KeyValue, header *etcdserverpb.ResponseHeader) error {
-			outCh <- Event{
-				Kv:     kv,
-				Type:   CreateEvent,
-				Header: header,
+		var events []Event
+		sendBatch := func() {
+			if len(events) > 0 {
+				outCh <- Events{Header: itr.Header(), Events: events}
+			}
+			events = nil
+		}
+
+		// Iterate and send batches of events
+		i := 1
+		err := itr.ForEach(func(kv *op.KeyValue, _ *etcdserverpb.ResponseHeader) error {
+			events = append(events, Event{Kv: kv, Type: CreateEvent})
+			if i%getAllBatchSize == 0 {
+				sendBatch()
 			}
 			return nil
 		})
+		sendBatch()
+
+		// Check iterator error
 		if err != nil {
-			// GetAll error is fatal
-			handleErr(err)
 			initDoneCh <- err
-			close(outCh)
 			close(initDoneCh)
 			return
 		}
 
 		// Continue with Watch where GetAll ended
 		close(initDoneCh)
-		opts = append(opts, etcd.WithRev(itr.Header().Revision+1))
-		v.doWatch(ctx, client, handleErr, outCh, opts...)
+		v.doWatch(ctx, client, handleErr, outCh, append(opts, etcd.WithRev(itr.Header().Revision+1))...)
 	}()
 
 	return outCh, initDoneCh
 }
 
-func (v Prefix) doWatch(ctx context.Context, client etcd.Watcher, handleErr func(err error), outCh chan Event, opts ...etcd.OpOption) {
+func (v Prefix) doWatch(ctx context.Context, client etcd.Watcher, handleErr func(err error), outCh chan Events, opts ...etcd.OpOption) {
 	opts = append([]etcd.OpOption{etcd.WithPrefix()}, opts...)
-	go func() {
-		defer close(outCh)
 
-		// In case of an error, the watch channel can be closed.
-		// It will be recreated, and it will continue from the last revision.
-		revision := int64(0)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				// If the watch is recreated, continue from the last received revision
-				watchOpts := opts
-				if revision > 0 {
-					watchOpts = append(watchOpts, etcd.WithRev(revision+1))
-				}
-
-				// Wait before recreate attempt
-				if revision > 0 {
-					select {
-					case <-ctx.Done():
-						return
-					case <-time.After(time.Second):
-						// continue
-					}
-				}
-
-				// Process watch events until the rawCh is closed
-				rawCh := client.Watch(ctx, v.Prefix(), watchOpts...)
-				processWatchEvents(ctx, &revision, handleErr, rawCh, outCh)
+	// In case of an error, the watch channel can be closed.
+	// It will be recreated, and it will continue from the last revision.
+	revision := int64(0)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// If the watch is recreated, continue from the last received revision
+			watchOpts := opts
+			if revision > 0 {
+				watchOpts = append(watchOpts, etcd.WithRev(revision+1))
 			}
+
+			// Wait before recreate attempt
+			if revision > 0 {
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(time.Second):
+					// continue
+				}
+			}
+
+			// Process watch events until the rawCh is closed
+			rawCh := client.Watch(ctx, v.Prefix(), watchOpts...)
+			processWatchEvents(ctx, &revision, handleErr, rawCh, outCh)
 		}
-	}()
+	}
 }
 
-func (v PrefixT[T]) Watch(ctx context.Context, client etcd.Watcher, handleErr func(error), opts ...etcd.OpOption) <-chan EventT[T] {
-	ch := make(chan EventT[T])
-	v.doWatch(ctx, client, handleErr, ch, opts...)
-	return ch
+func (v PrefixT[T]) Watch(ctx context.Context, client etcd.Watcher, handleErr func(error), opts ...etcd.OpOption) <-chan EventsT[T] {
+	outCh := make(chan EventsT[T])
+	go func() {
+		defer close(outCh)
+		v.doWatch(ctx, client, handleErr, outCh, opts...)
+	}()
+	return outCh
 }
 
 // GetAllAndWatch loads all keys in the prefix by the iterator and then watch for changes.
 // Values are decoded to the type T.
 // initDone channel signals end of the load phase and start of the watch phase.
-func (v PrefixT[T]) GetAllAndWatch(ctx context.Context, client *etcd.Client, handleErr func(err error), opts ...etcd.OpOption) (out <-chan EventT[T], initDone <-chan error) {
-	outCh := make(chan EventT[T])
+func (v PrefixT[T]) GetAllAndWatch(ctx context.Context, client *etcd.Client, handleErr func(err error), opts ...etcd.OpOption) (out <-chan EventsT[T], initDone <-chan error) {
+	outCh := make(chan EventsT[T])
 	initDoneCh := make(chan error)
 
 	go func() {
-		// GetAll
+		defer close(outCh)
+
+		// Get all iterator
 		itr := v.GetAll().Do(ctx, client)
-		err := itr.ForEachKV(func(kv op.KeyValueT[T], header *etcdserverpb.ResponseHeader) error {
-			outCh <- EventT[T]{
-				Kv:     kv.Kv,
-				Value:  kv.Value,
-				Type:   CreateEvent,
-				Header: header,
+		var events []EventT[T]
+		sendBatch := func() {
+			if len(events) > 0 {
+				outCh <- EventsT[T]{Header: itr.Header(), Events: events}
+			}
+			events = nil
+		}
+
+		// Iterate and send batches of events
+		i := 1
+		err := itr.ForEachKV(func(kv op.KeyValueT[T], _ *etcdserverpb.ResponseHeader) error {
+			events = append(events, EventT[T]{Kv: kv.Kv, Value: kv.Value, Type: CreateEvent})
+			if i%getAllBatchSize == 0 {
+				sendBatch()
 			}
 			return nil
 		})
+		sendBatch()
+
+		// Check iterator error
 		if err != nil {
-			// GetAll error is fatal
-			handleErr(err)
 			initDoneCh <- err
-			close(outCh)
 			close(initDoneCh)
 			return
 		}
 
 		// Continue with Watch where GetAll finished
 		close(initDoneCh)
-		opts = append(opts, etcd.WithRev(itr.Header().Revision+1))
-		v.doWatch(ctx, client, handleErr, outCh, opts...)
+		v.doWatch(ctx, client, handleErr, outCh, append(opts, etcd.WithRev(itr.Header().Revision+1))...)
 	}()
 
 	return outCh, initDoneCh
 }
 
-func (v PrefixT[T]) doWatch(ctx context.Context, client etcd.Watcher, handleErr func(err error), outCh chan EventT[T], opts ...etcd.OpOption) {
+func (v PrefixT[T]) doWatch(ctx context.Context, client etcd.Watcher, handleErr func(err error), outCh chan EventsT[T], opts ...etcd.OpOption) {
 	rawCh := v.prefix.Watch(ctx, client, handleErr, opts...)
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case rawEvents, ok := <-rawCh:
+			if !ok {
 				return
-			case rawEvent, ok := <-rawCh:
-				if !ok {
-					// Close output channel, if raw channel is closed
-					close(outCh)
-					return
-				}
+			}
 
+			outEvents := make([]EventT[T], len(rawEvents.Events))
+			for i, rawEvent := range rawEvents.Events {
 				outEvent := EventT[T]{
 					Kv:     rawEvent.Kv,
 					PrevKv: rawEvent.PrevKv,
 					Type:   rawEvent.Type,
-					Header: rawEvent.Header,
 				}
 
 				if rawEvent.Type == CreateEvent || rawEvent.Type == UpdateEvent {
@@ -214,13 +252,18 @@ func (v PrefixT[T]) doWatch(ctx context.Context, client etcd.Watcher, handleErr 
 					outEvent.Value = *target
 				}
 
-				outCh <- outEvent
+				outEvents[i] = outEvent
+			}
+
+			outCh <- EventsT[T]{
+				Header: rawEvents.Header,
+				Events: outEvents,
 			}
 		}
-	}()
+	}
 }
 
-func processWatchEvents(ctx context.Context, revision *int64, handleErr func(err error), rawCh etcd.WatchChan, outCh chan<- Event) {
+func processWatchEvents(ctx context.Context, revision *int64, handleErr func(err error), rawCh etcd.WatchChan, outCh chan<- Events) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -237,8 +280,19 @@ func processWatchEvents(ctx context.Context, revision *int64, handleErr func(err
 				continue
 			}
 
-			for _, rawEvent := range resp.Events {
-				outEvent := Event{Kv: rawEvent.Kv, PrevKv: rawEvent.PrevKv, Header: &resp.Header}
+			// Sort events from the batch (if multiple keys have been modified in one txn, in one revision)
+			// 1. By type, PUT before DELETE
+			// 2. By key, A->Z
+			sort.SliceStable(resp.Events, func(i, j int) bool {
+				if resp.Events[i].Type != resp.Events[j].Type {
+					return resp.Events[i].Type < resp.Events[j].Type
+				}
+				return string(resp.Events[i].Kv.Key) < string(resp.Events[j].Kv.Key)
+			})
+
+			outEvents := make([]Event, len(resp.Events))
+			for i, rawEvent := range resp.Events {
+				outEvent := Event{Kv: rawEvent.Kv, PrevKv: rawEvent.PrevKv}
 
 				// Map event type
 				switch rawEvent.Type {
@@ -254,7 +308,12 @@ func processWatchEvents(ctx context.Context, revision *int64, handleErr func(err
 					panic(errors.Errorf(`unexpected event type "%s"`, rawEvent.Type.String()))
 				}
 
-				outCh <- outEvent
+				outEvents[i] = outEvent
+			}
+
+			outCh <- Events{
+				Header: &resp.Header,
+				Events: outEvents,
 			}
 		}
 	}
