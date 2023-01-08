bbcffbf790ba39ddbc52f0f5b453bf0bcbf58006
Jakub Matějka <jakub@keboola.com>
2023-01-08 19:25:50 +0100

    Add expected-state for tables in buffer e2e tests

--- internal/pkg/utils/testproject/snapshot.go
+++ internal/pkg/utils/testproject/snapshot.go
@@ -188,7 +188,7 @@ func (p *Project) NewSnapshot() (*fixtures.ProjectSnapshot, error) {
 
 	// Join buckets with tables
 	for _, t := range tables {
-		b := bucketsMap[t.Bucket.ID]
+		b := bucketsMap[t.ID.BucketID]
 		b.Tables = append(b.Tables, &fixtures.Table{
 			ID:          t.ID,
 			URI:         t.Uri,
