b26ebce60785e8099326b6d13bb4f341bad49cb7
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-12 21:35:46 +0100

    Update watcher Worker node

--- internal/pkg/service/buffer/watcher/workernode/workernode.go
+++ internal/pkg/service/buffer/watcher/workernode/workernode.go
@@ -76,45 +76,41 @@ func (n *Node) WaitForRevision(requiredRev int64) <-chan struct{} {
 // watch for changes in revisions of API nodes.
 func (n *Node) watch(ctx context.Context, wg *sync.WaitGroup) error {
 	pfx := n.schema.Runtime().APINodes().Watchers().SlicesRevision()
-	ch := pfx.GetAllAndWatch(ctx, n.client, n.onError, etcd.WithCreatedNotify())
+	ch := pfx.GetAllAndWatch(ctx, n.client, etcd.WithCreatedNotify())
 	initDone := make(chan error)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 
+		// Reset the nodes on the restart event.
+		reset := false
+
 		// Channel is closed on shutdown, so the context does not have to be checked
-		for events := range ch {
-			if err := events.InitErr; err != nil {
-				initDone <- err
+		for resp := range ch {
+			switch {
+			case resp.InitErr != nil:
+				// Initialization error, stop worker via initDone channel
+				initDone <- resp.InitErr
 				close(initDone)
-			} else if events.Created {
+			case resp.Err != nil:
+				// An error occurred, it is logged.
+				// If it is a fatal error, then it is followed
+				// by the "Restarted" event handled bellow,
+				// and the operation starts from the beginning.
+				n.logger.Error(resp.Err)
+			case resp.Restarted:
+				// A fatal error (etcd ErrCompacted) occurred.
+				// It is not possible to continue watching, the operation must be restarted.
+				reset = true
+				n.logger.Warn(resp.RestartReason)
+			case resp.Created:
+				// The watcher has been successfully created.
+				// This means transition from GetAll to Watch phase.
 				close(initDone)
-			}
-
-			for _, event := range events.Events {
-				switch event.Type {
-				case etcdop.CreateEvent, etcdop.UpdateEvent:
-					// Cached state of th API node has been updated
-					n.revisionPerAPINode[string(event.Kv.Key)] = cast.ToInt64(string(event.Kv.Value))
-				case etcdop.DeleteEvent:
-					// The API node gone
-					delete(n.revisionPerAPINode, string(event.Kv.Key))
-				default:
-					panic(errors.Errorf(`unexpected event type "%s"`, event.Type.String()))
-				}
-			}
-
-			// Recompute and store minimal revision
-			if rev := minimalRevision(n.revisionPerAPINode); rev != noAPINode {
-				if old := n.minRevision.Swap(rev); old != rev {
-					// Trigger listeners if the minimal version has changed
-					if count := n.listeners.onChange(ctx, n.minRevision); count > 0 {
-						n.logger.Infof(`revision updated to "%v", unblocked "%d" listeners`, rev, count)
-					} else {
-						n.logger.Infof(`revision updated to "%v"`, rev)
-					}
-				}
+			default:
+				n.updateRevisionsFrom(ctx, resp, reset)
+				reset = false
 			}
 		}
 	}()
@@ -123,8 +119,35 @@ func (n *Node) watch(ctx context.Context, wg *sync.WaitGroup) error {
 	return <-initDone
 }
 
-func (n *Node) onError(err error) {
-	n.logger.Error(err)
+func (n *Node) updateRevisionsFrom(ctx context.Context, resp etcdop.WatchResponse, reset bool) {
+	if reset {
+		n.revisionPerAPINode = make(map[string]int64)
+	}
+
+	for _, event := range resp.Events {
+		switch event.Type {
+		case etcdop.CreateEvent, etcdop.UpdateEvent:
+			// Cached state of th API node has been updated
+			n.revisionPerAPINode[string(event.Kv.Key)] = cast.ToInt64(string(event.Kv.Value))
+		case etcdop.DeleteEvent:
+			// The API node gone
+			delete(n.revisionPerAPINode, string(event.Kv.Key))
+		default:
+			panic(errors.Errorf(`unexpected event type "%s"`, event.Type.String()))
+		}
+	}
+
+	// Recompute and store minimal revision
+	if rev := minimalRevision(n.revisionPerAPINode); rev != noAPINode {
+		if old := n.minRevision.Swap(rev); old != rev {
+			// Trigger listeners if the minimal version has changed
+			if count := n.listeners.onChange(ctx, n.minRevision); count > 0 {
+				n.logger.Infof(`revision updated to "%v", unblocked "%d" listeners`, rev, count)
+			} else {
+				n.logger.Infof(`revision updated to "%v"`, rev)
+			}
+		}
+	}
 }
 
 func minimalRevision(revisionPerAPINode map[string]int64) (min int64) {
