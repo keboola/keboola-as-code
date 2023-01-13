3bdb87cacea677e557e71e9b96a3495e58ef63d3
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-13 12:36:32 +0100

    Add docs

--- internal/pkg/service/common/etcdop/etcdop.go
+++ internal/pkg/service/common/etcdop/etcdop.go
@@ -12,10 +12,23 @@
 // New<Operation>(<operation factory>, <response processor>) function.
 //
 // On Operation.Do(ctx, etcdClient) call :
-//  - The <operation factory> is executed, result is an etcd operation.
-//  - The etcd operation is executed, result is an etcd response.
-//  - The <response processor> is executed, to process the etcd response.
+//   - The <operation factory> is executed, result is an etcd operation.
+//   - The etcd operation is executed, result is an etcd response.
+//   - The <response processor> is executed, to process the etcd response.
 //
 // If an error occurs, it will be returned and the operation will stop.
-
+//
+// # Watch Operations
+//
+// There are following watch operations:
+// - Prefix.Watch - watch for updates.
+// - Prefix.GetAllAndWatch - get all KVs and then watch for updates, restart on a fatal error.
+// - Prefix[T].Watch - watch for updates, values are deserialized to the type T.
+// - Prefix[T].GetAllAndWatch - get all KVs and then watch for updates, restart on a fatal error, values are deserialized to the type T.
+//
+// Watch layers:
+// - Prefix.Watch wraps low-level client.Watch
+// - Prefix.GetAllAndWatch calls wrapWatchWithRestart, Prefix.GetAll and Prefix.Watch
+// - Prefix[T].Watch calls Prefix.Watch and Prefix[T].decodeChannel
+// - Prefix[T].GetAllAndWatch calls Prefix.GetAllAndWatch and Prefix[T].decodeChannel
 package etcdop
