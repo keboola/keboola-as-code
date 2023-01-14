b3aff51812fdd342551fbbf2d09e964892e100ed
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-14 09:32:41 +0100

    etcdhelper: log txn

--- internal/pkg/utils/etcdhelper/logger.go
+++ internal/pkg/utils/etcdhelper/logger.go
@@ -1,9 +1,11 @@
 package etcdhelper
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io"
+	"strings"
 	"sync/atomic"
 	"time"
 
@@ -25,10 +27,8 @@ func KVLogWrapper(kv KV, out io.Writer) KV {
 	return &kvWrapper{KV: kv, out: out, id: 1}
 }
 
-func (v *kvWrapper) log(requestID uint64, format string, a ...any) {
-	format = "ETCD_REQUEST[%04d] " + format + "\n"
-	a = append([]any{requestID}, a...)
-	_, _ = fmt.Fprintf(v.out, format, a...)
+func (v *kvWrapper) log(requestID uint64, msg string) {
+	_, _ = fmt.Fprintf(v.out, "ETCD_REQUEST[%04d] %s\n", requestID, msg)
 }
 
 func (v *kvWrapper) nextRequestID() uint64 {
@@ -38,72 +38,127 @@ func (v *kvWrapper) nextRequestID() uint64 {
 func (v *kvWrapper) Put(ctx context.Context, key, val string, opts ...OpOption) (*PutResponse, error) {
 	requestID := v.nextRequestID()
 	startTime := time.Now()
-	v.logStart(requestID, "PUT", key)
+	v.log(requestID, v.start(nil, "PUT", quote(key), val))
 	r, err := v.KV.Put(ctx, key, val, opts...)
-	v.logEnd(requestID, "PUT", key, val, startTime, r.OpResponse(), err)
+	v.log(requestID, v.end("PUT", quote(key), startTime, r.OpResponse(), err))
 	return r, err
 }
 
 func (v *kvWrapper) Get(ctx context.Context, key string, opts ...OpOption) (*GetResponse, error) {
 	requestID := v.nextRequestID()
 	startTime := time.Now()
-	v.logStart(requestID, "GET", key)
+	v.log(requestID, v.start(nil, "GET", quote(key), ""))
 	r, err := v.KV.Get(ctx, key, opts...)
-	v.logEnd(requestID, "GET", key, "", startTime, r.OpResponse(), err)
+	v.log(requestID, v.end("GET", quote(key), startTime, r.OpResponse(), err))
 	return r, err
 }
 
 func (v *kvWrapper) Delete(ctx context.Context, key string, opts ...OpOption) (*DeleteResponse, error) {
 	requestID := v.nextRequestID()
 	startTime := time.Now()
-	v.logStart(requestID, "DEL", key)
+	v.log(requestID, v.start(nil, "DEL", quote(key), ""))
 	r, err := v.KV.Delete(ctx, key, opts...)
-	v.logEnd(requestID, "DEL", key, "", startTime, r.OpResponse(), err)
+	v.log(requestID, v.end("DEL", quote(key), startTime, r.OpResponse(), err))
 	return r, err
 }
 
 func (v *kvWrapper) Do(ctx context.Context, op Op) (OpResponse, error) {
-	key := string(op.KeyBytes())
-	var opName string
-	var val string
-	switch {
-	case op.IsGet():
-		opName = "GET"
-	case op.IsPut():
-		opName = "PUT"
-		val = string(op.ValueBytes())
-	case op.IsDelete():
-		opName = "DEL"
-	case op.IsTxn():
-		opName = "TXN"
-	}
-
 	requestID := v.nextRequestID()
 	startTime := time.Now()
-	v.logStart(requestID, opName, key)
-
+	v.log(requestID, v.startOp(op))
 	r, err := v.KV.Do(ctx, op)
-	v.logEnd(requestID, opName, key, val, startTime, r, err)
+	v.log(requestID, v.endOp(op, startTime, r, err))
 	return r, err
 }
 
-func (v *kvWrapper) logStart(requestID uint64, op, key string) {
-	v.log(requestID, `%s "%s" | start`, op, key)
+func (v *kvWrapper) startOp(op Op) string {
+	var value string
+	if op.IsPut() {
+		value = string(op.ValueBytes())
+	}
+	return v.start(&op, opToStr(op), keyToStr(op.KeyBytes(), op.RangeBytes()), value)
 }
 
-func (v *kvWrapper) logEnd(requestID uint64, op, key, val string, startTime time.Time, r OpResponse, err error) {
-	if err != nil {
-		v.log(requestID, `%s "%s" | error | %s | %s`, op, key, err, time.Since(startTime))
-	} else if get := r.Get(); get != nil {
-		v.log(requestID, `%s "%s" | done | rev: %v | count: %d | %s`, op, key, get.Header.Revision, get.Count, time.Since(startTime))
-	} else if put := r.Put(); put != nil {
-		v.log(requestID, "%s \"%s\" | done | rev: %v | %s | value:\n%s", op, key, put.Header.Revision, time.Since(startTime), val)
-	} else if del := r.Del(); del != nil {
-		v.log(requestID, `%s "%s" | done | rev: %v | deleted: %d| %s`, op, key, del.Header.Revision, del.Deleted, time.Since(startTime))
-	} else if txn := r.Txn(); txn != nil {
-		v.log(requestID, `%s "%s" | done | rev: %v | %s`, op, key, txn.Header.Revision, time.Since(startTime))
+func (v *kvWrapper) start(op *Op, opName, key, value string) string {
+	if key != "" {
+		var out strings.Builder
+		out.WriteString(fmt.Sprintf(`%s %s`, opName, key))
+		if value != "" {
+			out.WriteString(" | value:")
+			out.WriteString("\n")
+			out.WriteString(">>> ")
+			out.WriteString(value)
+		}
+		return out.String()
+	}
+
+	// Dump transaction
+	var dumpStr string
+	if op != nil && op.IsTxn() {
+		var dump strings.Builder
+		cmpOps, thenOps, elseOps := op.Txn()
+
+		if len(cmpOps) > 0 {
+			dump.WriteString("  >>> IF:\n")
+			for i, item := range cmpOps {
+				expectedResult := fmt.Sprintf("%v", item.TargetUnion)
+				expectedResult = strings.TrimPrefix(expectedResult, "&{")
+				expectedResult = strings.TrimSuffix(expectedResult, "}")
+				dump.WriteString(fmt.Sprintf("  %03d %s %s %v \"%s\"\n", i+1, keyToStr(item.Key, item.RangeEnd), item.Target, item.Result, expectedResult))
+			}
+		}
+
+		if len(thenOps) > 0 {
+			dump.WriteString("  >>> THEN:\n")
+			for i, item := range thenOps {
+				linePrefix := fmt.Sprintf("  %03d ", i+1)
+				prefixLines(linePrefix, v.startOp(item), &dump)
+			}
+		}
+
+		if len(elseOps) > 0 {
+			dump.WriteString("  >>> ELSE:\n")
+			for i, item := range thenOps {
+				linePrefix := fmt.Sprintf("  %03d ", i+1)
+				prefixLines(linePrefix, v.startOp(item), &dump)
+			}
+		}
+
+		dumpStr = dump.String()
+	}
+
+	if dumpStr == "" {
+		return opName
 	} else {
-		v.log(requestID, `%s "%s" | done | %s`, op, key, time.Since(startTime))
+		return fmt.Sprintf("%s\n%s", opName, dumpStr)
+	}
+}
+
+func (v *kvWrapper) endOp(op Op, startTime time.Time, r OpResponse, err error) string {
+	return v.end(opToStr(op), keyToStr(op.KeyBytes(), op.RangeBytes()), startTime, r, err)
+}
+
+func (v *kvWrapper) end(op, key string, startTime time.Time, r OpResponse, err error) string {
+	if err != nil {
+		if key == "" {
+			return fmt.Sprintf(`✖  %s | error | %s | %s`, op, err, time.Since(startTime))
+		} else {
+			return fmt.Sprintf(`✖  %s %s | error | %s | %s`, op, key, err, time.Since(startTime))
+		}
+	} else if get := r.Get(); get != nil {
+		return fmt.Sprintf(`✔️️  %s %s | rev: %v | count: %d | %s`, op, key, get.Header.Revision, get.Count, time.Since(startTime))
+	} else if put := r.Put(); put != nil {
+		return fmt.Sprintf("✔️️  %s %s | rev: %v | %s", op, key, put.Header.Revision, time.Since(startTime))
+	} else if del := r.Del(); del != nil {
+		return fmt.Sprintf(`✔️️  %s %s | rev: %v | deleted: %d| %s`, op, key, del.Header.Revision, del.Deleted, time.Since(startTime))
+	} else if txn := r.Txn(); txn != nil {
+		return fmt.Sprintf(`✔️️  %s | succeeded: %t | rev: %v | %s`, op, txn.Succeeded, txn.Header.Revision, time.Since(startTime))
+	} else {
+		if key == "" {
+			return fmt.Sprintf(`✔️️  %s | %s`, op, time.Since(startTime))
+		} else {
+			return fmt.Sprintf(`✔️️  %s %s | %s`, op, key, time.Since(startTime))
+		}
 	}
 }
 
@@ -114,14 +169,48 @@ func (v *kvWrapper) Txn(ctx context.Context) Txn {
 func (v *txnWrapper) Commit() (*TxnResponse, error) {
 	requestID := v.nextRequestID()
 	startTime := time.Now()
-	v.log(requestID, `TXN | start`)
-
+	v.log(requestID, v.start(nil, "TXN", "", ""))
 	r, err := v.Txn.Commit()
-	if err != nil {
-		v.log(requestID, `TXN | error | %s | %s`, err, time.Since(startTime))
-	} else {
-		v.log(requestID, `TXN | done | rev: %v | succeeded: %t | %s`, r.Succeeded, r.Header.Revision, time.Since(startTime))
-	}
-
+	v.log(requestID, v.end("TXN", "", startTime, r.OpResponse(), err))
 	return r, err
+}
+
+func quote(v string) string {
+	if v == "" {
+		return ""
+	}
+	return fmt.Sprintf(`"%s"`, v)
+}
+
+func keyToStr(key, end []byte) string {
+	if len(key) == 0 {
+		return ""
+	} else if len(end) > 0 {
+		return fmt.Sprintf(`["%s", "%s")`, string(key), string(end))
+	} else {
+		return quote(string(key))
+	}
+}
+
+func opToStr(op Op) string {
+	switch {
+	case op.IsGet():
+		return "GET"
+	case op.IsPut():
+		return "PUT"
+	case op.IsDelete():
+		return "DEL"
+	case op.IsTxn():
+		return "TXN"
+	}
+	return "n/a"
+}
+
+func prefixLines(prefix, lines string, out *strings.Builder) {
+	s := bufio.NewScanner(strings.NewReader(lines))
+	for s.Scan() {
+		out.WriteString(prefix)
+		out.WriteString(s.Text())
+		out.WriteString("\n")
+	}
 }
