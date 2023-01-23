266770bf4c3fd65c60ebbfebd47bae2d8ec2c437
Michal Jurečko <michal.jurecko@gmail.com>
2023-01-23 05:45:33 +0100

    store: Delete unused "GetStatsFrom" method

--- internal/pkg/service/buffer/store/stats.go
+++ internal/pkg/service/buffer/store/stats.go
@@ -1,19 +1,9 @@
 package store
 
 import (
-	"context"
-
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/iterator"
-	"github.com/keboola/keboola-as-code/internal/pkg/telemetry"
 )
-
-func GetStatsFrom[T model.StatsProvider](ctx context.Context, s *Store, prefix iterator.DefinitionT[T]) (out model.Stats, err error) {
-	_, span := s.tracer.Start(ctx, "keboola.go.buffer.store.GetStats")
-	defer telemetry.EndSpan(span, &err)
-	err = sumStatsOp(prefix, &out).DoOrErr(ctx, s.client)
-	return out, err
-}
 
 // sumStatsOp sums all stats from the iterator.
 func sumStatsOp[T model.StatsProvider](prefix iterator.DefinitionT[T], out *model.Stats) *iterator.ForEachOpT[T] {
