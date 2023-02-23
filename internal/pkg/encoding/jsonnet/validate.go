c3d8027112a8430a4312c8862ba3263532869150
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-02-23 11:01:41 +0100

    fix lint

--- internal/pkg/encoding/jsonnet/validate.go
+++ internal/pkg/encoding/jsonnet/validate.go
@@ -5,13 +5,13 @@ import (
 	"github.com/google/go-jsonnet/parser"
 )
 
-func (ctx *Context) Validate(code string) error {
+func (c *Context) Validate(code string) error {
 	// get global vars
-	vars := make([]ast.Identifier, 0, len(ctx.globalBinding))
-	for k := range ctx.globalBinding {
+	vars := make([]ast.Identifier, 0, len(c.globalBinding))
+	for k := range c.globalBinding {
 		vars = append(vars, k)
 	}
 
-	_, err := parser.SnippetToAst(code, ctx.FilePath(), vars...)
+	_, err := parser.SnippetToAst(code, c.FilePath(), vars...)
 	return err
 }
