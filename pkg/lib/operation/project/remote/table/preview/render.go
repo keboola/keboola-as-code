a24e7f1043018c88e76905dda18feaf4aeed0add
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-03-02 11:25:59 +0100

    add table rendering

---
+++ pkg/lib/operation/project/remote/table/preview/render.go
@@ -0,0 +1,120 @@
+package preview
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/keboola/go-client/pkg/keboola"
+	"github.com/keboola/keboola-as-code/internal/pkg/encoding/json"
+)
+
+func renderJson(table *keboola.TablePreview) string {
+	type output struct {
+		Columns []string   `json:"columns"`
+		Rows    [][]string `json:"rows"`
+	}
+
+	o := output{Columns: table.Columns, Rows: table.Rows}
+
+	return json.MustEncodeString(o, false)
+}
+
+func renderCsv(table *keboola.TablePreview) string {
+	var b strings.Builder
+
+	for _, col := range table.Columns[:len(table.Columns)-1] {
+		b.WriteString(col)
+		b.WriteString(",")
+	}
+	b.WriteString(table.Columns[len(table.Columns)-1])
+	b.WriteString("\n")
+
+	for _, row := range table.Rows[:len(table.Rows)-1] {
+		for _, col := range row[:len(row)-1] {
+			b.WriteString(col)
+			b.WriteString(",")
+		}
+		b.WriteString(row[len(row)-1])
+		b.WriteString("\n")
+	}
+	lastRow := table.Rows[len(table.Rows)-1]
+	for _, col := range lastRow[:len(lastRow)-1] {
+		b.WriteString(col)
+		b.WriteString(",")
+	}
+	b.WriteString(lastRow[len(lastRow)-1])
+
+	return b.String()
+}
+
+const (
+	boxV  = "┃"
+	boxH  = "━"
+	boxTL = "┏"
+	boxTR = "┓"
+	boxBL = "┗"
+	boxBR = "┛"
+	boxVL = "┣"
+	boxVR = "┫"
+	boxHT = "┳"
+	boxHB = "┻"
+	boxC  = "╋"
+)
+
+func renderTable(table *keboola.TablePreview) string {
+	widths := measureColumns(table)
+
+	var b strings.Builder
+
+	// draws a "border" line, e.g. `┏━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━┓`
+	border := func(left, middle, right string, lf bool) {
+		b.WriteString(left)
+		cols, last := widths[:len(widths)-1], widths[len(widths)-1]
+		for _, w := range cols {
+			b.WriteString(strings.Repeat(boxH, w+2))
+			b.WriteString(middle)
+		}
+		b.WriteString(strings.Repeat(boxH, last+2))
+		b.WriteString(right)
+		if lf {
+			b.WriteString("\n")
+		}
+	}
+	// draws a "content" line, e.g. `┃ asdf    ┃ my data ┃ 2015-09-01 ┃`
+	content := func(data []string) {
+		b.WriteString(boxV)
+		cols, last := widths[:len(widths)-1], widths[len(widths)-1]
+		for i, w := range cols {
+			fmt.Fprintf(&b, " %-*s ", w, data[i])
+			b.WriteString(boxV)
+		}
+		fmt.Fprintf(&b, " %-*s ", last, data[len(data)-1])
+		b.WriteString(boxV)
+		b.WriteString("\n")
+	}
+
+	border(boxTL, boxHT, boxTR, true)
+	content(table.Columns)
+	border(boxVL, boxC, boxVR, true)
+	for _, row := range table.Rows {
+		content(row)
+	}
+	border(boxBL, boxHB, boxBR, false)
+
+	return b.String()
+}
+
+func measureColumns(table *keboola.TablePreview) []int {
+	widths := make([]int, len(table.Columns))
+	for i, col := range table.Columns {
+		widths[i] = len(col)
+	}
+	for _, row := range table.Rows {
+		for i, col := range row {
+			if len(col) > widths[i] {
+				widths[i] = len(col)
+			}
+		}
+	}
+	return widths
+}
---
+++ pkg/lib/operation/project/remote/table/preview/render_test.go
@@ -0,0 +1,111 @@
+package preview
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/keboola/go-client/pkg/keboola"
+	"github.com/stretchr/testify/assert"
+)
+
+type testCase struct {
+	input    *keboola.TablePreview
+	expected string
+}
+
+func TestRenderJson(t *testing.T) {
+	t.Parallel()
+
+	cases := []testCase{
+		{
+			input: &keboola.TablePreview{
+				Columns: []string{"Id", "Name", "Region", "Status", "First_Order"},
+				Rows: [][]string{
+					{"f030ed64cbc8babbe50901a26675a2ee", "CSK Auto", "US West", "Active", "2015-01-23"},
+					{"06c0b954b0d2088e3da2132d1ba96f31", "AM/PM Camp", "Global", "Active", "2015-02-04"},
+					{"fffe0e30b4a34f01063330a4b908fde5", "Super Saver Foods", "Global", "Active", "2015-02-06"},
+					{"33025ad4a425b6ee832e76beb250ae1c", "Netcore", "Global", "Inactive", "2015-03-02"},
+				},
+			},
+			expected: `{"columns":["Id","Name","Region","Status","First_Order"],"rows":[["f030ed64cbc8babbe50901a26675a2ee","CSK Auto","US West","Active","2015-01-23"],["06c0b954b0d2088e3da2132d1ba96f31","AM/PM Camp","Global","Active","2015-02-04"],["fffe0e30b4a34f01063330a4b908fde5","Super Saver Foods","Global","Active","2015-02-06"],["33025ad4a425b6ee832e76beb250ae1c","Netcore","Global","Inactive","2015-03-02"]]}`,
+		},
+	}
+
+	for _, c := range cases {
+		actual := renderJson(c.input)
+		assert.Equal(t, c.expected, actual)
+	}
+}
+
+func TestRenderCsv(t *testing.T) {
+	t.Parallel()
+
+	cases := []testCase{
+		{
+			input: &keboola.TablePreview{
+				Columns: []string{"Id", "Name", "Region", "Status", "First_Order"},
+				Rows: [][]string{
+					{"f030ed64cbc8babbe50901a26675a2ee", "CSK Auto", "US West", "Active", "2015-01-23"},
+					{"06c0b954b0d2088e3da2132d1ba96f31", "AM/PM Camp", "Global", "Active", "2015-02-04"},
+					{"fffe0e30b4a34f01063330a4b908fde5", "Super Saver Foods", "Global", "Active", "2015-02-06"},
+					{"33025ad4a425b6ee832e76beb250ae1c", "Netcore", "Global", "Inactive", "2015-03-02"},
+				},
+			},
+			expected: `Id,Name,Region,Status,First_Order
+f030ed64cbc8babbe50901a26675a2ee,CSK Auto,US West,Active,2015-01-23
+06c0b954b0d2088e3da2132d1ba96f31,AM/PM Camp,Global,Active,2015-02-04
+fffe0e30b4a34f01063330a4b908fde5,Super Saver Foods,Global,Active,2015-02-06
+33025ad4a425b6ee832e76beb250ae1c,Netcore,Global,Inactive,2015-03-02`,
+		},
+	}
+
+	for _, c := range cases {
+		actual := renderCsv(c.input)
+		// using `expected` without normalizing, because the output should not have an empty line at the end
+		assert.Equal(t, c.expected, actual)
+	}
+}
+
+func TestRenderTable(t *testing.T) {
+	t.Parallel()
+
+	cases := []testCase{
+		{
+			input: &keboola.TablePreview{
+				Columns: []string{"Id", "Name", "Region", "Status", "First_Order"},
+				Rows: [][]string{
+					{"f030ed64cbc8babbe50901a26675a2ee", "CSK Auto", "US West", "Active", "2015-01-23"},
+					{"06c0b954b0d2088e3da2132d1ba96f31", "AM/PM Camp", "Global", "Active", "2015-02-04"},
+					{"fffe0e30b4a34f01063330a4b908fde5", "Super Saver Foods", "Global", "Active", "2015-02-06"},
+					{"33025ad4a425b6ee832e76beb250ae1c", "Netcore", "Global", "Inactive", "2015-03-02"},
+				},
+			},
+			expected: `
+				┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━━━━━┓
+				┃ Id                               ┃ Name              ┃ Region  ┃ Status   ┃ First_Order ┃
+				┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━━╋━━━━━━━━━━━━━┫
+				┃ f030ed64cbc8babbe50901a26675a2ee ┃ CSK Auto          ┃ US West ┃ Active   ┃ 2015-01-23  ┃
+				┃ 06c0b954b0d2088e3da2132d1ba96f31 ┃ AM/PM Camp        ┃ Global  ┃ Active   ┃ 2015-02-04  ┃
+				┃ fffe0e30b4a34f01063330a4b908fde5 ┃ Super Saver Foods ┃ Global  ┃ Active   ┃ 2015-02-06  ┃
+				┃ 33025ad4a425b6ee832e76beb250ae1c ┃ Netcore           ┃ Global  ┃ Inactive ┃ 2015-03-02  ┃
+				┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━━┻━━━━━━━━━━━━━┛
+			`,
+		},
+	}
+
+	for _, c := range cases {
+		actual := normalizeTable(renderTable(c.input))
+		expected := normalizeTable(c.expected)
+
+		assert.Equal(t, expected, actual)
+	}
+}
+
+func normalizeTable(s string) string {
+	s = strings.TrimSpace(s)
+	lines := strings.Split(s, "\n")
+	for i := range lines {
+		lines[i] = strings.TrimSpace(lines[i])
+	}
+	return strings.Join(lines, "\n")
+}
