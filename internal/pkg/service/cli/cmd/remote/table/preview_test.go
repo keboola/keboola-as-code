92dca3ab449367f642e0bda681986ba829cbe444
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-03-02 13:48:55 +0100

    fix gci lint

--- internal/pkg/service/cli/cmd/remote/table/preview_test.go
+++ internal/pkg/service/cli/cmd/remote/table/preview_test.go
@@ -3,9 +3,9 @@ package table
 import (
 	"testing"
 
+	"github.com/keboola/go-client/pkg/keboola"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/keboola/go-client/pkg/keboola"
 	"github.com/keboola/keboola-as-code/pkg/lib/operation/project/remote/table/preview"
 )
 
