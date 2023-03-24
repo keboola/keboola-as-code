5052816e32869bd1b98ad0aaa694dbbc31f13f94
Jakub Matějka <jakub@keboola.com>
2023-03-24 14:21:20 +0100

    Generate default name from display name + add interactive e2e test

--- internal/pkg/service/cli/dialog/create_bucket.go
+++ internal/pkg/service/cli/dialog/create_bucket.go
@@ -6,6 +6,7 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/options"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/prompt"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/strhelper"
 	"github.com/keboola/keboola-as-code/pkg/lib/operation/project/remote/create/bucket"
 )
 
@@ -15,19 +16,6 @@ type createBucketDeps interface {
 
 func (p *Dialogs) AskCreateBucket(d createBucketDeps) (bucket.Options, error) {
 	opts := bucket.Options{}
-
-	if d.Options().IsSet("name") {
-		opts.Name = d.Options().GetString("name")
-	} else {
-		name, ok := p.Ask(&prompt.Question{
-			Label:     "Enter a name for the bucket",
-			Validator: prompt.ValueRequired,
-		})
-		if !ok || len(name) == 0 {
-			return opts, errors.New("missing name, please specify it")
-		}
-		opts.Name = name
-	}
 
 	if d.Options().IsSet("stage") {
 		stage := d.Options().GetString("stage")
@@ -46,17 +34,6 @@ func (p *Dialogs) AskCreateBucket(d createBucketDeps) (bucket.Options, error) {
 		opts.Stage = v
 	}
 
-	if d.Options().IsSet("description") {
-		opts.Description = d.Options().GetString("description")
-	} else {
-		description, ok := p.Ask(&prompt.Question{
-			Label: "Enter a description for the bucket",
-		})
-		if ok {
-			opts.Description = description
-		}
-	}
-
 	if d.Options().IsSet("display-name") {
 		opts.DisplayName = d.Options().GetString("display-name")
 	} else {
@@ -65,6 +42,31 @@ func (p *Dialogs) AskCreateBucket(d createBucketDeps) (bucket.Options, error) {
 		})
 		if ok {
 			opts.DisplayName = displayName
+		}
+	}
+
+	if d.Options().IsSet("name") {
+		opts.Name = d.Options().GetString("name")
+	} else {
+		name, ok := p.Ask(&prompt.Question{
+			Label:     "Enter a name for the bucket",
+			Validator: prompt.ValueRequired,
+			Default:   strhelper.NormalizeName(opts.DisplayName),
+		})
+		if !ok || len(name) == 0 {
+			return opts, errors.New("missing name, please specify it")
+		}
+		opts.Name = name
+	}
+
+	if d.Options().IsSet("description") {
+		opts.Description = d.Options().GetString("description")
+	} else {
+		description, ok := p.Ask(&prompt.Question{
+			Label: "Enter a description for the bucket",
+		})
+		if ok {
+			opts.Description = description
 		}
 	}
 
