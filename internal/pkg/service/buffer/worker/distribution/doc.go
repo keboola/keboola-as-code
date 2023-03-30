c83dcc2e3a7cc2bad8289ecea40c9e07cf255b1d
Michal Jurečko <michal.jurecko@gmail.com>
2023-03-30 08:46:31 +0200

    Update doc

--- internal/pkg/service/buffer/worker/distribution/doc.go
+++ internal/pkg/service/buffer/worker/distribution/doc.go
@@ -1,12 +1,11 @@
-// Package distribution provides distribution of various keys/tasks between worker nodes.
-// The owner of the task is determined locally, in the worker node, without unnecessary communication in the cluster.
+// Package distribution provides distribution of various keys/tasks between cluster nodes.
+// The owner of the task is determined locally, in the node, without unnecessary communication in the cluster.
 //
 // The package consists of:
-// - Registration of a worker node in the cluster as an etcd key (with lease), see Node.
-// - Discovering of other worker nodes in the cluster by the etcd Watch API, see Node.
-// - Local decision and assignment of a key/task to a specific worker node (by a consistent hash/HashRing approach), see Assigner.
+// - Registration of a node in the cluster as an etcd key (with lease), see Node.
+// - Discovering of other nodes in the cluster by the etcd Watch API, see Node.
+// - Local decision and assignment of a key/task to a specific node (by a consistent hash/HashRing approach), see Assigner.
 // - Distribution change listeners, see Node.OnChangeListener.
-// - The ExecutorWork restarted on each distribution change, see Node.StartExecutor.
 //
 // # Key benefits
 //
@@ -17,8 +16,8 @@
 // # Atomicity
 //
 // - During watch propagation or lease timeout, individual nodes can have a different list of the active nodes.
-// - This could lead to the situation, when 2+ nodes have ownership of a task at the same time.
-// - Therefore, the task itself must be also protected by a transaction (version number validation).
+// - This could lead to the situation, when 2+ nodes have ownership of a key at the same time.
+// - Therefore, the task based on the key must be also protected by a transaction or distributed lock.
 // - Task locks are implemented in the "task" package.
 //
 // Read more:
