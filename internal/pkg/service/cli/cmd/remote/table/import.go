6913c3e0c88bea4cfb4d4d73332e01795aa4cbdc
Jakub Matějka <jakub@keboola.com>
2023-03-30 13:30:29 +0200

    Add optional flags to table import command (PSGO-152)

--- internal/pkg/service/cli/cmd/remote/table/import.go
+++ internal/pkg/service/cli/cmd/remote/table/import.go
@@ -91,8 +91,11 @@ func ImportCommand(p dependencies.Provider) *cobra.Command {
 				TableID:         tableID,
 				Columns:         d.Options().GetStringSlice("columns"),
 				IncrementalLoad: d.Options().GetBool("incremental-load"),
-				WithoutHeaders:  d.Options().GetBool("without-headers"),
+				WithoutHeaders:  d.Options().GetBool("file-without-headers"),
 				PrimaryKey:      primaryKey,
+				Delimiter:       d.Options().GetString("file-delimiter"),
+				Enclosure:       d.Options().GetString("file-enclosure"),
+				EscapedBy:       d.Options().GetString("file-escaped-by"),
 			}
 
 			return tableImport.Run(d.CommandCtx(), opts, d)
@@ -102,8 +105,10 @@ func ImportCommand(p dependencies.Provider) *cobra.Command {
 	cmd.Flags().StringP("storage-api-host", "H", "", "storage API host, eg. \"connection.keboola.com\"")
 	cmd.Flags().String("columns", "", "comma separated list of column names. If present, the first row in the CSV file is not treated as a header")
 	cmd.Flags().Bool("incremental-load", false, "data are either added to existing data in the table or replace the existing data")
-	cmd.Flags().Bool("without-headers", false, "states if the CSV file contains headers on the first row or not")
+	cmd.Flags().Bool("file-without-headers", false, "states if the CSV file contains headers on the first row or not")
 	cmd.Flags().StringSlice("primary-key", nil, "primary key for the newly created table if the table doesn't exist")
-
+	cmd.Flags().String("file-delimiter", ",", "field delimiter used in the CSV file")
+	cmd.Flags().String("file-enclosure", `"`, "field enclosure used in the CSV file")
+	cmd.Flags().String("file-escaped-by", "", "escape character used in the CSV file")
 	return cmd
 }
