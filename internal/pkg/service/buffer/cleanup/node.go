3c8b33d3ec0c0c7385c00b20ff8a3a84c2f5c77a
Michal Jurečko <michal.jurecko@gmail.com>
2023-04-01 10:53:39 +0200

    TMP: create file, if there is no one

--- internal/pkg/service/buffer/cleanup/node.go
+++ internal/pkg/service/buffer/cleanup/node.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/benbjohnson/clock"
+	"github.com/keboola/go-client/pkg/client"
 	etcd "go.etcd.io/etcd/client/v3"
 	"golang.org/x/sync/semaphore"
 
@@ -35,6 +36,8 @@ type dependencies interface {
 	Store() *store.Store
 	DistributionWorkerNode() *distribution.Node
 	TaskNode() *task.Node
+	StorageAPIHost() string
+	HTTPClient() client.Client
 }
 
 type Node struct {
