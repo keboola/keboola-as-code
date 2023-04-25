b3c6a2cd437148e903b98fa0c5905a444cae6ef4
Michal Jurečko <michal.jurecko@gmail.com>
2023-04-25 10:54:30 +0200

    TMP: temporary BA fixes

--- internal/pkg/service/buffer/cleanup/node.go
+++ internal/pkg/service/buffer/cleanup/node.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/benbjohnson/clock"
+	"github.com/keboola/go-client/pkg/client"
 	etcd "go.etcd.io/etcd/client/v3"
 	"golang.org/x/sync/semaphore"
 
@@ -37,6 +38,8 @@ type dependencies interface {
 	Store() *store.Store
 	DistributionNode() *distribution.Node
 	TaskNode() *task.Node
+	StorageAPIHost() string
+	HTTPClient() client.Client
 }
 
 type Node struct {
