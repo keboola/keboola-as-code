5f99302b184ec424b2032b25d981801d737448ad
Michal Jurečko <michal.jurecko@gmail.com>
2023-04-26 15:15:24 +0200

    BA config: Add MetricsListenAddress option

--- internal/pkg/service/buffer/api/config/config.go
+++ internal/pkg/service/buffer/api/config/config.go
@@ -24,7 +24,8 @@ const (
 type Config struct {
 	ServiceConfig              `mapstructure:",squash"`
 	PublicAddress              *url.URL          `mapstructure:"public-address" usage:"Public address of the Buffer API, to generate a link URL."`
-	ListenAddress              *url.URL          `mapstructure:"listen-address" usage:"Server listen address."`
+	ListenAddress              *url.URL          `mapstructure:"listen-address" usage:"API HTTP server listen address."`
+	MetricsListenAddress       *url.URL          `mapstructure:"metrics-listen-address" usage:"Prometheus /metrics HTTP endpoint listen address."`
 	StatisticsSyncInterval     time.Duration     `mapstructure:"statistics-sync-interval" usage:"Statistics synchronization interval from API node to etcd."`
 	ReceiverBufferSize         datasize.ByteSize `mapstructure:"receiver-buffer-size" usage:"Maximum buffered records size in etcd per receiver."`
 	ReceiverBufferSizeCacheTTL time.Duration     `mapstructure:"receiver-buffer-size-cache-ttl" usage:"Invalidation interval for receiver buffer size cache."`
@@ -38,6 +39,7 @@ func NewConfig() Config {
 	return Config{
 		ServiceConfig:              serviceConfig.NewConfig(),
 		ListenAddress:              &url.URL{Scheme: "http", Host: "0.0.0.0:8000"},
+		MetricsListenAddress:       &url.URL{Scheme: "http", Host: "0.0.0.0:9000"},
 		StatisticsSyncInterval:     DefaultStatisticsSyncInterval,
 		ReceiverBufferSize:         DefaultReceiverBufferSize,
 		ReceiverBufferSizeCacheTTL: DefaultReceiverBufferSizeCacheTTL,
@@ -68,6 +70,12 @@ func (c *Config) Normalize() {
 			c.ListenAddress.Scheme = "http"
 		}
 	}
+	if c.MetricsListenAddress != nil {
+		c.MetricsListenAddress.Host = strhelper.NormalizeHost(c.MetricsListenAddress.Host)
+		if c.MetricsListenAddress.Scheme == "" {
+			c.MetricsListenAddress.Scheme = "http"
+		}
+	}
 }
 
 func (c *Config) Validate() error {
@@ -80,6 +88,9 @@ func (c *Config) Validate() error {
 	}
 	if c.ListenAddress == nil || c.ListenAddress.String() == "" {
 		errs.Append(errors.New("listen address is not set"))
+	}
+	if c.MetricsListenAddress == nil || c.MetricsListenAddress.String() == "" {
+		errs.Append(errors.New("metrics listen address is not set"))
 	}
 	if c.ReceiverBufferSize <= 0 {
 		errs.Append(errors.New("receiver buffer size  must be a positive value"))
