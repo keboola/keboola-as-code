53d71625c28f374e5fe426e8d54fa46f17cffdc9
Michal Jurečko <michal.jurecko@gmail.com>
2023-05-02 22:21:31 +0200

    Validate conditions in mapper, before model.save

--- internal/pkg/service/buffer/api/service/mapper/conditions.go
+++ internal/pkg/service/buffer/api/service/mapper/conditions.go
@@ -1,6 +1,7 @@
 package mapper
 
 import (
+	"context"
 	"time"
 
 	"github.com/c2h5oh/datasize"
@@ -11,24 +12,29 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
-func (m Mapper) ConditionsModel(payload *buffer.Conditions) (r model.Conditions, err error) {
-	conditions := model.DefaultImportConditions()
+func (m Mapper) ConditionsModel(payload *buffer.Conditions) (conditions model.Conditions, err error) {
+	conditions = model.DefaultImportConditions()
 	if payload != nil {
 		conditions.Count = uint64(payload.Count)
 		conditions.Size, err = datasize.ParseString(payload.Size)
 		if err != nil {
 			return model.Conditions{}, serviceError.NewBadRequestError(errors.Errorf(
-				`value "%s" is not valid buffer size in bytes. Allowed units: B, kB, MB. For example: "5MB"`,
+				`invalid conditions: value "%s" is not valid buffer size in bytes. Allowed units: B, kB, MB. For example: "5MB"`,
 				payload.Size,
 			))
 		}
 		conditions.Time, err = time.ParseDuration(payload.Time)
 		if err != nil {
 			return model.Conditions{}, serviceError.NewBadRequestError(errors.Errorf(
-				`value "%s" is not valid time duration. Allowed units: s, m, h. For example: "30s"`,
+				`invalid conditions: value "%s" is not valid time duration. Allowed units: s, m, h. For example: "30s"`,
 				payload.Size,
 			))
 		}
 	}
+
+	if err := m.validator.Validate(context.Background(), conditions); err != nil {
+		return conditions, serviceError.NewBadRequestError(errors.Errorf(`invalid conditions: %w`, err))
+	}
+
 	return conditions, nil
 }
