92127ef56cc47af09e24d9629f5bd632b65c2537
Michal Jurečko <michal.jurecko@gmail.com>
2023-05-15 17:43:25 +0200

    OpenTelemetry middleware

---
+++ internal/pkg/service/common/httpserver/middleware/otel_config.go
@@ -0,0 +1,65 @@
+package middleware
+
+import (
+	"strings"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+)
+
+type otelConfig struct {
+	filters             []otelhttp.Filter
+	redactedRouteParams map[string]struct{}
+	redactedQueryParams map[string]struct{}
+	redactedHeaders     map[string]struct{}
+}
+
+type OTELOption func(config *otelConfig)
+
+func WithFilter(filters ...otelhttp.Filter) OTELOption {
+	return func(c *otelConfig) {
+		c.filters = append(c.filters, filters...)
+	}
+}
+
+func WithRedactedRouteParam(params ...string) OTELOption {
+	return func(c *otelConfig) {
+		for _, p := range params {
+			c.redactedRouteParams[strings.ToLower(p)] = struct{}{}
+		}
+	}
+}
+
+func WithRedactedQueryParam(params ...string) OTELOption {
+	return func(c *otelConfig) {
+		for _, p := range params {
+			c.redactedQueryParams[strings.ToLower(p)] = struct{}{}
+		}
+	}
+}
+
+func WithRedactedHeader(headers ...string) OTELOption {
+	return func(c *otelConfig) {
+		for _, p := range headers {
+			c.redactedHeaders[strings.ToLower(p)] = struct{}{}
+		}
+	}
+}
+
+func newOTELConfig(opts []OTELOption) otelConfig {
+	cfg := otelConfig{
+		redactedRouteParams: make(map[string]struct{}),
+		redactedQueryParams: make(map[string]struct{}),
+		redactedHeaders: map[string]struct{}{
+			"authorization":       {},
+			"www-authenticate":    {},
+			"proxy-authenticate":  {},
+			"proxy-authorization": {},
+			"cookie":              {},
+			"set-cookie":          {},
+		},
+	}
+	for _, o := range opts {
+		o(&cfg)
+	}
+	return cfg
+}
