4b438d467d43939ad52b30774ea1016bec048b0c
Michal Jurečko <michal.jurecko@gmail.com>
2023-06-06 11:05:04 +0200

    Worker E2E test: update 001 import records test

--- test/buffer/worker/001_import_records_test.go
+++ test/buffer/worker/001_import_records_test.go
@@ -6,12 +6,16 @@ import (
 
 // test001ImportRecords imports 4 records; 4 < 5 (uploadCountThreshold) - upload is not triggered.
 func (ts *testSuite) test001ImportRecords() {
+	ts.t.Logf("-------------------------")
+	ts.t.Logf("001 import records")
+	ts.t.Logf("-------------------------")
+
 	// Run imports immediately after the last check to prevent the check during imports.
 	ts.WaitForLogMessages(15*time.Second, `
-[worker-node-%s][service][conditions]INFO  checked "2" opened slices
+[worker-node-%s][bufferWorker][service][conditions]INFO checked "2" opened slices | %s
 	`)
 
-	// Import records
+	// Import records 1-4
 	for i := 1; i <= 4; i++ {
 		ts.Import(i)
 	}
