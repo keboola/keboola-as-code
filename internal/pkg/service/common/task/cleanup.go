d403a4cc0de9f1ad3b749d1d3db2992016e9d25b
Michal Jurečko <michal.jurecko@gmail.com>
2023-06-09 10:34:14 +0200

    Run tasks.cleanup with common StartTask, as a system task

--- internal/pkg/service/common/task/cleanup.go
+++ internal/pkg/service/common/task/cleanup.go
@@ -2,12 +2,13 @@ package task
 
 import (
 	"context"
+	"fmt"
 	"time"
 
-	etcd "go.etcd.io/etcd/client/v3"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/iterator"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
@@ -19,70 +20,50 @@ const (
 	CleanupSuccessfulTasksAfter = 1 * time.Hour
 	CleanupFailedTasksAfter     = 48 * time.Hour
 	CleanupUnfinishedTasksAfter = 48 * time.Hour
+	cleanupTaskType             = "tasks.cleanup"
 )
 
 // Cleanup deletes old tasks to free space in etcd.
 func (n *Node) Cleanup() (err error) {
-	logger := n.logger.AddPrefix("[cleanup]")
-
-	// Block shutdown during the cleanup
-	n.tasksWg.Add(1)
-	defer n.tasksWg.Done()
-
-	// Timeout
-	ctx, cancel := context.WithTimeout(context.Background(), CleanupTimeout)
-	defer cancel()
-
-	// Prevented running cleanup multiple times by etcd transaction/lock
-	lock := LockEtcdPrefix.Key("tasks.cleanup")
-	if ok, err := lock.PutIfNotExists(n.nodeID, etcd.WithLease(n.session.Lease())).Do(n.tasksCtx, n.client); err != nil {
-		return errors.Errorf(`cannot start: %w`, err)
-	} else if !ok {
-		logger.Infof(`skipped, the lock "%s" is in use`, lock.Key())
-		return nil
-	}
-	logger.Debugf(`lock acquired "%s"`, lock.Key())
-
-	// Release lock after the cleanup
-	defer func() {
-		// If release of the lock takes longer than the ttl, lease is expired anyway
-		ctx, cancel := context.WithTimeout(n.tasksCtx, time.Duration(n.config.ttlSeconds)*time.Second)
-		defer cancel()
-		if ok, err := lock.DeleteIfExists().Do(ctx, n.client); err != nil {
-			logger.Errorf(`cannot release lock: %s`, err)
-			return
-		} else if !ok {
-			logger.Errorf(`cannot release lock "%s", not found`, lock.Key())
-			return
-		}
-		logger.Debugf(`lock released "%s"`, lock.Key())
-	}()
-
-	// Setup telemetry
-	ctx, span := n.tracer.Start(ctx, spanName, trace.WithAttributes(attribute.String("resource.name", "tasks.cleanup")))
-	defer span.End(&err)
-
-	// Go through tasks and delete old ones
-	deletedTasksCount := int64(0)
-	errs := errors.NewMultiError()
-	err = n.taskEtcdPrefix.GetAll().Do(ctx, n.client).ForEachKV(func(kv op.KeyValueT[Task], header *iterator.Header) error {
-		if n.isForCleanup(kv.Value) {
-			if err := etcdop.Key(kv.Key()).Delete().DoOrErr(ctx, n.client); err == nil {
-				logger.Debugf(`deleted task "%s"`, kv.Value.Key.String())
-				deletedTasksCount++
-			} else {
+	return n.RunTaskOrErr(Config{
+		Type: cleanupTaskType,
+		Key:  Key{SystemTask: true, TaskID: cleanupTaskType},
+		Lock: cleanupTaskType,
+		Context: func() (context.Context, context.CancelFunc) {
+			return context.WithTimeout(context.Background(), CleanupTimeout)
+		},
+		Operation: func(ctx context.Context, logger log.Logger) Result {
+			// Go through tasks and delete old ones
+			deletedTasksCount := int64(0)
+			errs := errors.NewMultiError()
+			err = n.taskEtcdPrefix.GetAll().Do(ctx, n.client).ForEachKV(func(kv op.KeyValueT[Task], header *iterator.Header) error {
+				if n.isForCleanup(kv.Value) {
+					if err := etcdop.Key(kv.Key()).Delete().DoOrErr(ctx, n.client); err == nil {
+						logger.Debugf(`deleted task "%s"`, kv.Value.Key.String())
+						deletedTasksCount++
+					} else {
+						errs.Append(err)
+					}
+				}
+				return nil
+			})
+			if err != nil {
 				errs.Append(err)
 			}
-		}
-		return nil
-	})
-	if err != nil {
-		errs.Append(err)
-	}
 
-	logger.Infof(`deleted "%d" tasks`, deletedTasksCount)
-	span.SetAttributes(attribute.Int64("task.cleanup.deletedTasksCount", deletedTasksCount))
-	return errs.ErrorOrNil()
+			// Track number of deleted tasks
+			trace.SpanFromContext(ctx).SetAttributes(attribute.Int64("task.cleanup.deletedTasksCount", deletedTasksCount))
+			infoMsg := fmt.Sprintf(`deleted "%d" tasks`, deletedTasksCount)
+			logger.Info(infoMsg)
+
+			// Handle error
+			if errs.Len() > 0 {
+				ErrResult(errs)
+			}
+
+			return OkResult(infoMsg)
+		},
+	})
 }
 
 func (n *Node) isForCleanup(t Task) bool {
