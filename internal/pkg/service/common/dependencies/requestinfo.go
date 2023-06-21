c904a71c945fd01b2ba0b9f590e710830f6f2d1c
Michal Jurečko <michal.jurecko@gmail.com>
2023-06-21 16:03:50 +0200

    Add request info dependencies

---
+++ internal/pkg/service/common/dependencies/requestinfo.go
@@ -0,0 +1,51 @@
+package dependencies
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/httpserver/middleware"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/ip"
+)
+
+type requestInfo struct {
+	request   *http.Request
+	requestID string
+}
+
+func NewRequestInfo(req *http.Request) RequestInfo {
+	return newRequestInfo(req)
+}
+
+func newRequestInfo(req *http.Request) *requestInfo {
+	requestID, _ := req.Context().Value(middleware.RequestIDCtxKey).(string)
+	return &requestInfo{request: req, requestID: requestID}
+}
+
+func (v *requestInfo) check() {
+	if v == nil {
+		panic(errors.New("dependencies request info scope is not initialized"))
+	}
+}
+
+func (v *requestInfo) RequestCtx() context.Context {
+	v.check()
+	return v.request.Context()
+}
+
+func (v *requestInfo) RequestID() string {
+	v.check()
+	return v.requestID
+}
+
+func (v *requestInfo) RequestHeader() http.Header {
+	v.check()
+	return v.request.Header.Clone()
+}
+
+func (v *requestInfo) RequestClientIP() net.IP {
+	v.check()
+	return ip.From(v.request)
+}
