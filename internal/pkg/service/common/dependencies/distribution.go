3f2bd812a214b4e23a35d862a6b33d209b842cda
Michal Jurečko <michal.jurecko@gmail.com>
2023-06-21 16:04:41 +0200

    Add distribution dependencies

---
+++ internal/pkg/service/common/dependencies/distribution.go
@@ -0,0 +1,46 @@
+package dependencies
+
+import (
+	"context"
+
+	distributionPkg "github.com/keboola/keboola-as-code/internal/pkg/service/common/distribution"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+// distributionScope implements DistributionScope interface.
+type distributionScope struct {
+	node *distributionPkg.Node
+}
+
+type distributionScopeDeps interface {
+	BaseScope
+	EtcdClientScope
+	TaskScope
+}
+
+func NewDistributionScope(ctx context.Context, d distributionScopeDeps, distributionGroup string, opts ...distributionPkg.NodeOption) (DistributionScope, error) {
+	return newDistributionScope(ctx, d, distributionGroup, opts...)
+}
+
+func newDistributionScope(ctx context.Context, d distributionScopeDeps, distributionGroup string, opts ...distributionPkg.NodeOption) (v *distributionScope, err error) {
+	ctx, span := d.Telemetry().Tracer().Start(ctx, "keboola.go.common.dependencies.NewDistributionScope")
+	defer span.End(&err)
+
+	node, err := distributionPkg.NewNode(distributionGroup, d, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &distributionScope{node: node}, nil
+}
+
+func (v *distributionScope) check() {
+	if v == nil {
+		panic(errors.New("dependencies distribution scope is not initialized"))
+	}
+}
+
+func (v *distributionScope) DistributionNode() *distributionPkg.Node {
+	v.check()
+	return v.node
+}
