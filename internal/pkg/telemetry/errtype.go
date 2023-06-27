e3bae70dd1eb722d1be1715c393fc0dfa3f66714
Michal Jurečko <michal.jurecko@gmail.com>
2023-06-27 07:55:52 +0200

    Track API error types in telemetry

--- internal/pkg/telemetry/errtype.go
+++ internal/pkg/telemetry/errtype.go
@@ -2,7 +2,9 @@ package telemetry
 
 import (
 	"context"
+	"github.com/keboola/go-client/pkg/keboola"
 	"net"
+	"strconv"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
@@ -10,6 +12,11 @@ import (
 // ErrorType detect error type for a metric.
 func ErrorType(err error) string {
 	var netErr net.Error
+	var storageAPIErr *keboola.StorageError
+	var encryptionAPIErr *keboola.EncryptionError
+	var schedulerAPIErr *keboola.SchedulerError
+	var queueAPIErr *keboola.QueueError
+	var workspacesAPIErr *keboola.WorkspacesError
 	errors.As(err, &netErr)
 	switch {
 	case err == nil:
@@ -22,6 +29,16 @@ func ErrorType(err error) string {
 		return "net_timeout"
 	case netErr != nil:
 		return "net"
+	case errors.As(err, &storageAPIErr):
+		return "storage_api_" + storageAPIErr.ErrCode
+	case errors.As(err, &encryptionAPIErr):
+		return "encryption_api_" + strconv.Itoa(encryptionAPIErr.ErrCode)
+	case errors.As(err, &schedulerAPIErr):
+		return "scheduler_api_" + strconv.Itoa(schedulerAPIErr.ErrCode)
+	case errors.As(err, &queueAPIErr):
+		return "queue_api_" + strconv.Itoa(queueAPIErr.ErrCode)
+	case errors.As(err, &workspacesAPIErr):
+		return "workspaces_api"
 	default:
 		return "other"
 	}
--- internal/pkg/telemetry/errtype_test.go
+++ internal/pkg/telemetry/errtype_test.go
@@ -2,6 +2,7 @@ package telemetry
 
 import (
 	"context"
+	"github.com/keboola/go-client/pkg/keboola"
 	"net"
 	"testing"
 
@@ -18,5 +19,10 @@ func TestErrorType(t *testing.T) {
 	assert.Equal(t, "deadline_exceeded", ErrorType(errors.Errorf(`some error: %w`, context.DeadlineExceeded)))
 	assert.Equal(t, "net", ErrorType(&net.DNSError{}))
 	assert.Equal(t, "net_timeout", ErrorType(&net.DNSError{IsTimeout: true}))
+	assert.Equal(t, "storage_api_foo", ErrorType(&keboola.StorageError{ErrCode: "foo"}))
+	assert.Equal(t, "encryption_api_123", ErrorType(&keboola.EncryptionError{ErrCode: 123}))
+	assert.Equal(t, "scheduler_api_123", ErrorType(&keboola.SchedulerError{ErrCode: 123}))
+	assert.Equal(t, "queue_api_123", ErrorType(&keboola.QueueError{ErrCode: 123}))
+	assert.Equal(t, "workspaces_api", ErrorType(&keboola.WorkspacesError{}))
 	assert.Equal(t, "other", ErrorType(errors.New("some error")))
 }
