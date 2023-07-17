cb8831c02b2d93218e7a2e1ef7347c398938c5a5
Michal Jurečko <michal.jurecko@gmail.com>
2023-07-17 12:55:43 +0200

    Make SumStatsOp public

--- internal/pkg/service/buffer/store/stats.go
+++ internal/pkg/service/buffer/store/stats.go
@@ -10,11 +10,11 @@ import (
 )
 
 func SumStats[T model.StatsProvider](ctx context.Context, client *etcd.Client, prefix iterator.DefinitionT[T], out *model.Stats) error {
-	return sumStatsOp(prefix, out).DoOrErr(ctx, client)
+	return SumStatsOp(prefix, out).DoOrErr(ctx, client)
 }
 
-// sumStatsOp sums all stats from the iterator.
-func sumStatsOp[T model.StatsProvider](prefix iterator.DefinitionT[T], out *model.Stats) *iterator.ForEachOpT[T] {
+// SumStatsOp sums all stats from the iterator.
+func SumStatsOp[T model.StatsProvider](prefix iterator.DefinitionT[T], out *model.Stats) *iterator.ForEachOpT[T] {
 	return prefix.ForEachOp(func(item T, _ *iterator.Header) error {
 		*out = out.Add(item.GetStats())
 		return nil
