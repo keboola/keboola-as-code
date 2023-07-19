f237b6a4b421797711f2e3187749bdd67a55e894
Michal Jurečko <michal.jurecko@gmail.com>
2023-07-19 08:35:09 +0200

    Update config package: add StatisticsL2CacheTTL field, move Conditions struct

--- internal/pkg/service/buffer/config/service.go
+++ internal/pkg/service/buffer/config/service.go
@@ -8,20 +8,25 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/strhelper"
 )
 
+const (
+	DefaultStatisticsL2CacheTTL = 1 * time.Second
+)
+
 // ServiceConfig is a common config of the Buffer ServiceConfig.
 type ServiceConfig struct {
-	DebugLog           bool                   `mapstructure:"debug-log" usage:"Enable debug log level."`
-	DebugEtcd          bool                   `mapstructure:"debug-etcd" usage:"Enable logging of each etcd KV operation as a debug message."`
-	DebugHTTP          bool                   `mapstructure:"debug-http" usage:"Log HTTP client request and response bodies."`
-	DatadogEnabled     bool                   `mapstructure:"datadog-enabled" usage:"Enable Datadog telemetry integration."`
-	DatadogDebug       bool                   `mapstructure:"datadog-debug" usage:"Enable Datadog debug logs."`
-	CPUProfFilePath    string                 `mapstructure:"cpu-profile" usage:"Write cpu profile to the file."`
-	StorageAPIHost     string                 `mapstructure:"storage-api-host" usage:"Host of the Storage API."`
-	Etcd               etcdclient.Credentials `mapstructure:",squash" usage:"etcd client credentials."`
-	EtcdConnectTimeout time.Duration          `mapstructure:"etcd-connect-timeout" usage:"etcd connect timeout."`
+	DebugLog             bool                   `mapstructure:"debug-log" usage:"Enable debug log level."`
+	DebugEtcd            bool                   `mapstructure:"debug-etcd" usage:"Enable logging of each etcd KV operation as a debug message."`
+	DebugHTTP            bool                   `mapstructure:"debug-http" usage:"Log HTTP client request and response bodies."`
+	DatadogEnabled       bool                   `mapstructure:"datadog-enabled" usage:"Enable Datadog telemetry integration."`
+	DatadogDebug         bool                   `mapstructure:"datadog-debug" usage:"Enable Datadog debug logs."`
+	CPUProfFilePath      string                 `mapstructure:"cpu-profile" usage:"Write cpu profile to the file."`
+	StorageAPIHost       string                 `mapstructure:"storage-api-host" usage:"Host of the Storage API."`
+	Etcd                 etcdclient.Credentials `mapstructure:",squash" usage:"etcd client credentials."`
+	EtcdConnectTimeout   time.Duration          `mapstructure:"etcd-connect-timeout" usage:"etcd connect timeout."`
+	StatisticsL2CacheTTL time.Duration          `mapstructure:"statistics-l2-cache-ttl" usage:"Invalidation interval fast L2 statistics cache."`
 }
 
-func newServiceConfig() ServiceConfig {
+func NewServiceConfig() ServiceConfig {
 	return ServiceConfig{
 		DebugLog:        false,
 		DebugEtcd:       false,
@@ -36,7 +41,8 @@ func newServiceConfig() ServiceConfig {
 			Username:  "",
 			Password:  "",
 		},
-		EtcdConnectTimeout: 30 * time.Second, // longer default timeout, the etcd could be started at the same time as the API/Worker
+		EtcdConnectTimeout:   30 * time.Second, // longer default timeout, the etcd could be started at the same time as the API/Worker
+		StatisticsL2CacheTTL: DefaultStatisticsL2CacheTTL,
 	}
 }
 
@@ -55,6 +61,9 @@ func (c *ServiceConfig) Validate() error {
 	}
 	if c.EtcdConnectTimeout <= 0 {
 		errs.Append(errors.Errorf(`etcd connect timeout must be positive time.Duration, found "%v"`, c.EtcdConnectTimeout))
+	}
+	if c.StatisticsL2CacheTTL <= 0 {
+		errs.Append(errors.New("statistics L2 cache TTL must be a positive value"))
 	}
 	return errs.ErrorOrNil()
 }
