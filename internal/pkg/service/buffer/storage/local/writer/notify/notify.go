a82623728f9310d3098a226a7d7784f19d111c9d
Michal Jurečko <michal.jurecko@gmail.com>
2023-08-28 08:46:31 +0200

    Add storage/local/writer/notify package

---
+++ internal/pkg/service/buffer/storage/local/writer/notify/notify.go
@@ -0,0 +1,38 @@
+package notify
+
+import (
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"sync"
+)
+
+// Notifier allows multiple listeners to Wait for the completion of an operation that may end with an error.
+// It is used while awaiting synchronization, see the disksync package.
+type Notifier struct {
+	doneCh chan struct{}
+	error  error
+}
+
+func New() *Notifier {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	return &Notifier{doneCh: make(chan struct{})}
+}
+
+func (n *Notifier) Done(err error) {
+	if n == nil {
+		panic(errors.New("notifier is not initialized"))
+	}
+
+	n.error = err
+	close(n.doneCh)
+}
+
+// Wait for the operation.
+func (n *Notifier) Wait() error {
+	if n == nil {
+		return nil
+	}
+
+	<-n.doneCh
+	return n.error
+}
