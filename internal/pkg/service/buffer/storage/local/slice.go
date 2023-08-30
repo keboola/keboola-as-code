1a824d7b5942e0979331540764a89a784413769f
Michal Jurečko <michal.jurecko@gmail.com>
2023-08-30 08:00:50 +0200

    Add local.Slice.Filename

--- internal/pkg/service/buffer/storage/local/slice.go
+++ internal/pkg/service/buffer/storage/local/slice.go
@@ -8,7 +8,10 @@ import (
 
 type Slice struct {
 	// Dir defines slice directory in the data volume.
+	// The Dir also contains a lock and other auxiliary files.
 	Dir string `json:"dir" validate:"required"`
+	// Filename of the Slice data file, in the Dir.
+	Filename string `json:"filename" validate:"required"`
 	// IsEmpty is set if the upload was skipped because we did not receive any data.
 	IsEmpty bool `json:"isEmpty,omitempty"`
 	// AllocateSpace when creating the local file.
