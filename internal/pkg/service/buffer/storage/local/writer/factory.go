732c69300bcb768bd6c50cff8fa61fbb3e77c78d
Michal Jurečko <michal.jurecko@gmail.com>
2023-08-31 18:00:34 +0200

    Create local/writer.DefaultFactory

--- internal/pkg/service/buffer/storage/local/writer/factory.go
+++ internal/pkg/service/buffer/storage/local/writer/factory.go
@@ -1,7 +1,20 @@
 package writer
 
 import (
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/local/writer/base"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/local/writer/csv"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 type Factory func(w *base.Writer) (SliceWriter, error)
+
+func DefaultFactory(w *base.Writer) (SliceWriter, error) {
+	// Create writer according to the file type
+	switch w.Type() {
+	case storage.FileTypeCSV:
+		return csv.NewWriter(w)
+	default:
+		return nil, errors.Errorf(`unexpected file type "%s"`, w.Type())
+	}
+}
---
+++ internal/pkg/service/buffer/storage/local/writer/factory_test.go
@@ -0,0 +1,44 @@
+package writer
+
+import (
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/local/writer/csv"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// TestDefaultFactory_FileTypeCSV tests that csv.Writer is created for the storage.FileTypeCSV.
+// Test for csv.Writer itself are in the "csv" package.
+func TestDefaultFactory_FileTypeCSV(t *testing.T) {
+	t.Parallel()
+	tc := newVolumeTestCase(t)
+
+	v, err := tc.OpenVolume(WithWriterFactory(DefaultFactory))
+	assert.NoError(t, err)
+
+	slice := newTestSlice(t)
+	slice.Type = storage.FileTypeCSV
+
+	w, err := v.NewWriterFor(slice)
+	assert.NoError(t, err)
+	assert.NotNil(t, w)
+
+	_, ok := w.(*csv.Writer)
+	assert.True(t, ok)
+}
+
+// TestDefaultFactory_FileTypeInvalid test handling of an invalid file type.
+func TestDefaultFactory_FileTypeInvalid(t *testing.T) {
+	t.Parallel()
+	tc := newVolumeTestCase(t)
+
+	v, err := tc.OpenVolume(WithWriterFactory(DefaultFactory))
+	assert.NoError(t, err)
+
+	slice := newTestSlice(t)
+	slice.Type = "invalid"
+	_, err = v.NewWriterFor(slice)
+	if assert.Error(t, err) {
+		assert.Equal(t, `unexpected file type "invalid"`, err.Error())
+	}
+}
