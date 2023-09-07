63a7e605f6573d88f77d13fbb950e4f020648de8
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-07 15:08:09 +0200

    Add reader.Volumes

---
+++ internal/pkg/service/buffer/storage/local/reader/volumes.go
@@ -0,0 +1,27 @@
+package reader
+
+import (
+	"context"
+
+	"github.com/benbjohnson/clock"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/local/volume"
+)
+
+type baseVolumes = volume.Volumes[*Volume]
+
+type Volumes struct {
+	*baseVolumes
+}
+
+func OpenVolumes(ctx context.Context, logger log.Logger, clock clock.Clock, path string, opts ...Option) (*Volumes, error) {
+	v, err := volume.OpenVolumes(logger, path, func(info volumeInfo) (*Volume, error) {
+		return OpenVolume(ctx, logger, clock, info, opts...)
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Volumes{baseVolumes: v}, nil
+}
