14fc9e8ad5a2ef5562399a557a86fd6b959f6c4b
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-08 00:27:03 +0200

    Update model

--- internal/pkg/service/buffer/storage/local/file.go
+++ internal/pkg/service/buffer/storage/local/file.go
@@ -12,4 +12,18 @@ type File struct {
 	Compression compression.Config `json:"compression"`
 	// Sync writer configuration.
 	Sync disksync.Config `json:"sync"`
+	// Volumes configures the assignment of pod volumes to the File.
+	Volumes VolumesAssignment `json:"volumes"`
+}
+
+type VolumesAssignment struct {
+	// PerPod defines the quantity of volumes simultaneously utilized per pod by the File.
+	// This value also corresponds to the number of slices simultaneously opened per pod and the File.
+	// If the specified number of volumes is unavailable, all available volumes will be used.
+	// With the growing number of volumes, the per pod throughput increases.
+	PerPod int `json:"perPod" validate:"min=1"`
+	// PreferredTypes contains a list of preferred volume types,
+	// the value is used when assigning volumes to the file slices, see writer.Volumes.VolumesFor.
+	// The first value is the most preferred volume type.
+	PreferredTypes []string `json:"prefTypes"`
 }
