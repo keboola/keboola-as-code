9ac074d2cef4fd7503fb08f3a1513dd8a4be3b63
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-11 08:32:02 +0200

    Move buffer/storage/{local,staging,target} -> buffer/storage/level/{local,staging,target}

---
+++ internal/pkg/service/buffer/storage/level/local/writer/base/base_test.go
@@ -0,0 +1,159 @@
+package base_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/benbjohnson/clock"
+	"github.com/c2h5oh/datasize"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/compression"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer/base"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer/disksync"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer/writechain"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/staging"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model/column"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/validator"
+)
+
+func TestBaseWriter(t *testing.T) {
+	t.Parallel()
+
+	logger := log.NewDebugLogger()
+	clk := clock.New()
+	dirPath := t.TempDir()
+	filePath := filepath.Join(dirPath, "file")
+	slice := newTestSlice(t)
+
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
+	assert.NoError(t, err)
+	chain := writechain.New(logger, file)
+	w := base.NewWriter(logger, clk, slice, dirPath, filePath, chain)
+
+	// Test getters
+	assert.Equal(t, logger, w.Logger())
+	assert.Equal(t, slice.SliceKey, w.SliceKey())
+	assert.Equal(t, slice.Columns, w.Columns())
+	assert.NotSame(t, slice.Columns, w.Columns())
+	assert.Equal(t, slice.Type, w.Type())
+	assert.Equal(t, slice.LocalStorage.Compression, w.Compression())
+	assert.Equal(t, dirPath, w.DirPath())
+	assert.Equal(t, filePath, w.FilePath())
+
+	// Test write methods
+	n, notifier, err := w.WriteWithNotify([]byte("123"))
+	assert.Equal(t, 3, n)
+	assert.NoError(t, err)
+	assert.NoError(t, notifier.Wait())
+	n, err = w.Write([]byte("456"))
+	assert.Equal(t, 3, n)
+	assert.NoError(t, err)
+	n, err = w.WriteString("789")
+	w.AddWriteOp(1)
+	assert.Equal(t, 3, n)
+	assert.NoError(t, err)
+	notifier, err = w.DoWithNotify(func() error {
+		_, err := w.Write([]byte("abc"))
+		w.AddWriteOp(1)
+		return err
+	})
+	assert.NoError(t, err)
+	assert.NoError(t, notifier.Wait())
+
+	// Test Close method
+	assert.NoError(t, w.Close())
+
+	// Try Close again
+	err = w.Close()
+	if assert.Error(t, err) {
+		assert.Equal(t, "syncer is already stopped: context canceled", err.Error())
+	}
+
+	// Check file content
+	content, err := os.ReadFile(filePath)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("123456789abc"), content)
+}
+
+func TestBaseWriter_CloseError(t *testing.T) {
+	t.Parallel()
+
+	logger := log.NewDebugLogger()
+	clk := clock.NewMock()
+	dirPath := t.TempDir()
+	filePath := filepath.Join(dirPath, "file")
+	slice := newTestSlice(t)
+
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
+	assert.NoError(t, err)
+	chain := writechain.New(logger, file)
+	w := base.NewWriter(logger, clk, slice, dirPath, filePath, chain)
+
+	w.AppendCloseFn("my-closer", func() error {
+		return errors.New("some error")
+	})
+
+	// Test Close method
+	err = w.Close()
+	if assert.Error(t, err) {
+		assert.Equal(t, "chain close error: cannot close \"my-closer\": some error", err.Error())
+	}
+}
+
+func newTestSlice(tb testing.TB) *storage.Slice {
+	tb.Helper()
+
+	s := &storage.Slice{
+		SliceKey: storage.SliceKey{
+			FileKey: storage.FileKey{
+				ExportKey: key.ExportKey{
+					ReceiverKey: key.ReceiverKey{
+						ProjectID:  123,
+						ReceiverID: "my-receiver",
+					},
+					ExportID: "my-export",
+				},
+				FileID: storage.FileID{
+					OpenedAt: utctime.MustParse("2000-01-01T19:00:00.000Z"),
+				},
+			},
+			SliceID: storage.SliceID{
+				VolumeID: "my-volume",
+				OpenedAt: utctime.MustParse("2000-01-01T19:00:00.000Z"),
+			},
+		},
+		Type:  storage.FileTypeCSV,
+		State: storage.SliceWriting,
+		Columns: column.Columns{
+			column.ID{},
+			column.Headers{},
+			column.Body{},
+		},
+		LocalStorage: local.Slice{
+			Dir:           "my-dir",
+			Filename:      "slice.csv",
+			AllocateSpace: 10 * datasize.KB,
+			Compression:   compression.DefaultNoneConfig(),
+			Sync:          disksync.DefaultConfig(),
+		},
+		StagingStorage: staging.Slice{
+			Path:        "slice.csv",
+			Compression: compression.DefaultNoneConfig(),
+		},
+	}
+
+	// Slice definition must be valid
+	val := validator.New()
+	require.NoError(tb, val.Validate(context.Background(), s))
+	return s
+}
---
+++ internal/pkg/service/buffer/storage/level/local/writer/test/benchmark/benchmark.go
@@ -0,0 +1,168 @@
+package benchmark
+
+import (
+	"context"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/benbjohnson/clock"
+	"github.com/c2h5oh/datasize"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/compression"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/volume"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer/disksync"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/staging"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/store/model/column"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/testhelper"
+	"github.com/keboola/keboola-as-code/internal/pkg/validator"
+)
+
+// WriterBenchmark is a generic benchmark for writer.SliceWriter.
+type WriterBenchmark struct {
+	// Parallelism is number of parallel write operations.
+	Parallelism int
+	FileType    storage.FileType
+	Columns     column.Columns
+	Allocate    datasize.ByteSize
+	Sync        disksync.Config
+	Compression compression.Config
+
+	// DataChFactory must return the channel with table rows, the channel must be closed after the n reads.
+	DataChFactory func(ctx context.Context, n int, g *RandomStringGenerator) <-chan []any
+
+	latencySum   *atomic.Float64
+	latencyCount *atomic.Int64
+}
+
+func (wb *WriterBenchmark) Run(b *testing.B) {
+	b.Helper()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wb.latencySum = atomic.NewFloat64(0)
+	wb.latencyCount = atomic.NewInt64(0)
+
+	// Init random string generator
+	gen := newRandomStringGenerator()
+
+	// Setup logger
+	logger := log.NewDebugLogger()
+	logger.ConnectTo(testhelper.VerboseStdout())
+
+	// Open volume
+	vol, err := writer.OpenVolume(ctx, logger, clock.New(), volume.NewInfo(b.TempDir(), "hdd", "1"))
+	require.NoError(b, err)
+
+	// Create writer
+	sliceWriter, err := vol.NewWriterFor(wb.newSlice(b, vol))
+	require.NoError(b, err)
+
+	// Create data channel
+	dataCh := wb.DataChFactory(ctx, b.N, gen)
+
+	// Run benchmark
+	b.ResetTimer()
+	start := time.Now()
+
+	// Write data in parallel, see Parallelism option.
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// Start wb.Parallelism goroutines
+		for i := 0; i < wb.Parallelism; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				var latencySum float64
+				var latencyCount int64
+
+				// Read from the channel until the N rows are processed, together by all goroutines
+				for row := range dataCh {
+					start := time.Now()
+					assert.NoError(b, sliceWriter.WriteRow(row))
+					latencySum += time.Since(start).Seconds()
+					latencyCount++
+				}
+
+				wb.latencySum.Add(latencySum)
+				wb.latencyCount.Add(latencyCount)
+			}()
+		}
+	}()
+	wg.Wait()
+	end := time.Now()
+
+	// Close volume
+	assert.NoError(b, vol.Close())
+
+	// Report extra metrics
+	duration := end.Sub(start)
+	b.ReportMetric(float64(b.N)/duration.Seconds(), "wr/s")
+	b.ReportMetric(wb.latencySum.Load()/float64(wb.latencyCount.Load())*1000, "ms/wr")
+	b.ReportMetric(sliceWriter.UncompressedSize().MBytes()/duration.Seconds(), "in_MB/s")
+	b.ReportMetric(sliceWriter.CompressedSize().MBytes()/duration.Seconds(), "out_MB/s")
+	b.ReportMetric(float64(sliceWriter.UncompressedSize())/float64(sliceWriter.CompressedSize()), "ratio")
+
+	// Check rows count
+	assert.Equal(b, uint64(b.N), sliceWriter.RowsCount())
+
+	// Check file real size
+	if wb.Compression.Type == compression.TypeNone {
+		assert.Equal(b, sliceWriter.CompressedSize(), sliceWriter.UncompressedSize())
+	}
+	stat, err := os.Stat(sliceWriter.FilePath())
+	assert.NoError(b, err)
+	assert.Equal(b, sliceWriter.CompressedSize(), datasize.ByteSize(stat.Size()))
+}
+
+func (wb *WriterBenchmark) newSlice(b *testing.B, volume *writer.Volume) *storage.Slice {
+	b.Helper()
+
+	openedAt := utctime.From(time.Now())
+	s := &storage.Slice{
+		SliceKey: storage.SliceKey{
+			FileKey: storage.FileKey{
+				ExportKey: key.ExportKey{
+					ReceiverKey: key.ReceiverKey{ProjectID: 123, ReceiverID: "my-receiver"},
+					ExportID:    "my-export",
+				},
+				FileID: storage.FileID{OpenedAt: openedAt},
+			},
+			SliceID: storage.SliceID{VolumeID: volume.ID(), OpenedAt: openedAt},
+		},
+		Type:    wb.FileType,
+		State:   storage.SliceWriting,
+		Columns: wb.Columns,
+		LocalStorage: local.Slice{
+			Dir:           openedAt.String(),
+			Filename:      "slice",
+			AllocateSpace: wb.Allocate,
+			Compression:   wb.Compression,
+			Sync:          wb.Sync,
+		},
+		StagingStorage: staging.Slice{
+			Path:        "slice",
+			Compression: wb.Compression,
+		},
+	}
+
+	// Slice definition must be valid
+	val := validator.New()
+	require.NoError(b, val.Validate(context.Background(), s))
+	return s
+}
