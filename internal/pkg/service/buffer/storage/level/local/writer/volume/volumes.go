90d692030f9efb6c8fa5134d695fe15de5058777
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-14 18:01:44 +0200

    Rename volume.OpenVolumes -> volume.DetectVolumes

--- internal/pkg/service/buffer/storage/level/local/writer/volume/volumes.go
+++ internal/pkg/service/buffer/storage/level/local/writer/volume/volumes.go
@@ -18,8 +18,8 @@ type Volumes struct {
 	*baseVolumes
 }
 
-func OpenVolumes(ctx context.Context, logger log.Logger, clock clock.Clock, path string, opts ...Option) (*Volumes, error) {
-	v, err := volume.OpenVolumes(logger, path, func(info volumeInfo) (*Volume, error) {
+func DetectVolumes(ctx context.Context, logger log.Logger, clock clock.Clock, path string, opts ...Option) (*Volumes, error) {
+	v, err := volume.DetectVolumes(logger, path, func(info volumeInfo) (*Volume, error) {
 		return Open(ctx, logger, clock, info, opts...)
 	})
 	if err != nil {
