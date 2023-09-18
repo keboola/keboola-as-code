1d2e709ffe8a7afbb1495f08bfaeb355e3c554f1
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-18 21:43:34 +0200

    Update volumes

--- internal/pkg/service/buffer/storage/level/local/writer/volume/volumes.go
+++ internal/pkg/service/buffer/storage/level/local/writer/volume/volumes.go
@@ -10,24 +10,31 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/volume"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/level/local/writer"
 )
 
 type baseVolumes = volume.Volumes[*Volume]
 
 type Volumes struct {
 	*baseVolumes
+	events *writer.Events
 }
 
 // DetectVolumes function detects and opens all volumes in the path.
-func DetectVolumes(ctx context.Context, logger log.Logger, clock clock.Clock, path string, opts ...Option) (*Volumes, error) {
-	v, err := volume.DetectVolumes(logger, path, func(info volumeInfo) (*Volume, error) {
-		return Open(ctx, logger, clock, info, opts...)
+func DetectVolumes(ctx context.Context, logger log.Logger, clock clock.Clock, path string, opts ...Option) (out *Volumes, err error) {
+	events := writer.NewEvents()
+	out = &Volumes{events: events}
+	out.baseVolumes, err = volume.DetectVolumes(logger, path, func(info volumeInfo) (*Volume, error) {
+		return Open(ctx, logger, clock, events.Clone(), info, opts...)
 	})
 	if err != nil {
 		return nil, err
 	}
+	return out, nil
+}
 
-	return &Volumes{baseVolumes: v}, nil
+func (v *Volumes) Events() *writer.Events {
+	return v.events
 }
 
 // VolumesFor returns volumes list according to the file settings.
