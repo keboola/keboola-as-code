7be4bba00b8d836a51492b28005365bc7a3983a2
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-20 13:09:43 +0200

    Create delete.go, merge with RollupImportedOnCleanupOp, add test

--- internal/pkg/service/buffer/storage/statistics/repository/repository.go
+++ internal/pkg/service/buffer/storage/statistics/repository/repository.go
@@ -1,14 +1,10 @@
 package repository
 
 import (
-	"context"
-	"fmt"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage"
-
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/serde"
 	"github.com/keboola/keboola-as-code/internal/pkg/telemetry"
-	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 	etcd "go.etcd.io/etcd/client/v3"
 )
 
@@ -45,14 +41,6 @@ func (r *Repository) AtomicProvider() *AtomicProvider {
 	return r.atomicProvider
 }
 
-func (r *Repository) DeleteOp(objectKey fmt.Stringer) op.Op {
-	txn := op.NewTxnOp()
-	for _, level := range storage.AllLevels() {
-		txn.Then(r.schema.InLevel(level).InObject(objectKey).DeleteAll())
-	}
-	return txn
-}
-
 func (r *Repository) MoveOp(ctx context.Context, sliceKey storage.SliceKey, from, to storage.Level, modifyStatsFn func(*Value)) (op.Op, error) {
 	if from == to {
 		panic(errors.Errorf(`from and to categories are same and equal to "%s"`, to))
@@ -71,34 +59,4 @@ func (r *Repository) MoveOp(ctx context.Context, sliceKey storage.SliceKey, from
 	}
 
 	return op.MergeToTxn(fromKey.Delete(), toKey.Put(stats.Value)), nil
-}
-
-func (r *Repository) RollupImportedOnCleanupOp(fileKey storage.FileKey) *op.AtomicOp {
-	fileStatsPfx := r.schema.InLevel(storage.LevelTarget).InFile(fileKey)
-	exportSumKey := r.schema.InLevel(storage.LevelTarget).InExport(fileKey.ExportKey).CleanupSum()
-
-	var sumValue Value
-	var sliceValue Value
-	return op.
-		Atomic().
-		Read(func() op.Op {
-			// Get export sum
-			return exportSumKey.Get().WithOnResult(func(result *op.KeyValueT[Value]) {
-				if result != nil {
-					sumValue = result.Value
-				}
-			})
-		}).
-		Read(func() op.Op {
-			// Get file stats
-			return SumStatsOp(fileStatsPfx.GetAll(), &sliceValue)
-		}).
-		Write(func() op.Op {
-			// Sum both values and save
-			return exportSumKey.Put(sumValue.Add(sliceValue))
-		}).
-		Write(func() op.Op {
-			// Delete file stats
-			return fileStatsPfx.DeleteAll()
-		})
 }
