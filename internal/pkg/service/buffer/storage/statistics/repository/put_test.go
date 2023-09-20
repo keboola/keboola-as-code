d6b17caa558b08b07011e72da656a195ab8526dd
Michal Jurečko <michal.jurecko@gmail.com>
2023-09-20 05:49:30 +0200

    Create put.go, add test

---
+++ internal/pkg/service/buffer/storage/statistics/repository/put_test.go
@@ -0,0 +1,79 @@
+package repository_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/statistics"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/statistics/repository"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/storage/test"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
+)
+
+func TestRepository_Put(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	d := dependencies.NewMocked(t, dependencies.WithEnabledEtcdClient())
+	repo := repository.New(d)
+
+	// Empty
+	assert.NoError(t, repo.Put(ctx, []statistics.PerSlice{}))
+	etcdhelper.AssertKVsString(t, d.EtcdClient(), ``)
+
+	// One
+	assert.NoError(t, repo.Put(ctx, []statistics.PerSlice{
+		{
+			SliceKey: test.NewSliceKeyOpenedAt("2000-01-20T00:00:00.000Z"),
+			Value: statistics.Value{
+				FirstRecordAt:    utctime.MustParse("2000-01-20T00:00:00.000Z"),
+				LastRecordAt:     utctime.MustParse("2000-01-21T00:00:00.000Z"),
+				RecordsCount:     1,
+				UncompressedSize: 1,
+				CompressedSize:   1,
+			},
+		},
+	}))
+	etcdhelper.AssertKVsString(t, d.EtcdClient(), `
+<<<<<
+storage/stats/local/123/my-receiver/my-export/2000-01-01T19:00:00.000Z/my-volume/2000-01-20T00:00:00.000Z/value
+-----
+{
+  "firstRecordAt": "2000-01-20T00:00:00.000Z",
+  "lastRecordAt": "2000-01-21T00:00:00.000Z",
+  "recordsCount": 1,
+  "uncompressedSize": "1B",
+  "compressedSize": "1B"
+}
+>>>>>
+`)
+
+	// Many
+	var records []statistics.PerSlice
+	start := utctime.MustParse("2000-01-21T00:00:00.000Z")
+	for i := 0; i < 150; i++ {
+		openedAt := start.Add(time.Duration(i) * time.Second)
+		records = append(records, statistics.PerSlice{
+			SliceKey: test.NewSliceKeyOpenedAt(openedAt.String()),
+			Value: statistics.Value{
+				FirstRecordAt:    openedAt,
+				LastRecordAt:     openedAt.Add(time.Hour),
+				RecordsCount:     1,
+				UncompressedSize: 1,
+				CompressedSize:   1,
+			},
+		})
+	}
+	assert.Len(t, records, 150)
+	assert.NoError(t, repo.Put(ctx, records))
+	kvs, err := etcdhelper.DumpAll(ctx, d.EtcdClient())
+	assert.NoError(t, err)
+	assert.Len(t, kvs, 151)
+}
