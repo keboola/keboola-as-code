6d3e7145c48da8b29d3c1c3adc3fe77b7655b223
Michal Jurečko <michal.jurecko@gmail.com>
2023-10-31 16:01:19 +0100

    Rename Response -> RawResponse

--- internal/pkg/service/common/etcdop/op/response.go
+++ internal/pkg/service/common/etcdop/op/response.go
@@ -1,13 +1,14 @@
 package op
 
-import (
-	etcd "go.etcd.io/etcd/client/v3"
-)
+import etcd "go.etcd.io/etcd/client/v3"
 
-// Response wraps etcdOpResponse and adds client and options with which the request was made.
-// The Response is used by the MapResponse methods.
-type Response struct {
+type RawResponse struct {
 	etcd.OpResponse
 	Client  etcd.KV
 	Options []Option
+}
+
+func (v RawResponse) SubResponse(response etcd.OpResponse) RawResponse {
+	v.OpResponse = response
+	return v
 }
