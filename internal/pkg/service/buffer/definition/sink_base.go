11be5924fe8ca63af3f49dae9267e71cced3afbd
Michal Jurečko <michal.jurecko@gmail.com>
2023-11-01 06:42:05 +0100

    Add Sink entity

---
+++ internal/pkg/service/buffer/definition/sink_base.go
@@ -0,0 +1,18 @@
+package definition
+
+import (
+	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/definition/key"
+)
+
+type SinkType string
+
+type Sink struct {
+	key.SinkKey
+	Versioned
+	Switchable
+	SoftDeletable
+	Type        SinkType   `json:"type" validate:"required,oneof=table"`
+	Name        string     `json:"name" validate:"required,min=1,max=40"`
+	Description string     `json:"description,omitempty" validate:"max=4096"`
+	Table       *TableSink `json:"table" validate:"required_if=Type table"`
+}
