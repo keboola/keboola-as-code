1549d44cc912c7a5b712f7bfd32a2987d850e347
Michal Jurečko <michal.jurecko@gmail.com>
2023-11-03 09:42:41 +0100

    Add Switchable

---
+++ internal/pkg/service/buffer/definition/switchable_test.go
@@ -0,0 +1,31 @@
+package definition
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+)
+
+func TestSwitchable(t *testing.T) {
+	t.Parallel()
+
+	var v SwitchableInterface
+
+	v = &Switchable{}
+	assert.True(t, v.IsEnabled())
+
+	now := utctime.MustParse("2006-01-02T15:04:05.000Z")
+	v.Disable(now.Time(), "system", "some reason")
+	assert.False(t, v.IsEnabled())
+	assert.Equal(t, "system", v.GetDisabledBy())
+	assert.Equal(t, "2006-01-02T15:04:05.000Z", v.GetDisabledAt().String())
+	assert.Equal(t, "some reason", v.GetDisabledReason())
+
+	v.Enable()
+	assert.True(t, v.IsEnabled())
+	assert.Empty(t, v.GetDisabledBy())
+	assert.Nil(t, v.GetDisabledAt())
+	assert.Empty(t, v.GetDisabledReason())
+}
