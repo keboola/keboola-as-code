30e9dbcd8d31ce84854ef6604ed2d1d611713e0f
Michal Jurečko <michal.jurecko@gmail.com>
2023-11-14 18:47:06 +0100

    Value struct

---
+++ internal/pkg/service/common/configmap/value_test.go
@@ -0,0 +1,19 @@
+package configmap_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/configmap"
+)
+
+func TestValue_IsSet(t *testing.T) {
+	t.Parallel()
+
+	v1 := configmap.Value[string]{Value: "foo", SetBy: configmap.SetByDefault}
+	assert.False(t, v1.IsSet())
+
+	v2 := configmap.Value[string]{Value: "foo", SetBy: configmap.SetByFlag}
+	assert.True(t, v2.IsSet())
+}
