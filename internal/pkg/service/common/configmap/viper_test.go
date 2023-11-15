e7c9b32718b2de102e64c2ce9f2e439d042d9e64
Michal Jurečko <michal.jurecko@gmail.com>
2023-11-15 06:26:12 +0100

    BindToViper function

---
+++ internal/pkg/service/common/configmap/viper_test.go
@@ -0,0 +1,42 @@
+package configmap_test
+
+import (
+	"testing"
+
+	"github.com/keboola/go-utils/pkg/orderedmap"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/env"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/configmap"
+)
+
+func TestBindToViper(t *testing.T) {
+	t.Parallel()
+
+	v := viper.New()
+
+	flagToField := func(flag *pflag.Flag) (orderedmap.Path, bool) {
+		return orderedmap.PathFromStr(flag.Name), true
+	}
+
+	fs := pflag.NewFlagSet("app", pflag.ContinueOnError)
+	fs.Bool("verbose", false, "")
+	fs.String("foo", "foo", "")
+	fs.String("foo-bar", "bar", "")
+	require.NoError(t, fs.Parse([]string{"app", "--foo", "from flag"}))
+
+	envs := env.Empty()
+	envs.Set("MY_APP_FOO_BAR", "from env")
+
+	// Bind
+	setBy, err := configmap.BindToViper(v, fs, flagToField, envs, env.NewNamingConvention("MY_APP_"), nil)
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]configmap.SetBy{
+		"verbose": configmap.SetByDefault,
+		"foo":     configmap.SetByFlag,
+		"foo-bar": configmap.SetByEnv,
+	}, setBy)
+}
