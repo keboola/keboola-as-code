acbe4d332a997c75adeea77fce57d5585bae9796
Jáchym Toušek <jachym.tousek@keboola.com>
2023-12-08 11:45:22 +0100

    refactor: Use contextLogger in defaultbucket

--- internal/pkg/mapper/defaultbucket/local_save.go
+++ internal/pkg/mapper/defaultbucket/local_save.go
@@ -19,7 +19,7 @@ func (m *defaultBucketMapper) MapBeforeLocalSave(ctx context.Context, recipe *mo
 	}
 
 	if err := m.visitStorageInputTables(config, config.GetContent(), m.replaceDefaultBucketWithPlaceholder); err != nil {
-		m.logger.Warnf(`Warning: %s`, err)
+		m.logger.WarnfCtx(ctx, `Warning: %s`, err)
 	}
 	return nil
 }
