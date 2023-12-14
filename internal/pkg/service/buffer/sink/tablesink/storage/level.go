7a886fdbc839b62eb8239f56bd0f939ace962410
Michal Jurečko <michal.jurecko@gmail.com>
2023-12-14 22:29:52 +0100

    style: Rename "ToLevel" method to "Level"

--- internal/pkg/service/buffer/sink/tablesink/storage/level.go
+++ internal/pkg/service/buffer/sink/tablesink/storage/level.go
@@ -24,8 +24,8 @@ func (l Level) String() string {
 	return string(l)
 }
 
-// ToLevel gets the lowest storage.Level at which at least one file slice is present.
-func (s FileState) ToLevel() Level {
+// Level gets the lowest storage.Level at which at least one file slice is present.
+func (s FileState) Level() Level {
 	switch s {
 	case FileWriting, FileClosing:
 		return LevelLocal
@@ -38,8 +38,8 @@ func (s FileState) ToLevel() Level {
 	}
 }
 
-// ToLevel gets the storage.Level at which the slice is present.
-func (s SliceState) ToLevel() Level {
+// Level gets the storage.Level at which the slice is present.
+func (s SliceState) Level() Level {
 	switch s {
 	case SliceWriting, SliceClosing, SliceUploading:
 		return LevelLocal
