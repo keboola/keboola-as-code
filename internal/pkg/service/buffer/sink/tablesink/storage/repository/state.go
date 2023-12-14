8128fb1e13c950b8c4d1f391c29d95d89acbc4d1
Michal Jurečko <michal.jurecko@gmail.com>
2023-12-14 23:29:58 +0100

    style: Simplify code

--- internal/pkg/service/buffer/sink/tablesink/storage/repository/state.go
+++ internal/pkg/service/buffer/sink/tablesink/storage/repository/state.go
@@ -8,34 +8,28 @@ import (
 
 // validateFileAndSliceStates validates combination of file and slice state.
 func validateFileAndSliceStates(fileState storage.FileState, sliceState storage.SliceState) error {
-	valid := false
-
 	switch fileState {
 	case storage.FileWriting, storage.FileClosing:
 		// Check allowed states
 		switch sliceState {
 		case storage.SliceWriting, storage.SliceClosing, storage.SliceUploading, storage.SliceUploaded:
-			valid = true
+			return nil
 		}
 	case storage.FileImporting:
 		// Slice must be uploaded
 		if sliceState == storage.SliceUploaded {
-			valid = true
+			return nil
 		}
 	case storage.FileImported:
 		// Slice must be marked as imported
 		if sliceState == storage.SliceImported {
-			valid = true
+			return nil
 		}
 	default:
 		panic(errors.Errorf(`unexpected file state "%s`, fileState))
 	}
 
-	if !valid {
-		return serviceError.NewBadRequestError(
-			errors.Errorf(`unexpected combination: file state "%s" and slice state "%s"`, fileState, sliceState),
-		)
-	}
-
-	return nil
+	return serviceError.NewBadRequestError(
+		errors.Errorf(`unexpected combination: file state "%s" and slice state "%s"`, fileState, sliceState),
+	)
 }
