571590346e0578f15f5f0ce167cee08c4e9f80c2
Jáchym Toušek <jachym.tousek@keboola.com>
2023-12-18 11:53:07 +0100

    feat: Add ctxattr.ZapFields

--- internal/pkg/ctxattr/ctxattr.go
+++ internal/pkg/ctxattr/ctxattr.go
@@ -4,15 +4,18 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/attribute"
+	"go.uber.org/zap"
 )
 
 type ctxKey string
 
 const (
 	contextAttributes = ctxKey("contextAttributes")
+	contextZapFields  = ctxKey("contextZapFields")
 )
 
 func ContextWith(ctx context.Context, attributes ...attribute.KeyValue) context.Context {
+	// Add attributes to context.
 	var newAttributes []attribute.KeyValue
 	value := ctx.Value(contextAttributes)
 	if value != nil {
@@ -23,8 +26,15 @@ func ContextWith(ctx context.Context, attributes ...attribute.KeyValue) context.
 	}
 
 	set := attribute.NewSet(newAttributes...)
+	ctx = context.WithValue(ctx, contextAttributes, &set)
 
-	return context.WithValue(ctx, contextAttributes, &set)
+	// Add attributes as zap fields for logger.
+	zapFields := make([]zap.Field, set.Len())
+	for i, keyValue := range set.ToSlice() {
+		zapFields[i] = convertAttributeToZapField(keyValue)
+	}
+
+	return context.WithValue(ctx, contextZapFields, zapFields)
 }
 
 func Attributes(ctx context.Context) *attribute.Set {
@@ -34,4 +44,39 @@ func Attributes(ctx context.Context) *attribute.Set {
 	}
 
 	return attribute.EmptySet()
+}
+
+func ZapFields(ctx context.Context) []zap.Field {
+	value := ctx.Value(contextZapFields)
+	if value != nil {
+		return value.([]zap.Field)
+	}
+
+	return []zap.Field{}
+}
+
+func convertAttributeToZapField(keyValue attribute.KeyValue) zap.Field {
+	key := string(keyValue.Key)
+	value := keyValue.Value
+
+	switch value.Type() {
+	case attribute.BOOL:
+		return zap.Bool(key, value.AsBool())
+	case attribute.BOOLSLICE:
+		return zap.Bools(key, value.AsBoolSlice())
+	case attribute.INT64:
+		return zap.Int64(key, value.AsInt64())
+	case attribute.INT64SLICE:
+		return zap.Int64s(key, value.AsInt64Slice())
+	case attribute.FLOAT64:
+		return zap.Float64(key, value.AsFloat64())
+	case attribute.FLOAT64SLICE:
+		return zap.Float64s(key, value.AsFloat64Slice())
+	case attribute.STRING:
+		return zap.String(key, value.AsString())
+	case attribute.STRINGSLICE:
+		return zap.Strings(key, value.AsStringSlice())
+	default:
+		return zap.Any(key, value.AsInterface())
+	}
 }
--- internal/pkg/ctxattr/ctxattr_test.go
+++ internal/pkg/ctxattr/ctxattr_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/attribute"
+	"go.uber.org/zap"
 )
 
 func TestContextAttributes(t *testing.T) {
@@ -15,14 +16,17 @@ func TestContextAttributes(t *testing.T) {
 	ctx := context.Background()
 
 	set := Attributes(ctx)
-
 	_, ok := set.Value("isValid")
 	assert.False(t, ok)
+
+	fields := ZapFields(ctx)
+	assert.Empty(t, fields)
 
 	ctx = ContextWith(ctx, attribute.Bool("isValid", true), attribute.String("status", "success"))
 	ctx = ContextWith(ctx, attribute.Bool("isValid", false), attribute.Int("count", 5))
 
 	set = Attributes(ctx)
+	assert.Equal(t, 3, set.Len())
 
 	value, ok := set.Value("isValid")
 	require.True(t, ok)
@@ -35,4 +39,11 @@ func TestContextAttributes(t *testing.T) {
 	value, ok = set.Value("count")
 	require.True(t, ok)
 	assert.Equal(t, "5", value.Emit())
+
+	fields = ZapFields(ctx)
+	assert.Len(t, fields, 3)
+
+	assert.True(t, fields[0].Equals(zap.Int64("count", 5)))
+	assert.True(t, fields[1].Equals(zap.Bool("isValid", false)))
+	assert.True(t, fields[2].Equals(zap.String("status", "success")))
 }
