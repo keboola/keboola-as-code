c9a1552f63f4c21327756de033613f5b55c7dd57
Jáchym Toušek <jachym.tousek@keboola.com>
2024-01-08 12:20:27 +0100

    test: Fix failed test

--- internal/pkg/service/cli/cmd/cmd_test.go
+++ internal/pkg/service/cli/cmd/cmd_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/keboola/go-utils/pkg/wildcards"
 	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 
@@ -172,7 +173,10 @@ func TestTearDown_Panic(t *testing.T) {
 	root.logger = logger
 	exitCode := root.tearDown(0, errors.New("panic error"))
 	assert.Equal(t, 1, exitCode)
-	expected := `INFO  
+	expected := `
+DEBUG  Unexpected panic: panic error
+%A
+INFO  
 ---------------------------------------------------
 Keboola CLI had a problem and crashed.
 
@@ -186,7 +190,7 @@ We take privacy seriously, and do not perform any automated log file collection.
 
 Thank you kindly!
 `
-	assert.Equal(t, expected, logger.AllMessagesTxt())
+	wildcards.Assert(t, expected, logger.AllMessagesTxt())
 }
 
 func TestGetLogFileTempFile(t *testing.T) {
