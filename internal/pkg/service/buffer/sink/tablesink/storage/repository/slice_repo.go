af77601ada00e605efd958eb556785a1e79bfa26
Michal Jurečko <michal.jurecko@gmail.com>
2024-01-09 17:40:35 +0100

    style: New slice_new.go file

--- internal/pkg/service/buffer/sink/tablesink/storage/repository/slice_repo.go
+++ internal/pkg/service/buffer/sink/tablesink/storage/repository/slice_repo.go
@@ -246,34 +246,3 @@ func (r *SliceRepository) updateAllInFile(parentKey storage.FileKey, updateFn fu
 			return nil, nil
 		})
 }
-
-// newSlice creates slice definition.
-func newSlice(now time.Time, file storage.File, volumeID storage.VolumeID, prevSliceSize datasize.ByteSize) (s storage.Slice, err error) {
-	// Validate compression type.
-	// Other parts of the system are also prepared for other types of compression,
-	// but now only GZIP is supported in the Keboola platform.
-	switch file.LocalStorage.Compression.Type {
-	case compression.TypeNone, compression.TypeGZIP: // ok
-	default:
-		return storage.Slice{}, errors.Errorf(`file compression type "%s" is not supported`, file.LocalStorage.Compression.Type)
-	}
-
-	// Convert path separator, on Windows
-	sliceKey := storage.SliceKey{FileKey: file.FileKey, SliceID: storage.SliceID{VolumeID: volumeID, OpenedAt: utctime.From(now)}}
-	sliceDir := filepath.FromSlash(sliceKey.SliceID.OpenedAt.String()) //nolint: forbidigo
-
-	// Generate unique staging storage path
-	stagingPath := fmt.Sprintf(`%s_%s`, sliceKey.OpenedAt().String(), sliceKey.VolumeID)
-
-	s.SliceKey = sliceKey
-	s.Type = file.Type
-	s.State = storage.SliceWriting
-	s.Columns = file.Columns
-	if s.LocalStorage, err = file.LocalStorage.NewSlice(sliceDir, prevSliceSize); err != nil {
-		return storage.Slice{}, err
-	}
-	if s.StagingStorage, err = file.StagingStorage.NewSlice(stagingPath, s.LocalStorage); err != nil {
-		return storage.Slice{}, err
-	}
-	return s, nil
-}
--- internal/pkg/service/buffer/sink/tablesink/storage/repository/slice_repo_test.go
+++ internal/pkg/service/buffer/sink/tablesink/storage/repository/slice_repo_test.go
@@ -15,12 +15,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/definition"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/definition/column"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/definition/key"
 	defRepository "github.com/keboola/keboola-as-code/internal/pkg/service/buffer/definition/repository"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/sink/tablesink/storage"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/sink/tablesink/storage/compression"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/buffer/sink/tablesink/storage/test"
 	deps "github.com/keboola/keboola-as-code/internal/pkg/service/common/dependencies"
 	serviceError "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
@@ -441,43 +438,4 @@ storage/slice/level/target/123/456/my-source/my-sink-1/2000-01-03T02:00:00.000Z/
 }
 >>>>>
 `
-}
-
-func TestNewSlice_InvalidCompressionType(t *testing.T) {
-	t.Parallel()
-
-	// Fixtures
-	now := utctime.MustParse("2000-01-01T19:00:00.000Z").Time()
-	projectID := keboola.ProjectID(123)
-	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 456}
-	sourceKey := key.SourceKey{BranchKey: branchKey, SourceID: "my-source"}
-	sinkKey := key.SinkKey{SourceKey: sourceKey, SinkID: "my-sink-1"}
-	fileKey := storage.FileKey{SinkKey: sinkKey, FileID: storage.FileID{OpenedAt: utctime.From(now)}}
-	cfg := storage.NewConfig()
-	mapping := definition.TableMapping{
-		TableID: keboola.MustParseTableID("in.bucket.table"),
-		Columns: column.Columns{
-			column.Datetime{Name: "datetime"},
-			column.Body{Name: "body"},
-		},
-	}
-	credentials := &keboola.FileUploadCredentials{
-		S3UploadParams: &s3.UploadParams{
-			Credentials: s3.Credentials{
-				Expiration: iso8601.Time{Time: now.Add(time.Hour)},
-			},
-		},
-	}
-
-	// Create file
-	file, err := newFile(fileKey, cfg, mapping, credentials)
-	require.NoError(t, err)
-
-	// Set unsupported compression type
-	file.LocalStorage.Compression.Type = compression.TypeZSTD
-
-	// Assert
-	_, err = newSlice(now, file, "my-volume", 0)
-	require.Error(t, err)
-	assert.Equal(t, `file compression type "zstd" is not supported`, err.Error())
 }
