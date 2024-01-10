9b6fa954707b2c9f140c49f36cf428fa2ef7231e
Jáchym Toušek <jachym.tousek@keboola.com>
2024-01-10 10:06:56 +0100

    feat: Add span id and trace id attributes

--- internal/pkg/telemetry/ddtracer.go
+++ internal/pkg/telemetry/ddtracer.go
@@ -2,8 +2,13 @@ package telemetry
 
 import (
 	"context"
+	"strconv"
 
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/ctxattr"
 )
 
 type wrappedDDTracer struct {
@@ -15,8 +20,17 @@ type wrappedDDTracer struct {
 func (t *wrappedDDTracer) Start(parentCtx context.Context, spanName string, opts ...trace.SpanStartOption) (ctx context.Context, span trace.Span) {
 	ctx, span = t.Tracer.Start(parentCtx, spanName, opts...)
 	if span != nil {
-		span = &wrappedDDSpan{Span: span, tp: t.tp}
-		ctx = trace.ContextWithSpan(ctx, span)
+		if ddspan, ok := span.(ddtrace.Span); ok {
+			spanCtx := ddspan.Context()
+			ctx = ctxattr.ContextWith(
+				ctx,
+				attribute.String("dd.trace_id", strconv.FormatUint(spanCtx.TraceID(), 10)),
+				attribute.String("dd.span_id", strconv.FormatUint(spanCtx.SpanID(), 10)),
+			)
+
+			span = &wrappedDDSpan{Span: span, tp: t.tp}
+			ctx = trace.ContextWithSpan(ctx, span)
+		}
 	}
 	return ctx, span
 }
---
+++ internal/pkg/telemetry/ddtracer_test.go
@@ -0,0 +1,59 @@
+package telemetry
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/trace"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/mocktracer"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/ctxattr"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/servicectx"
+)
+
+func TestContextAttributes(t *testing.T) {
+	t.Parallel()
+
+	mockTracer := mocktracer.Start()
+	defer mockTracer.Stop()
+
+	logger := log.NewDebugLogger()
+
+	proc, err := servicectx.New(servicectx.WithLogger(logger), servicectx.WithUniqueID("mockid"))
+	require.NoError(t, err)
+
+	// Setup telemetry
+	tel, err := New(
+		func() (trace.TracerProvider, error) {
+			return NewDDTracerProvider(logger, proc), nil
+		},
+		nil,
+	)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	ctx, span := tel.Tracer().Start(ctx, "keboola.go.test")
+	defer span.End(nil)
+
+	traceID, ok := ctxattr.Attributes(ctx).Value("dd.trace_id")
+	assert.True(t, ok)
+
+	spanID, ok := ctxattr.Attributes(ctx).Value("dd.span_id")
+	assert.True(t, ok)
+
+	spans := mockTracer.OpenSpans()
+	assert.Len(t, spans, 1)
+
+	assert.Equal(t, fmt.Sprint(spans[0].TraceID()), traceID.Emit())
+	assert.Equal(t, fmt.Sprint(spans[0].SpanID()), spanID.Emit())
+
+	logger.InfoCtx(ctx, "test")
+
+	expected := `{"level":"info","message":"test","dd.trace_id":"%s","dd.span_id":"%s"}`
+	log.AssertJSONMessages(t, expected, logger.AllMessages())
+}
