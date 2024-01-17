4a01dbd5d54ab8bc8b6ae28d38ce1e20455598b3
Michal Jurečko <michal.jurecko@gmail.com>
2024-01-17 22:15:44 +0100

    feat: Append attributes from the context to the spDn

--- internal/pkg/telemetry/ddtracer.go
+++ internal/pkg/telemetry/ddtracer.go
@@ -18,19 +18,29 @@ type wrappedDDTracer struct {
 
 // Start method wraps underlying DD span to wrappedDDSpan, see details there.
 func (t *wrappedDDTracer) Start(parentCtx context.Context, spanName string, opts ...trace.SpanStartOption) (ctx context.Context, span trace.Span) {
+	// Create span
 	ctx, span = t.Tracer.Start(parentCtx, spanName, opts...)
-	if span != nil {
-		if ddspan, ok := span.(ddtrace.Span); ok {
-			spanCtx := ddspan.Context()
-			ctx = ctxattr.ContextWith(
-				ctx,
-				attribute.String("dd.trace_id", strconv.FormatUint(spanCtx.TraceID(), 10)),
-				attribute.String("dd.span_id", strconv.FormatUint(spanCtx.SpanID(), 10)),
-			)
-
-			span = &wrappedDDSpan{Span: span, tp: t.tp}
-			ctx = trace.ContextWithSpan(ctx, span)
-		}
+	if span == nil {
+		return ctx, span
 	}
+
+	// Add TraceID and SpanID attributes for logs
+	if ddspan, ok := span.(ddtrace.Span); ok {
+		spanCtx := ddspan.Context()
+		ctx = ctxattr.ContextWith(
+			ctx,
+			attribute.String("dd.trace_id", strconv.FormatUint(spanCtx.TraceID(), 10)),
+			attribute.String("dd.span_id", strconv.FormatUint(spanCtx.SpanID(), 10)),
+		)
+
+		span = &wrappedDDSpan{Span: span, tp: t.tp}
+		ctx = trace.ContextWithSpan(ctx, span)
+	}
+
+	// Append attributes from the context
+	if attrs := ctxattr.Attributes(ctx).ToSlice(); len(attrs) > 0 {
+		span.SetAttributes(attrs...)
+	}
+
 	return ctx, span
 }
