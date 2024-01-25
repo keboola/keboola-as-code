b1664a23164219d27065c7b025120c28b871fc0b
Michal Jurečko <michal.jurecko@gmail.com>
2024-01-25 22:51:24 +0100

    tests: Test Visit on a nil pointer

---
+++ internal/pkg/service/common/configmap/visit_test.go
@@ -0,0 +1,51 @@
+package configmap
+
+import (
+	"github.com/keboola/go-utils/pkg/orderedmap"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"reflect"
+	"testing"
+)
+
+type VisitStruct struct {
+	String  Value[CustomStringType]
+	Int     Value[CustomIntType]
+	Nested1 Nested
+	Nested2 *Nested
+}
+
+func TestVisit(t *testing.T) {
+	t.Parallel()
+
+	var keys []string
+	err := Visit(reflect.ValueOf(VisitStruct{}), VisitConfig{
+		OnField: func(field reflect.StructField, path orderedmap.Path) (fieldName string, ok bool) {
+			return field.Name, true
+		},
+		OnValue: func(vc *VisitContext) error {
+			keys = append(keys, vc.MappedPath.String())
+			return nil
+		},
+	})
+
+	require.NoError(t, err)
+	assert.Equal(t, []string{
+		"",
+		"String",
+		"String.Value",
+		"String.SetBy",
+		"Int",
+		"Int.Value",
+		"Int.SetBy",
+		"Nested1",
+		"Nested1.Ignored",
+		"Nested1.Foo",
+		"Nested1.Bar",
+		// Visit function visits also empty pointers, see Nested2 field
+		"Nested2",
+		"Nested2.Ignored",
+		"Nested2.Foo",
+		"Nested2.Bar",
+	}, keys)
+}
