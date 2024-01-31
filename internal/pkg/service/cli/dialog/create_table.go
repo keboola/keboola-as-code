e4857039c8ccc513573c731580ba82e53b9dffe0
hosan <hosanp11@gmail.com>
2024-01-31 10:08:04 +0100

    feat: updated dialog for definition column types

--- internal/pkg/service/cli/dialog/create_table.go
+++ internal/pkg/service/cli/dialog/create_table.go
@@ -2,6 +2,8 @@ package dialog
 
 import (
 	"encoding/json"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"gopkg.in/yaml.v3"
 	"os"
 	"strings"
 
@@ -11,9 +13,19 @@ import (
 	"github.com/keboola/keboola-as-code/pkg/lib/operation/project/remote/create/table"
 )
 
+const (
+	columnsNamesFlag columnsDefinitionMethod = iota
+	columnsNamesInteractive
+	columnsDefinitionFlag
+	columnsDefinitionInteractive
+)
+
+type columnsDefinitionMethod int
+
 func (p *Dialogs) AskCreateTable(args []string, branchKey keboola.BranchKey, allBuckets []*keboola.Bucket) (table.Options, error) {
 	opts := table.Options{}
 
+	// Table ID and name
 	if len(args) == 1 {
 		tableID, err := keboola.ParseTableID(args[0])
 		if err != nil {
@@ -34,43 +46,101 @@ func (p *Dialogs) AskCreateTable(args []string, branchKey keboola.BranchKey, all
 			name, _ = p.Ask(&prompt.Question{
 				Label:       "Table name",
 				Description: "Enter the table name.",
+				Validator:   prompt.ValueRequired,
 			})
 		}
 		opts.CreateTableRequest.Name = name
 	}
 
-	columnsStr := p.options.GetString(`columns`)
-	if !p.options.IsSet(`columns`) {
-		columnsStr, _ = p.Ask(&prompt.Question{
-			Label:       "Columns",
-			Description: "Enter a comma-separated list of column names.",
-		})
+	// Columns
+	columnsMethod, err := p.columnsDefinitionMethod()
+	if err != nil {
+		return opts, err
 	}
-	colNames := strings.Split(strings.TrimSpace(columnsStr), ",")
 
+	switch columnsMethod {
+	case columnsNamesFlag:
+		columnsStr := p.options.GetString(`columns`)
+		colNames := strings.Split(strings.TrimSpace(columnsStr), ",")
+		opts.CreateTableRequest.Columns = getOptionCreateRequest(colNames)
+	case columnsDefinitionFlag:
+		filePath := p.options.GetString("columns-from")
+		columnsDefinition, err := parseJSONInputForCreateTable(filePath)
+		if err != nil {
+			return table.Options{}, err
+		}
+		opts.CreateTableRequest.Columns = columnsDefinition
+	case columnsNamesInteractive:
+		columnsStr := p.options.GetString(`columns`)
+		if !p.options.IsSet(`columns`) {
+			columnsStr, _ = p.Ask(&prompt.Question{
+				Label:       "Columns",
+				Description: "Enter a comma-separated list of column names.",
+			})
+		}
+		colNames := strings.Split(strings.TrimSpace(columnsStr), ",")
+		opts.CreateTableRequest.Columns = getOptionCreateRequest(colNames)
+	case columnsDefinitionInteractive:
+		input, _ := p.Editor("yaml", &prompt.Question{
+			Label:       "Columns definition from file",
+			Description: "Columns definition from file",
+			Default:     p.defaultValue(),
+			Validator: func(val any) error {
+				_, err := parseColumnsDefinitionFromFile(val.(string))
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		})
+		res, err := parseColumnsDefinitionFromFile(input)
+		if err != nil {
+			return table.Options{}, err
+		}
+		opts.CreateTableRequest.Columns = res
+	default:
+		panic(errors.New("unexpected state"))
+	}
+
+	// Primary keys
 	if p.options.IsSet(`primary-key`) {
 		opts.CreateTableRequest.PrimaryKeyNames = strings.Split(strings.TrimSpace(p.options.GetString(`primary-key`)), ",")
 	} else {
 		primaryKey, _ := p.MultiSelect(&prompt.MultiSelect{
 			Label:   "Select columns for primary key",
-			Options: colNames,
+			Options: getColumnsName(opts.CreateTableRequest.Columns),
 		})
 		opts.CreateTableRequest.PrimaryKeyNames = primaryKey
 	}
 
-	filePath := p.options.GetString("columns-from")
-	if p.options.IsSet("columns-from") {
-		columnsDefinition, err := parseJSONInputForCreateTable(filePath)
-		if err != nil {
-			return table.Options{}, err
-		}
-
-		opts.CreateTableRequest.Columns = columnsDefinition
-	} else {
-		opts.CreateTableRequest.Columns = getOptionCreateRequest(strings.Split(strings.TrimSpace(columnsStr), ","))
-	}
-
 	return opts, nil
+}
+
+func (p *Dialogs) columnsDefinitionMethod() (columnsDefinitionMethod, error) {
+	switch {
+	case !p.options.IsSet("columns-from") && !p.options.IsSet("columns"):
+		// Ask for method
+		specifyTypes := p.Prompt.Confirm(&prompt.Confirm{
+			Label:       "Columns Types Definition",
+			Description: "Would you define column types?",
+			Default:     true,
+			Help:        `"In Keboola you can define column types, if types won't be specified default value will be "string"`,
+		})
+		if specifyTypes {
+			return columnsDefinitionInteractive, nil
+		} else {
+			return columnsNamesInteractive, nil
+		}
+	case p.options.IsSet("columns-from") && p.options.IsSet("columns"):
+		// Only one flag can be specified at the same time
+		return 0, errors.New("can't be specified both flag together, use only one of them")
+	case p.options.IsSet("columns"):
+		return columnsNamesFlag, nil
+	case p.options.IsSet("columns-from"):
+		return columnsDefinitionFlag, nil
+	default:
+		panic(errors.New("unexpected state"))
+	}
 }
 
 func parseJSONInputForCreateTable(filePath string) ([]keboola.Column, error) {
@@ -100,4 +170,48 @@ func getOptionCreateRequest(columns []string) []keboola.Column {
 	}
 
 	return c
+}
+
+func parseColumnsDefinitionFromFile(input string) ([]keboola.Column, error) {
+	var result []keboola.Column
+
+	err := yaml.Unmarshal([]byte(input), &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
+
+func (d *Dialogs) defaultValue() string {
+	fileHeader :=
+		`#Command "remote create table"
+
+#The CLI command creates a new storage table, and the table can be defined either by specifying the full table ID in the argument or by using the --bucket and --name flags.
+#In addition, you can specify column names and specify the primary key using the appropriate flags.
+
+#Using the --columns-from flag to specify a yaml definition file with columns types.
+
+#Another way to define columns is to use the --columns-from flag and then specify the path to your json file.
+
+
+#Example definition file for --columns-from flag:
+
+- name: id
+  definition:
+    type: VARCHAR
+  basetype: STRING
+- name: name
+  definition:
+    type: VARCHAR
+  basetype: STRING
+`
+	return fileHeader
+}
+
+func getColumnsName(columns []keboola.Column) []string {
+	var result []string
+	for _, column := range columns {
+		result = append(result, column.Name)
+	}
+	return result
 }
