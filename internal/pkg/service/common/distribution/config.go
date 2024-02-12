58fd3a6491b64835f04c196ae9ec1d31281916b7
Michal Jurečko <michal.jurecko@gmail.com>
2024-02-12 08:44:51 +0100

    fix: Typo

--- internal/pkg/service/common/distribution/config.go
+++ internal/pkg/service/common/distribution/config.go
@@ -6,15 +6,14 @@ import (
 
 type Config struct {
 	// StartupTimeout configures timeout for the node registration to the cluster.
-	StartupTimeout time.Duration `configkey:"startupTimeout" configusage:"Timeout for the node registration to the cluster." validate:"required,minDuration=1s,maxDuration=5m"`
+	StartupTimeout time.Duration `configKey:"startupTimeout" configUsage:"Timeout for the node registration to the cluster." validate:"required,minDuration=1s,maxDuration=5m"`
 	// ShutdownTimeout configures timeout for the node un-registration from the cluster.
-	ShutdownTimeout time.Duration `configkey:"shutdownTimeout" configusage:"Timeout for the node un-registration from the cluster." validate:"required,minDuration=1s,maxDuration=5m"`
+	ShutdownTimeout time.Duration `configKey:"shutdownTimeout" configUsage:"Timeout for the node un-registration from the cluster." validate:"required,minDuration=1s,maxDuration=5m"`
 	// EventsGroupInterval configures how often changes in the cluster topology are processed.
-	// All changes in the interval are grouped together, so that updates do not occur too often.
-	// Use 0 to disable the feature.
-	EventsGroupInterval time.Duration `configkey:"eventsGroupInterval" configusage:"Interval of processing changes in the topology."  validate:"maxDuration=30s"`
+	// All changes in the interval are grouped together, so that updates do not occur too often. Use 0 to disable the grouping.
+	EventsGroupInterval time.Duration `configKey:"eventsGroupInterval" configUsage:"Interval of processing changes in the topology. Use 0 to disable the grouping."  validate:"maxDuration=30s"`
 	// TTLSeconds configures the number seconds after which the node is automatically un-registered if an outage occurs.
-	TTLSeconds int `configkey:"ttlSeconds" configusage:"Seconds after which the node is automatically un-registered if an outage occurs."  validate:"required,minDuration=1s,maxDuration=30s"`
+	TTLSeconds int `configKey:"ttlSeconds" configUsage:"Seconds after which the node is automatically un-registered if an outage occurs."  validate:"required,minDuration=1s,maxDuration=30s"`
 }
 
 func NewConfig() Config {
