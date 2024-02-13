de2ee8425b1c471dac26321d9fc785fd7ad9faf4
hosan <hosanp11@gmail.com>
2024-02-13 12:33:18 +0100

    delete: removed cliconfig folder

--- internal/pkg/service/common/cliconfig/doc.go
+++
@@ -1,6 +0,0 @@
-// Package cliconfig provides functionality for generating flags from a config structure,
-// and mapping flags and environment variables to the config structure.
-// This package is based on the [Viper] library.
-//
-// [Viper]: https://github.com/spf13/viper
-package cliconfig
