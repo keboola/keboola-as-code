59b83fa0ee2ef4ba86429a827809d3c92484a8f6
hosan <hosanp11@gmail.com>
2024-02-16 10:56:11 +0100

    feat: sync diff command

--- internal/pkg/service/cli/cmd/sync/cmd.go
+++ internal/pkg/service/cli/cmd/sync/cmd.go
@@ -3,6 +3,9 @@ package sync
 import (
 	"github.com/spf13/cobra"
 
+	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/cmd/sync/diff"
+	syncInit "github.com/keboola/keboola-as-code/internal/pkg/service/cli/cmd/sync/init"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/cmd/sync/pull"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/helpmsg"
 )
@@ -14,10 +17,10 @@ func Commands(p dependencies.Provider) *cobra.Command {
 		Long:  helpmsg.Read(`sync/long`),
 	}
 	cmd.AddCommand(
-		InitCommand(p),
-		PullCommand(p),
-		PushCommand(p),
-		DiffCommand(p),
+		syncInit.InitCommand(p),
+		pull.PullCommand(p),
+		pull.PushCommand(p),
+		diff.Command(p),
 	)
 	return cmd
 }
--- internal/pkg/service/cli/cmd/sync/pull/pull.go
+++ internal/pkg/service/cli/cmd/sync/pull/pull.go
@@ -1,4 +1,4 @@
-package sync
+package pull
 
 import (
 	"time"
