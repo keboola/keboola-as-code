f11632bcdb6090b96e6111a01496811db992578f
hosan <hosanp11@gmail.com>
2024-02-19 13:46:35 +0100

    feat: profile command

--- internal/pkg/service/cli/cmd/dbt/generate/profile/cmd.go
+++ internal/pkg/service/cli/cmd/dbt/generate/profile/cmd.go
@@ -1,14 +1,20 @@
-package generate
+package profile
 
 import (
+	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/cmd/utils"
 	"github.com/spf13/cobra"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/helpmsg"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/configmap"
 	"github.com/keboola/keboola-as-code/pkg/lib/operation/dbt/generate/profile"
 )
 
-func ProfileCommand(p dependencies.Provider) *cobra.Command {
+type Flag struct {
+	TargetName configmap.Value[string] `configKey:"target-name" configShorthand:"T" configUsage:"target name of the profile"`
+}
+
+func Command(p dependencies.Provider) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   `profile`,
 		Short: helpmsg.Read(`dbt/generate/profile/short`),
@@ -19,20 +25,25 @@ func ProfileCommand(p dependencies.Provider) *cobra.Command {
 				return err
 			}
 
-			// Ask options
-			targetName, err := p.BaseScope().Dialogs().AskTargetName()
-			if err != nil {
+			// Get dependencies
+			d := p.BaseScope()
+
+			f := Flag{}
+			if err := configmap.Bind(utils.GetBindConfig(cmd.Flags(), args), &f); err != nil {
 				return err
 			}
 
-			// Get dependencies
-			d := p.BaseScope()
+			// Ask options
+			targetName, err := p.BaseScope().Dialogs().AskTargetName(f.TargetName)
+			if err != nil {
+				return err
+			}
 
 			return profile.Run(cmd.Context(), profile.Options{TargetName: targetName}, d)
 		},
 	}
 
-	cmd.Flags().StringP("target-name", "T", "", "target name of the profile")
+	configmap.MustGenerateFlags(cmd.Flags(), Flag{})
 
 	return cmd
 }
