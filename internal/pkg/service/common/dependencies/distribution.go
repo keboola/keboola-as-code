052fd93d2126ed0480dc3a9140fc7b1452099522
Michal Jurečko <michal.jurecko@gmail.com>
2024-02-19 19:39:45 +0100

    feat: Make distribution config modifiable

--- internal/pkg/service/common/dependencies/distribution.go
+++ internal/pkg/service/common/dependencies/distribution.go
@@ -18,15 +18,15 @@ type distributionScopeDeps interface {
 	TaskScope
 }
 
-func NewDistributionScope(ctx context.Context, nodeID string, d distributionScopeDeps) (DistributionScope, error) {
-	return newDistributionScope(ctx, nodeID, d)
+func NewDistributionScope(ctx context.Context, nodeID string, cfg distributionPkg.Config, d distributionScopeDeps) (DistributionScope, error) {
+	return newDistributionScope(ctx, nodeID, cfg, d)
 }
 
-func newDistributionScope(ctx context.Context, nodeID string, d distributionScopeDeps) (v *distributionScope, err error) {
+func newDistributionScope(ctx context.Context, nodeID string, cfg distributionPkg.Config, d distributionScopeDeps) (v *distributionScope, err error) {
 	ctx, span := d.Telemetry().Tracer().Start(ctx, "keboola.go.common.dependencies.NewDistributionScope")
 	defer span.End(&err)
 
-	node := distributionPkg.NewNode(nodeID, distributionPkg.NewConfig(), d)
+	node := distributionPkg.NewNode(nodeID, cfg, d)
 
 	return &distributionScope{node: node}, nil
 }
