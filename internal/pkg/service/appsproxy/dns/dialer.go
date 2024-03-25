c7c9b6481db61d44c611bee7477b9fd46e6f4f98
Jáchym Toušek <jachym.tousek@keboola.com>
2024-03-25 12:25:32 +0100

    feat: Prepare DNS resolver and HTTP Transport

---
+++ internal/pkg/service/appsproxy/dns/dialer.go
@@ -0,0 +1,23 @@
+package dns
+
+import (
+	"net"
+	"time"
+)
+
+// DialTimeout specifies the default maximum connection initialization time.
+const DialTimeout = 2 * time.Second
+
+// KeepAlive specifies the default interval between keep-alive probes.
+const KeepAlive = 15 * time.Second
+
+func newDialer() *net.Dialer {
+	return &net.Dialer{
+		Timeout:   DialTimeout,
+		KeepAlive: KeepAlive,
+		Resolver: &net.Resolver{
+			PreferGo:     true,
+			StrictErrors: true,
+		},
+	}
+}
