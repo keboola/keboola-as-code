f72725da31b3e83fe2464bd930dec4094634313e
Matovidlo <martin.vasko@keboola.com>
2024-04-15 14:08:29 +0200

    fix: Rename ok_legacy -> ok-legacy

--- test/cli/push/ok_legacy/in/bar/transformation/keboola.snowflake-transformation/snowflake-transformation/blocks/001-block-1/002-code-y/code.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "test2" AS SELECT $1, $2
-FROM VALUES( 1, 'two' ), ( 3, 'four' ), ( 5, 'six' );
--- test/cli/push/ok_legacy/out/bar/transformation/keboola.snowflake-transformation/snowflake-transformation/blocks/002-block-2-my-name/001-code-z/code.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "test3" AS SELECT $1, $2
-FROM VALUES( 123, 'x' ), ( 345, 'y' );
