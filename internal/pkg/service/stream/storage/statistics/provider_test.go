761a23641425aa7c2e7c51366b30818770487178
Michal Jurečko <michal.jurecko@gmail.com>
2024-04-25 10:56:48 +0200

    tests: Improve statistics providers tests, test also repository

--- internal/pkg/service/stream/storage/statistics/provider_test.go
+++ internal/pkg/service/stream/storage/statistics/provider_test.go
@@ -2,7 +2,6 @@ package statistics_test
 
 import (
 	"context"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics"
 	"testing"
 	"time"
 
@@ -13,19 +12,21 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics/cache"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics/repository"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 )
 
-type testCase struct {
+type providerTestCase struct {
 	Description string
 	Prepare     func()
 	Assert      func(repository.Provider)
 }
 
-func TestCaches(t *testing.T) {
+// TestStatisticsProviders - repository, L1 and L2 cache.
+func TestStatisticsProviders(t *testing.T) {
 	t.Parallel()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
@@ -50,7 +51,7 @@ func TestCaches(t *testing.T) {
 	sliceKey3 := test.NewSliceKeyOpenedAt("2000-01-01T03:00:00.000Z")
 
 	// Define test cases
-	cases := []testCase{
+	cases := []providerTestCase{
 		{
 			Description: "Empty",
 			Assert: func(provider repository.Provider) {
@@ -371,6 +372,9 @@ func TestCaches(t *testing.T) {
 			header := etcdhelper.ExpectModification(t, client, tc.Prepare)
 			expectedRevision = header.Revision
 		}
+
+		// Test repository
+		tc.Assert(repo)
 
 		// Wait for cache sync
 		if expectedRevision > 0 {
