218ffae650392cac5dcf32646cec1bd103ef6983
Matovidlo <martin.vasko@keboola.com>
2024-04-30 10:42:58 +0200

    ci: remove linter cache and add test cache instead.
    Linter goes faster than tests, remove the linter cache so the CI is
    efficient and cache less poluted.
    
    Fix makefile target with auto verbose tag.

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -19,6 +19,9 @@ TEST_DETECT_RACE="${TEST_DETECT_RACE:=true}"
 TEST_COVERAGE="${TEST_COVERAGE:=true}"
 TEST_PACKAGE="${TEST_PACKAGE:=./...}"
 TEST_ARGS="${TEST_ARGS:=}"
+if [[ $TEST_VERBOSE == "true" ]]; then
+  TEST_ARGS="$TEST_ARGS -v"
+fi
 if [[ $TEST_DETECT_RACE == "true" ]]; then
   TEST_ARGS="$TEST_ARGS -race"
 fi
@@ -29,7 +32,7 @@ fi
 # Run tests, sequentially because the API is shared resource
 echo "Running tests ..."
 export KBC_VERSION_CHECK=false # do not check the latest version in the tests
-cmd="gotestsum --no-color=false --format \"$TEST_LOG_FORMAT\" -- -timeout 1200s -p $TEST_PARALLELISM_PKG -parallel $TEST_PARALLELISM -v $TEST_ARGS  "$TEST_PACKAGE" $@"
+cmd="gotestsum --no-color=false --format \"$TEST_LOG_FORMAT\" -- -timeout 1200s -p $TEST_PARALLELISM_PKG -parallel $TEST_PARALLELISM $TEST_ARGS "$TEST_PACKAGE" $@"
 echo $cmd
 eval $cmd
 echo "Ok. All tests passed."
