52af83ccf54c3208d1ed2a570ae153f02e719511
Jáchym Toušek <jachym.tousek@keboola.com>
2024-05-09 12:42:35 +0200

    WIP

--- internal/pkg/service/stream/storage/statistics/repository/files.go
+++ internal/pkg/service/stream/storage/statistics/repository/files.go
@@ -4,15 +4,17 @@ import (
 	"context"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/iterator"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics/aggregate"
+	"strings"
 )
 
-func (r *Repository) FilesStats(ctx context.Context, sinkKey key.SinkKey, first, last model.FileKey) *op.TxnOp[map[model.FileID]*statistics.Aggregated] {
-	result := make(map[model.FileID]*statistics.Aggregated)
+func (r *Repository) FilesStats(ctx context.Context, sinkKey key.SinkKey, first, last model.FileKey) *op.TxnOp[map[model.FileKey]*statistics.Aggregated] {
+	result := make(map[model.FileKey]*statistics.Aggregated)
 	txn := op.TxnWithResult(r.client, &result)
 	for _, level := range level.AllLevels() {
 		// Get stats prefix for the slice state
@@ -21,10 +23,13 @@ func (r *Repository) FilesStats(ctx context.Context, sinkKey key.SinkKey, first,
 		// Sum
 		txn.Then(
 			pfx.GetAll(r.client, iterator.WithStartOffset(first.OpenedAt().String()), iterator.WithEndOffset(last.OpenedAt().String())).
-				ForEach(func(v statistics.Value, header *iterator.Header) error {
-					// TODO: how to get FileID?
-					fileID := model.FileID{}
-					aggregate.Aggregate(level, v, result[fileID])
+				ForEachKV(func(kv *op.KeyValueT[statistics.Value], header *iterator.Header) error {
+					// Extract fileID part from the key
+					// It can be a method in the schema.go
+					relativeKey := strings.TrimPrefix(kv.Key(), pfx.Prefix())
+					openedAt, _, _ := strings.Cut(relativeKey, "/")
+					fileKey := model.FileKey{SinkKey: sinkKey, FileID: model.FileID{OpenedAt: utctime.MustParse(openedAt)}}
+					aggregate.Aggregate(level, kv.Value, result[fileKey])
 					return nil
 				}),
 		)
