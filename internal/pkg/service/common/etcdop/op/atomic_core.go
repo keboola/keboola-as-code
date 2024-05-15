63a801bbea9018f2a142e450a031797e2c4e6a62
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-15 09:02:50 +0200

    refactor: Move logic to AtomicOpCore

--- internal/pkg/service/common/etcdop/op/atomic_core.go
+++ internal/pkg/service/common/etcdop/op/atomic_core.go
@@ -16,6 +16,11 @@ type AtomicOpCore struct {
 	locks          []Mutex
 	readPhase      []HighLevelFactory
 	writePhase     []HighLevelFactory
+	// processorFactory allows injection of the type-specific processors from the AtomicOp[R].
+	processorFactory atomicOpCoreProcessorFactory
+}
+
+type atomicOpCoreProcessorFactory func() func(ctx context.Context, r *TxnResult[NoResult])
 
 // SkipPrefixKeysCheck disables the feature.
 //
@@ -30,6 +35,33 @@ type AtomicOpCore struct {
 func (v *AtomicOpCore) SkipPrefixKeysCheck() *AtomicOpCore {
 	v.checkPrefixKey = false
 	return v
+}
+
+func (v *AtomicOpCore) ReadPhaseOps() (out []HighLevelFactory) {
+	return slices.Clone(v.readPhase)
+}
+
+func (v *AtomicOpCore) WritePhaseOps() (out []HighLevelFactory) {
+	if v.processorFactory == nil && len(v.locks) == 0 {
+		return slices.Clone(v.writePhase)
+	}
+
+	return []HighLevelFactory{func(ctx context.Context) Op {
+		txn, err := v.writeTxn(ctx)
+		if err == nil {
+			return txn
+		} else {
+			return ErrorOp(err)
+		}
+	}}
+}
+
+func (v *AtomicOpCore) Core() *AtomicOpCore {
+	return v
+}
+
+func (v *AtomicOpCore) Empty() bool {
+	return len(v.locks) == 0 && len(v.readPhase) == 0 && len(v.writePhase) == 0
 }
 
 func (v *AtomicOpCore) AddFrom(ops ...AtomicOpInterface) *AtomicOpCore {
@@ -65,4 +97,109 @@ func (v *AtomicOpCore) Read(factories ...HighLevelFactory) *AtomicOpCore {
 func (v *AtomicOpCore) Write(factories ...HighLevelFactory) *AtomicOpCore {
 	v.writePhase = append(v.writePhase, factories...)
 	return v
+}
+
+func (v *AtomicOpCore) newEmpty() *AtomicOpCore {
+	out := newAtomicCore(v.client)
+	out.checkPrefixKey = v.checkPrefixKey
+	return out
+}
+
+func (v *AtomicOpCore) setProcessorFactory(fn atomicOpCoreProcessorFactory) {
+	v.processorFactory = fn
+}
+
+func (v *AtomicOpCore) locksIfs() (cmps []etcd.Cmp, err error) {
+	for _, lock := range v.locks {
+		if key := lock.Key(); key == "" || key == "\x00" {
+			return nil, NotLockedError{}
+		}
+		cmps = append(cmps, lock.IsOwner())
+	}
+	return cmps, nil
+}
+
+func (v *AtomicOpCore) readTxn(ctx context.Context, tracker *TrackerKV) (*TxnOp[NoResult], error) {
+	// Create READ transaction
+	readTxn := Txn(tracker)
+	for _, opFactory := range v.readPhase {
+		op := opFactory(ctx)
+		if errOp, ok := op.(*errorOp); ok {
+			return nil, errOp.err
+		} else if op != nil {
+			readTxn.Merge(op)
+		}
+	}
+
+	// Stop the READ phase, if a lock is not locked
+	if lockIfs, err := v.locksIfs(); err == nil && len(lockIfs) > 0 {
+		readTxn.Merge(Txn(nil).
+			If(lockIfs...).
+			OnFailed(func(r *TxnResult[NoResult]) {
+				r.AddErr(errors.PrefixError(LockedError{}, "read phase"))
+			}),
+		)
+	} else if err != nil {
+		return nil, errors.PrefixError(err, "read phase")
+	}
+
+	return readTxn, nil
+}
+
+func (v *AtomicOpCore) writeTxn(ctx context.Context) (*TxnOp[NoResult], error) {
+	// Create WRITE transaction
+	writeTxn := Txn(v.client)
+	for _, opFactory := range v.writePhase {
+		op := opFactory(ctx)
+		if errOp, ok := op.(*errorOp); ok {
+			return nil, errOp.err
+		} else if op != nil {
+			writeTxn.Merge(op)
+		}
+	}
+
+	// Stop the WRITE phase, if a lock is not locked
+	if lockIfs, err := v.locksIfs(); err == nil && len(lockIfs) > 0 {
+		writeTxn.Merge(Txn(nil).
+			If(lockIfs...).
+			OnFailed(func(r *TxnResult[NoResult]) {
+				r.AddErr(errors.PrefixError(LockedError{}, "write phase"))
+			}),
+		)
+	} else if err != nil {
+		return nil, errors.PrefixError(err, "write phase")
+	}
+
+	// Attach processor callback, if any
+	var processor func(ctx context.Context, r *TxnResult[NoResult])
+	if v.processorFactory != nil {
+		processor = v.processorFactory()
+	}
+	if processor != nil {
+		writeTxn.AddProcessor(processor)
+	}
+
+	return writeTxn, nil
+}
+
+func (v *AtomicOpCore) do(ctx context.Context, tracker *TrackerKV, opts ...Option) (writeTxn *TxnOp[NoResult], readRevision int64, err error) {
+	// Create READ phase
+	readTxn, err := v.readTxn(ctx, tracker)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// Run READ phase, track used keys/prefixes
+	readResult := readTxn.Do(ctx, opts...)
+	if err = readResult.Err(); err != nil {
+		return nil, 0, err
+	}
+
+	// Create WRITE transaction
+	writeTxn, err = v.writeTxn(ctx)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return writeTxn, readResult.Header().Revision, nil
 }
