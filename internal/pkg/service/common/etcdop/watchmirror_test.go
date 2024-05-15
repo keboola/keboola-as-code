e309d46b06ea5d8f8b2297413e6ece1a5fac1181
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-15 09:08:31 +0200

    feat: SetupFullMirror

--- internal/pkg/service/common/etcdop/watchmirror_test.go
+++ internal/pkg/service/common/etcdop/watchmirror_test.go
@@ -38,7 +38,7 @@ func TestMirror(t *testing.T) {
 	require.NoError(t, pfx.Key("key1").Put(client, testUser{FirstName: "John", LastName: "Brown", Age: 10}).Do(ctx).Err())
 	require.NoError(t, pfx.Key("key2").Put(client, testUser{FirstName: "Paul", LastName: "Green", Age: 20}).Do(ctx).Err())
 
-	// Setup mirroring of a prefix tree to the memory, with custom key and value mapping.
+	// Setup mirroring of the etcd prefix tree to the memory, with custom key and value mapping.
 	// The result are in-memory KV pairs "<first name> <last name>" => <age>.
 	logger := log.NewDebugLogger()
 	mirror, errCh := SetupMirror(
@@ -62,7 +62,10 @@ func TestMirror(t *testing.T) {
 	require.NoError(t, <-errCh)
 
 	// Test state after initialization
-	mirror.ToMap()
+	assert.Equal(t, map[string]int{
+		"John Brown": 10,
+		"Paul Green": 20,
+	}, mirror.ToMap())
 
 	// Insert
 	header, err := pfx.Key("key3").Put(client, testUser{FirstName: "Luke", LastName: "Blue", Age: 30}).Do(ctx).HeaderOrErr()
@@ -100,5 +103,82 @@ func TestMirror(t *testing.T) {
 	assert.Equal(t, map[string]int{
 		"Jacob Brown": 15,
 		"Luke Blue":   30,
+	}, mirror.ToMap())
+}
+
+func TestFullMirror(t *testing.T) {
+	t.Parallel()
+
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// Create a typed prefix with some keys
+	client := etcdhelper.ClientForTest(t, etcdhelper.TmpNamespace(t))
+	pfx := NewTypedPrefix[testUser]("my/prefix", serde.NewJSON(serde.NoValidation))
+	require.NoError(t, pfx.Key("key1").Put(client, testUser{FirstName: "John", LastName: "Brown", Age: 10}).Do(ctx).Err())
+	require.NoError(t, pfx.Key("key2").Put(client, testUser{FirstName: "Paul", LastName: "Green", Age: 20}).Do(ctx).Err())
+
+	// Setup full mirroring of the etcd prefix tree to the memory.
+	logger := log.NewDebugLogger()
+	mirror, errCh := SetupFullMirror(logger, pfx.GetAllAndWatch(ctx, client, clientv3.WithPrevKV())).
+		WithFilter(func(event WatchEventT[testUser]) bool {
+			return !strings.Contains(event.Kv.String(), "/ignore")
+		}).
+		StartMirroring(ctx, wg)
+
+	// waitForSync:  it waits until the memory mirror is synchronized with the revision of the last change
+	var header *op.Header
+	waitForSync := func() {
+		assert.Eventually(t, func() bool { return mirror.Revision() >= header.Revision }, time.Second, 100*time.Millisecond)
+	}
+
+	// Wait for initialization
+	require.NoError(t, <-errCh)
+
+	// Test state after initialization
+	assert.Equal(t, map[string]testUser{
+		"my/prefix/key1": {FirstName: "John", LastName: "Brown", Age: 10},
+		"my/prefix/key2": {FirstName: "Paul", LastName: "Green", Age: 20},
+	}, mirror.ToMap())
+
+	// Insert
+	header, err := pfx.Key("key3").Put(client, testUser{FirstName: "Luke", LastName: "Blue", Age: 30}).Do(ctx).HeaderOrErr()
+	require.NoError(t, err)
+	waitForSync()
+	assert.Equal(t, map[string]testUser{
+		"my/prefix/key1": {FirstName: "John", LastName: "Brown", Age: 10},
+		"my/prefix/key2": {FirstName: "Paul", LastName: "Green", Age: 20},
+		"my/prefix/key3": {FirstName: "Luke", LastName: "Blue", Age: 30},
+	}, mirror.ToMap())
+
+	// Update
+	header, err = pfx.Key("key1").Put(client, testUser{FirstName: "Jacob", LastName: "Brown", Age: 15}).Do(ctx).HeaderOrErr()
+	require.NoError(t, err)
+	waitForSync()
+	assert.Equal(t, map[string]testUser{
+		"my/prefix/key1": {FirstName: "Jacob", LastName: "Brown", Age: 15},
+		"my/prefix/key2": {FirstName: "Paul", LastName: "Green", Age: 20},
+		"my/prefix/key3": {FirstName: "Luke", LastName: "Blue", Age: 30},
+	}, mirror.ToMap())
+
+	// Delete
+	header, err = pfx.Key("key2").Delete(client).Do(ctx).HeaderOrErr()
+	require.NoError(t, err)
+	waitForSync()
+	assert.Equal(t, map[string]testUser{
+		"my/prefix/key1": {FirstName: "Jacob", LastName: "Brown", Age: 15},
+		"my/prefix/key3": {FirstName: "Luke", LastName: "Blue", Age: 30},
+	}, mirror.ToMap())
+
+	// Filter
+	header, err = pfx.Key("ignore").Put(client, testUser{FirstName: "Ignored", LastName: "User", Age: 50}).Do(ctx).HeaderOrErr()
+	require.NoError(t, err)
+	waitForSync()
+	assert.Equal(t, map[string]testUser{
+		"my/prefix/key1": {FirstName: "Jacob", LastName: "Brown", Age: 15},
+		"my/prefix/key3": {FirstName: "Luke", LastName: "Blue", Age: 30},
 	}, mirror.ToMap())
 }
