366fece93dd3ee9bc9217507449db8fc79fae987
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-16 18:43:39 +0200

    definition repo source

---
+++ internal/pkg/service/stream/definition/repository/source/source_get.go
@@ -0,0 +1,24 @@
+package source
+
+import (
+	serviceError "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+)
+
+func (r *Repository) Get(k key.SourceKey) op.WithResult[definition.Source] {
+	return r.schema.
+		Active().ByKey(k).Get(r.client).
+		WithEmptyResultAsError(func() error {
+			return serviceError.NewResourceNotFoundError("source", k.SourceID.String(), "branch")
+		})
+}
+
+func (r *Repository) GetDeleted(k key.SourceKey) op.WithResult[definition.Source] {
+	return r.schema.
+		Deleted().ByKey(k).Get(r.client).
+		WithEmptyResultAsError(func() error {
+			return serviceError.NewResourceNotFoundError("deleted source", k.SourceID.String(), "branch")
+		})
+}
