1818de4ea68609a6d184b504170a891972b6218b
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-16 18:50:41 +0200

    storage repository slice

--- internal/pkg/service/stream/storage/repository/slice/slice_new.go
+++ internal/pkg/service/stream/storage/repository/slice/slice_new.go
@@ -1,10 +1,8 @@
-package repository
+package slice
 
 import (
 	"fmt"
 	"time"
-
-	"github.com/c2h5oh/datasize"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/compression"
@@ -13,8 +11,8 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
-// newSlice creates slice definition.
-func newSlice(now time.Time, file model.File, volumeID volume.ID, prevSliceSize datasize.ByteSize) (s model.Slice, err error) {
+// newSlice creates slice entity.
+func (r *Repository) newSlice(now time.Time, file model.File, volumeID volume.ID) (s model.Slice, err error) {
 	// Validate compression type.
 	// Other parts of the system are also prepared for other types of compression,
 	// but now only GZIP is supported in the Keboola platform.
@@ -38,7 +36,7 @@ func newSlice(now time.Time, file model.File, volumeID volume.ID, prevSliceSize 
 	s.Type = file.Type
 	s.State = model.SliceWriting
 	s.Columns = file.Columns
-	if s.LocalStorage, err = file.LocalStorage.NewSlice(localDir, prevSliceSize); err != nil {
+	if s.LocalStorage, err = file.LocalStorage.NewSlice(localDir); err != nil {
 		return model.Slice{}, err
 	}
 	if s.StagingStorage, err = file.StagingStorage.NewSlice(stagingPath, s.LocalStorage); err != nil {
