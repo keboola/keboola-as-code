1edd5a92c3b210b19749e37ad6016bd30d3e7785
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-16 08:34:49 +0200

    feat: AtomicFromCtx

--- internal/pkg/service/common/etcdop/op/atomic.go
+++ internal/pkg/service/common/etcdop/op/atomic.go
@@ -26,6 +26,9 @@ import (
 // - The DoWithoutRetry method returns false.
 //
 // Retries on network errors are always performed.
+//
+// The atomic operation can be read from the context during generation to be extended, see AtomicFromCtx function.
+// This can lead to several subsequent READ phases, but there is always only one WRITE phase, after all READ phases.
 type AtomicOp[R any] struct {
 	*AtomicOpCore
 	result     *R
