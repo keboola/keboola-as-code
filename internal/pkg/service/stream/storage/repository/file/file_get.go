8fe6783b98eec0eefc03ddbb9510e58c9b9d61e6
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-16 18:49:26 +0200

    storage repository file

---
+++ internal/pkg/service/stream/storage/repository/file/file_get.go
@@ -0,0 +1,14 @@
+package file
+
+import (
+	serviceError "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
+)
+
+// Get file entity.
+func (r *Repository) Get(k model.FileKey) op.WithResult[model.File] {
+	return r.schema.AllLevels().ByKey(k).Get(r.client).WithEmptyResultAsError(func() error {
+		return serviceError.NewResourceNotFoundError("file", k.String(), "sink")
+	})
+}
