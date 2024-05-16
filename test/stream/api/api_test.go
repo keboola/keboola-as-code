f1934ade8c859c744f2d2d294b7a89fba2e3818a
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-16 18:52:02 +0200

    improve stream api test

--- test/stream/api/api_test.go
+++ test/stream/api/api_test.go
@@ -3,6 +3,8 @@ package api
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"runtime"
 	"testing"
 
@@ -11,6 +13,7 @@ import (
 
 	"github.com/keboola/keboola-as-code/internal/pkg/env"
 	"github.com/keboola/keboola-as-code/internal/pkg/filesystem"
+	volume "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/volume/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/testhelper"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/testhelper/runner"
@@ -47,10 +50,17 @@ func TestStreamApiE2E(t *testing.T) {
 				assert.NoError(test.T(), err)
 			}
 
+			// Simulate an empty volume with fixed volumeID
+			volumesPath := t.TempDir()
+			volumePath := filepath.Join(volumesPath, "hdd", "my-volume")
+			require.NoError(t, os.MkdirAll(volumePath, 0o700))
+			require.NoError(t, os.WriteFile(filepath.Join(volumePath, volume.IDFile), []byte("my-volume"), 0o600))
+
 			addEnvs := env.FromMap(map[string]string{
 				"STREAM_DATADOG_ENABLED":        "false",
 				"STREAM_NODE_ID":                "test-node",
 				"STREAM_STORAGE_API_HOST":       test.TestProject().StorageAPIHost(),
+				"STREAM_STORAGE_VOLUMES_PATH":   volumesPath,
 				"STREAM_API_PUBLIC_URL":         "https://stream.keboola.local",
 				"STREAM_SOURCE_HTTP_PUBLIC_URL": "https://stream-in.keboola.local",
 				"STREAM_ETCD_NAMESPACE":         etcdCfg.Namespace,
@@ -64,7 +74,7 @@ func TestStreamApiE2E(t *testing.T) {
 				runner.WithInitProjectState(),
 				runner.WithRunAPIServerAndRequests(
 					binaryPath,
-					[]string{"api"}, // start only the API component
+					[]string{"api", "disk-writer"}, // start api component and disk-writer to detect the volume
 					addEnvs,
 					nil,
 				),
