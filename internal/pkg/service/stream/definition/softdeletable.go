55fd13b214527047fd777234d3daa6631213264e
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 14:27:53 +0200

    feat: Improve SoftDeletable, track By, UndeletedAt, ...

--- internal/pkg/service/stream/definition/softdeletable.go
+++ internal/pkg/service/stream/definition/softdeletable.go
@@ -7,41 +7,98 @@ import (
 )
 
 type SoftDeletableInterface interface {
-	Delete(now time.Time, deletedWithParent bool)
-	Undelete()
+	// Delete marks the entity as deleted.
+	Delete(now time.Time, by By, directly bool)
+	// Undelete marks the entity as undeleted.
+	Undelete(now time.Time, by By)
+	// IsDeleted returns true if the entity is marked as deleted.
 	IsDeleted() bool
-	WasDeletedWithParent() bool
-	GetDeletedAt() *utctime.UTCTime
+	IsDeletedAt(at time.Time) bool
+	// IsUndeleted returns true if the entity is marked as undeleted.
+	IsUndeleted() bool
+	IsUndeletedAt(at time.Time) bool
+	// IsDeletedDirectly returns true if the entity has been deleted directly, not together with its parent.
+	IsDeletedDirectly() bool
+	DeletedBy() *By
+	DeletedAt() utctime.UTCTime
+	UndeletedBy() *By
+	UndeletedAt() utctime.UTCTime
 }
 
 type SoftDeletable struct {
-	Deleted   bool             `json:"deleted,omitempty"`
-	DeletedAt *utctime.UTCTime `json:"deletedAt,omitempty" validate:"required_if=Deleted true,excluded_if=Deleted false"`
-	// DeletedWithParent is true when the object has not been deleted directly but was deleted together with its parent.
-	DeletedWithParent bool `json:"deletedWithParent,omitempty" validate:"excluded_if=Deleted false"`
+	Deleted   *Deleted   `json:"deleted,omitempty" validate:"excluded_with=Undeleted"`
+	Undeleted *Undeleted `json:"undeleted,omitempty" validate:"excluded_with=Deleted"`
 }
 
-func (v *SoftDeletable) Delete(now time.Time, deletedWithParent bool) {
-	at := utctime.From(now)
-	v.Deleted = true
-	v.DeletedAt = &at
-	v.DeletedWithParent = deletedWithParent
+type Deleted struct {
+	// Directly is true if the entity has been deleted directly, not together with its parent.
+	Directly bool            `json:"directly"`
+	At       utctime.UTCTime `json:"at" validate:"required"`
+	By       By              `json:"by" validate:"required"`
 }
 
-func (v *SoftDeletable) Undelete() {
-	v.Deleted = false
-	v.DeletedWithParent = false
-	v.DeletedAt = nil
+type Undeleted struct {
+	At utctime.UTCTime `json:"at" validate:"required"`
+	By By              `json:"by" validate:"required"`
+}
+
+func (v *SoftDeletable) Delete(now time.Time, by By, directly bool) {
+	v.Deleted = &Deleted{By: by, At: utctime.From(now), Directly: directly}
+	v.Undeleted = nil
+}
+
+func (v *SoftDeletable) Undelete(now time.Time, by By) {
+	v.Deleted = nil
+	v.Undeleted = &Undeleted{By: by, At: utctime.From(now)}
 }
 
 func (v *SoftDeletable) IsDeleted() bool {
-	return v.Deleted
+	return v.Deleted != nil
 }
 
-func (v *SoftDeletable) WasDeletedWithParent() bool {
-	return v.DeletedWithParent
+func (v *SoftDeletable) IsDeletedAt(at time.Time) bool {
+	return v.Deleted != nil && v.Deleted.At.Time().Equal(at)
 }
 
-func (v *SoftDeletable) GetDeletedAt() *utctime.UTCTime {
-	return v.DeletedAt
+// IsDeletedDirectly returns true if the object has been deleted directly, not together with its parent.
+func (v *SoftDeletable) IsDeletedDirectly() bool {
+	return v.Deleted != nil && v.Deleted.Directly
+}
+
+func (v *SoftDeletable) DeletedBy() *By {
+	if v.Deleted == nil {
+		return nil
+	}
+	value := v.Deleted.By
+	return &value
+}
+
+func (v *SoftDeletable) DeletedAt() utctime.UTCTime {
+	if v.Deleted == nil {
+		return utctime.UTCTime{}
+	}
+	return v.Deleted.At
+}
+
+func (v *SoftDeletable) IsUndeleted() bool {
+	return v.Undeleted != nil
+}
+
+func (v *SoftDeletable) IsUndeletedAt(at time.Time) bool {
+	return v.Undeleted != nil && v.Undeleted.At.Time().Equal(at)
+}
+
+func (v *SoftDeletable) UndeletedBy() *By {
+	if v.Undeleted == nil {
+		return nil
+	}
+	value := v.Undeleted.By
+	return &value
+}
+
+func (v *SoftDeletable) UndeletedAt() utctime.UTCTime {
+	if v.Undeleted == nil {
+		return utctime.UTCTime{}
+	}
+	return v.Undeleted.At
 }
