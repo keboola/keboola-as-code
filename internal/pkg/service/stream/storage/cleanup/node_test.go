6d0c0f057aa35a2fafeee4b6469206c91bfd2059
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 11:19:26 +0200

    feat: Update storage metadata cleanup

--- internal/pkg/service/stream/storage/cleanup/node_test.go
+++ internal/pkg/service/stream/storage/cleanup/node_test.go
@@ -12,14 +12,12 @@ import (
 	"go.etcd.io/etcd/client/v3/concurrency"
 
 	commonDeps "github.com/keboola/keboola-as-code/internal/pkg/service/common/dependencies"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/common/rollback"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/cleanup"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test/testconfig"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 )
 
@@ -30,6 +28,7 @@ func TestNode(t *testing.T) {
 
 	clk := clock.NewMock()
 	clk.Set(utctime.MustParse("2000-01-01T00:00:00.000Z").Time())
+	by := test.ByUser()
 
 	// Fixtures
 	projectID := keboola.ProjectID(123)
@@ -39,42 +38,40 @@ func TestNode(t *testing.T) {
 	ignoredEtcdKeys := etcdhelper.WithIgnoredKeyPattern(`^definition/|storage/secret/|storage/volume/|storage/file/all/|storage/slice/all/|runtime/`)
 
 	// Get services
-	d, mocked := dependencies.NewMockedTableSinkScope(t, commonDeps.WithClock(clk))
+	d, mocked := dependencies.NewMockedLocalStorageScope(t, commonDeps.WithClock(clk))
 
 	logger := mocked.DebugLogger()
 	client := mocked.TestEtcdClient()
-	rb := rollback.New(d.Logger())
 	defRepo := d.DefinitionRepository()
 	storageRepo := d.StorageRepository()
 	fileRepo := storageRepo.File()
 	sliceRepo := storageRepo.Slice()
-	tokenRepo := storageRepo.Token()
 	volumeRepo := storageRepo.Volume()
 
 	// Setup cleanup interval
 	cleanupInterval := 12 * time.Hour
-	activeFileExpiration := 24 * time.Hour
-	importedFileExpiration := time.Hour
+	importedFileExpiration := time.Hour    // the first call of the doCleanup triggers it
+	activeFileExpiration := 30 * time.Hour // the third call of the doCleanup triggers it
 	cfg := cleanup.NewConfig()
 	cfg.Interval = cleanupInterval
 	cfg.ActiveFileExpiration = activeFileExpiration
 	cfg.ArchivedFileExpiration = importedFileExpiration
 	require.NoError(t, cleanup.NewNode(cfg, d))
 
-	// The cleanup is triggered by a timer
-	startTime := clk.Now()
-	cleanupAttempt := 0
-	doCleanup := func() {
-		cleanupAttempt++
-		clk.Set(startTime.Add(time.Duration(cleanupAttempt) * cleanupInterval))
-		assert.Eventually(t, func() bool {
-			return logger.CompareJSONMessages(`{"level":"info","message":"deleted \"%d\" files"}`) == nil
-		}, 2*time.Second, 100*time.Millisecond)
+	// Prepare doCleanup helper
+	// -----------------------------------------------------------------------------------------------------------------
+	var doCleanup func()
+	{
+		startTime := clk.Now()
+		cleanupAttempt := 0
+		doCleanup = func() {
+			cleanupAttempt++
+			clk.Set(startTime.Add(time.Duration(cleanupAttempt) * cleanupInterval))
+			assert.Eventually(t, func() bool {
+				return logger.CompareJSONMessages(`{"level":"info","message":"deleted \"%d\" files"}`) == nil
+			}, 2*time.Second, 100*time.Millisecond)
+		}
 	}
-
-	// Mock file API calls
-	transport := mocked.MockedHTTPTransport()
-	test.MockCreateFilesStorageAPICalls(t, clk, branchKey, transport)
 
 	// Register active volumes
 	// -----------------------------------------------------------------------------------------------------------------
@@ -82,139 +79,85 @@ func TestNode(t *testing.T) {
 		session, err := concurrency.NewSession(client)
 		require.NoError(t, err)
 		defer func() { require.NoError(t, session.Close()) }()
-		test.RegisterWriterVolumes(t, ctx, volumeRepo, session, 2)
+		test.RegisterWriterVolumes(t, ctx, volumeRepo, session, 1)
 	}
 
-	// Create parent branch, source, sink
+	// Create parent branch, source, sink, file
 	// -----------------------------------------------------------------------------------------------------------------
 	{
 		branch := test.NewBranch(branchKey)
-		require.NoError(t, defRepo.Branch().Create(clk.Now(), &branch).Do(ctx).Err())
+		require.NoError(t, defRepo.Branch().Create(&branch, clk.Now(), by).Do(ctx).Err())
 		source := test.NewSource(sourceKey)
-		require.NoError(t, defRepo.Source().Create(clk.Now(), "Create source", &source).Do(ctx).Err())
-		sink := test.NewSink(sinkKey)
-		sink.Config = sink.Config.With(testconfig.LocalVolumeConfig(2, []string{"default"}))
-		require.NoError(t, defRepo.Sink().Create(clk.Now(), "Create sink", &sink).Do(ctx).Err())
-		require.NoError(t, tokenRepo.Put(sink.SinkKey, keboola.Token{Token: "my-token"}).Do(ctx).Err())
+		require.NoError(t, defRepo.Source().Create(&source, clk.Now(), by, "Create source").Do(ctx).Err())
+		sink := test.NewSinkWithLocalStorage(sinkKey)
+		require.NoError(t, defRepo.Sink().Create(&sink, clk.Now(), by, "Create sink").Do(ctx).Err())
 	}
 
-	// Create 5 files
+	// Create the second file
 	// -----------------------------------------------------------------------------------------------------------------
-	var files []model.FileKey
-	slices := make(map[model.FileKey][]model.SliceKey)
-	for i := 0; i < 4; i++ {
+	{
 		clk.Add(time.Hour)
-		file, err := fileRepo.Rotate(rb, clk.Now(), sinkKey).Do(ctx).ResultOrErr()
-		require.NoError(t, err)
-		files = append(files, file.FileKey)
-		fileSlices, err := sliceRepo.ListIn(file.FileKey).Do(ctx).All()
-		require.Len(t, fileSlices, 2)
-		require.NoError(t, err)
-		for _, slice := range fileSlices {
-			slices[file.FileKey] = append(slices[file.FileKey], slice.SliceKey)
-		}
+		require.NoError(t, fileRepo.Rotate(sinkKey, clk.Now()).Do(ctx).Err())
 	}
-	etcdhelper.AssertKeys(t, client, []string{
-		"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z",
-		"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z",
-		"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z",
-		"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-1/2000-01-01T01:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-2/2000-01-01T01:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-1/2000-01-01T02:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-2/2000-01-01T02:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-1/2000-01-01T03:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-2/2000-01-01T03:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-1/2000-01-01T04:00:00.000Z",
-		"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-2/2000-01-01T04:00:00.000Z",
-	}, ignoredEtcdKeys)
 
-	// Switch files and slices to different states
+	// Switch file1 to the FileImported state, file2 is kept in FileWriting state
 	// -----------------------------------------------------------------------------------------------------------------
-	clk.Add(time.Minute)
 	{
-		// File 4 is in the model.FileWriting state
-		file, err := fileRepo.Get(files[3]).Do(ctx).ResultOrErr()
+		files, err := fileRepo.ListIn(sinkKey).Do(ctx).All()
 		require.NoError(t, err)
-		assert.Equal(t, model.FileWriting, file.State)
-	}
-	{
-		// File 3 is in the model.FileWriting state
-		file, err := fileRepo.Get(files[2]).Do(ctx).ResultOrErr()
+		require.Len(t, files, 2)
+
+		slices, err := sliceRepo.ListIn(sinkKey).Do(ctx).All()
 		require.NoError(t, err)
-		assert.Equal(t, model.FileClosing, file.State)
-	}
-	{
-		// Switch file 2 to the model.FileImporting state
-		fileKey := files[1]
-		for _, sliceKey := range slices[fileKey] {
-			test.SwitchSliceStates(t, ctx, clk, sliceRepo, sliceKey, time.Minute, []model.SliceState{
-				model.SliceClosing, model.SliceUploading, model.SliceUploaded,
-			})
-		}
-		test.SwitchFileStates(t, ctx, clk, fileRepo, fileKey, time.Minute, []model.FileState{
-			model.FileClosing, model.FileImporting,
-		})
-		file, err := fileRepo.Get(fileKey).Do(ctx).ResultOrErr()
+		require.Len(t, slices, 2)
+		file1 := files[0]
+		slice1 := slices[0]
+		require.Equal(t, file1.FileKey, slice1.FileKey)
+
+		clk.Add(time.Hour)
+		require.Equal(t, model.SliceClosing, slice1.State)
+		slice1, err = sliceRepo.SwitchToUploading(slice1.SliceKey, clk.Now()).Do(ctx).ResultOrErr()
 		require.NoError(t, err)
-		assert.Equal(t, model.FileImporting, file.State)
-	}
-	{
-		// Switch file 1 to the model.FileImported state
-		fileKey := files[0]
-		for _, sliceKey := range slices[fileKey] {
-			test.SwitchSliceStates(t, ctx, clk, sliceRepo, sliceKey, time.Minute, []model.SliceState{
-				model.SliceClosing, model.SliceUploading, model.SliceUploaded,
-			})
-		}
-		test.SwitchFileStates(t, ctx, clk, fileRepo, fileKey, time.Minute, []model.FileState{
-			model.FileClosing, model.FileImporting, model.FileImported,
-		})
-		file, err := fileRepo.Get(fileKey).Do(ctx).ResultOrErr()
+		require.Equal(t, model.SliceUploading, slice1.State)
+		slice1, err = sliceRepo.SwitchToUploaded(slice1.SliceKey, clk.Now()).Do(ctx).ResultOrErr()
 		require.NoError(t, err)
-		assert.Equal(t, model.FileImported, file.State)
+		require.Equal(t, model.SliceUploaded, slice1.State)
+
+		clk.Add(time.Hour)
+		require.Equal(t, model.FileClosing, file1.State)
+		file1, err = fileRepo.SwitchToImporting(file1.FileKey, clk.Now()).Do(ctx).ResultOrErr()
+		require.NoError(t, err)
+		require.Equal(t, model.FileImporting, file1.State)
+		file1, err = fileRepo.SwitchToImported(file1.FileKey, clk.Now()).Do(ctx).ResultOrErr()
+		require.NoError(t, err)
+		require.Equal(t, model.FileImported, file1.State)
 	}
 	{
 		// Check database state
 		etcdhelper.AssertKeys(t, client, []string{
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z",
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z",
-			"storage/file/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z",
-			"storage/file/level/target/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-1/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-2/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-1/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-2/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-1/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-2/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/target/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-1/2000-01-01T01:00:00.000Z",
-			"storage/slice/level/target/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-2/2000-01-01T01:00:00.000Z",
+			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z",
+			"storage/file/level/target/123/456/my-source/my-sink/2000-01-01T00:00:00.000Z",
+			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-1/2000-01-01T01:00:00.000Z",
+			"storage/slice/level/target/123/456/my-source/my-sink/2000-01-01T00:00:00.000Z/my-volume-1/2000-01-01T00:00:00.000Z",
 		}, ignoredEtcdKeys)
 	}
 
-	// Delete import files, they have shorten expiration
+	// Delete imported file, they have shorten expiration
 	// -----------------------------------------------------------------------------------------------------------------
 	{
 		logger.Truncate()
 		doCleanup()
 		logger.AssertJSONMessages(t, `
 {"level":"info","message":"deleting metadata of expired files"}
-{"level":"info","message":"deleted expired file","file.state":"imported","file.age":"7h48m0s","file.key":"123/456/my-source/my-sink/2000-01-01T01:00:00.000Z"}
+{"level":"info","message":"deleted expired file","file.state":"imported","file.age":"9h0m0s","file.key":"123/456/my-source/my-sink/2000-01-01T00:00:00.000Z"}
 {"level":"info","message":"deleted \"1\" files","deletedFilesCount":1}
 `)
 	}
 	{
 		// Check database state
 		etcdhelper.AssertKeys(t, client, []string{
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z",
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z",
-			"storage/file/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-1/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-2/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-1/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-2/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-1/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-2/2000-01-01T02:00:00.000Z",
+			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z",
+			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-1/2000-01-01T01:00:00.000Z",
 		}, ignoredEtcdKeys)
 	}
 
@@ -231,31 +174,21 @@ func TestNode(t *testing.T) {
 	{
 		// Check database state
 		etcdhelper.AssertKeys(t, client, []string{
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z",
-			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z",
-			"storage/file/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-1/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T03:00:00.000Z/my-volume-2/2000-01-01T03:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-1/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T04:00:00.000Z/my-volume-2/2000-01-01T04:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-1/2000-01-01T02:00:00.000Z",
-			"storage/slice/level/staging/123/456/my-source/my-sink/2000-01-01T02:00:00.000Z/my-volume-2/2000-01-01T02:00:00.000Z",
+			"storage/file/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z",
+			"storage/slice/level/local/123/456/my-source/my-sink/2000-01-01T01:00:00.000Z/my-volume-1/2000-01-01T01:00:00.000Z",
 		}, ignoredEtcdKeys)
 	}
 
-	// Delete active files, they have longer expiration
-	// Note: files are deleted in parallel, so log records have random order
+	// Delete active file, they have longer expiration
 	// -----------------------------------------------------------------------------------------------------------------
 	{
 		logger.Truncate()
 		doCleanup()
 		logger.AssertJSONMessages(t, `
 {"level":"info","message":"deleting metadata of expired files"}
-{"level":"info","message":"deleted \"3\" files","deletedFilesCount":3,"component":"storage.cleanup"}
+{"level":"info","message":"deleted expired file","file.state":"writing","file.age":"35h0m0s","file.key":"123/456/my-source/my-sink/2000-01-01T01:00:00.000Z"}
+{"level":"info","message":"deleted \"1\" files","deletedFilesCount":1}
 `)
-		logger.AssertJSONMessages(t, `{"level":"info","message":"deleted expired file","file.state":"importing","file.age":"31h54m0s","file.key":"123/456/my-source/my-sink/2000-01-01T02:00:00.000Z"}`)
-		logger.AssertJSONMessages(t, `{"level":"info","message":"deleted expired file","file.state":"closing","file.age":"32h0m0s","file.key":"123/456/my-source/my-sink/2000-01-01T03:00:00.000Z"}`)
-		logger.AssertJSONMessages(t, `{"level":"info","message":"deleted expired file","file.state":"writing","file.age":"32h0m0s","file.key":"123/456/my-source/my-sink/2000-01-01T04:00:00.000Z"}`)
 	}
 	{
 		// Check database state
