c3588185bb18c3df37c911cd8f8f7f8295595b9d
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 11:18:34 +0200

    build: Update linter

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -42,7 +42,7 @@ if ! command -v golangci-lint &> /dev/null
 then
   # Disabled due to issues with windows curl command not working
   # Related to https://github.com/keboola/keboola-as-code/pull/1818/commits/df23a4e2855557a01460593f7d6ba59b5dc8825c
-  # curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$GOBIN" v1.57.2
+  # curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$GOBIN" v1.58.0
   go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.57.2
 fi
 
