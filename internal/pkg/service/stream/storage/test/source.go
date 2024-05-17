c27fdd9f1ad963dfc32ec0c1810ea00abc07a0d5
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 11:16:49 +0200

    feat: Update test entities

--- internal/pkg/service/stream/storage/test/source.go
+++ internal/pkg/service/stream/storage/test/source.go
@@ -5,7 +5,20 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
 )
 
+const (
+	SourceType = definition.SourceType("test")
+)
+
 func NewSource(k key.SourceKey) definition.Source {
+	return definition.Source{
+		SourceKey:   k,
+		Type:        SourceType,
+		Name:        "My Source",
+		Description: "My Description",
+	}
+}
+
+func NewHTTPSource(k key.SourceKey) definition.Source {
 	return definition.Source{
 		SourceKey:   k,
 		Type:        definition.SourceTypeHTTP,
