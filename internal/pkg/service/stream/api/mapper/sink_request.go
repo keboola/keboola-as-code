ef1e7b552ddae58676d35a44575d9b59f2bec366
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 09:30:13 +0200

    feat: Update API mapper

---
+++ internal/pkg/service/stream/api/mapper/sink_request.go
@@ -0,0 +1,185 @@
+package mapper
+
+import (
+	"github.com/keboola/go-client/pkg/keboola"
+
+	svcerrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	api "github.com/keboola/keboola-as-code/internal/pkg/service/stream/api/gen/stream"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/table"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/table/column"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/strhelper"
+)
+
+func (m *Mapper) NewSinkEntity(parent key.SourceKey, payload *api.CreateSinkPayload) (entity definition.Sink, err error) {
+	entity.SourceKey = parent
+
+	// Generate source ID from Name if not set
+	if payload.SinkID == nil || len(*payload.SinkID) == 0 {
+		entity.SinkID = key.SinkID(strhelper.NormalizeName(payload.Name))
+	} else {
+		entity.SinkID = key.SinkID(strhelper.NormalizeName(string(*payload.SinkID)))
+	}
+
+	// Name
+	entity.Name = payload.Name
+
+	// Description is optional
+	if payload.Description != nil {
+		entity.Description = *payload.Description
+	}
+
+	// Sink type
+	entity.Type = payload.Type
+	switch entity.Type {
+	case definition.SinkTypeTable:
+		if tableEntity, err := m.newTableSinkEntity(payload); err == nil {
+			entity.Table = &tableEntity
+		} else {
+			return definition.Sink{}, err
+		}
+	default:
+		return definition.Sink{}, svcerrors.NewBadRequestError(errors.Errorf(`unexpected "type" "%s"`, payload.Type.String()))
+	}
+
+	return entity, nil
+}
+
+func (m *Mapper) UpdateSinkEntity(entity definition.Sink, payload *api.UpdateSinkPayload) (definition.Sink, error) {
+	// Name
+	if payload.Name != nil {
+		entity.Name = *payload.Name
+	}
+
+	// Description
+	if payload.Description != nil {
+		entity.Description = *payload.Description
+	}
+
+	// Type
+	if payload.Type != nil {
+		entity.Type = *payload.Type
+	}
+
+	// Type specific updates
+	switch entity.Type {
+	case definition.SinkTypeTable:
+		if entity.Table == nil {
+			entity.Table = &definition.TableSink{}
+		}
+		if payload.Table != nil {
+			if err := m.updateTableSinkEntity(entity.Table, payload); err != nil {
+				return definition.Sink{}, err
+			}
+		}
+	default:
+		return definition.Sink{}, svcerrors.NewBadRequestError(errors.Errorf(`unexpected "type" "%s"`, payload.Type.String()))
+	}
+
+	return entity, nil
+}
+
+func (m *Mapper) newTableSinkEntity(payload *api.CreateSinkPayload) (entity definition.TableSink, err error) {
+	// User has to specify table definition
+	if payload.Table == nil {
+		return definition.TableSink{}, svcerrors.NewBadRequestError(errors.Errorf(`"table" must be configured for the "%s" sink type`, definition.SinkTypeTable))
+	}
+
+	// Common table mapping
+	entity.Mapping, err = m.newTableSinkMappingEntity(payload.Table.Mapping)
+	if err != nil {
+		return definition.TableSink{}, err
+	}
+
+	// Table type
+	entity.Type = payload.Table.Type
+
+	// Table type specific fields
+	switch entity.Type {
+	case definition.TableTypeKeboola:
+		// Keboola table
+		entity.Keboola = &definition.KeboolaTable{}
+
+		// TableID
+		if tableID, err := keboola.ParseTableID(string(payload.Table.TableID)); err == nil {
+			entity.Keboola.TableID = tableID
+		} else {
+			return definition.TableSink{}, svcerrors.NewBadRequestError(errors.Errorf(`invalid "tableId" value "%s": %w`, payload.Table.TableID, err))
+		}
+	default:
+		return definition.TableSink{}, svcerrors.NewBadRequestError(errors.Errorf(`unexpected "type" "%s"`, payload.Type.String()))
+	}
+
+	return entity, err
+}
+
+func (m *Mapper) newTableSinkMappingEntity(payload *api.TableMapping) (entity table.Mapping, err error) {
+	// User has to specify table mapping definition
+	if payload == nil {
+		return table.Mapping{}, errors.Errorf(`"table.mapping" must be configured for the "%s" sink type`, definition.SinkTypeTable)
+	}
+
+	// Columns
+	for _, columnPayload := range payload.Columns {
+		columnEntity, err := column.MakeColumn(columnPayload.Type, columnPayload.Name, columnPayload.PrimaryKey)
+		if err != nil {
+			return table.Mapping{}, err
+		}
+
+		// Template column
+		if tmplColumn, ok := columnEntity.(column.Template); ok {
+			if columnPayload.Template == nil {
+				return table.Mapping{}, svcerrors.NewBadRequestError(errors.Errorf(`column "%s" is missing template`, columnPayload.Name))
+			}
+
+			// if err := m.jsonnetValidator.Validate(columnPayload.Template.Content); err != nil {
+			//	return table.Mapping{}, svcerrors.NewBadRequestError(errors.Errorf(`column "%s" template is invalid: %w`, columnPayload.Name, err))
+			//}
+
+			tmplColumn.Template.Language = columnPayload.Template.Language
+			tmplColumn.Template.Content = columnPayload.Template.Content
+			columnEntity = tmplColumn
+		}
+
+		entity.Columns = append(entity.Columns, columnEntity)
+	}
+
+	return entity, nil
+}
+
+func (m *Mapper) updateTableSinkEntity(entity *definition.TableSink, payload *api.UpdateSinkPayload) (err error) {
+	// Common table mapping
+	if payload.Table.Mapping != nil {
+		entity.Mapping, err = m.newTableSinkMappingEntity(payload.Table.Mapping)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Table type
+	if payload.Table.Type != nil {
+		entity.Type = *payload.Table.Type
+	}
+
+	// Table type specific fields
+	switch entity.Type {
+	case definition.TableTypeKeboola:
+		// Keboola table
+		entity.Keboola = &definition.KeboolaTable{}
+
+		// TableID
+		if payload.Table.TableID != nil {
+			if tableID, err := keboola.ParseTableID(string(*payload.Table.TableID)); err == nil {
+				entity.Keboola.TableID = tableID
+			} else {
+				return svcerrors.NewBadRequestError(errors.Errorf(`invalid "tableId" value "%s": %w`, *payload.Table.TableID, err))
+			}
+		}
+	default:
+		return svcerrors.NewBadRequestError(errors.Errorf(`unexpected "type" "%s"`, payload.Type.String()))
+	}
+
+	return err
+}
---
+++ internal/pkg/service/stream/api/mapper/source_response.go
@@ -0,0 +1,102 @@
+package mapper
+
+import (
+	"context"
+
+	etcd "go.etcd.io/etcd/client/v3"
+
+	svcerrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/iterator"
+	api "github.com/keboola/keboola-as-code/internal/pkg/service/stream/api/gen/stream"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/api/receive/receivectx"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/table/column"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+func (m *Mapper) NewSourceResponse(entity definition.Source) (*api.Source, error) {
+	out := &api.Source{
+		ProjectID:   entity.ProjectID,
+		BranchID:    entity.BranchID,
+		SourceID:    entity.SourceID,
+		Type:        entity.Type,
+		Name:        entity.Name,
+		Description: entity.Description,
+		Created:     m.NewCreatedResponse(entity.Created),
+		Version:     m.NewVersionResponse(entity.Version),
+		Deleted:     m.NewDeletedResponse(entity.SoftDeletable),
+		Disabled:    m.NewDisabledResponse(entity.Switchable),
+	}
+
+	// Type
+	switch out.Type {
+	case definition.SourceTypeHTTP:
+		out.HTTP = &api.HTTPSource{
+			URL: m.formatHTTPSourceURL(entity),
+		}
+	default:
+		return nil, svcerrors.NewBadRequestError(errors.Errorf(`unexpected "type" "%s"`, out.Type.String()))
+	}
+
+	return out, nil
+}
+
+func (m *Mapper) NewSourcesResponse(
+	ctx context.Context,
+	k key.BranchKey,
+	sinceId string,
+	limit int,
+	list func(...iterator.Option) iterator.DefinitionT[definition.Source],
+) (*api.SourcesList, error) {
+	sources, page, err := loadPage(ctx, sinceId, limit, etcd.SortAscend, list, m.NewSourceResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.SourcesList{
+		ProjectID: k.ProjectID,
+		BranchID:  k.BranchID,
+		Page:      page,
+		Sources:   sources,
+	}, nil
+}
+
+func (m *Mapper) NewTestResultResponse(sourceKey key.SourceKey, sinks []definition.Sink, receiveCtx *receivectx.Context) (*api.TestResult, error) {
+	result := &api.TestResult{
+		ProjectID: sourceKey.ProjectID,
+		BranchID:  sourceKey.BranchID,
+		SourceID:  sourceKey.SourceID,
+	}
+
+	renderer := column.NewRenderer()
+
+	for _, sink := range sinks {
+		row := &api.TestResultRow{}
+		for _, c := range sink.Table.Mapping.Columns {
+			csvValue, err := renderer.CSVValue(c, receiveCtx)
+			if err != nil {
+				return nil, err
+			}
+
+			row.Columns = append(row.Columns, &api.TestResultColumn{
+				Name:  c.ColumnName(),
+				Value: csvValue,
+			})
+		}
+
+		result.Tables = append(result.Tables, &api.TestResultTable{
+			SinkID:  sink.SinkID,
+			TableID: api.TableID(sink.Table.Keboola.TableID.String()),
+			Rows:    []*api.TestResultRow{row},
+		})
+	}
+
+	return result, nil
+}
+
+func (m *Mapper) formatHTTPSourceURL(entity definition.Source) string {
+	return m.httpSourcePublicURL.
+		JoinPath("projects", entity.ProjectID.String(), "sources", entity.SourceID.String(), entity.HTTP.Secret).
+		String()
+}
