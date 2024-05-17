8036d0df3288414205b46c678ee8f6fbfdfc56b6
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-17 11:18:23 +0200

    feat: etcdlogger.AssertFromFile

--- internal/pkg/utils/etcdlogger/assert.go
+++ internal/pkg/utils/etcdlogger/assert.go
@@ -1,16 +1,23 @@
+//nolint:forbidigo // no virtual fs
+
 package etcdlogger
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/keboola/go-utils/pkg/wildcards"
 	"github.com/stretchr/testify/assert"
 	"github.com/umisama/go-regexpcache"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 type tHelper interface {
 	Helper()
 }
 
-// Assert logs captured by the KVLogWrapper.
+// Assert compares logs captured by the KVLogWrapper.
 // Comments "// ..." and empty lines are ignored.
 func Assert(t assert.TestingT, expected, actual string) bool {
 	if h, ok := t.(tHelper); ok {
@@ -26,4 +33,23 @@ func Assert(t assert.TestingT, expected, actual string) bool {
 	actual = stripEmptyLines.ReplaceAllString(actual, "\n")
 
 	return wildcards.Assert(t, expected, actual)
+}
+
+// AssertFromFile compares logs captured by the KVLogWrapper.
+// Comments "// ..." and empty lines are ignored.
+func AssertFromFile(t assert.TestingT, path, actual string) bool {
+	result := false
+	data, err := os.ReadFile(path) //nolint:forbidigo // no virtual FS
+	if assert.NoError(t, err) || errors.Is(err, os.ErrNotExist) {
+		expected := string(data)
+		result = Assert(t, expected, actual)
+	}
+
+	// Dump actual state to the .out dir
+	outDir := filepath.Join(filepath.Dir(path), ".out")              //nolint:forbidigo // no virtual FS
+	filePath := filepath.Join(outDir, filepath.Base(path)+".actual") //nolint:forbidigo // no virtual FS
+	assert.NoError(t, os.MkdirAll(outDir, 0o750))                    //nolint:forbidigo // no virtual FS
+	assert.NoError(t, os.WriteFile(filePath, []byte(actual), 0o600)) //nolint:forbidigo // no virtual FS
+
+	return result
 }
