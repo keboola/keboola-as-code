4af400bc68a83942d68b60eb9f153f73af9b949a
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-20 16:57:27 +0200

    feat: Branch repository: exists

---
+++ internal/pkg/service/stream/definition/repository/branch/branch_exists_test.go
@@ -0,0 +1,54 @@
+package branch_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/keboola/go-client/pkg/keboola"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	serviceErrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
+)
+
+func TestBranchRepository_ExistsOrErr(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	now := utctime.MustParse("2000-01-01T01:00:00.000Z").Time()
+	by := test.ByUser()
+
+	d, _ := dependencies.NewMockedServiceScope(t)
+	repo := d.DefinitionRepository().Branch()
+
+	// Fixtures
+	projectID := keboola.ProjectID(123)
+	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 567}
+
+	// ExistsOrErr - not found
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		if err := repo.ExistsOrErr(branchKey).Do(ctx).Err(); assert.Error(t, err) {
+			assert.Equal(t, `branch "567" not found in the project`, err.Error())
+			serviceErrors.AssertErrorStatusCode(t, http.StatusNotFound, err)
+		}
+	}
+
+	// Create - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		branch := test.NewBranch(branchKey)
+		require.NoError(t, repo.Create(&branch, now, by).Do(ctx).Err())
+	}
+
+	// ExistsOrErr - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		assert.NoError(t, repo.ExistsOrErr(branchKey).Do(ctx).Err())
+	}
+}
