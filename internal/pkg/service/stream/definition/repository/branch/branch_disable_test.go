984f9462fe5639699a217284aea5cfd5471315b4
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-20 16:57:16 +0200

    feat: Branch repository: disable

---
+++ internal/pkg/service/stream/definition/repository/branch/branch_disable_test.go
@@ -0,0 +1,73 @@
+package branch_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/keboola/go-client/pkg/keboola"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	serviceErrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
+)
+
+func TestBranchRepository_Disable(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	now := utctime.MustParse("2000-01-01T01:00:00.000Z").Time()
+	by := test.ByUser()
+
+	d, mocked := dependencies.NewMockedServiceScope(t)
+	client := mocked.TestEtcdClient()
+	repo := d.DefinitionRepository().Branch()
+
+	// Fixtures
+	projectID := keboola.ProjectID(123)
+	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 567}
+
+	// Disable - not found
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		if err := repo.Disable(branchKey, now, by, "some reason").Do(ctx).Err(); assert.Error(t, err) {
+			assert.Equal(t, `branch "567" not found in the project`, err.Error())
+			serviceErrors.AssertErrorStatusCode(t, http.StatusNotFound, err)
+		}
+	}
+
+	// Create - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		branch := test.NewBranch(branchKey)
+		require.NoError(t, repo.Create(&branch, now, by).Do(ctx).Err())
+	}
+
+	// Get - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		require.NoError(t, repo.Get(branchKey).Do(ctx).Err())
+	}
+
+	// Disable - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		assert.NoError(t, repo.Disable(branchKey, now, by, "some reason").Do(ctx).Err())
+		etcdhelper.AssertKVsFromFile(t, client, "fixtures/branch_disable_snapshot_001.txt")
+	}
+
+	// Get - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		branch, err := repo.Get(branchKey).Do(ctx).ResultOrErr()
+		require.NoError(t, err)
+		assert.True(t, branch.IsDisabled())
+		assert.True(t, branch.IsDisabledAt(now))
+		assert.True(t, branch.IsDisabledDirectly())
+	}
+}
