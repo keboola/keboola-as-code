077d0258c9f00a87c863df68ec5a6ae817ce4afb
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-20 16:57:10 +0200

    feat: Branch repository: delete

---
+++ internal/pkg/service/stream/definition/repository/branch/branch_delete_test.go
@@ -0,0 +1,78 @@
+package branch_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/keboola/go-client/pkg/keboola"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	serviceErrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
+)
+
+func TestBranchRepository_SoftDelete(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	now := utctime.MustParse("2000-01-01T01:00:00.000Z").Time()
+	by := test.ByUser()
+
+	d, mocked := dependencies.NewMockedServiceScope(t)
+	client := mocked.TestEtcdClient()
+	repo := d.DefinitionRepository().Branch()
+
+	// Fixtures
+	projectID := keboola.ProjectID(123)
+	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 567}
+
+	// SoftDelete - not found
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		if err := repo.SoftDelete(branchKey, now, by).Do(ctx).Err(); assert.Error(t, err) {
+			assert.Equal(t, `branch "567" not found in the project`, err.Error())
+			serviceErrors.AssertErrorStatusCode(t, http.StatusNotFound, err)
+		}
+	}
+
+	// Create - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		branch := test.NewBranch(branchKey)
+		require.NoError(t, repo.Create(&branch, now, by).Do(ctx).Err())
+	}
+
+	// Get - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		require.NoError(t, repo.Get(branchKey).Do(ctx).Err())
+	}
+
+	// SoftDelete - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		assert.NoError(t, repo.SoftDelete(branchKey, now, by).Do(ctx).Err())
+		etcdhelper.AssertKVsFromFile(t, client, "fixtures/branch_delete_snapshot_001.txt")
+	}
+
+	// Get - not found
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		err := repo.Get(branchKey).Do(ctx).Err()
+		if assert.Error(t, err) {
+			assert.Equal(t, `branch "567" not found in the project`, err.Error())
+		}
+	}
+
+	// GetDeleted - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		assert.NoError(t, repo.GetDeleted(branchKey).Do(ctx).Err())
+	}
+}
