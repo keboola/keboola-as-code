3f81887554a494b117d8bee2e1d26c2449261507
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-21 06:44:18 +0200

    refactor: Update staging.File, (re)moved Keboola specific fields

--- internal/pkg/service/stream/storage/level/staging/file.go
+++ internal/pkg/service/stream/storage/level/staging/file.go
@@ -1,28 +1,34 @@
 package staging
 
 import (
-	"github.com/keboola/go-client/pkg/keboola"
+	"time"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/compression"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local"
 )
 
+const (
+	DefaultFileExpiration = 24 * time.Hour
+)
+
 type File struct {
 	// IsEmpty is set if the import was skipped because there is no slice in the file.
 	IsEmpty bool `json:"isEmpty,omitempty"`
+	// Provider of the staging file.
+	Provider FileProvider `json:"provider" validate:"required"`
 	// Compression configuration.
-	Compression compression.Config `json:"compression"  `
-	// UploadCredentials to the staging storage.
-	UploadCredentials           *keboola.FileUploadCredentials `json:"credentials" validate:"required"`
-	UploadCredentialsExpiration utctime.UTCTime                `json:"credentialsExpiration" validate:"required"`
+	Compression compression.Config `json:"compression" validate:"required"`
+	// Expiration determines how long it is possible to write to the staging file, e.g. due to expiring credentials.
+	Expiration utctime.UTCTime `json:"expiration" validate:"required"`
 }
 
-func NewFile(localFile local.File, credentials *keboola.FileUploadCredentials) File {
+type FileProvider string
+
+func NewFile(localFile local.File, openedAt time.Time) File {
 	// Note: Compression in the staging storage is same as in the local storage, but it can be modified in the future.
 	return File{
-		Compression:                 localFile.Compression,
-		UploadCredentials:           credentials,
-		UploadCredentialsExpiration: utctime.From(credentials.CredentialsExpiration()),
+		Compression: localFile.Compression,
+		Expiration:  utctime.From(openedAt.Add(DefaultFileExpiration)),
 	}
 }
