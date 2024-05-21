0f5cabe3245cb346b9c5c23963ac4f625c0ffc16
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-21 06:44:18 +0200

    refactor: Update staging.File, (re)moved Keboola specific fields

--- internal/pkg/service/stream/storage/level/staging/file_test.go
+++ internal/pkg/service/stream/storage/level/staging/file_test.go
@@ -1,52 +1,35 @@
 package staging
 
 import (
-	"context"
-	"strings"
 	"testing"
-
-	"github.com/keboola/go-client/pkg/keboola"
-	"github.com/stretchr/testify/assert"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/compression"
-	"github.com/keboola/keboola-as-code/internal/pkg/validator"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test/testvalidation"
 )
 
 func TestFile_Validation(t *testing.T) {
 	t.Parallel()
 
-	cases := []struct {
-		Name          string
-		ExpectedError string
-		Value         File
-	}{
+	cases := testvalidation.TestCases[File]{
 		{
-			Name:          "empty",
-			ExpectedError: "- \"credentials\" is a required field\n- \"credentialsExpiration\" is a required field",
-			Value: File{
-				Compression: compression.NewConfig(),
-			},
+			Name: "empty",
+			ExpectedError: `
+- "provider" is a required field
+- "compression" is a required field
+- "expiration" is a required field
+`,
+			Value: File{},
 		},
 		{
 			Name: "ok",
 			Value: File{
-				Compression:                 compression.NewConfig(),
-				UploadCredentials:           &keboola.FileUploadCredentials{},
-				UploadCredentialsExpiration: utctime.MustParse("2006-01-02T15:04:05.000Z"),
+				Provider:    "foo",
+				Compression: compression.NewConfig(),
+				Expiration:  utctime.MustParse("2006-01-02T15:04:05.000Z"),
 			},
 		},
 	}
 
-	// Run test cases
-	ctx := context.Background()
-	val := validator.New()
-	for _, tc := range cases {
-		err := val.Validate(ctx, tc.Value)
-		if tc.ExpectedError == "" {
-			assert.NoError(t, err, tc.Name)
-		} else if assert.Error(t, err, tc.Name) {
-			assert.Equal(t, strings.TrimSpace(tc.ExpectedError), strings.TrimSpace(err.Error()), tc.Name)
-		}
-	}
+	cases.Run(t)
 }
