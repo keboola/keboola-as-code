318b7ad493affd75a86cdfff16e902bf896d85a3
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-24 14:01:42 +0200

    tests: Add TestSourceRepository_MustNotExists

--- internal/pkg/service/stream/definition/repository/source/source_exists_test.go
+++ internal/pkg/service/stream/definition/repository/source/source_exists_test.go
@@ -70,3 +70,58 @@ func TestSourceRepository_ExistsOrErr(t *testing.T) {
 		assert.NoError(t, sourceRepo.ExistsOrErr(sourceKey).Do(ctx).Err())
 	}
 }
+
+func TestSourceRepository_MustNotExists(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	now := utctime.MustParse("2000-01-01T01:00:00.000Z").Time()
+	by := test.ByUser()
+
+	d, _ := dependencies.NewMockedServiceScope(t)
+	branchRepo := d.DefinitionRepository().Branch()
+	sourceRepo := d.DefinitionRepository().Source()
+
+	// Fixtures
+	projectID := keboola.ProjectID(123)
+	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 567}
+	sourceKey := key.SourceKey{BranchKey: branchKey, SourceID: "my-source"}
+
+	// MustNotExists - branch not found
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		if err := sourceRepo.MustNotExists(sourceKey).Do(ctx).Err(); assert.Error(t, err) {
+			assert.Equal(t, `branch "567" not found in the project`, err.Error())
+			serviceErrors.AssertErrorStatusCode(t, http.StatusNotFound, err)
+		}
+	}
+
+	// Create branch
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		branch := test.NewBranch(branchKey)
+		require.NoError(t, branchRepo.Create(&branch, now, by).Do(ctx).Err())
+	}
+
+	// MustNotExists - source not found - ok
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		require.NoError(t, sourceRepo.MustNotExists(sourceKey).Do(ctx).Err())
+	}
+
+	// Create source
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		source := test.NewSource(sourceKey)
+		require.NoError(t, sourceRepo.Create(&source, now, by, "Create source").Do(ctx).Err())
+	}
+
+	// MustNotExists - source found - error
+	// -----------------------------------------------------------------------------------------------------------------
+	{
+		if err := sourceRepo.MustNotExists(sourceKey).Do(ctx).Err(); assert.Error(t, err) {
+			assert.Equal(t, `source "my-source" already exists in the branch`, err.Error())
+			serviceErrors.AssertErrorStatusCode(t, http.StatusConflict, err)
+		}
+	}
+}
