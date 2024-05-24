a169cee8be086d5c9dc51ad7b9a3387c4012a45f
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-24 09:14:40 +0200

    feat: Improve etcdlogger

--- internal/pkg/utils/etcdlogger/assert.go
+++ internal/pkg/utils/etcdlogger/assert.go
@@ -19,7 +19,7 @@ type tHelper interface {
 
 // Assert compares logs captured by the KVLogWrapper.
 // Comments "// ..." and empty lines are ignored.
-func Assert(t assert.TestingT, expected, actual string) bool {
+func Assert(t assert.TestingT, expected, actual string, msgAndArgs ...any) bool {
 	if h, ok := t.(tHelper); ok {
 		h.Helper()
 	}
@@ -32,7 +32,7 @@ func Assert(t assert.TestingT, expected, actual string) bool {
 	expected = stripEmptyLines.ReplaceAllString(expected, "\n")
 	actual = stripEmptyLines.ReplaceAllString(actual, "\n")
 
-	return wildcards.Assert(t, expected, actual)
+	return wildcards.Assert(t, expected, actual, msgAndArgs...)
 }
 
 // AssertFromFile compares logs captured by the KVLogWrapper.
@@ -42,7 +42,7 @@ func AssertFromFile(t assert.TestingT, path, actual string) bool {
 	data, err := os.ReadFile(path) //nolint:forbidigo // no virtual FS
 	if assert.NoError(t, err) || errors.Is(err, os.ErrNotExist) {
 		expected := string(data)
-		result = Assert(t, expected, actual)
+		result = Assert(t, expected, actual, `defined in file "%s"`, path)
 	}
 
 	// Dump actual state to the .out dir
