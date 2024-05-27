e79ae0434ac88572f9b2ab811de9fa6de22118b9
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 08:57:07 +0200

    feat: Use AtomicOpFromCtx in plugin

--- internal/pkg/service/stream/plugin/executor.go
+++ internal/pkg/service/stream/plugin/executor.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition"
 	storage "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 )
@@ -16,7 +17,7 @@ type Executor struct {
 
 func (e *Executor) OnBranchSave(ctx context.Context, now time.Time, by definition.By, old, updated *definition.Branch) error {
 	e.logger.Infof(ctx, `executing OnBranchSave "%s"`, updated.BranchKey.String())
-	ctx = context.WithValue(ctx, updatedBranch, updated)
+	op.AtomicOpFromCtx(ctx).SetValue(updated.BranchKey, updated)
 	return e.collection.onBranchSave.forEach(func(fn onBranchSaveFn) error {
 		return fn(ctx, now, by, old, updated)
 	})
@@ -24,7 +25,7 @@ func (e *Executor) OnBranchSave(ctx context.Context, now time.Time, by definitio
 
 func (e *Executor) OnSourceSave(ctx context.Context, now time.Time, by definition.By, old, updated *definition.Source) error {
 	e.logger.Infof(ctx, `executing OnSourceSave "%s"`, updated.SourceKey.String())
-	ctx = context.WithValue(ctx, updatedSource, updated)
+	op.AtomicOpFromCtx(ctx).SetValue(updated.SourceKey, updated)
 	return e.collection.onSourceSave.forEach(func(fn onSourceSaveFn) error {
 		return fn(ctx, now, by, old, updated)
 	})
@@ -32,7 +33,7 @@ func (e *Executor) OnSourceSave(ctx context.Context, now time.Time, by definitio
 
 func (e *Executor) OnSinkSave(ctx context.Context, now time.Time, by definition.By, old, updated *definition.Sink) error {
 	e.logger.Infof(ctx, `executing OnSinkSave "%s"`, updated.SinkKey.String())
-	ctx = context.WithValue(ctx, updatedSink, updated)
+	op.AtomicOpFromCtx(ctx).SetValue(updated.SinkKey, updated)
 	return e.collection.onSinkSave.forEach(func(fn onSinkSaveFn) error {
 		return fn(ctx, now, by, old, updated)
 	})
@@ -40,7 +41,7 @@ func (e *Executor) OnSinkSave(ctx context.Context, now time.Time, by definition.
 
 func (e *Executor) OnFileOpen(ctx context.Context, now time.Time, sink definition.Sink, file *storage.File) error {
 	e.logger.Infof(ctx, `executing OnFileOpen "%s"`, file.SinkKey.String())
-	ctx = context.WithValue(ctx, updatedFile, file)
+	op.AtomicOpFromCtx(ctx).SetValue(file.FileKey, file)
 	return e.collection.onFileOpen.forEach(func(fn onFileOpenFn) error {
 		return fn(ctx, now, sink, file)
 	})
@@ -48,7 +49,7 @@ func (e *Executor) OnFileOpen(ctx context.Context, now time.Time, sink definitio
 
 func (e *Executor) OnFileSave(ctx context.Context, now time.Time, original, updated *storage.File) error {
 	e.logger.Infof(ctx, `executing OnFileSave "%s"`, updated.FileKey.String())
-	ctx = context.WithValue(ctx, updatedFile, updated)
+	op.AtomicOpFromCtx(ctx).SetValue(updated.FileKey, updated)
 	return e.collection.onFileSave.forEach(func(fn onFileSaveFn) error {
 		return fn(ctx, now, original, updated)
 	})
@@ -56,7 +57,7 @@ func (e *Executor) OnFileSave(ctx context.Context, now time.Time, original, upda
 
 func (e *Executor) OnSliceOpen(ctx context.Context, now time.Time, file storage.File, slice *storage.Slice) error {
 	e.logger.Infof(ctx, `executing OnSliceOpen "%s"`, slice.SliceKey.String())
-	ctx = context.WithValue(ctx, updatedSlice, slice)
+	op.AtomicOpFromCtx(ctx).SetValue(slice.SliceKey, slice)
 	return e.collection.onSliceOpen.forEach(func(fn onSliceOpenFn) error {
 		return fn(ctx, now, file, slice)
 	})
@@ -64,7 +65,7 @@ func (e *Executor) OnSliceOpen(ctx context.Context, now time.Time, file storage.
 
 func (e *Executor) OnSliceSave(ctx context.Context, now time.Time, old, updated *storage.Slice) error {
 	e.logger.Infof(ctx, `executing OnSliceSave "%s"`, updated.SliceKey.String())
-	ctx = context.WithValue(ctx, updatedSlice, updated)
+	op.AtomicOpFromCtx(ctx).SetValue(updated.SliceKey, updated)
 	return e.collection.onSliceSave.forEach(func(fn onSliceSaveFn) error {
 		return fn(ctx, now, old, updated)
 	})
