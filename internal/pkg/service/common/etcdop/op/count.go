99419cc9399ffae7fc9eb5dcb62a6fdd5f09ef6c
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 07:28:37 +0200

    refactor: Unify *op.RawResponse usage, use always pointer

--- internal/pkg/service/common/etcdop/op/count.go
+++ internal/pkg/service/common/etcdop/op/count.go
@@ -9,7 +9,7 @@ import (
 type CountOp = WithResult[int64]
 
 // CountMapper an etcd response to a count.
-type CountMapper func(ctx context.Context, raw RawResponse) (int64, error)
+type CountMapper func(ctx context.Context, raw *RawResponse) (int64, error)
 
 // NewCountOp wraps an operation, the result of which is a count.
 func NewCountOp(client etcd.KV, factory LowLevelFactory, mapper CountMapper) CountOp {
