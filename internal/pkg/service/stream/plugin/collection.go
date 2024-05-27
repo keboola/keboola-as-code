db41d248a3be8186e00c323ec8d7608aa59d0e80
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 08:56:51 +0200

    feat: Add more methods to plugin.Collection

--- internal/pkg/service/stream/plugin/collection.go
+++ internal/pkg/service/stream/plugin/collection.go
@@ -37,9 +37,45 @@ func (c *Collection) OnBranchSave(fn onBranchSaveFn) {
 	c.onBranchSave = append(c.onBranchSave, fn)
 }
 
+func (c *Collection) OnBranchEnable(fn onBranchSaveFn) {
+	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
+		if isEnabledNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnBranchDisable(fn onBranchSaveFn) {
+	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
+		if isDisabledNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnBranchActivation(fn onBranchSaveFn) {
+	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
+		if isActivatedNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnBranchDeactivation(fn onBranchSaveFn) {
+	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
+		if isDeactivatedNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
 func (c *Collection) OnBranchDelete(fn onBranchSaveFn) {
 	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
-		if isDeletedNow(now, branch) {
+		if isDeletedNow(original, branch) {
 			return fn(ctx, now, by, original, branch)
 		}
 		return nil
@@ -48,7 +84,7 @@ func (c *Collection) OnBranchDelete(fn onBranchSaveFn) {
 
 func (c *Collection) OnBranchUndelete(fn onBranchSaveFn) {
 	c.onBranchSave = append(c.onBranchSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Branch) error {
-		if isUndeletedNow(now, branch) {
+		if isUndeletedNow(original, branch) {
 			return fn(ctx, now, by, original, branch)
 		}
 		return nil
@@ -59,9 +95,54 @@ func (c *Collection) OnSourceSave(fn onSourceSaveFn) {
 	c.onSourceSave = append(c.onSourceSave, fn)
 }
 
+func (c *Collection) OnSourceEnabled(fn onSourceSaveFn) {
+	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
+		if isEnabledNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSourceDisabled(fn onSourceSaveFn) {
+	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
+		if isDisabledNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSourceActivation(fn onSourceSaveFn) {
+	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
+		if isActivatedNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSourceDeactivation(fn onSourceSaveFn) {
+	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
+		if isDeactivatedNow(original, branch) {
+			return fn(ctx, now, by, original, branch)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSourceModification(fn onSourceSaveFn) {
+	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Source) error {
+		if !isActivatedNow(original, updated) && !isDeactivatedNow(original, updated) && !reflect.DeepEqual(original, updated) {
+			return fn(ctx, now, by, original, updated)
+		}
+		return nil
+	})
+}
+
 func (c *Collection) OnSourceDelete(fn onSourceSaveFn) {
 	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
-		if isDeletedNow(now, branch) {
+		if isDeletedNow(original, branch) {
 			return fn(ctx, now, by, original, branch)
 		}
 		return nil
@@ -70,7 +151,7 @@ func (c *Collection) OnSourceDelete(fn onSourceSaveFn) {
 
 func (c *Collection) OnSourceUndelete(fn onSourceSaveFn) {
 	c.onSourceSave = append(c.onSourceSave, func(ctx context.Context, now time.Time, by definition.By, original, branch *definition.Source) error {
-		if isUndeletedNow(now, branch) {
+		if isUndeletedNow(original, branch) {
 			return fn(ctx, now, by, original, branch)
 		}
 		return nil
@@ -81,9 +162,27 @@ func (c *Collection) OnSinkSave(fn onSinkSaveFn) {
 	c.onSinkSave = append(c.onSinkSave, fn)
 }
 
+func (c *Collection) OnSinkEnabled(fn onSinkSaveFn) {
+	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Sink) error {
+		if isEnabledNow(original, updated) {
+			return fn(ctx, now, by, original, updated)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSinkDisabled(fn onSinkSaveFn) {
+	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Sink) error {
+		if isDisabledNow(original, updated) {
+			return fn(ctx, now, by, original, updated)
+		}
+		return nil
+	})
+}
+
 func (c *Collection) OnSinkActivation(fn onSinkSaveFn) {
 	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Sink) error {
-		if isActivatedNow(now, updated) {
+		if isActivatedNow(original, updated) {
 			return fn(ctx, now, by, original, updated)
 		}
 		return nil
@@ -92,7 +191,7 @@ func (c *Collection) OnSinkActivation(fn onSinkSaveFn) {
 
 func (c *Collection) OnSinkDeactivation(fn onSinkSaveFn) {
 	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Sink) error {
-		if isDeactivatedNow(now, updated) {
+		if isDeactivatedNow(original, updated) {
 			return fn(ctx, now, by, original, updated)
 		}
 		return nil
@@ -100,9 +199,9 @@ func (c *Collection) OnSinkDeactivation(fn onSinkSaveFn) {
 }
 
 func (c *Collection) OnSinkModification(fn onSinkSaveFn) {
-	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, old, updated *definition.Sink) error {
-		if !isActivatedNow(now, updated) && !isDeactivatedNow(now, updated) && !reflect.DeepEqual(old, updated) {
-			return fn(ctx, now, by, old, updated)
+	c.onSinkSave = append(c.onSinkSave, func(ctx context.Context, now time.Time, by definition.By, original, updated *definition.Sink) error {
+		if !isActivatedNow(original, updated) && !isDeactivatedNow(original, updated) && !reflect.DeepEqual(original, updated) {
+			return fn(ctx, now, by, original, updated)
 		}
 		return nil
 	})
@@ -110,6 +209,24 @@ func (c *Collection) OnSinkModification(fn onSinkSaveFn) {
 
 func (c *Collection) OnFileOpen(fn onFileOpenFn) {
 	c.onFileOpen = append(c.onFileOpen, fn)
+}
+
+func (c *Collection) OnFileClose(fn onFileSaveFn) {
+	c.OnFileSave(func(ctx context.Context, now time.Time, original, file *storage.File) error {
+		if original != nil && original.State != file.State && file.State == storage.FileClosing {
+			return fn(ctx, now, original, file)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnFileUpdate(fn onFileSaveFn) {
+	c.OnFileSave(func(ctx context.Context, now time.Time, original, file *storage.File) error {
+		if original != nil && !file.Deleted && !reflect.DeepEqual(original, file) {
+			return fn(ctx, now, original, file)
+		}
+		return nil
+	})
 }
 
 func (c *Collection) OnFileDelete(fn onFileSaveFn) {
@@ -127,6 +244,24 @@ func (c *Collection) OnFileSave(fn onFileSaveFn) {
 
 func (c *Collection) OnSliceOpen(fn onSliceOpenFn) {
 	c.onSliceOpen = append(c.onSliceOpen, fn)
+}
+
+func (c *Collection) OnSliceClose(fn onSliceSaveFn) {
+	c.OnSliceSave(func(ctx context.Context, now time.Time, original, slice *storage.Slice) error {
+		if original != nil && original.State != slice.State && slice.State == storage.SliceClosing {
+			return fn(ctx, now, original, slice)
+		}
+		return nil
+	})
+}
+
+func (c *Collection) OnSliceUpdate(fn onSliceSaveFn) {
+	c.OnSliceSave(func(ctx context.Context, now time.Time, original, slice *storage.Slice) error {
+		if original != nil && !slice.Deleted && !reflect.DeepEqual(original, slice) {
+			return fn(ctx, now, original, slice)
+		}
+		return nil
+	})
 }
 
 func (c *Collection) OnSliceDelete(fn onSliceSaveFn) {
