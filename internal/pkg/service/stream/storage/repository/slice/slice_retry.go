192f88c5ce2a15933c4a785183ae59aabaf14f01
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 16:45:23 +0200

    feat: Slice repository: increment retry attempt

---
+++ internal/pkg/service/stream/storage/repository/slice/slice_retry.go
@@ -0,0 +1,17 @@
+package slice
+
+import (
+	"time"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
+)
+
+// IncrementRetryAttempt increments retry attempt and backoff delay on an error.
+// Retry is reset on a state transition, model.Slice.WithState method.
+func (r *Repository) IncrementRetryAttempt(now time.Time, sliceKey model.SliceKey, reason string) *op.AtomicOp[model.Slice] {
+	return r.update(sliceKey, now, func(slice model.Slice) (model.Slice, error) {
+		slice.IncrementRetryAttempt(r.backoff, now, reason)
+		return slice, nil
+	})
+}
