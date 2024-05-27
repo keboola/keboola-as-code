274827f2b4d182e3ab090de9010b40c327d3fc39
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 16:44:14 +0200

    feat: Slice repository: new slice

---
+++ internal/pkg/service/stream/storage/repository/slice/slice_new_test.go
@@ -0,0 +1,31 @@
+package slice
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/utctime"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/compression"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
+)
+
+func TestNewSlice_InvalidCompressionType(t *testing.T) {
+	t.Parallel()
+
+	// Fixtures
+	now := utctime.MustParse("2000-01-01T01:00:00.000Z").Time()
+
+	// Create file
+	file := model.File{}
+
+	// Set unsupported compression type
+	file.LocalStorage.Compression.Type = compression.TypeZSTD
+
+	// Assert
+	r := &Repository{}
+	_, err := r.newSlice(now, file, "my-volume")
+	require.Error(t, err)
+	assert.Equal(t, `file compression type "zstd" is not supported`, err.Error())
+}
