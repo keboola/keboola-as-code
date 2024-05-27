5c9142fc9dd96449a15ce4534edf0d19ff45b1e9
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 08:59:28 +0200

    feat: Update definition repository

--- internal/pkg/service/stream/definition/repository/branch/branch_undelete.go
+++ internal/pkg/service/stream/definition/repository/branch/branch_undelete.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/keboola/go-utils/pkg/deepcopy"
+
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
@@ -13,19 +15,20 @@ import (
 // if they were deleted in cascade with the Branch (SoftDeletable.Deleted.Directly == false).
 func (r *Repository) Undelete(k key.BranchKey, now time.Time, by definition.By) *op.AtomicOp[definition.Branch] {
 	// Move entity from the deleted to the active prefix
-	var created definition.Branch
-	return op.Atomic(r.client, &created).
+	var old, undeleted definition.Branch
+	return op.Atomic(r.client, &undeleted).
 		// Check prerequisites
 		Read(func(ctx context.Context) op.Op {
 			return r.checkMaxBranchesPerProject(k.ProjectID, 1)
 		}).
 		// Read the entity
 		Read(func(ctx context.Context) op.Op {
-			return r.GetDeleted(k).WithResultTo(&created)
+			return r.GetDeleted(k).WithResultTo(&old)
 		}).
 		// Mark undeleted
 		Write(func(ctx context.Context) op.Op {
-			created.Undelete(now, by)
-			return r.save(ctx, now, by, nil, &created)
+			undeleted = deepcopy.Copy(old).(definition.Branch)
+			undeleted.Undelete(now, by)
+			return r.save(ctx, now, by, &old, &undeleted)
 		})
 }
