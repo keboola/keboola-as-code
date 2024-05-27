4e751b1463087e7c216f5cd2048550df9cd5203d
Michal Jurečko <michal.jurecko@gmail.com>
2024-05-27 16:44:36 +0200

    feat: Slice repository: slice close

---
+++ internal/pkg/service/stream/storage/repository/slice/slice_close.go
@@ -0,0 +1,33 @@
+package slice
+
+import (
+	"context"
+	"time"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
+)
+
+func (r *Repository) closeSliceOnFileClose() {
+	r.plugins.Collection().OnFileClose(func(ctx context.Context, now time.Time, original, file *model.File) error {
+		op.AtomicOpFromCtx(ctx).AddFrom(r.closeSlicesInFile(*file, now))
+		return nil
+	})
+}
+
+// closeSlicesInFile all active slices, in the SliceWriting state, in the file.
+// Slices are switched to the SliceClosing state.
+func (r *Repository) closeSlicesInFile(file model.File, now time.Time) *op.AtomicOp[[]model.Slice] {
+	var slices, closedSlices []model.Slice
+	return op.Atomic(r.client, &closedSlices).
+		// Load active slices
+		Read(func(ctx context.Context) op.Op {
+			return r.ListInState(file.FileKey, model.SliceWriting).WithAllTo(&slices)
+		}).
+		// Close active slices
+		Write(func(ctx context.Context) op.Op {
+			return r.
+				switchStateInBatch(ctx, file.State, slices, now, model.SliceWriting, model.SliceClosing).
+				SetResultTo(&closedSlices)
+		})
+}
