e8e2e9a1af8d71c0d8e2d8b69098d5eac3696c7d
Martin Vaško <Matovidlo2@gmail.com>
2024-06-03 16:40:11 +0200

    lint: Add intrange linter to reduce code complexity.

--- internal/pkg/service/common/etcdop/prefix_test.go
+++ internal/pkg/service/common/etcdop/prefix_test.go
@@ -267,7 +267,7 @@ func BenchmarkPrefix_GetAll(b *testing.B) {
 	if err := Key("key0").Put(client, "foo").Do(ctx).Err(); err != nil {
 		b.Fatalf("cannot create etcd key: %s", err)
 	}
-	for i := 0; i < 100; i++ {
+	for i := range 100 {
 		key := fmt.Sprintf("key%04d", i)
 		if err := pfx.Key(key).Put(client, "bar").Do(ctx).Err(); err != nil {
 			b.Fatalf(`cannot create etcd key "%s": %s`, key, err)
@@ -367,7 +367,7 @@ func BenchmarkPrefixT_GetAll(b *testing.B) {
 	if err := Key("key0").Put(client, "foo").Do(ctx).Err(); err != nil {
 		b.Fatalf("cannot create etcd key: %s", err)
 	}
-	for i := 0; i < 100; i++ {
+	for i := range 100 {
 		key := fmt.Sprintf("key%04d", i)
 		if err := pfx.Key(key).Put(client, "bar").Do(ctx).Err(); err != nil {
 			b.Fatalf(`cannot create etcd key "%s": %s`, key, err)
