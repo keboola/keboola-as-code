d6ca8db2fd45efd602647c00ebe164214d4dabb4
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-07 15:16:44 +0200

    fix: Remove useless query when creating a sink

--- internal/pkg/service/stream/storage/statistics/repository/stats_allocation_test.go
+++ internal/pkg/service/stream/storage/statistics/repository/stats_allocation_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdhelper"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/etcdlogger"
 )
 
@@ -38,6 +39,7 @@ func TestRepository_EstimateSliceSizeOnSliceCreate(t *testing.T) {
 	branchKey := key.BranchKey{ProjectID: projectID, BranchID: 456}
 	sourceKey := key.SourceKey{BranchKey: branchKey, SourceID: "my-source"}
 	sinkKey := key.SinkKey{SourceKey: sourceKey, SinkID: "my-sink"}
+	ignoredKeys := etcdhelper.WithIgnoredKeyPattern("^definition/|storage/file/all/|storage/slice/all/|storage/volume/|storage/stats/")
 
 	// Get services
 	d, mocked := dependencies.NewMockedLocalStorageScope(t, commonDeps.WithClock(clk))
@@ -63,15 +65,31 @@ func TestRepository_EstimateSliceSizeOnSliceCreate(t *testing.T) {
 		test.RegisterWriterVolumes(t, ctx, volumeRepo, session, 1)
 	}
 
-	// Create branch, source, sink, file, slice
+	// Create branch, source
 	// -----------------------------------------------------------------------------------------------------------------
 	{
 		branch := test.NewBranch(branchKey)
 		require.NoError(t, defRepo.Branch().Create(&branch, clk.Now(), by).Do(ctx).Err())
 		source := test.NewSource(sourceKey)
 		require.NoError(t, defRepo.Source().Create(&source, clk.Now(), by, "Create source").Do(ctx).Err())
+	}
+
+	// Create sink, file, slice
+	// -----------------------------------------------------------------------------------------------------------------
+	var createEtcdLogs string
+	{
 		sink := test.NewSinkWithLocalStorage(sinkKey)
+		etcdLogs.Reset()
 		require.NoError(t, defRepo.Sink().Create(&sink, clk.Now(), by, "Create sink").Do(ctx).Err())
+		createEtcdLogs = etcdLogs.String()
+	}
+	{
+		// Etcd state
+		etcdhelper.AssertKVsFromFile(t, client, "fixtures/stats_alloc_on_create_snapshot.txt", ignoredKeys)
+	}
+	{
+		// Etcd operations
+		etcdlogger.AssertFromFile(t, "fixtures/stats_alloc_on_create_ops.txt", createEtcdLogs)
 	}
 
 	// Create 2 more files/slices (3 totally)
@@ -130,16 +148,29 @@ func TestRepository_EstimateSliceSizeOnSliceCreate(t *testing.T) {
 
 	// Create file4/slice4 - pre-allocated disk space should be calculated from the slice1, CompressedSize:   1000
 	// -----------------------------------------------------------------------------------------------------------------
+	var rotateEtcdLogs string
 	{
 		clk.Add(time.Hour)
+
+		etcdLogs.Reset()
 		file4, err := fileRepo.Rotate(sinkKey, clk.Now()).Do(ctx).ResultOrErr()
 		require.NoError(t, err)
+		rotateEtcdLogs = etcdLogs.String()
 		assert.True(t, file4.LocalStorage.DiskAllocation.Enabled)
 		assert.Equal(t, 110, file4.LocalStorage.DiskAllocation.Relative)
+
 		slices, err := sliceRepo.ListIn(file4.FileKey).Do(ctx).All()
 		require.NoError(t, err)
 		require.Len(t, slices, 1)
 		slice4 := slices[0]
 		assert.Equal(t, datasize.ByteSize(1000*110/100), slice4.LocalStorage.AllocatedDiskSpace)
+	}
+	{
+		// Etcd state
+		etcdhelper.AssertKVsFromFile(t, client, "fixtures/stats_alloc_on_rotate_snapshot.txt", ignoredKeys)
+	}
+	{
+		// Etcd operations
+		etcdlogger.AssertFromFile(t, "fixtures/stats_alloc_on_rotate_ops.txt", rotateEtcdLogs)
 	}
 }
