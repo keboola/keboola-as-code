72281a0fa17dd7dc756244862920b2b9fa8a3f59
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-07 08:23:36 +0200

    feat: Bridge: Provider of authorized Keboola Storage API

---
+++ internal/pkg/service/stream/keboolasink/bridge/api.go
@@ -0,0 +1,20 @@
+package bridge
+
+import (
+	"context"
+
+	"github.com/keboola/go-client/pkg/keboola"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+// apiProvider defines how the API instance should be obtained from the context.
+type apiProvider func(ctx context.Context) *keboola.AuthorizedAPI
+
+func (fn apiProvider) APIFromContext(ctx context.Context) (*keboola.AuthorizedAPI, error) {
+	api := fn(ctx)
+	if api == nil {
+		return nil, errors.New("keboola.AuthorizedAPI is not present in the context")
+	}
+	return api, nil
+}
