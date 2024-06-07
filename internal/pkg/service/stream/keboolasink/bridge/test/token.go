9f13ccfa0d2d1d28394310dab611a9a8d2165ac7
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-07 08:16:11 +0200

    tests: Bridge: test helpers

---
+++ internal/pkg/service/stream/keboolasink/bridge/test/token.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"io"
+	"net/http"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/keboola/go-client/pkg/keboola"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/encoding/json"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+func MockTokenStorageAPICalls(t *testing.T, transport *httpmock.MockTransport) {
+	t.Helper()
+	lock := &sync.Mutex{}
+
+	tokenCounter := 1000
+	transport.RegisterResponder(
+		http.MethodPost,
+		`=~/v2/storage/tokens$`,
+		func(request *http.Request) (*http.Response, error) {
+			lock.Lock()
+			defer lock.Unlock()
+
+			dataBytes, err := io.ReadAll(request.Body)
+			if err != nil {
+				return nil, err
+			}
+
+			token := keboola.Token{}
+			if err := json.Decode(dataBytes, &token); err != nil {
+				return nil, err
+			}
+
+			tokenCounter++
+			token.ID = strconv.Itoa(tokenCounter)
+			token.Token = "my-token"
+			return httpmock.NewJsonResponse(http.StatusCreated, token)
+		},
+	)
+
+	transport.RegisterResponder(
+		http.MethodDelete,
+		`=~/v2/storage/tokens/[0-9]+$`,
+		func(request *http.Request) (*http.Response, error) {
+			lock.Lock()
+			defer lock.Unlock()
+
+			tokenID, err := extractTokenIDFromURL(request.URL.String())
+			if err != nil {
+				return nil, err
+			}
+
+			tokenIDInt, err := strconv.Atoi(tokenID)
+			if err != nil {
+				return nil, err
+			}
+
+			if tokenIDInt < tokenCounter {
+				return nil, errors.Errorf(`unexpected token ID "%d"`, tokenIDInt)
+			}
+
+			return httpmock.NewStringResponse(http.StatusNoContent, ""), nil
+		},
+	)
+}
