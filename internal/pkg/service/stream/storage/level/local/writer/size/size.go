5a8db6a3062a82e8e19dd4aba3a6aaad0eb4dae2
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-10 11:54:04 +0200

    refactor: Save size.Meter backup periodically, not on each flush

--- internal/pkg/service/stream/storage/level/local/writer/size/size.go
+++ internal/pkg/service/stream/storage/level/local/writer/size/size.go
@@ -2,13 +2,18 @@
 package size
 
 import (
+	"context"
 	"io"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/benbjohnson/clock"
 	"github.com/c2h5oh/datasize"
 
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/strhelper"
 )
@@ -16,13 +21,16 @@ import (
 // Meter implements io.Writer interface to measure size of the data written to the underlying writer.
 type Meter struct {
 	w    io.Writer
+	lock sync.Mutex
 	size datasize.ByteSize
 }
 
 // MeterWithBackup in addition to Meter allows to read and save the counter value to a backup file.
+// The backup is saved automatically if backupInterval > 0 , and manually using the SyncBackup or the Close methods.
 type MeterWithBackup struct {
 	*Meter
-	backup backup
+	backup       backup
+	backupTicker *clock.Ticker
 }
 
 // backup interface contains used methods from *os.File.
@@ -37,13 +45,13 @@ func NewMeter(w io.Writer) *Meter {
 	return &Meter{w: w}
 }
 
-func NewMeterWithBackupFile(w io.Writer, filePath string) (*MeterWithBackup, error) {
-	backupFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0o640)
+func NewMeterWithBackupFile(ctx context.Context, clk clock.Clock, logger log.Logger, w io.Writer, backupPath string, backupInterval time.Duration) (*MeterWithBackup, error) {
+	backupFile, err := os.OpenFile(backupPath, os.O_CREATE|os.O_RDWR, 0o640)
 	if err != nil {
 		return nil, err
 	}
 
-	meter, err := NewMeterWithBackup(w, backupFile)
+	meter, err := NewMeterWithBackup(ctx, clk, logger, w, backupFile, backupInterval)
 	if err != nil {
 		_ = backupFile.Close()
 		return nil, err
@@ -52,7 +60,7 @@ func NewMeterWithBackupFile(w io.Writer, filePath string) (*MeterWithBackup, err
 	return meter, nil
 }
 
-func NewMeterWithBackup(w io.Writer, backup backup) (*MeterWithBackup, error) {
+func NewMeterWithBackup(ctx context.Context, clk clock.Clock, logger log.Logger, w io.Writer, backup backup, backupInterval time.Duration) (*MeterWithBackup, error) {
 	// Read value
 	buffer := make([]byte, 32)
 	n, err := io.ReadFull(backup, buffer)
@@ -72,22 +80,41 @@ func NewMeterWithBackup(w io.Writer, backup backup) (*MeterWithBackup, error) {
 	}
 
 	// Create writer and set the counter value
-	out := &MeterWithBackup{Meter: NewMeter(w), backup: backup}
-	out.size = datasize.ByteSize(value)
-	return out, nil
+	m := &MeterWithBackup{Meter: NewMeter(w), backup: backup}
+	m.size = datasize.ByteSize(value)
+
+	// Start backup ticker
+	if backupInterval > 0 {
+		m.backupTicker = clk.Ticker(backupInterval)
+		go func() {
+			for range m.backupTicker.C {
+				if err = m.SyncBackup(); err != nil {
+					err = errors.PrefixErrorf(err, `cannot flush meter backup %v`, backup)
+					logger.Error(ctx, err.Error())
+				}
+			}
+		}()
+	}
+
+	return m, nil
 }
 
-func (w *Meter) Write(p []byte) (int, error) {
-	n, err := w.w.Write(p)
-	w.size += datasize.ByteSize(n)
+func (m *Meter) Write(p []byte) (int, error) {
+	n, err := m.w.Write(p)
+	m.lock.Lock()
+	m.size += datasize.ByteSize(n)
+	m.lock.Unlock()
 	return n, err
 }
 
-func (w *Meter) Size() datasize.ByteSize {
-	return w.size
+func (m *Meter) Size() datasize.ByteSize {
+	m.lock.Lock()
+	size := m.size
+	m.lock.Unlock()
+	return size
 }
 
-func (w *MeterWithBackup) Flush() error {
+func (w *MeterWithBackup) SyncBackup() error {
 	// Seek to the beginning of the file
 	// The size counter can only grow, so it guarantees that the entire file will be overwritten.
 	if _, err := w.backup.Seek(0, io.SeekStart); err != nil {
@@ -102,7 +129,11 @@ func (w *MeterWithBackup) Flush() error {
 }
 
 func (w *MeterWithBackup) Close() error {
-	if err := w.Flush(); err != nil {
+	if w.backupTicker != nil {
+		w.backupTicker.Stop()
+	}
+
+	if err := w.SyncBackup(); err != nil {
 		return err
 	}
 
