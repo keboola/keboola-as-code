4e851b552ddc9c3dd5c95586b25a3f7cf687ee7b
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-11 13:04:23 +0200

    feat: Optimize logger, if debug level is off

--- internal/pkg/log/logger.go
+++ internal/pkg/log/logger.go
@@ -59,19 +59,27 @@ func (l *zapLogger) WithDuration(v time.Duration) Logger {
 }
 
 func (l *zapLogger) Debug(ctx context.Context, message string) {
-	l.logger.Debug(l.message(ctx, message), l.fields(ctx)...)
+	if l.core.Enabled(DebugLevel) {
+		l.logger.Debug(l.message(ctx, message), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Info(ctx context.Context, message string) {
-	l.logger.Info(l.message(ctx, message), l.fields(ctx)...)
+	if l.core.Enabled(InfoLevel) {
+		l.logger.Info(l.message(ctx, message), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Warn(ctx context.Context, message string) {
-	l.logger.Warn(l.message(ctx, message), l.fields(ctx)...)
+	if l.core.Enabled(WarnLevel) {
+		l.logger.Warn(l.message(ctx, message), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Error(ctx context.Context, message string) {
-	l.logger.Error(l.message(ctx, message), l.fields(ctx)...)
+	if l.core.Enabled(ErrorLevel) {
+		l.logger.Error(l.message(ctx, message), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Log(ctx context.Context, level, message string) {
@@ -79,19 +87,27 @@ func (l *zapLogger) Log(ctx context.Context, level, message string) {
 }
 
 func (l *zapLogger) Debugf(ctx context.Context, template string, args ...any) {
-	l.logger.Debug(l.template(ctx, template, args), l.fields(ctx)...)
+	if l.core.Enabled(DebugLevel) {
+		l.logger.Debug(l.template(ctx, template, args), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Infof(ctx context.Context, template string, args ...any) {
-	l.logger.Info(l.template(ctx, template, args), l.fields(ctx)...)
+	if l.core.Enabled(InfoLevel) {
+		l.logger.Info(l.template(ctx, template, args), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Warnf(ctx context.Context, template string, args ...any) {
-	l.logger.Warn(l.template(ctx, template, args), l.fields(ctx)...)
+	if l.core.Enabled(WarnLevel) {
+		l.logger.Warn(l.template(ctx, template, args), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Errorf(ctx context.Context, template string, args ...any) {
-	l.logger.Error(l.template(ctx, template, args), l.fields(ctx)...)
+	if l.core.Enabled(ErrorLevel) {
+		l.logger.Error(l.template(ctx, template, args), l.fields(ctx)...)
+	}
 }
 
 func (l *zapLogger) Logf(ctx context.Context, level, template string, args ...any) {
