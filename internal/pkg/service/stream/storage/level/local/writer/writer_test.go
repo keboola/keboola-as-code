b129950557c75e48fbccb803e3f45d57dadafa53
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-12 12:34:27 +0200

    refactor: writer.Base -> writer.Writer

--- internal/pkg/service/stream/storage/level/local/writer/writer_test.go
+++ internal/pkg/service/stream/storage/level/local/writer/writer_test.go
@@ -1,4 +1,4 @@
-package writer
+package writer_test
 
 import (
 	"context"
@@ -11,56 +11,39 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/writer/writechain"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/writer"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/writer/test"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/test"
 	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 	"github.com/keboola/keboola-as-code/internal/pkg/validator"
 )
 
-func TestBaseWriter(t *testing.T) {
+func TestWriter(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
+	cfg := writer.NewConfig()
 	logger := log.NewDebugLogger()
 	clk := clock.New()
 	dirPath := t.TempDir()
 	filePath := filepath.Join(dirPath, "file")
 	slice := newTestSlice(t)
-
 	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
 	assert.NoError(t, err)
-	w := NewBaseWriter(logger, clk, slice, dirPath, filePath, writechain.New(logger, file), NewEvents())
+
+	helper := test.NewWriterHelper()
+
+	w, err := writer.New(cfg, file, dirPath, filePath, logger, clk, slice, writer.NewEvents(), helper.NewRowWriter)
+	require.NoError(t, err)
 
 	// Test getters
-	assert.Equal(t, logger, w.Logger())
 	assert.Equal(t, slice.SliceKey, w.SliceKey())
-	assert.Equal(t, slice.Columns, w.Columns())
-	assert.NotSame(t, slice.Columns, w.Columns())
-	assert.Equal(t, slice.Type, w.Type())
-	assert.Equal(t, slice.LocalStorage.Compression, w.Compression())
 	assert.Equal(t, dirPath, w.DirPath())
 	assert.Equal(t, filePath, w.FilePath())
 
 	// Test write methods
-	n, notifier, err := w.WriteWithNotify([]byte("123"))
-	assert.Equal(t, 3, n)
-	assert.NoError(t, err)
-	assert.NoError(t, notifier.Wait())
-	n, err = w.Write([]byte("456"))
-	assert.Equal(t, 3, n)
-	assert.NoError(t, err)
-	n, err = w.WriteString("789")
-	w.AddWriteOp(1)
-	assert.Equal(t, 3, n)
-	assert.NoError(t, err)
-	notifier, err = w.DoWithNotify(func() error {
-		_, err := w.Write([]byte("abc"))
-		w.AddWriteOp(1)
-		return err
-	})
-	assert.NoError(t, err)
-	assert.NoError(t, notifier.Wait())
+	assert.NoError(t, w.WriteRow(clk.Now(), []any{"123", "456", "789"}))
+	assert.NoError(t, w.WriteRow(clk.Now(), []any{"abc", "def", "ghj"}))
 
 	// Test Close method
 	assert.NoError(t, w.Close(ctx))
@@ -74,31 +57,32 @@ func TestBaseWriter(t *testing.T) {
 	// Check file content
 	content, err := os.ReadFile(filePath)
 	assert.NoError(t, err)
-	assert.Equal(t, []byte("123456789abc"), content)
+	assert.Equal(t, []byte("123,456,789\nabc,def,ghj\n"), content)
 }
 
-func TestBaseWriter_CloseError(t *testing.T) {
+func TestWriter_CloseError(t *testing.T) {
 	t.Parallel()
 
 	ctx := context.Background()
+	cfg := writer.NewConfig()
 	logger := log.NewDebugLogger()
 	clk := clock.NewMock()
 	dirPath := t.TempDir()
 	filePath := filepath.Join(dirPath, "file")
 	slice := newTestSlice(t)
-
 	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
 	assert.NoError(t, err)
-	w := NewBaseWriter(logger, clk, slice, dirPath, filePath, writechain.New(logger, file), NewEvents())
 
-	w.AppendCloseFn("my-closer", func() error {
-		return errors.New("some error")
-	})
+	helper := test.NewWriterHelper()
+	helper.CloseError = errors.New("some error")
+
+	w, err := writer.New(cfg, file, dirPath, filePath, logger, clk, slice, writer.NewEvents(), helper.NewRowWriter)
+	require.NoError(t, err)
 
 	// Test Close method
 	err = w.Close(ctx)
 	if assert.Error(t, err) {
-		assert.Equal(t, "chain close error: cannot close \"my-closer\": some error", err.Error())
+		assert.Equal(t, "chain close error:\n- cannot close \"*test.RowWriter\": some error", err.Error())
 	}
 }
 
@@ -110,5 +94,6 @@ func newTestSlice(tb testing.TB) *model.Slice {
 	// Slice definition must be valid
 	val := validator.New()
 	require.NoError(tb, val.Validate(context.Background(), s))
+
 	return s
 }
