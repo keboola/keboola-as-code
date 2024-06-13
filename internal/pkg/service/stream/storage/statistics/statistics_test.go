af5ca08ea4c2ef1187dce6e9dd8a8a17ce2f9f88
Jáchym Toušek <jachym.tousek@keboola.com>
2024-06-13 14:29:03 +0200

    test: Refactor statistics.Value test

--- internal/pkg/service/stream/storage/statistics/statistics_test.go
+++ internal/pkg/service/stream/storage/statistics/statistics_test.go
@@ -9,67 +9,94 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/statistics"
 )
 
+type testCase struct {
+	name     string
+	value    statistics.Value
+	addition statistics.Value
+	expected statistics.Value
+}
+
 func TestValue_Add(t *testing.T) {
 	t.Parallel()
 
-	// Create an empty value
-	v := statistics.Value{}
+	testCases := []testCase{
+		{
+			name:     "empty values",
+			value:    statistics.Value{},
+			addition: statistics.Value{},
+			expected: statistics.Value{},
+		},
+		{
+			name:  "add to empty",
+			value: statistics.Value{},
+			addition: statistics.Value{
+				FirstRecordAt:    utctime.MustParse("2000-01-10T00:00:00.000Z"),
+				LastRecordAt:     utctime.MustParse("2000-01-20T00:00:00.000Z"),
+				RecordsCount:     100,
+				UncompressedSize: 2000,
+				CompressedSize:   300,
+			},
+			expected: statistics.Value{
+				FirstRecordAt:    utctime.MustParse("2000-01-10T00:00:00.000Z"),
+				LastRecordAt:     utctime.MustParse("2000-01-20T00:00:00.000Z"),
+				RecordsCount:     100,
+				UncompressedSize: 2000,
+				CompressedSize:   300,
+			},
+		},
+		{
+			name: "earlier first record",
+			value: statistics.Value{
+				FirstRecordAt: utctime.MustParse("2000-01-10T00:00:00.000Z"),
+				LastRecordAt:  utctime.MustParse("2000-01-20T00:00:00.000Z"),
+			},
+			addition: statistics.Value{
+				FirstRecordAt: utctime.MustParse("2000-01-05T00:00:00.000Z"),
+			},
+			expected: statistics.Value{
+				FirstRecordAt: utctime.MustParse("2000-01-05T00:00:00.000Z"),
+				LastRecordAt:  utctime.MustParse("2000-01-20T00:00:00.000Z"),
+			},
+		},
+		{
+			name: "later last record",
+			value: statistics.Value{
+				FirstRecordAt: utctime.MustParse("2000-01-10T00:00:00.000Z"),
+				LastRecordAt:  utctime.MustParse("2000-01-20T00:00:00.000Z"),
+			},
+			addition: statistics.Value{
+				LastRecordAt: utctime.MustParse("2000-01-30T00:00:00.000Z"),
+			},
+			expected: statistics.Value{
+				FirstRecordAt: utctime.MustParse("2000-01-10T00:00:00.000Z"),
+				LastRecordAt:  utctime.MustParse("2000-01-30T00:00:00.000Z"),
+			},
+		},
+		{
+			name: "increment values",
+			value: statistics.Value{
+				RecordsCount:     100,
+				UncompressedSize: 2000,
+				CompressedSize:   300,
+			},
+			addition: statistics.Value{
+				RecordsCount:     1,
+				UncompressedSize: 1,
+				CompressedSize:   1,
+			},
+			expected: statistics.Value{
+				RecordsCount:     101,
+				UncompressedSize: 2001,
+				CompressedSize:   301,
+			},
+		},
+	}
 
-	// Add an empty value
-	v = v.Add(statistics.Value{})
-	assert.Equal(t, statistics.Value{}, v)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	// Add to the empty value
-	v = v.Add(statistics.Value{
-		FirstRecordAt:    utctime.MustParse("2000-01-10T00:00:00.000Z"),
-		LastRecordAt:     utctime.MustParse("2000-01-20T00:00:00.000Z"),
-		RecordsCount:     100,
-		UncompressedSize: 2000,
-		CompressedSize:   300,
-	})
-	assert.Equal(t, statistics.Value{
-		FirstRecordAt:    utctime.MustParse("2000-01-10T00:00:00.000Z"),
-		LastRecordAt:     utctime.MustParse("2000-01-20T00:00:00.000Z"),
-		RecordsCount:     100,
-		UncompressedSize: 2000,
-		CompressedSize:   300,
-	}, v)
-
-	// FirstRecordAt before the original
-	v = v.Add(statistics.Value{
-		FirstRecordAt: utctime.MustParse("2000-01-05T00:00:00.000Z"),
-	})
-	assert.Equal(t, statistics.Value{
-		FirstRecordAt:    utctime.MustParse("2000-01-05T00:00:00.000Z"),
-		LastRecordAt:     utctime.MustParse("2000-01-20T00:00:00.000Z"),
-		RecordsCount:     100,
-		UncompressedSize: 2000,
-		CompressedSize:   300,
-	}, v)
-
-	// LastRecordAt after the original
-	v = v.Add(statistics.Value{
-		LastRecordAt: utctime.MustParse("2000-01-30T00:00:00.000Z"),
-	})
-	assert.Equal(t, statistics.Value{
-		FirstRecordAt:    utctime.MustParse("2000-01-05T00:00:00.000Z"),
-		LastRecordAt:     utctime.MustParse("2000-01-30T00:00:00.000Z"),
-		RecordsCount:     100,
-		UncompressedSize: 2000,
-		CompressedSize:   300,
-	}, v)
-
-	// Increment all values
-	v = v.Add(statistics.Value{
-		RecordsCount:     1,
-		UncompressedSize: 1,
-		CompressedSize:   1,
-	})
-	assert.Equal(t, statistics.Value{
-		FirstRecordAt:    utctime.MustParse("2000-01-05T00:00:00.000Z"),
-		LastRecordAt:     utctime.MustParse("2000-01-30T00:00:00.000Z"),
-		RecordsCount:     101,
-		UncompressedSize: 2001,
-		CompressedSize:   301,
-	}, v)
+			assert.Equal(t, tc.expected, tc.value.Add(tc.addition))
+		})
+	}
 }
