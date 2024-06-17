cba3e566e2f2b5243da360a6f374eea27f6dbe79
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-17 16:24:50 +0200

    feat: Create coordinator node

---
+++ internal/pkg/service/stream/storage/node/coordinator/coordinator_test.go
@@ -0,0 +1,39 @@
+package coordinator_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+func TestStart_Ok(t *testing.T) {
+	t.Parallel()
+
+	d, mock := dependencies.NewMockedServiceScope(t)
+
+	// Start
+	ctx := context.Background()
+	require.NoError(t, stream.StartComponents(ctx, d, mock.TestConfig(), stream.ComponentStorageCoordinator))
+
+	// Shutdown
+	d.Process().Shutdown(ctx, errors.New("bye bye"))
+	d.Process().WaitForShutdown()
+
+	// Logs
+	mock.DebugLogger().AssertJSONMessages(t, `
+{"level":"info","message":"starting storage coordinator node","component":"storage.node.coordinator"}
+{"level":"info","message":"exiting (bye bye)"}
+{"level":"info","message":"received shutdown request","component":"distribution.mutex.provider"}
+{"level":"info","message":"closing etcd session: context canceled","component":"distribution.mutex.provider.etcd.session"}
+{"level":"info","message":"closed etcd session","component":"distribution.mutex.provider.etcd.session"}
+{"level":"info","message":"shutdown done","component":"distribution.mutex.provider"}
+{"level":"info","message":"closing etcd connection","component":"etcd.client"}
+{"level":"info","message":"closed etcd connection","component":"etcd.client"}
+{"level":"info","message":"exited"}
+`)
+}
