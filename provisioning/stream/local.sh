3cfbfdfdb570bdc353426ea42811fd9942f5fe14
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-19 14:17:41 +0200

    feat: Local deployment, build always new image

--- provisioning/stream/local.sh
+++ provisioning/stream/local.sh
@@ -8,6 +8,29 @@ then
     exit 1
 fi
 
+# Default values for the local deployment
+export MINIKUBE_PROFILE="${MINIKUBE_PROFILE:=stream}"
+export KEBOOLA_STACK="${KEBOOLA_STACK:=local-machine}"
+export HOSTNAME_SUFFIX="${HOSTNAME_SUFFIX:=keboola.com}"
+export STREAM_IMAGE_REPOSITORY="${STREAM_IMAGE_REPOSITORY:=docker.io/keboola/stream-api}"
+export STREAM_IMAGE_TAG="${STREAM_IMAGE_TAG:=$(git rev-parse --short HEAD)-$(date +%s)}"
+
+export STREAM_ETCD_REPLICAS=2
+export STREAM_API_REPLICAS=2
+export STREAM_HTTP_SOURCE_REPLICAS=2
+export STREAM_STORAGE_READER_WRITER_REPLICAS=2
+export STREAM_STORAGE_COORDINATOR_REPLICAS=2
+
+export STREAM_ETCD_STORAGE_CLASS_NAME="standard"
+export STREAM_VOLUME_FAST_STORAGE_CLASS_NAME="standard"
+export STREAM_VOLUME_MEDIUM_STORAGE_CLASS_NAME="standard"
+export STREAM_VOLUME_SLOW_STORAGE_CLASS_NAME="standard"
+
+export STREAM_VOLUME_FAST_STORAGE_SIZE="100Mi"
+export STREAM_VOLUME_MEDIUM_STORAGE_SIZE="100Mi"
+export STREAM_VOLUME_SLOW_STORAGE_SIZE="100Mi"
+
+
 # Start minikube if needed
 if ! minikube status > /dev/null; then
   ./../common/scripts/minikube/start.sh
@@ -39,7 +62,8 @@ echo "--------------------------"
 . common.sh
 
 # Local specific part of the deploy
-kubectl apply -f ./kubernetes/deploy/cloud/local/service.yaml
+kubectl apply -f ./kubernetes/deploy/cloud/local/service-api.yaml
+kubectl apply -f ./kubernetes/deploy/cloud/local/service-http-source.yaml
 
 # Wait for the deployment
 . ./wait.sh
