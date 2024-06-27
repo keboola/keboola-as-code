7e1d7c3fe1613641ef2542a15770bd5ca971e90d
Martin Vaško <Matovidlo2@gmail.com>
2024-06-27 08:45:09 +0200

    fix: Add sensitive tag for not exposing passowrd in logs.
    Update p -> password, do not panic in handler.
    Change error message when host:port cannot be splitted. The error is
    just internal for client. We have spans in DD to locate the issue.

--- internal/pkg/service/appsproxy/dataapps/auth/provider/basic.go
+++ internal/pkg/service/appsproxy/dataapps/auth/provider/basic.go
@@ -2,7 +2,7 @@ package provider
 
 type Basic struct {
 	Base
-	Password string `json:"password"`
+	Password string `json:"password" sensitive:"true"`
 }
 
 func (p *Basic) IsAuthorized(password string) bool {
