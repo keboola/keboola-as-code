d4b1f5155c14a1207a5840117a2a428e20c334b8
Michal Jurečko <michal.jurecko@gmail.com>
2024-06-30 21:13:36 +0200

    feat: Move graceful shutdown to reader/volumes

--- internal/pkg/service/stream/storage/node/readernode/readernode.go
+++ internal/pkg/service/stream/storage/node/readernode/readernode.go
@@ -12,7 +12,6 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/servicectx"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/config"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/reader/volume"
-	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 type dependencies interface {
@@ -27,18 +26,10 @@ func Start(ctx context.Context, d dependencies, cfg config.Config) error {
 	logger.Info(ctx, `starting storage reader node`)
 
 	// Open volumes
-	volumes, err := volume.OpenVolumes(ctx, logger, d.Clock(), cfg.NodeID, cfg.Storage.VolumesPath)
+	_, err := volume.OpenVolumes(ctx, logger, d.Clock(), d.Process(), cfg.NodeID, cfg.Storage.VolumesPath)
 	if err != nil {
 		return err
 	}
-
-	// Graceful shutdown
-	d.Process().OnShutdown(func(ctx context.Context) {
-		if err := volumes.Close(ctx); err != nil {
-			err := errors.PrefixError(err, "`cannot close reader volumes")
-			logger.Error(ctx, err.Error())
-		}
-	})
 
 	return nil
 }
