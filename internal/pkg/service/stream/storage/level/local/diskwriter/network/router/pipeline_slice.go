dac7043ba9b506c14ab0e5e359f8908a0141f0a8
Michal Jurečko <michal.jurecko@gmail.com>
2024-07-19 11:16:18 +0200

    feat: Storage route impl

---
+++ internal/pkg/service/stream/storage/level/local/diskwriter/network/router/pipeline_slice.go
@@ -0,0 +1,147 @@
+package router
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/recordctx"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/sink/pipeline"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
+)
+
+type SlicePipeline interface {
+	IsReady() bool
+	WriteRecord(c recordctx.Context) (pipeline.RecordStatus, error)
+	Close(ctx context.Context) (err error)
+}
+
+type slicePipeline struct {
+	router *Router
+	slice  *sliceData
+
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
+
+	lock     sync.RWMutex
+	pipeline encoding.Pipeline
+}
+
+func newSlicePipeline(ctx context.Context, router *Router, slice *sliceData) *slicePipeline {
+	p := &slicePipeline{router: router, slice: slice}
+	p.ctx, p.cancel = context.WithCancel(context.WithoutCancel(ctx))
+
+	// Try to open pipeline in background, see IsReady method
+	p.wg.Add(1)
+	go func() {
+		b := newOpenPipelineBackoff()
+		defer p.wg.Done()
+		for {
+			// Try open pipeline
+			if err := p.tryOpen(); err != nil {
+				// Wait before retry
+				delay := b.NextBackOff()
+				p.router.logger.Warnf(p.ctx, "%s, waiting %s", err, delay)
+				select {
+				case <-time.After(delay):
+					continue
+				case <-p.ctx.Done():
+					return
+				}
+			}
+
+			// Pipeline opened, close goroutine
+			return
+		}
+	}()
+
+	return p
+}
+
+// IsReady returns true if the pipeline and underlying network connection is healthy.
+func (p *slicePipeline) IsReady() bool {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+	return p.pipeline != nil && p.pipeline.IsReady()
+}
+
+func (p *slicePipeline) WriteRecord(c recordctx.Context) (pipeline.RecordStatus, error) {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
+	// pipeline is not nil, balancedPipeline checks IsReady method
+	if err := p.pipeline.WriteRecord(c); err != nil {
+		return pipeline.RecordError, err
+	}
+
+	// Record has been stored to OS disk cache or physical disk
+	if p.slice.Encoding.Sync.Wait {
+		return pipeline.RecordProcessed, nil
+	}
+
+	// Record has been stored in a in-memory buffer
+	return pipeline.RecordAccepted, nil
+}
+
+func (p *slicePipeline) Close(ctx context.Context) (err error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	// Closed pipeline, if opened
+	if p.pipeline != nil {
+		err = p.pipeline.Close(ctx)
+		if err != nil {
+			p.router.logger.Errorf(ctx, "cannot close encoding pipeline for the slice %q", p.slice.SliceKey)
+		} else {
+			p.router.logger.Infof(ctx, "closed encoding pipeline for the slice %q", p.slice.SliceKey)
+		}
+	}
+
+	// Cancel open loop, if running
+	p.cancel()
+	p.wg.Wait()
+
+	return err
+}
+
+func (p *slicePipeline) tryOpen() error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	// Get connection
+	conn, found := p.router.connections.ConnectionToVolume(p.slice.SliceKey.VolumeID)
+	if !found || !conn.IsConnected() {
+		return errors.Errorf("no connection to the volume %q for the slice %q", p.slice.SliceKey.VolumeID.String(), p.slice.SliceKey.String())
+	}
+
+	// Open remote RPC file
+	remoteFile, err := rpc.OpenNetworkFile(p.ctx, conn, p.slice.SliceKey)
+	if err != nil {
+		return errors.PrefixErrorf(err, "cannot open network file for the slice %q", p.slice.SliceKey.String())
+	}
+
+	// Open pipeline
+	p.pipeline, err = p.router.encoding.OpenPipeline(p.ctx, p.slice.SliceKey, p.slice.Mapping, p.slice.Encoding, remoteFile)
+	if err != nil {
+		_ = remoteFile.Close(p.ctx)
+		return errors.PrefixErrorf(err, "cannot open encoding pipeline for the slice %q", p.slice.SliceKey.String())
+	}
+
+	p.router.logger.Infof(p.ctx, "opened encoding pipeline for the slice %q", p.slice.SliceKey)
+	return nil
+}
+
+func newOpenPipelineBackoff() *backoff.ExponentialBackOff {
+	b := backoff.NewExponentialBackOff()
+	b.RandomizationFactor = 0.1
+	b.Multiplier = 4
+	b.InitialInterval = 5 * time.Second
+	b.MaxInterval = 60 * time.Second
+	b.MaxElapsedTime = 0 // don't stop
+	return b
+}
--- internal/pkg/service/stream/storage/level/local/diskwriter/network/router/router.go
+++ internal/pkg/service/stream/storage/level/local/diskwriter/network/router/router.go
@@ -2,34 +2,74 @@ package router
 
 import (
 	"context"
+	"sync"
+
+	etcd "go.etcd.io/etcd/client/v3"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/distribution"
-	svcErrors "github.com/keboola/keboola-as-code/internal/pkg/service/common/errors"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/op"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/servicectx"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/table"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/sink/pipeline"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/connection"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding"
+	encodingCfg "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding/config"
+	storage "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 	storageRepo "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model/repository"
+	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 type Router struct {
-	distribution *distribution.GroupNode
+	config       network.Config
+	logger       log.Logger
+	balancer     Balancer
 	connections  *connection.Manager
+	distribution *distribution.GroupNode
+	encoding     *encoding.Manager
+
+	// slices field contains in-memory snapshot of all opened storage file slices
+	slices *etcdop.Mirror[storage.Slice, *sliceData]
+
+	closed <-chan struct{}
+	wg     sync.WaitGroup
+}
+
+type sliceData struct {
+	SliceKey storage.SliceKey
+	State    storage.SliceState
+	Encoding encodingCfg.Config
+	Mapping  table.Mapping
 }
 
 type dependencies interface {
 	Logger() log.Logger
 	Process() *servicectx.Process
-	StorageRepository() *storageRepo.Repository
 	DistributionNode() *distribution.Node
+	StorageRepository() *storageRepo.Repository
+	ConnectionManager() *connection.Manager
 }
 
-func New(d dependencies, sourceType string, cfg network.Config) (*Router, error) {
-	r := &Router{}
+func New(d dependencies, sourceType string, config network.Config) (r *Router, err error) {
+	r = &Router{
+		config:      config,
+		logger:      d.Logger().WithComponent("storage.router"),
+		balancer:    NewRandomBalancer(),
+		connections: d.ConnectionManager(),
+	}
 
-	var err error
+	// Graceful shutdown
+	ctx, cancel := context.WithCancel(context.Background())
+	r.closed = ctx.Done()
+	d.Process().OnShutdown(func(_ context.Context) {
+		r.logger.Info(ctx, "closing storage router")
+		cancel()
+		r.wg.Wait()
+		r.logger.Info(ctx, "closed storage router")
+	})
 
 	// Join a distribution group, it contains all source nodes of the same type
 	r.distribution, err = d.DistributionNode().Group("storage.router.sources." + sourceType)
@@ -37,15 +77,72 @@ func New(d dependencies, sourceType string, cfg network.Config) (*Router, error)
 		return nil, err
 	}
 
-	// Create connections to all disk writer nodes
-	r.connections, err = connection.NewManager(d, cfg, r.distribution.NodeID())
-	if err != nil {
-		return nil, err
+	// Start slices mirroring, only necessary data is saved
+	{
+		r.slices = etcdop.
+			SetupMirror(
+				r.logger,
+				d.StorageRepository().Slice().GetAllInLevelAndWatch(ctx, storage.LevelLocal, etcd.WithPrevKV()),
+				func(kv *op.KeyValue, slice storage.Slice) string {
+					return slice.SliceKey.String()
+				},
+				func(kv *op.KeyValue, slice storage.Slice) *sliceData {
+					return &sliceData{
+						SliceKey: slice.SliceKey,
+						State:    slice.State,
+						Encoding: slice.Encoding,
+						Mapping:  slice.Mapping,
+					}
+				},
+			).
+			WithOnUpdate(func(_ etcdop.MirrorUpdate) {
+			}).
+			Build()
+		if err := <-r.slices.StartMirroring(ctx, &r.wg); err != nil {
+			return nil, err
+		}
 	}
+
+	// Update slices on distribution change
+	r.wg.Add(1)
+	go func() {
+		defer r.wg.Done()
+
+		listener := r.distribution.OnChangeListener()
+		defer listener.Stop()
+
+		for {
+			select {
+			case <-listener.C:
+			case <-r.closed:
+				return
+			}
+		}
+	}()
 
 	return r, nil
 }
 
-func (r *Router) OpenPipeline(ctx context.Context, sinKey key.SinkKey) (pipeline.Pipeline, error) {
-	return nil, svcErrors.NewNotImplementedError()
+func (r *Router) OpenPipeline(ctx context.Context, sinkKey key.SinkKey) (pipeline.Pipeline, error) {
+	return newBalancedPipeline(ctx, r, sinkKey)
+}
+
+// sinkOpenedSlices method gets all slices in SliceWriting state for the sink.
+func (r *Router) sinkOpenedSlices(sinkKey key.SinkKey) (out map[storage.SliceKey]*sliceData, err error) {
+	out = make(map[storage.SliceKey]*sliceData)
+
+	// Get sink slices
+	r.slices.WalkPrefix(sinkKey.String(), func(key string, slice *sliceData) (stop bool) {
+		if slice.State == storage.SliceWriting {
+			out[slice.SliceKey] = slice
+		}
+		return false
+	})
+
+	// At least one opened slice should exist
+	if len(out) == 0 {
+		return nil, errors.Errorf(`no opened slice found for the sink %q`, sinkKey)
+	}
+
+	return out, nil
 }
