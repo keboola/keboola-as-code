21fe7355437beb81aaa80b2ae762f2594e85121a
Michal Jurečko <michal.jurecko@gmail.com>
2024-07-27 21:40:21 +0200

    feat: Start file/slice rotation operators

--- internal/pkg/service/stream/storage/node/coordinator/coordinator.go
+++ internal/pkg/service/stream/storage/node/coordinator/coordinator.go
@@ -5,11 +5,14 @@ package coordinator
 
 import (
 	"context"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+
 	"go.opentelemetry.io/otel/attribute"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/ctxattr"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/config"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/node/coordinator/filerotation"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/node/coordinator/slicerotation"
 )
 
 func Start(ctx context.Context, d dependencies.CoordinatorScope, cfg config.Config) error {
@@ -17,6 +20,14 @@ func Start(ctx context.Context, d dependencies.CoordinatorScope, cfg config.Conf
 
 	logger := d.Logger().WithComponent("storage.node.coordinator")
 	logger.Info(ctx, `starting storage coordinator node`)
+
+	if err := filerotation.Start(d, cfg.Storage.Level.Target.Operator); err != nil {
+		return err
+	}
+
+	if err := slicerotation.Start(d, cfg.Storage.Level.Staging.Operator); err != nil {
+		return err
+	}
 
 	return nil
 }
