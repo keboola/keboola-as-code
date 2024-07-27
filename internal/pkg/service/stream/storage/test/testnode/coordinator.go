a781c08f37b5d47697155e48ac7ef7e6e47f8010
Michal Jurečko <michal.jurecko@gmail.com>
2024-07-27 07:52:37 +0200

    tests: Improve tests helpers (2)

---
+++ internal/pkg/service/stream/storage/test/testnode/coordinator.go
@@ -0,0 +1,28 @@
+package testnode
+
+import (
+	"testing"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	commonDeps "github.com/keboola/keboola-as-code/internal/pkg/service/common/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdclient"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/config"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
+)
+
+func StartCoordinatorNode(tb testing.TB, logger log.DebugLogger, etcdCfg etcdclient.Config, modifyConfig func(cfg *config.Config), opts ...commonDeps.MockedOption) (dependencies.CoordinatorScope, dependencies.Mocked) {
+	tb.Helper()
+
+	opts = append(opts, commonDeps.WithDebugLogger(logger), commonDeps.WithEtcdConfig(etcdCfg))
+	return dependencies.NewMockedCoordinatorScopeWithConfig(
+		tb,
+		func(cfg *config.Config) {
+			if modifyConfig != nil {
+				modifyConfig(cfg)
+			}
+			cfg.NodeID = "api"
+			cfg.Hostname = "localhost"
+		},
+		opts...,
+	)
+}
