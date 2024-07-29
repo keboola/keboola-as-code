a6716c708fb09eedd7c35a0215d4de673c03bc26
Petr Hosek <hosanp11@gmail.com>
2024-07-29 11:34:47 +0200

    feat: added a new flag

--- internal/pkg/service/cli/cmd/remote/create/branch/cmd.go
+++ internal/pkg/service/cli/cmd/remote/create/branch/cmd.go
@@ -1,8 +1,12 @@
 package branch
 
 import (
+	"context"
+	"encoding/json"
+	"strings"
 	"time"
 
+	"github.com/keboola/go-client/pkg/keboola"
 	"github.com/spf13/cobra"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/model"
@@ -19,6 +23,7 @@ type Flags struct {
 	StorageAPIHost  configmap.Value[string] `configKey:"storage-api-host" configShorthand:"H" configUsage:"if command is run outside the project directory"`
 	StorageAPIToken configmap.Value[string] `configKey:"storage-api-token" configShorthand:"t" configUsage:"storage API token from your project"`
 	Name            configmap.Value[string] `configKey:"name" configShorthand:"n" configUsage:"name of the new branch"`
+	Output          configmap.Value[string] `configKey:"output-json" configUsage:"output as JSON file"`
 }
 
 func DefaultFlags() Flags {
@@ -88,10 +93,45 @@ func Command(p dependencies.Provider) *cobra.Command {
 					return errors.PrefixError(err, "pull failed")
 				}
 			}
+
+			// create a file including newBranchId
+			if f.Output.Value != "" {
+				if err = createFile(cmd.Context(), d, f, branch); err != nil {
+					return err
+				}
+			}
+
 			return nil
 		},
 	}
 
 	configmap.MustGenerateFlags(cmd.Flags(), DefaultFlags())
 	return cmd
+}
+
+type output struct {
+	NewBranchID keboola.BranchID `json:"newBranchId"`
+}
+
+func createFile(ctx context.Context, d dependencies.RemoteCommandScope, f Flags, branch *keboola.Branch) error {
+	if !strings.HasSuffix(f.Output.Value, ".json") {
+		return errors.New("output value is not a JSON file")
+	}
+
+	file, err := d.Fs().Create(ctx, f.Output.Value)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	b, err := json.MarshalIndent(output{NewBranchID: branch.ID}, "", "	")
+	if err != nil {
+		return err
+	}
+
+	if _, err := file.Write(b); err != nil {
+		return err
+	}
+	return nil
 }
