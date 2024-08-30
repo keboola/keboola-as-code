f0a8d0bca9a00baab9fa1d957909d046e1556762
Michal Jurečko <michal.jurecko@gmail.com>
2024-08-30 14:50:47 +0200

    feat: Update rpc package, implement WaitForServerTermination

--- internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc/server.go
+++ internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"time"
 
 	etcd "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
@@ -29,6 +30,8 @@ type NetworkFileServer struct {
 	volumes    *diskwriter.Volumes
 	volumesMap map[volume.ID]bool
 
+	terminating chan struct{}
+
 	lock      sync.Mutex
 	idCounter uint64
 	writers   map[uint64]diskwriter.Writer
@@ -44,10 +47,11 @@ type serverDependencies interface {
 
 func StartNetworkFileServer(d serverDependencies, nodeID, hostname string, cfg local.Config) error {
 	f := &NetworkFileServer{
-		logger:     d.Logger().WithComponent("storage.node.writer.network-file"),
-		volumes:    d.Volumes(),
-		volumesMap: make(map[volume.ID]bool),
-		writers:    make(map[uint64]diskwriter.Writer),
+		logger:      d.Logger().WithComponent("storage.node.writer.rpc"),
+		volumes:     d.Volumes(),
+		volumesMap:  make(map[volume.ID]bool),
+		terminating: make(chan struct{}),
+		writers:     make(map[uint64]diskwriter.Writer),
 	}
 
 	// Create volumes map, to quick check, if the volume is managed by the node
@@ -60,32 +64,29 @@ func StartNetworkFileServer(d serverDependencies, nodeID, hostname string, cfg l
 	if err != nil {
 		return err
 	}
-	d.Process().OnShutdown(func(ctx context.Context) {
-		_ = listener.Close()
-	})
+
+	// Register volumes to database, so source nodes know about this disk writer node.
+	nodeAddress := volume.RemoteAddr(fmt.Sprintf("%s:%s", hostname, listener.Port()))
+	err = registration.RegisterVolumes(cfg.Volume.Registration, d, nodeID, nodeAddress, d.Volumes().Collection(), d.StorageRepository().Volume().RegisterWriterVolume)
+	if err != nil {
+		return err
+	}
 
 	// Graceful shutdown
 	ctx := context.Background()
 	d.Process().OnShutdown(func(_ context.Context) {
 		f.logger.Info(ctx, "closing network file server")
 
+		// Gracefully close active writers
+		f.terminate(ctx)
+
 		// Stop network listener
 		if err := listener.Close(); err != nil {
 			f.logger.Error(ctx, err.Error())
 		}
 
-		// Close active writers
-		f.closeWriters(ctx)
-
 		f.logger.Info(ctx, "closed network file server")
 	})
-
-	// Register volumes to database
-	nodeAddress := volume.RemoteAddr(fmt.Sprintf("%s:%s", hostname, listener.Port()))
-	err = registration.RegisterVolumes(cfg.Volume.Registration, d, nodeID, nodeAddress, d.Volumes().Collection(), d.StorageRepository().Volume().RegisterWriterVolume)
-	if err != nil {
-		return err
-	}
 
 	// Start server
 	d.Process().Add(func(shutdown servicectx.ShutdownFn) {
@@ -104,6 +105,10 @@ func (s *NetworkFileServer) serve(listener net.Listener) error {
 }
 
 func (s *NetworkFileServer) Open(ctx context.Context, req *pb.OpenRequest) (*pb.OpenResponse, error) {
+	if s.isTerminating() {
+		return nil, errors.New("disk writer node is terminating")
+	}
+
 	var data sliceData
 	if err := json.Decode(req.SliceDataJson, &data); err != nil {
 		return nil, err
@@ -131,6 +136,11 @@ func (s *NetworkFileServer) Open(ctx context.Context, req *pb.OpenRequest) (*pb.
 	return &pb.OpenResponse{FileId: fileID}, nil
 }
 
+func (s *NetworkFileServer) WaitForServerTermination(_ *pb.WaitForServerTerminationRequest, stream pb.NetworkFile_WaitForServerTerminationServer) error {
+	<-s.terminating
+	return stream.Send(&pb.ServerIsTerminatingResponse{})
+}
+
 func (s *NetworkFileServer) Write(ctx context.Context, req *pb.WriteRequest) (*pb.WriteResponse, error) {
 	w, err := s.writer(req.FileId)
 	if err != nil {
@@ -154,15 +164,14 @@ func (s *NetworkFileServer) Sync(ctx context.Context, req *pb.SyncRequest) (*pb.
 }
 
 func (s *NetworkFileServer) Close(ctx context.Context, req *pb.CloseRequest) (*pb.CloseResponse, error) {
-	defer func() {
-		s.lock.Lock()
-		defer s.lock.Unlock()
-		delete(s.writers, req.FileId)
-	}()
 	w, err := s.writer(req.FileId)
 	if err != nil {
 		return nil, err
 	}
+
+	s.lock.Lock()
+	delete(s.writers, req.FileId)
+	s.lock.Unlock()
 
 	return nil, w.Close(ctx)
 }
@@ -179,21 +188,62 @@ func (s *NetworkFileServer) writer(id uint64) (diskwriter.Writer, error) {
 	return w, nil
 }
 
-func (s *NetworkFileServer) closeWriters(ctx context.Context) {
+func (s *NetworkFileServer) isTerminating() bool {
+	select {
+	case <-s.terminating:
+		return true
+	default:
+		return false
+	}
+}
+
+func (s *NetworkFileServer) terminate(ctx context.Context) {
+	// Prevent opening of a new writer, see Open method.
+	// It also notifies source nodes about server termination, see WaitForServerTermination method.
+	close(s.terminating)
+
+	// Notified source nodes should close their pipelines, and finally the network file client, see Close method.
+	s.logger.Infof(ctx, "waiting for close of %d disk writers by source nodes", len(s.writers))
+
+	// Wait until all writers are closed
+	s.waitForWritersClosing(ctx)
+
+	// Force close remaining writers
 	s.lock.Lock()
 	defer s.lock.Unlock()
-
-	s.logger.Infof(ctx, "closing %d disk writers", len(s.writers))
-
-	wg := &sync.WaitGroup{}
-	for _, w := range s.writers {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if err := w.Close(ctx); err != nil {
-				s.logger.Errorf(ctx, "cannot close disk writer %q: %s", w.SliceKey(), err)
-			}
-		}()
+	if l := len(s.writers); l > 0 {
+		s.logger.Errorf(ctx, "force closing %d disk writers", len(s.writers))
+		wg := &sync.WaitGroup{}
+		for _, w := range s.writers {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if err := w.Close(ctx); err != nil {
+					s.logger.Errorf(ctx, "cannot close disk writer %q: %s", w.SliceKey(), err)
+				}
+			}()
+		}
+		wg.Wait()
 	}
-	wg.Wait()
+}
+
+func (s *NetworkFileServer) waitForWritersClosing(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(50 * time.Millisecond):
+			// try again
+		}
+
+		s.lock.Lock()
+		l := len(s.writers)
+		s.lock.Unlock()
+		if l == 0 {
+			s.logger.Infof(ctx, "all writers have been gracefully closed")
+			return
+		}
+	}
 }
