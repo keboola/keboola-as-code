f160da986ef140ea51d923e6694d0b3573675d3b
Michal Jurečko <michal.jurecko@gmail.com>
2024-08-30 14:52:20 +0200

    feat: Update network/router code

--- internal/pkg/service/stream/storage/level/local/diskwriter/network/router/router.go
+++ internal/pkg/service/stream/storage/level/local/diskwriter/network/router/router.go
@@ -6,7 +6,6 @@ import (
 	"slices"
 	"strings"
 	"sync"
-	"time"
 
 	etcd "go.etcd.io/etcd/client/v3"
 	"golang.org/x/exp/maps"
@@ -18,14 +17,14 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop/serde"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/servicectx"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/mapping/table"
-	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/sink/pipeline"
+	pipelinePkg "github.com/keboola/keboola-as-code/internal/pkg/service/stream/sink/pipeline"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/connection"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/router/assignment"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/router/balancer"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/router/closesync"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/router/pipeline"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding"
-	encodingCfg "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding/config"
-	localModel "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/model"
 	storage "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
 	storageRepo "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model/repository"
 )
@@ -34,28 +33,16 @@ type Router struct {
 	nodeID       string
 	config       network.Config
 	logger       log.Logger
-	balancer     Balancer
+	balancer     balancer.Balancer
 	connections  *connection.Manager
 	encoding     *encoding.Manager
 	distribution *distribution.GroupNode
 	closeSyncer  *closesync.SourceNode
 
+	pipelines *pipeline.Collection[key.SinkKey, *pipeline.SinkPipeline]
+
 	// slices field contains in-memory snapshot of all opened storage file slices
-	slices *etcdop.MirrorTree[storage.Slice, *sliceData]
-
-	closed <-chan struct{}
-	wg     sync.WaitGroup
-
-	pipelinesLock sync.Mutex
-	pipelines     map[key.SinkKey]*balancedPipeline
-}
-
-type sliceData struct {
-	SliceKey     storage.SliceKey
-	State        storage.SliceState
-	Encoding     encodingCfg.Config
-	Mapping      table.Mapping
-	LocalStorage localModel.Slice
+	slices *etcdop.MirrorTree[storage.Slice, *pipeline.SliceData]
 }
 
 type dependencies interface {
@@ -70,32 +57,35 @@ type dependencies interface {
 }
 
 func New(d dependencies, sourceNodeID, sourceType string, config network.Config) (r *Router, err error) {
-	balancer, err := NewBalancer(config.PipelineBalancer)
-	if err != nil {
-		return nil, err
-	}
+	logger := d.Logger().WithComponent("storage.router")
 
 	r = &Router{
 		nodeID:      sourceNodeID,
 		config:      config,
-		logger:      d.Logger().WithComponent("storage.router"),
-		balancer:    balancer,
+		logger:      logger,
 		connections: d.ConnectionManager(),
 		encoding:    d.EncodingManager(),
-		pipelines:   make(map[key.SinkKey]*balancedPipeline),
+		pipelines:   pipeline.NewCollection[key.SinkKey, *pipeline.SinkPipeline](logger),
+	}
+
+	r.balancer, err = balancer.NewBalancer(config.PipelineBalancer)
+	if err != nil {
+		return nil, err
 	}
 
 	// Graceful shutdown
+	wg := &sync.WaitGroup{}
 	ctx, cancel := context.WithCancel(context.Background())
-	r.closed = ctx.Done()
-	d.Process().OnShutdown(func(_ context.Context) {
+	d.Process().OnShutdown(func(ctx context.Context) {
 		r.logger.Info(ctx, "closing storage router")
 
 		// Stop mirroring
 		cancel()
-		r.wg.Wait()
+		wg.Wait()
 
-		r.closePipelines()
+		// Close sink pipelines
+		r.logger.Infof(ctx, "closing %d sink pipelines", r.pipelines.Len())
+		r.pipelines.Close(ctx)
 
 		r.logger.Info(ctx, "closed storage router")
 	})
@@ -107,7 +97,7 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 		return nil, err
 	}
 
-	// Create utility, to report processed changes in slices (closed pipelines)
+	// Create utility to report processed changes in slices (closed pipelines)
 	r.closeSyncer, err = closesync.NewSourceNode(d, sourceNodeID)
 	if err != nil {
 		return nil, err
@@ -121,8 +111,8 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 			func(key string, slice storage.Slice) string {
 				return slice.SliceKey.String()
 			},
-			func(key string, slice storage.Slice, rawValue *op.KeyValue, oldValue **sliceData) *sliceData {
-				return &sliceData{
+			func(key string, slice storage.Slice, rawValue *op.KeyValue, oldValue **pipeline.SliceData) *pipeline.SliceData {
+				return &pipeline.SliceData{
 					SliceKey:     slice.SliceKey,
 					State:        slice.State,
 					Encoding:     slice.Encoding,
@@ -131,7 +121,7 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 				}
 			},
 		).
-			WithOnChanges(func(changes etcdop.MirrorUpdateChanges[string, *sliceData]) {
+			WithOnChanges(func(changes etcdop.MirrorUpdateChanges[string, *pipeline.SliceData]) {
 				// Collect all modified sinks, iterate all created, updated and deleted slices.
 				modifiedSinksMap := make(map[key.SinkKey]bool)
 				for _, kv := range changes.All() {
@@ -145,7 +135,7 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 				})
 
 				// Update all affected pipelines
-				r.updatePipelines(ctx, sinks)
+				r.onSinksModification(ctx, sinks)
 
 				// All changes up to the revision have been processed,
 				// pipelines have been closed.
@@ -154,7 +144,7 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 				}
 			}).
 			BuildMirror()
-		if err := <-r.slices.StartMirroring(ctx, &r.wg, r.logger); err != nil {
+		if err := <-r.slices.StartMirroring(ctx, wg, r.logger); err != nil {
 			return nil, err
 		}
 	}
@@ -162,44 +152,43 @@ func New(d dependencies, sourceNodeID, sourceType string, config network.Config)
 	return r, nil
 }
 
-func (r *Router) SinksCount() int {
+func (r *Router) SlicesCount() int {
 	return r.slices.Len()
 }
 
-func (r *Router) OpenPipeline(ctx context.Context, sinkKey key.SinkKey) (pipeline.Pipeline, error) {
-	p, err := openBalancedPipeline(ctx, r, sinkKey)
-	if err != nil {
+func (r *Router) OpenPipeline(ctx context.Context, sinkKey key.SinkKey, onClose func()) (pipelinePkg.Pipeline, error) {
+	assignedSlices := r.assignSinkSlices(sinkKey)
+	if len(assignedSlices) == 0 {
+		return nil, NoOpenedSliceFoundError{}
+	}
+
+	onClose2 := func(ctx context.Context, sinkKey key.SinkKey) {
+		r.pipelines.Unregister(ctx, sinkKey)
+		onClose()
+	}
+
+	p := pipeline.NewSinkPipeline(sinkKey, r.logger, r.connections, r.encoding, r.balancer, onClose2)
+
+	r.pipelines.Register(ctx, sinkKey, p)
+
+	if err := p.UpdateSlicePipelines(ctx, assignedSlices); err != nil {
 		return nil, err
 	}
 
 	return p, nil
 }
 
-func (r *Router) registerPipeline(p *balancedPipeline) {
-	r.pipelinesLock.Lock()
-	defer r.pipelinesLock.Unlock()
-	r.pipelines[p.sinkKey] = p
-}
-
-func (r *Router) unregisterPipeline(p *balancedPipeline) {
-	r.pipelinesLock.Lock()
-	defer r.pipelinesLock.Unlock()
-	delete(p.router.pipelines, p.sinkKey)
-}
-
-func (r *Router) updatePipelines(ctx context.Context, modifiedSinks []key.SinkKey) {
+// onSinksModification updates sink pipelines when a sink slice has been opened/closed.
+func (r *Router) onSinksModification(ctx context.Context, modifiedSinks []key.SinkKey) {
 	wg := &sync.WaitGroup{}
 	defer wg.Wait()
 
-	r.pipelinesLock.Lock()
-	defer r.pipelinesLock.Unlock()
-
 	for _, sinkKey := range modifiedSinks {
-		if p := r.pipelines[sinkKey]; p != nil {
+		if p := r.pipelines.Get(sinkKey); p != nil {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				if err := p.update(ctx, false); err != nil {
+				if err := p.UpdateSlicePipelines(ctx, r.assignSinkSlices(sinkKey)); err != nil {
 					r.logger.Errorf(ctx, `cannot update sink slices pipelines: %s, sink %q`, err, sinkKey)
 				}
 			}()
@@ -207,39 +196,27 @@ func (r *Router) updatePipelines(ctx context.Context, modifiedSinks []key.SinkKe
 	}
 }
 
-func (r *Router) closePipelines() {
-	wg := &sync.WaitGroup{}
-	defer wg.Wait()
-
-	r.pipelinesLock.Lock()
-	defer r.pipelinesLock.Unlock()
-
-	for _, p := range r.pipelines {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			defer cancel()
-
-			if err := p.Close(ctx); err != nil {
-				r.logger.Errorf(ctx, "cannot close balanced pipeline: %s", err)
-			}
-		}()
-	}
-}
-
-// sinkOpenedSlices method gets all slices in SliceWriting state for the sink.
-func (r *Router) sinkOpenedSlices(sinkKey key.SinkKey) (out map[storage.SliceKey]*sliceData) {
-	out = make(map[storage.SliceKey]*sliceData)
-
+// assignSinkSlices assigns part of all sink slices to this source node.
+func (r *Router) assignSinkSlices(sinkKey key.SinkKey) (out []*pipeline.SliceData) {
 	// Get sink slices
-	r.slices.WalkPrefix(sinkKey.String(), func(key string, slice *sliceData) (stop bool) {
+	sinkSlices := make(map[storage.SliceKey]*pipeline.SliceData)
+	r.slices.WalkPrefix(sinkKey.String(), func(key string, slice *pipeline.SliceData) (stop bool) {
 		if slice.State == storage.SliceWriting {
-			out[slice.SliceKey] = slice
+			sinkSlices[slice.SliceKey] = slice
 		}
 		return false
 	})
+
+	// Assign part of all sink slices to this source node
+	assignedSlicesKeys := assignment.AssignSlices(
+		maps.Keys(sinkSlices),
+		r.distribution.Nodes(),
+		r.distribution.NodeID(),
+		r.config.MinSlicesPerSourceNode,
+	)
+	for _, sliceKey := range assignedSlicesKeys {
+		out = append(out, sinkSlices[sliceKey])
+	}
 
 	return out
 }
