878c58619b666938c537a99d0c5dde47680dc603
Michal Jurečko <michal.jurecko@gmail.com>
2024-09-05 08:28:03 +0200

    fix: Empty block/mutex profile

--- internal/pkg/telemetry/pprof/http.go
+++ internal/pkg/telemetry/pprof/http.go
@@ -3,10 +3,13 @@ package pprof
 import (
 	"net/http"
 	"net/http/pprof"
+	"runtime"
 	"time"
 )
 
 func NewHTTPServer(addr string) *http.Server {
+	runtime.SetBlockProfileRate(20)     // 5%
+	runtime.SetMutexProfileFraction(20) // 5%
 	mux := http.NewServeMux()
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
