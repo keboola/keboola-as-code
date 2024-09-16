cad6b5ccc10b62e290ad4c94f9a6edc7153a1111
Michal Jurečko <michal.jurecko@gmail.com>
2024-09-16 11:44:55 +0200

    fix: Typo

--- internal/pkg/service/stream/storage/level/local/diskwriter/volume.go
+++ internal/pkg/service/stream/storage/level/local/diskwriter/volume.go
@@ -1,4 +1,4 @@
-// Package volume provides detection and opening of volumes intended for slices writing.
+// Package diskwriter provides detection and opening of volumes intended for slices writing.
 package diskwriter
 
 import (
