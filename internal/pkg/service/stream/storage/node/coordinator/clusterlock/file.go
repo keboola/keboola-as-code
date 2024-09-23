b35871d85dee9f9147046774800e47f4bc33bac7
Jáchym Toušek <jachym.tousek@keboola.com>
2024-09-23 12:56:03 +0200

    fix: Cluster lock timeout

--- internal/pkg/service/stream/storage/node/coordinator/clusterlock/file.go
+++ internal/pkg/service/stream/storage/node/coordinator/clusterlock/file.go
@@ -3,6 +3,7 @@ package clusterlock
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/distlock"
@@ -18,6 +19,8 @@ func LockFile(ctx context.Context, locks *distlock.Provider, logger log.Logger, 
 	}
 
 	unlock = func() {
+		ctx, cancel := context.WithTimeout(context.WithoutCancel(ctx), 10*time.Second)
+		defer cancel()
 		if err := lock.Unlock(ctx); err != nil {
 			logger.Warnf(ctx, "cannot unlock lock %q: %s", lock.Key(), err)
 		}
