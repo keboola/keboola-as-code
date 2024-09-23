98407f69a063b85f8eb2b27608d45f79a1d1a6b0
Jáchym Toušek <jachym.tousek@keboola.com>
2024-09-23 16:57:28 +0200

    feat: Add lock key attribute

--- internal/pkg/service/stream/storage/node/coordinator/clusterlock/file.go
+++ internal/pkg/service/stream/storage/node/coordinator/clusterlock/file.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"time"
 
+	"go.opentelemetry.io/otel/attribute"
+
 	"github.com/keboola/keboola-as-code/internal/pkg/log"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/ctxattr"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/distlock"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/common/etcdop"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/model"
@@ -13,19 +16,28 @@ import (
 )
 
 func LockFile(ctx context.Context, locks *distlock.Provider, logger log.Logger, fileKey model.FileKey) (lock *etcdop.Mutex, unlock func(), err error) {
-	lock = locks.NewMutex(fmt.Sprintf("operator.file.%s", fileKey))
+	key := fmt.Sprintf("operator.file.%s", fileKey)
+	ctx = ctxattr.ContextWith(ctx, attribute.String("lock.key", key))
+
+	lock = locks.NewMutex(key)
 	if err := lock.Lock(ctx); err != nil {
-		return nil, nil, errors.PrefixErrorf(err, "cannot lock %q:", lock.Key())
+		logger.Warnf(ctx, "cannot acquire lock: %s", err)
+
+		return nil, nil, errors.PrefixErrorf(err, "cannot acquire lock %q:", lock.Key())
 	}
 
-	logger.Debug(ctx, "acquired lock")
+	logger.Infof(ctx, "acquired lock")
 
 	unlock = func() {
 		ctx, cancel := context.WithTimeout(context.WithoutCancel(ctx), 10*time.Second)
 		defer cancel()
 		if err := lock.Unlock(ctx); err != nil {
-			logger.Warnf(ctx, "cannot unlock lock %q: %s", lock.Key(), err)
+			logger.Warnf(ctx, "cannot release lock: %s", err)
+
+			return
 		}
+
+		logger.Infof(ctx, "released lock")
 	}
 
 	return lock, unlock, nil
