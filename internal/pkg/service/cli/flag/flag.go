571c94d2b3c3564edd73cdada0e668f5bc6d0e43
Petr Hosek <hosanp11@gmail.com>
2024-09-26 10:42:36 +0200

    refactor: moved flag to specific commands

--- internal/pkg/service/cli/flag/flag.go
+++ internal/pkg/service/cli/flag/flag.go
@@ -5,15 +5,14 @@ import (
 )
 
 type GlobalFlags struct {
-	Help             configmap.Value[bool]   `configKey:"help" configShorthand:"h" configUsage:"print help for command"`
-	LogFile          configmap.Value[string] `configKey:"log-file" configShorthand:"l" configUsage:"path to a log file for details"`
-	LogFormat        configmap.Value[string] `configKey:"log-format" configUsage:"format of stdout and stderr"`
-	NonInteractive   configmap.Value[bool]   `configKey:"non-interactive" configUsage:"disable interactive dialogs"`
-	WorkingDir       configmap.Value[string] `configKey:"working-dir" configShorthand:"d" configUsage:"use other working directory"`
-	Verbose          configmap.Value[bool]   `configKey:"verbose" configShorthand:"v" configUsage:"print details"`
-	VerboseAPI       configmap.Value[bool]   `configKey:"verbose-api" configUsage:"log each API request and response"`
-	VersionCheck     configmap.Value[bool]   `configKey:"version-check" configUsage:"checks if there is a newer version of the CLI"`
-	TestProjectsFile configmap.Value[string] `configKey:"test-projects-file" configUsage:"file containing projects that could be used for templates"`
+	Help           configmap.Value[bool]   `configKey:"help" configShorthand:"h" configUsage:"print help for command"`
+	LogFile        configmap.Value[string] `configKey:"log-file" configShorthand:"l" configUsage:"path to a log file for details"`
+	LogFormat      configmap.Value[string] `configKey:"log-format" configUsage:"format of stdout and stderr"`
+	NonInteractive configmap.Value[bool]   `configKey:"non-interactive" configUsage:"disable interactive dialogs"`
+	WorkingDir     configmap.Value[string] `configKey:"working-dir" configShorthand:"d" configUsage:"use other working directory"`
+	Verbose        configmap.Value[bool]   `configKey:"verbose" configShorthand:"v" configUsage:"print details"`
+	VerboseAPI     configmap.Value[bool]   `configKey:"verbose-api" configUsage:"log each API request and response"`
+	VersionCheck   configmap.Value[bool]   `configKey:"version-check" configUsage:"checks if there is a newer version of the CLI"`
 }
 
 func DefaultGlobalFlags() GlobalFlags {
