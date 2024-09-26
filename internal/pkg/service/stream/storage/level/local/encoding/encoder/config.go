8a4d177ed69fc8d6f03e8b55d33b30e7e272fb00
Martin Vaško <Matovidlo2@gmail.com>
2024-09-26 13:09:27 +0200

    fix: Limit -> RowSizeLimit

--- internal/pkg/service/stream/storage/level/local/encoding/encoder/config.go
+++ internal/pkg/service/stream/storage/level/local/encoding/encoder/config.go
@@ -10,9 +10,9 @@ type Type string
 
 // Config configures the local writer.
 type Config struct {
-	Type        Type              `json:"type" configKey:"type" configUsage:"Encoder type." validate:"required,oneof=csv"`
-	Concurrency int               `json:"concurrency" configKey:"concurrency" configUsage:"Concurrency of the format writer for the specified file type. 0 = auto = num of CPU cores" validate:"min=0,max=256"`
-	Limit       datasize.ByteSize `json:"limit" configKey:"limit" configUsage:"Set's the limit of single row to be encoded. Limit should be bigger than accepted request on source otherwise received message will never be encoded" validate:"minBytes=1kB,maxBytes=2MB"`
+	Type         Type              `json:"type" configKey:"type" configUsage:"Encoder type." validate:"required,oneof=csv"`
+	Concurrency  int               `json:"concurrency" configKey:"concurrency" configUsage:"Concurrency of the format writer for the specified file type. 0 = auto = num of CPU cores" validate:"min=0,max=256"`
+	RowSizeLimit datasize.ByteSize `json:"rowSizeLimit" configKey:"rowSizeLimit" configUsage:"Set's the limit of single row to be encoded. Limit should be bigger than accepted request on source otherwise received message will never be encoded" validate:"minBytes=1kB,maxBytes=2MB"`
 	// OverrideEncoderFactory overrides encoder factory.
 	// A custom implementation can be useful for tests.
 	OverrideEncoderFactory Factory `json:"-"`
@@ -25,8 +25,8 @@ type ConfigPatch struct {
 
 func NewConfig() Config {
 	return Config{
-		Type:        TypeCSV,
-		Concurrency: 0,                  // 0 = auto = CPU cores
-		Limit:       1536 * datasize.KB, // ~1.5 MB
+		Type:         TypeCSV,
+		Concurrency:  0,                  // 0 = auto = CPU cores
+		RowSizeLimit: 1536 * datasize.KB, // ~1.5 MB
 	}
 }
