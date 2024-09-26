69587c487f896a6c58fa7b8a76d2d137b57cec8c
Martin Vaško <Matovidlo2@gmail.com>
2024-09-26 13:47:58 +0200

    fix: Add configuration output in tests

--- internal/pkg/service/stream/storage/model/slice_test.go
+++ internal/pkg/service/stream/storage/model/slice_test.go
@@ -182,6 +182,7 @@ func TestSlice_Validation(t *testing.T) {
 - "state" is a required field
 - "mapping.columns" is a required field
 - "encoding.encoder.type" is a required field
+- "encoding.encoder.rowSizeLimit" must be 1KB or greater
 - "encoding.maxChunkSize" is a required field
 - "encoding.failedChunksThreshold" is a required field
 - "encoding.compression.type" is a required field
