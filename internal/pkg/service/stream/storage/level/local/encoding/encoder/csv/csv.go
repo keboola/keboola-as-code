1f3dff451745701849b35b29e6ac7f0e09c8abc3
Martin Vaško <Matovidlo2@gmail.com>
2024-10-18 11:14:44 +0200

    fix: Do not obtain notifier before writeRow
    It causes incorrect behaviour of adding writeRow to previous notifier
    instead of the new one, which causes guarantees of writing to be not
    aligned.

--- internal/pkg/service/stream/storage/level/local/encoding/encoder/csv/csv.go
+++ internal/pkg/service/stream/storage/level/local/encoding/encoder/csv/csv.go
@@ -29,7 +29,13 @@ var columnRenderer = column.NewRenderer() //nolint:gochecknoglobals // contains 
 // The order of the lines is not preserved, because we use the writers pool,
 // but also because there are several source nodes with a load balancer in front of them.
 // In case of encoder accepts too big csv row, it returns error.
-func NewEncoder(concurrency int, rowSizeLimit datasize.ByteSize, mapping any, out io.Writer, notifier func() *notify.Notifier) (*Encoder, error) {
+func NewEncoder(
+	concurrency int,
+	rowSizeLimit datasize.ByteSize,
+	mapping any,
+	out io.Writer,
+	notifier func() *notify.Notifier,
+) (*Encoder, error) {
 	tableMapping, ok := mapping.(table.Mapping)
 	if !ok {
 		return nil, errors.Errorf("csv encoder supports only table mapping, given %v", mapping)
@@ -49,7 +55,6 @@ func NewEncoder(concurrency int, rowSizeLimit datasize.ByteSize, mapping any, ou
 }
 
 func (w *Encoder) WriteRecord(record recordctx.Context) (result.WriteRecordResult, error) {
-	writeRecordResult := result.NewNotifierWriteRecordResult(w.notifier())
 	// Reduce memory allocations
 	values := w.valuesPool.Get().(*[]any)
 	defer w.valuesPool.Put(values)
@@ -58,29 +63,30 @@ func (w *Encoder) WriteRecord(record recordctx.Context) (result.WriteRecordResul
 	for i, col := range w.columns {
 		value, err := columnRenderer.CSVValue(col, record)
 		if err != nil {
-			return writeRecordResult, errors.PrefixErrorf(err, "cannot convert column %q to CSV value", col)
+			return result.WriteRecordResult{}, errors.PrefixErrorf(err, "cannot convert column %q to CSV value", col)
 		}
 		(*values)[i] = value
 	}
 
 	// Encode the values to CSV format
 	n, err := w.writersPool.WriteRow(values)
-	writeRecordResult.N = n
 	if err != nil {
 		var valErr fastcsv.ValueError
 		if errors.As(err, &valErr) {
 			columnName := w.columns[valErr.ColumnIndex].ColumnName()
-			return writeRecordResult, errors.Errorf(`cannot convert value of the column "%s" to the string: %w`, columnName, err)
+			return result.WriteRecordResult{}, errors.Errorf(`cannot convert value of the column "%s" to the string: %w`, columnName, err)
 		}
 		var limitErr fastcsv.LimitError
 		if errors.As(err, &limitErr) {
 			columnName := w.columns[limitErr.ColumnIndex].ColumnName()
-			return writeRecordResult, svcerrors.NewPayloadTooLargeError(errors.Errorf(`too big CSV row, column: "%s", row limit: %s`, columnName, limitErr.Limit.HumanReadable()))
+			return result.WriteRecordResult{}, svcerrors.NewPayloadTooLargeError(errors.Errorf(`too big CSV row, column: "%s", row limit: %s`, columnName, limitErr.Limit.HumanReadable()))
 		}
 
-		return writeRecordResult, err
+		return result.WriteRecordResult{}, err
 	}
 
+	// Get notifier after succcessful written record
+	writeRecordResult := result.NewNotifierWriteRecordResult(n, w.notifier())
 	// Buffers can be released
 	// Important: values slice contains reference to the body []byte buffer, so it can be released sooner.
 	record.ReleaseBuffers()
