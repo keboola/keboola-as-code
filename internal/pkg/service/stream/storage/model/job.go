b72a0f7d3f6380b2db342b7ef5ef80554180c9db
Martin Vaško <Matovidlo2@gmail.com>
2024-11-11 14:18:01 +0100

    fix: Comment was copied from keboolasink

--- internal/pkg/service/stream/storage/model/job.go
+++ internal/pkg/service/stream/storage/model/job.go
@@ -4,7 +4,8 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
 )
 
-// Job contains all Keboola-specific data we need for polling jobs.
+// Job represents workload on sink. It is used to limit the sink when too many jobs are ongoing.
+// At the end throttle the sink, so it does not overloads other service
 type Job struct {
 	key.JobKey
 	Deleted bool `json:"-"` // internal field to mark the entity for deletion, there is no soft delete
