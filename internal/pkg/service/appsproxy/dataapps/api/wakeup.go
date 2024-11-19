61546728f950389c1bd241d23e2f48b1353f36c7
Martin Vaško <Matovidlo2@gmail.com>
2024-11-19 09:09:08 +0100

    feat: Add telemetry for all unsuccessful status codes returned from Wakeup calls

--- internal/pkg/service/appsproxy/dataapps/api/wakeup.go
+++ internal/pkg/service/appsproxy/dataapps/api/wakeup.go
@@ -1,6 +1,12 @@
 package api
 
-import "github.com/keboola/go-client/pkg/request"
+import (
+	"context"
+
+	"github.com/keboola/go-client/pkg/request"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
 
 type wakeupBody struct {
 	DesiredState string `json:"desiredState"`
@@ -9,6 +15,14 @@ type wakeupBody struct {
 func (a *API) WakeupApp(appID AppID) request.APIRequest[request.NoResult] {
 	return request.NewAPIRequest(request.NoResult{}, a.newRequest().
 		WithError(&Error{}).
+		WithOnError(func(ctx context.Context, response request.HTTPResponse, err error) error {
+			span := trace.SpanFromContext(ctx)
+			attrs := []attribute.KeyValue{
+				attribute.Int(attrSandboxesServiceStatusCode, response.StatusCode()),
+			}
+			span.SetAttributes(attrs...)
+			return nil
+		}).
 		WithPatch("apps/{appId}").
 		AndPathParam("appId", appID.String()).
 		WithJSONBody(wakeupBody{
