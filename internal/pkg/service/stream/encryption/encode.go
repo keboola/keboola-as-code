05fdca915783362a6b383d19120ecafb76b3c208
Jáchym Toušek <jachym.tousek@keboola.com>
2024-12-20 14:23:59 +0100

    debug: New encoding method

--- internal/pkg/service/stream/encryption/encode.go
+++ internal/pkg/service/stream/encryption/encode.go
@@ -2,58 +2,38 @@ package encryption
 
 import (
 	"bytes"
-	"compress/gzip"
-	"encoding/base64"
 	"encoding/gob"
+	"sort"
 
 	"github.com/pkg/errors"
 )
 
-func Encode(data any) ([]byte, error) {
+// Encode converts a metadata map to byte slice.
+func Encode(data map[string]string) ([]byte, error) {
+	type pair struct {
+		Key   string
+		Value string
+	}
+
+	slice := make([]pair, len(data))
+	i := 0
+	for k, v := range data {
+		slice[i] = pair{
+			Key:   k,
+			Value: v,
+		}
+		i += 1
+	}
+
+	// Sort the slice to make the result deterministic
+	sort.Slice(slice, func(i, j int) bool { return slice[i].Key < slice[j].Key })
+
 	var buffer bytes.Buffer
 
-	// Base64 encode
-	encoder := base64.NewEncoder(base64.StdEncoding, &buffer)
-
-	// Gzip compress
-	writer := gzip.NewWriter(encoder)
-
-	// gob encode
-	err := gob.NewEncoder(writer).Encode(data)
+	err := gob.NewEncoder(&buffer).Encode(slice)
 	if err != nil {
 		return nil, errors.Wrapf(err, "gob encoder failed: %s", err.Error())
 	}
 
-	err = writer.Close()
-	if err != nil {
-		return nil, errors.Wrapf(err, "can't close gzip writer: %s", err.Error())
-	}
-
-	err = encoder.Close()
-	if err != nil {
-		return nil, errors.Wrapf(err, "base64 encoder failed: %s", err.Error())
-	}
-
 	return buffer.Bytes(), nil
-}
-
-func Decode[T any](data []byte) (decoded T, err error) {
-	// Base64 decode
-	decoder := base64.NewDecoder(base64.StdEncoding, bytes.NewReader(data))
-
-	// Gzip uncompress
-	reader, err := gzip.NewReader(decoder)
-	if err != nil {
-		return decoded, errors.Wrapf(err, "can't create gzip reader: %s", err.Error())
-	}
-
-	defer reader.Close()
-
-	// gob decode
-	err = gob.NewDecoder(reader).Decode(&decoded)
-	if err != nil {
-		return decoded, errors.Wrapf(err, "gob decoder failed: %s", err.Error())
-	}
-
-	return decoded, nil
 }
