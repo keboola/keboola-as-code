ebd1458a2669d1fbb3ad796c59d1c78101ae786c
Jáchym Toušek <jachym.tousek@keboola.com>
2025-02-03 10:37:15 +0100

    fix: Rename field

--- internal/pkg/service/appsproxy/proxy/apphandler/authproxy/oauthproxy/config.go
+++ internal/pkg/service/appsproxy/proxy/apphandler/authproxy/oauthproxy/config.go
@@ -23,16 +23,16 @@ func proxyConfig(
 	selector *selector.Selector,
 	pageWriter *pagewriter.Writer,
 	app api.AppConfig,
-	authProvider provider.OAuthProvider,
+	oAuthProvider provider.OAuthProvider,
 	upstream chain.Handler,
 ) (*options.Options, error) {
 	// Generate unique cookies secret
-	secret, err := generateCookieSecret(cfg, app, authProvider.ID())
+	secret, err := generateCookieSecret(cfg, app, oAuthProvider.ID())
 	if err != nil {
 		return nil, err
 	}
 
-	proxyProvider, err := authProvider.ProxyProviderOptions()
+	proxyProvider, err := oAuthProvider.ProxyProviderOptions()
 	if err != nil {
 		return nil, err
 	}
