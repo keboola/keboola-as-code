b3922885b91076d8b460f5f81fa2f336d3b47321
Jáchym Toušek <jachym.tousek@keboola.com>
2025-02-12 11:53:39 +0100

    wip

--- internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc/rpc_test.go
+++ internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc/rpc_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/definition/key"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/dependencies"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/rpc"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/diskwriter/network/transport"
 	"github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/encoding"
 	localModel "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/model"
 	volume "github.com/keboola/keboola-as-code/internal/pkg/service/stream/storage/level/local/volume/model"
@@ -66,13 +67,84 @@ func TestNetworkFile(t *testing.T) {
 	slice := slices[0]
 
 	// Open file on a source node
-	file, sourceNode := openNetworkFile(t, ctx, etcdCfg, "source", slice.SliceKey, slice.LocalStorage)
+	file, sourceNode, _ := openNetworkFile(t, ctx, etcdCfg, "source", slice.SliceKey, slice.LocalStorage)
 	assert.True(t, file.IsReady())
 
 	// Write
 	n, err := file.Write(ctx, true, []byte("foo\n"))
 	assert.Equal(t, 4, n)
 	require.NoError(t, err)
+	n, err = file.Write(ctx, true, []byte("bar\n"))
+	assert.Equal(t, 4, n)
+	require.NoError(t, err)
+
+	// Sync
+	require.NoError(t, file.Sync(ctx))
+
+	// Close
+	require.NoError(t, file.Close(ctx))
+
+	// Shutdown nodes, the writer first
+	writerNode.Process().Shutdown(ctx, errors.New("bye bye"))
+	writerNode.Process().WaitForShutdown()
+	sourceNode.Process().Shutdown(ctx, errors.New("bye bye"))
+	sourceNode.Process().WaitForShutdown()
+
+	// Check file content
+	filePath := slice.LocalStorage.FileName(volumePath, "source")
+	content, err := os.ReadFile(filePath)
+	require.NoError(t, err)
+	assert.Equal(t, "foo\nbar\n", string(content))
+}
+
+func TestNetworkFile_ClosedFile(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	etcdCfg := etcdhelper.TmpNamespace(t)
+
+	// Create volume directory, with volume ID file
+	volumeID := volume.ID("my-volume")
+	volumesPath := t.TempDir()
+	volumePath := filepath.Join(volumesPath, "hdd", "001")
+	require.NoError(t, os.MkdirAll(volumePath, 0o700))
+	require.NoError(t, os.WriteFile(filepath.Join(volumePath, volume.IDFile), []byte(volumeID), 0o600))
+
+	// Start disk writer node
+	writerNode := startDiskWriterNode(t, ctx, etcdCfg, "disk-writer", volumesPath)
+
+	// Create resources in an API node
+	apiScp, _ := dependencies.NewMockedAPIScope(t, ctx, commonDeps.WithEtcdConfig(etcdCfg))
+	branchKey := key.BranchKey{ProjectID: 123, BranchID: 111}
+	branch := test.NewBranch(branchKey)
+	source := test.NewHTTPSource(key.SourceKey{BranchKey: branchKey, SourceID: "my-source"})
+	source.HTTP.Secret = strings.Repeat("1", 48)
+	sink := dummy.NewSinkWithLocalStorage(key.SinkKey{SourceKey: source.SourceKey, SinkID: "my-sink"})
+	require.NoError(t, apiScp.DefinitionRepository().Branch().Create(&branch, apiScp.Clock().Now(), test.ByUser()).Do(ctx).Err())
+	require.NoError(t, apiScp.DefinitionRepository().Source().Create(&source, apiScp.Clock().Now(), test.ByUser(), "create").Do(ctx).Err())
+	require.NoError(t, apiScp.DefinitionRepository().Sink().Create(&sink, apiScp.Clock().Now(), test.ByUser(), "create").Do(ctx).Err())
+
+	// Load created slice
+	slices, err := apiScp.StorageRepository().Slice().ListIn(sink.SinkKey).Do(ctx).All()
+	require.NoError(t, err)
+	require.Len(t, slices, 1)
+	slice := slices[0]
+
+	// Open file on a source node
+	file, sourceNode, connection := openNetworkFile(t, ctx, etcdCfg, "source", slice.SliceKey, slice.LocalStorage)
+	assert.True(t, file.IsReady())
+
+	// Write
+	n, err := file.Write(ctx, true, []byte("foo\n"))
+	assert.Equal(t, 4, n)
+	require.NoError(t, err)
+
+	// Close connection (in practice it can get closed by MirrorMap sync)
+	require.NoError(t, connection.Close(ctx))
+
+	// TODO: Expect failure
 	n, err = file.Write(ctx, true, []byte("bar\n"))
 	assert.Equal(t, 4, n)
 	require.NoError(t, err)
@@ -115,7 +187,7 @@ func startDiskWriterNode(t *testing.T, ctx context.Context, etcdCfg etcdclient.C
 	return m
 }
 
-func openNetworkFile(t *testing.T, ctx context.Context, etcdCfg etcdclient.Config, sourceNodeID string, sliceKey model.SliceKey, slice localModel.Slice) (encoding.NetworkOutput, dependencies.Mocked) {
+func openNetworkFile(t *testing.T, ctx context.Context, etcdCfg etcdclient.Config, sourceNodeID string, sliceKey model.SliceKey, slice localModel.Slice) (encoding.NetworkOutput, dependencies.Mocked, *transport.ClientConnection) {
 	t.Helper()
 
 	d, m := dependencies.NewMockedSourceScopeWithConfig(
@@ -136,5 +208,5 @@ func openNetworkFile(t *testing.T, ctx context.Context, etcdCfg etcdclient.Confi
 	file, err := rpc.OpenNetworkFile(ctx, d.Logger(), d.Telemetry(), sourceNodeID, conn, sliceKey, slice, onServerTermination)
 	require.NoError(t, err)
 
-	return file, m
+	return file, m, conn
 }
