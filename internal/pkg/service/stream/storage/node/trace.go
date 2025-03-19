b050aa358d03f70a2b460b04ae560cb95e91fb11
Petr Hosek <hosanp11@gmail.com>
2025-03-19 14:04:09 +0100

    feat: Add FileImportFailed metric to track failed file imports in stream storage

--- internal/pkg/service/stream/storage/node/trace.go
+++ internal/pkg/service/stream/storage/node/trace.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Metrics struct {
-	Duration     metric.Float64Histogram
-	Compressed   metric.Int64Counter
-	Uncompressed metric.Int64Counter
+	Duration         metric.Float64Histogram
+	Compressed       metric.Int64Counter
+	Uncompressed     metric.Int64Counter
+	FileImportFailed metric.Int64Counter
 }
 
 func NewMetrics(meter telemetry.Meter) *Metrics {
@@ -20,7 +21,8 @@ func NewMetrics(meter telemetry.Meter) *Metrics {
 			"ms",
 			metric.WithExplicitBucketBoundaries(0, 10, 30, 100, 300, 1000, 3000, 10000, 30000, 100000, 300000, 1000000),
 		),
-		Compressed:   meter.IntCounter("keboola.go.stream.operation.bytes.compressed", "Compressed bytes processed by operator.", "B"),
-		Uncompressed: meter.IntCounter("keboola.go.stream.operation.bytes.uncompressed", "Uncompressed bytes processed by operator.", "B"),
+		Compressed:       meter.IntCounter("keboola.go.stream.operation.bytes.compressed", "Compressed bytes processed by operator.", "B"),
+		Uncompressed:     meter.IntCounter("keboola.go.stream.operation.bytes.uncompressed", "Uncompressed bytes processed by operator.", "B"),
+		FileImportFailed: meter.IntCounter("keboola.go.stream.operation.fileimport.failed", "Count of file imports that will be retried", "count"),
 	}
 }
