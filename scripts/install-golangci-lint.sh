0475cca4376d9b10674baa822ca191f62f88528f
Jáchym Toušek <jachym.tousek@keboola.com>
2025-04-15 15:47:11 +0200

    build: Fix golangci-lint installation
    # Conflicts:
    #	provisioning/dev/docker/Dockerfile

--- scripts/install-golangci-lint.sh
+++ scripts/install-golangci-lint.sh
@@ -6,17 +6,12 @@ set -o nounset          # Disallow expansion of unset variables
 set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
-# Change directory to the project root
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$SCRIPT_DIR/.."
-pwd
-
 # Version to install
 GOLANGCI_LINT_VERSION="v2.0.2"
 
 # Install or update golangci-lint
 echo "Installing golangci-lint $GOLANGCI_LINT_VERSION..."
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOPATH)/bin" "$GOLANGCI_LINT_VERSION"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "$(go env GOBIN)" "$GOLANGCI_LINT_VERSION"
 
 # Verify installation
 echo "Verifying golangci-lint installation..."
