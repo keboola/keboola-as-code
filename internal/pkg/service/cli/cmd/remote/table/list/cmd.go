fd672031e4dee0ebc463db02550bff0184fd5f23
Martin Vaško <Matovidlo2@gmail.com>
2025-11-11 08:21:19 +0100

    feat: Add support for table list command

---
+++ internal/pkg/service/cli/cmd/remote/table/list/cmd.go
@@ -0,0 +1,55 @@
+package list
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/dependencies"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/cli/helpmsg"
+	"github.com/keboola/keboola-as-code/internal/pkg/service/common/configmap"
+	"github.com/keboola/keboola-as-code/pkg/lib/operation/project/remote/table/list"
+)
+
+type Flags struct {
+	StorageAPIHost  configmap.Value[string] `configKey:"storage-api-host" configShorthand:"H" configUsage:"storage API host, eg. \"connection.keboola.com\""`
+	StorageAPIToken configmap.Value[string] `configKey:"storage-api-token" configShorthand:"t" configUsage:"storage API token from your project"`
+}
+
+func DefaultFlags() Flags {
+	return Flags{}
+}
+
+func Command(p dependencies.Provider) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   `list`,
+		Short: helpmsg.Read(`remote/table/list/short`),
+		Long:  helpmsg.Read(`remote/table/list/long`),
+		RunE: func(cmd *cobra.Command, args []string) (cmdErr error) {
+			// flags
+			f := Flags{}
+			if err := p.BaseScope().ConfigBinder().Bind(cmd.Context(), cmd.Flags(), args, &f); err != nil {
+				return err
+			}
+
+			// Get dependencies
+			d, err := p.RemoteCommandScope(cmd.Context(), f.StorageAPIHost, f.StorageAPIToken)
+			if err != nil {
+				return err
+			}
+
+			// Ask options
+			err = list.Run(cmd.Context(), d)
+			if err != nil {
+				return err
+			}
+
+			// Send cmd successful/failed event
+			defer d.EventSender().SendCmdEvent(cmd.Context(), d.Clock().Now(), &cmdErr, "remote-table-list")
+
+			return nil
+		},
+	}
+
+	configmap.MustGenerateFlags(cmd.Flags(), DefaultFlags())
+
+	return cmd
+}
