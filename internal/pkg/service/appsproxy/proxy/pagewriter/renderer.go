5e1f0a955706b4fbb681bd1bde47e50955ba1436
Jakub Kotek <blaztar@gmail.com>
2025-12-02 11:03:51 +0100

    feat: add support for plain text response

---
+++ internal/pkg/service/appsproxy/proxy/pagewriter/renderer.go
@@ -0,0 +1,21 @@
+package pagewriter
+
+import (
+	"fmt"
+)
+
+func renderPlainText(page string, status int) string {
+	switch page {
+	case "spinner.gohtml":
+		return `The application is re-starting. Please wait...`
+
+	case "restart_disabled.gohtml":
+		return `The application has been stopped and cannot be restarted automatically.`
+
+	case "error.gohtml":
+		return fmt.Sprintf("An error occurred (status code: %d). Please try again later.", status)
+		
+	default:
+		return `No additional information is available.`
+	}
+}
