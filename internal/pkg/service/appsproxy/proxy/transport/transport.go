e703ee210af28ab6f8ac153876ce889694dadca6
Pepa Martinec <josef.martinec@keboola.com>
2026-01-06 14:43:57 +0100

    wip: Disable custom DNS resolver

--- internal/pkg/service/appsproxy/proxy/transport/transport.go
+++ internal/pkg/service/appsproxy/proxy/transport/transport.go
@@ -6,16 +6,12 @@ import (
 	"context"
 	"net"
 	"net/http"
-	"net/http/httptrace"
 	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel/attribute"
 	"golang.org/x/net/http2"
 
-	"github.com/keboola/keboola-as-code/internal/pkg/service/appsproxy/proxy/transport/dns"
 	"github.com/keboola/keboola-as-code/internal/pkg/telemetry"
-	"github.com/keboola/keboola-as-code/internal/pkg/utils/errors"
 )
 
 // TLSHandshakeTimeout specifies the default timeout of TLS handshake.
@@ -55,75 +51,9 @@ func New(d dependencies, dnsServer string) (http.RoundTripper, error) {
 func NewWithDNSServer(d dependencies, dnsServerAddress string) (http.RoundTripper, error) {
 	dialer := newDialer()
 
-	var dnsClient *dns.Client
-	var err error
-	if dnsServerAddress == "" {
-		dnsClient, err = dns.NewClientFromEtc(dialer)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		dnsClient = dns.NewClient(dialer, dnsServerAddress)
-	}
-
-	tel := d.Telemetry()
-
 	dialContext := func(ctx context.Context, network, address string) (net.Conn, error) {
-		host, port, err := net.SplitHostPort(address)
-		if err != nil {
-			return nil, err
-		}
-
-		if net.ParseIP(host) != nil {
-			return dialer.DialContext(ctx, network, address)
-		}
-
-		// Get tracing hooks
-		trace := httptrace.ContextClientTrace(ctx)
-
-		// Trace DNSStart
-		ctx, dnsSpan := tel.Tracer().Start(ctx, "keboola.go.apps-proxy.transport.dns.resolve")
-		dnsSpan.SetAttributes(
-			attribute.String("transport.dns.resolve.host", host),
-			attribute.String("transport.dns.resolve.server", dnsClient.DNSServer()),
-		)
-		if trace != nil && trace.DNSStart != nil {
-			trace.DNSStart(httptrace.DNSStartInfo{Host: host})
-		}
-
-		// Create context for DNS resolving
-		// It separates the events/tracing of the connection to the DNS server, from the connection to the target server.
-		resolveCtx, cancel := context.WithTimeoutCause(context.WithoutCancel(ctx), DNSResolveTimeout, errors.New("DNS resolve timeout"))
-		defer cancel()
-
-		// We are using custom DNS resolving to detect if the target host - data app, is running.
-		// The DNS query is not recursive and not cached, see "dns" package for details.
-		ip, err := dnsClient.Resolve(resolveCtx, host) //nolint:contextcheck
-
-		// Trace DNSDone
-		if trace != nil && trace.DNSDone != nil {
-			trace.DNSDone(httptrace.DNSDoneInfo{
-				Addrs: []net.IPAddr{{IP: net.ParseIP(ip)}},
-				Err:   err,
-			})
-		}
-
-		// Handle DNS error
-		dnsSpan.End(&err)
-		if err != nil {
-			return nil, err
-		}
-
-		// Dial
-		ctx, dialSpan := tel.Tracer().Start(ctx, "keboola.go.apps-proxy.transport.dial")
-		dialSpan.SetAttributes(
-			attribute.String("transport.dial.network", network),
-			attribute.String("transport.dial.ip", ip),
-			attribute.String("transport.dial.port", port),
-		)
-		conn, err := dialer.DialContext(ctx, network, net.JoinHostPort(ip, port))
-		dialSpan.End(&err)
-		return conn, err
+		// TEMPORARY: Use default DNS resolution instead of custom non-recursive DNS
+		return dialer.DialContext(ctx, network, address)
 	}
 
 	httpTransport := &http.Transport{
