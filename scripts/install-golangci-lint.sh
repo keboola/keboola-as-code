008f0a8ddc8d6fa62c623389706da7f7ca48f765
Jáchym Toušek <jachym.tousek@keboola.com>
2026-04-27 11:55:11 +0200

    build: Update linter

--- scripts/install-golangci-lint.sh
+++ scripts/install-golangci-lint.sh
@@ -7,7 +7,7 @@ set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
 # Version to install
-GOLANGCI_LINT_VERSION="v2.11.3"
+GOLANGCI_LINT_VERSION="v2.11.4"
 
 # Determine installation directory
 BIN_DIR="$(go env GOBIN)"
